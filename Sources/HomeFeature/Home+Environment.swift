8d5fc6b6e4976c35a6eb916fed5edc7f40b6666a
Nikola Milicevic <12729242+nikola-milicevic@users.noreply.github.com>
2022-07-21 15:23:08 +0200

    Add HomeFeature

---
+++ Sources/HomeFeature/Home+Environment.swift
@@ -0,0 +1,6 @@
+import Foundation
+
+public extension Home {
+	// MARK: Environment
+	struct Environment {}
+}
---
+++ Sources/HomeFeature/Home+View.swift
@@ -0,0 +1,35 @@
+import ComposableArchitecture
+import SwiftUI
+
+public extension Home {
+	struct Coordinator: SwiftUI.View {
+		public typealias Store = ComposableArchitecture.Store<State, Action>
+		private let store: Store
+
+		public init(store: Store) {
+			self.store = store
+		}
+	}
+}
+
+public extension Home.Coordinator {
+	// MARK: Body
+	var body: some View {
+		Text("Hello, Babylon!")
+	}
+}
+
+// MARK: - HomeView_Previews
+#if DEBUG
+struct HomeView_Previews: PreviewProvider {
+	static var previews: some View {
+		Home.Coordinator(
+			store: .init(
+				initialState: .init(),
+				reducer: Home.reducer,
+				environment: .init()
+			)
+		)
+	}
+}
+#endif // DEBUG
