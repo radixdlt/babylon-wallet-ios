291fb6a1445100e995798f48670002b77807b0c7
Nikola Milicevic <12729242+nikola-milicevic@users.noreply.github.com>
2022-07-26 17:31:40 +0200

    Remove UIColor extension, move custom colors to Color extension

--- Sources/Common/UI/Colors.swift
+++ Sources/Common/UI/Colors.swift
@@ -9,7 +9,34 @@ import Foundation
 import SwiftUI
 
 public extension Color {
-	static let appBackgroundDark: Color = .black
-	static let appBackgroundLight: Color = .white
-	static let appGrey2: Color = .init(uiColor: .init(hex: "#8A8FA4"))
+	static let appBackgroundDark: Self = .black
+	static let appBackgroundLight: Self = .white
+	static let appGrey2 = Self(hex: .appGrey2)
+}
+
+extension Double {
+	static let defaultOpacity: Self = 1
+}
+
+extension Color {
+	enum Hex: UInt32 {
+		case appGrey2 = 0x8A8FA4
+	}
+
+	init(hex: Hex, opacity: Double = .defaultOpacity) {
+		self.init(hex: hex.rawValue, opacity: opacity)
+	}
+
+	init(hex: UInt32, opacity: Double = .defaultOpacity) {
+		func value(shift: Int) -> Double {
+			Double((hex >> shift) & 0xFF) / 255
+		}
+
+		self.init(
+			red: value(shift: 16),
+			green: value(shift: 08),
+			blue: value(shift: 00),
+			opacity: opacity
+		)
+	}
 }
