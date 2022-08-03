6ec99631ed82d076cf58db6bf5cdaf688494a99b
Nikola Milicevic <12729242+nikola-milicevic@users.noreply.github.com>
2022-08-03 14:20:48 +0200

    Add test for settingsButtonTapped action

--- Tests/HomeFeatureTests/HomeFeatureTests.swift
+++ Tests/HomeFeatureTests/HomeFeatureTests.swift
@@ -1,15 +1,18 @@
-//
-//  File.swift
-//
-//
-//  Created by Nikola Milicevic on 2022-07-21.
-//
-
+import ComposableArchitecture
 @testable import HomeFeature
 import TestUtils
+import XCTest
 
 final class HomeFeatureTests: TestCase {
-	func testTrivial() {
-		XCTAssert(true)
+	func testSettingsButtonTapped() {
+		let store = TestStore(
+			initialState: Home.State(),
+			reducer: Home.reducer,
+			environment: Home.Environment()
+		)
+
+		store.send(.header(.internal(.user(.settingsButtonTapped))))
+		store.receive(.header(.coordinate(.displaySettings)))
+		store.receive(.coordinate(.displaySettings))
 	}
 }
