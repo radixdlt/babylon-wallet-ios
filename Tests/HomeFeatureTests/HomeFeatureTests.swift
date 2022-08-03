5eacae90e7525f45bdeb02d726666779a8e22806
Nikola Milicevic <12729242+nikola-milicevic@users.noreply.github.com>
2022-08-03 15:17:19 +0200

    Split tests

--- Tests/HomeFeatureTests/HomeFeatureTests.swift
+++ Tests/HomeFeatureTests/HomeFeatureTests.swift
@@ -11,8 +11,18 @@ final class HomeFeatureTests: TestCase {
 			environment: Home.Environment()
 		)
 
-		store.send(.header(.internal(.user(.settingsButtonTapped))))
-		store.receive(.header(.coordinate(.displaySettings)))
+		store.send(.header(.coordinate(.displaySettings)))
+		store.receive(.coordinate(.displaySettings))
+	}
+
+	func testSettingsButtonTappedFromHeader() {
+		let store = TestStore(
+			initialState: Home.Header.State(),
+			reducer: Home.Header.reducer,
+			environment: Home.Header.Environment()
+		)
+
+		store.send(.internal(.user(.settingsButtonTapped)))
 		store.receive(.coordinate(.displaySettings))
 	}
 }
