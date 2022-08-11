efa6fb3e16b27afba84d91784a1792a04319046c
Alexander Cyon <alex.cyon@rdx.works>
2022-08-11 13:46:23 +0200

    clean up LocalAuthenticationClient impl and make it throwing

--- Sources/LocalAuthenticationClient/LocalAuthenticationClient+Interface.swift
+++ Sources/LocalAuthenticationClient/LocalAuthenticationClient+Interface.swift
@@ -11,34 +11,36 @@ import Foundation
 ///
 /// Example usage in SwiftUI:
 ///
-///     struct LocalAuthView: View {
-///         @State var laConfig: LocalAuthenticationConfig?
-///         var body: some View {
+///      import LocalAuthenticationClient
 ///
-///             if let laConfig = laConfig {
-///                 VStack {
-///                     Text("\(String(describing: laConfig)) set up.")
-///                     Button("Query again") {
-///                         self.laConfig = nil
-///                     }
-///                 }
-///             } else {
-///                 Button("Ask for passcode/biometrics") {
-///                     queryLAConfig()
-///                 }
-///             }
-///         }
+///      struct LocalAuthView: View {
+///          @State var laConfig: LocalAuthenticationConfig?
+///          var body: some View {
+///              if let laConfig = laConfig {
+///                  VStack {
+///                      Text("\(String(describing: laConfig)) set up.")
+///                      Button("Query again") {
+///                          self.laConfig = nil
+///                      }
+///                  }
+///              } else {
+///                  Button("Ask for passcode/biometrics") {
+///                      queryLAConfig()
+///                  }
+///              }
+///          }
 ///
-///         func queryLAConfig() {
-///             Task {
-///                 do {
-///                     self.laConfig =  try await LocalAuthenticationClient.live().queryConfig() {
-///                 } cath {
-///                     print("User cancelled LA config query or failed: \(error)")
-///                 }
-///             }
-///         }
-///     }
+///          func queryLAConfig() {
+///              Task {
+///                  do {
+///                      self.laConfig = try await LocalAuthenticationClient.live().queryConfig()
+///                  } catch {
+///                      print("User cancelled LA config query or failed, error: \(String(describing: error))")
+///                  }
+///              }
+///          }
+///      }
+///
 public struct LocalAuthenticationClient {
 	/// The return value (`LocalAuthenticationConfig`) might be `nil` if app goes to background or stuff like that.
 	public typealias QueryConfig = @Sendable () async throws -> LocalAuthenticationConfig
--- Sources/LocalAuthenticationClient/LocalAuthenticationClient+Live.swift
+++ Sources/LocalAuthenticationClient/LocalAuthenticationClient+Live.swift
@@ -33,71 +33,72 @@ public extension LocalAuthenticationClient {
 private extension LAContext {
 	typealias Error = LocalAuthenticationClient.Error
 
-	func evaluateIfPasscodeIsSetUp() async throws -> Bool {
+	func canEvaluate(
+		policy: LAPolicy,
+		errorHandling: (LAError) -> Result<Bool, Error>?
+	) async throws -> Bool {
 		try await withCheckedThrowingContinuation { [weak self] (continuation: CheckedContinuation<Bool, Swift.Error>) in
 			guard let self = self else {
 				continuation.resume(throwing: Error.contextDeinitialized)
 				return
 			}
 			var error: NSError?
-			let canEvaluate = self.canEvaluatePolicy(.deviceOwnerAuthentication, error: &error)
+			let canEvaluate = self.canEvaluatePolicy(policy, error: &error)
 
-			if let evaluationError = error {
-				if let laError = evaluationError as? LAError {
-					switch laError.code {
-					case .appCancel, .userCancel:
-						continuation.resume(throwing: Error.queryCancelled)
-						return
-					case .passcodeNotSet:
-						continuation.resume(returning: false)
-						return
-					case .biometryNotEnrolled, .biometryLockout, .biometryNotAvailable:
-						break // irrelevant for passcode
-					default:
-						continuation.resume(throwing: Error.evaluationError(laError))
-						return
-					}
-				} else {
-					let reason = String(describing: error)
-					continuation.resume(throwing: Error.evaluationFailedWithOtherError(reason: reason))
-					return
-				}
+			guard let evaluationError = error else {
+				continuation.resume(returning: canEvaluate)
+				return
 			}
-			continuation.resume(returning: canEvaluate)
+
+			guard let laError = evaluationError as? LAError else {
+				let reason = String(describing: error)
+				continuation.resume(throwing: Error.evaluationFailedWithOtherError(reason: reason))
+				return
+			}
+
+			guard let result = errorHandling(laError) else {
+				continuation.resume(returning: canEvaluate)
+				return
+			}
+
+			switch result {
+			case let .success(canEvaluate_):
+				continuation.resume(returning: canEvaluate_)
+				return
+			case let .failure(error):
+				continuation.resume(throwing: error)
+				return
+			}
+		}
+	}
+
+	func evaluateIfPasscodeIsSetUp() async throws -> Bool {
+		try await canEvaluate(policy: .deviceOwnerAuthentication) { laError in
+			switch laError.code {
+			case .appCancel, .userCancel:
+				return .failure(.queryCancelled)
+			case .passcodeNotSet:
+				return .success(false)
+			case .biometryNotEnrolled, .biometryLockout, .biometryNotAvailable:
+				return nil // irrelevant for passcode
+			default:
+				return .failure(.evaluationError(laError))
+			}
 		}
 	}
 
 	func evaluateIfBiometricsIsSetUp() async throws -> Bool {
-		try await withCheckedThrowingContinuation { [weak self] (continuation: CheckedContinuation<Bool, Swift.Error>) in
-			guard let self = self else {
-				continuation.resume(throwing: Error.contextDeinitialized)
-				return
+		try await canEvaluate(policy: .deviceOwnerAuthenticationWithBiometrics) { laError in
+			switch laError.code {
+			case .appCancel, .userCancel:
+				return .failure(.queryCancelled)
+			case .passcodeNotSet:
+				return .failure(.evaluateBioDiscrepancyPasscodeNotSetButExpectedToBe)
+			case .biometryNotEnrolled, .biometryLockout, .biometryNotAvailable:
+				return .success(false)
+			default:
+				return .failure(.evaluationError(laError))
 			}
-			var error: NSError?
-			let canEvaluate = self.canEvaluatePolicy(.deviceOwnerAuthenticationWithBiometrics, error: &error)
-
-			if let evaluationError = error {
-				if let laError = evaluationError as? LAError {
-					switch laError.code {
-					case .appCancel, .userCancel:
-						continuation.resume(throwing: Error.queryCancelled)
-						return
-					case .passcodeNotSet:
-						continuation.resume(throwing: Error.evaluateBioDiscrepancyPasscodeNotSetButExpectedToBe)
-					case .biometryNotEnrolled, .biometryLockout, .biometryNotAvailable:
-						continuation.resume(returning: false)
-						return
-					default:
-						continuation.resume(throwing: Error.evaluationError(laError))
-						return
-					}
-				} else {
-					let reason = String(describing: error)
-					continuation.resume(throwing: Error.evaluationFailedWithOtherError(reason: reason))
-					return
-				}
-			}
-			continuation.resume(returning: canEvaluate)
 		}
 	}
 
