7f12e7fae3c5ae30f75a1f9b1cb08cb98b30c851
Alexander Cyon <alex.cyon@rdx.works>
2022-08-11 12:10:47 +0200

    Clean up

--- Sources/LocalAuthenticationClient/LocalAuthenticationClient+Live.swift
+++ Sources/LocalAuthenticationClient/LocalAuthenticationClient+Live.swift
@@ -10,11 +10,11 @@ import LocalAuthentication
 
 public extension LocalAuthenticationClient {
 	static func live(
-		localAuthenticationContext: LAContext = .init()
+		localAuthenticationContext laContext: LAContext = .init()
 	) -> Self {
 		.init(
 			queryConfig: {
-				try await localAuthenticationContext.queryLocalAuthenticationConfig()
+				try await laContext.queryLocalAuthenticationConfig()
 			}
 		)
 	}
@@ -26,11 +26,12 @@ private extension LAContext {
 	) async -> Bool? {
 		await withCheckedContinuation { [weak self] (continuation: CheckedContinuation<Bool?, Never>) in
 			guard let self = self else {
-				fatalError("What to do")
+				return nil
 			}
 			var error: NSError?
 			let canEvaluate = self.canEvaluatePolicy(policy, error: &error)
-			if let evaluationError = error {
+			
+            if let evaluationError = error {
 				if let laError = evaluationError as? LAError {
 					switch laError.code {
 					case .appCancel, .userCancel:
@@ -40,10 +41,11 @@ private extension LAContext {
 					case .biometryNotEnrolled, .biometryLockout, .biometryNotAvailable:
 						continuation.resume(returning: false)
 					default:
-						fatalError("Unhandled error case: \(laError)")
+						debugPrint("Unhandled error case: \(laError)")
+                        return nil
 					}
 				} else {
-					fatalError("Bad error type")
+                    debugPrint("Bad error type, expected `LAError`, error was: \(String(describing: error))")
 				}
 			} else {
 				continuation.resume(returning: canEvaluate)
@@ -53,19 +55,30 @@ private extension LAContext {
 
 	// Returns `nil` if user presses "cancel" button
 	func queryLocalAuthenticationConfig() async throws -> LocalAuthenticationConfig? {
-		guard let passcodeSupportedResult = await canEvaluate(policy: .deviceOwnerAuthentication) else {
+		guard
+            let passcodeSupportedResult = await canEvaluate(
+                policy: .deviceOwnerAuthentication
+            )
+        else {
 			return nil
 		}
+        
 		guard passcodeSupportedResult else {
 			return .neitherBiometricsNorPasscodeSetUp
 		}
 
-		guard let biometricsSupportedResult = await canEvaluate(policy: .deviceOwnerAuthenticationWithBiometrics) else {
+		guard
+            let biometricsSupportedResult = await canEvaluate(
+                policy: .deviceOwnerAuthenticationWithBiometrics
+            )
+        else {
 			return .passcodeSetUpButBiometricsIsUnknown
 		}
+        
 		guard biometricsSupportedResult else {
 			return .passcodeSetUpButNotBiometrics
 		}
+        
 		return .biometricsAndPasscodeSetUp
 	}
 }
