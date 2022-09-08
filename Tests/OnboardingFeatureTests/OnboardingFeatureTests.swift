ac3a48486037ce3daf50a37cad80ed5d95603261
Nikola Milicevic <12729242+nikola-milicevic@users.noreply.github.com>
2022-09-08 17:01:14 +0200

    wip

--- Tests/OnboardingFeatureTests/OnboardingFeatureTests.swift
+++ Tests/OnboardingFeatureTests/OnboardingFeatureTests.swift
@@ -1,8 +1,30 @@
+import ComposableArchitecture
 @testable import OnboardingFeature
+import Profile
 import TestUtils
+import Wallet
 
+@MainActor
 final class OnboardingFeatureTests: TestCase {
-	func testTrivial() {
-		XCTAssert(true)
+	private let environment = Onboarding.Environment(
+		backgroundQueue: .unimplemented,
+		mainQueue: .unimplemented,
+		userDefaultsClient: .noop
+	)
+
+	func testCreateWallet() async {
+		let profileName = "Profile"
+		let store = TestStore(
+			initialState: Onboarding.State(profileName: profileName, canProceed: true),
+			reducer: Onboarding.reducer,
+			environment: environment
+		)
+
+		let profile = try! Profile(name: profileName)
+		let wallet: Wallet = .init(profile: profile, deviceFactorTypeMnemonic: "")
+		_ = await store.send(.internal(.user(.createWallet)))
+		await store.receive(.internal(.system(.createWallet)))
+		await store.receive(.internal(.system(.createdWallet(wallet))))
+		await store.receive(.coordinate(.onboardedWithWallet(wallet)))
 	}
 }
