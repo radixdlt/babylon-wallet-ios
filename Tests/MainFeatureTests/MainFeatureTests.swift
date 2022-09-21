a6d3df3448af90753483cf296ea43067a0304d92
Nikola Milicevic <12729242+nikola-milicevic@users.noreply.github.com>
2022-09-21 16:30:21 +0200

    Fix/ABW-417 MainFeature tests (#25)
    * Adding tests
    
    * Add tests
    
    * fix tests
    
    * Create App environment unimplemented
    
    * Change throwing error logic
    
    * Fix compilation error
    
    * wip
    
    * wip
    
    * add tests
    
    * Minor fix
    
    * Remove setUp and tearDown
    
    Co-authored-by: Alexander Cyon <alex.cyon@rdx.works>
    Co-authored-by: Alexander Cyon <Sajjon@users.noreply.github.com>

--- Tests/MainFeatureTests/MainFeatureTests.swift
+++ Tests/MainFeatureTests/MainFeatureTests.swift
@@ -2,69 +2,51 @@ import ComposableArchitecture
 @testable import MainFeature
 import TestUtils
 
+@MainActor
 final class MainFeatureTests: TestCase {
-	func testTrivial() throws {
-		XCTAssertEqual(Main.State(home: .init(wallet: .placeholder)), Main.State(home: .init(wallet: .placeholder)))
+	func test_removeWallet_whenTappedOnRemoveWallet_thenCoordinateRemovalResult() async {
+		// given
+		let store = TestStore(
+			initialState: Main.State(home: .placeholder),
+			reducer: Main.reducer,
+			environment: .noop
+		)
+
+		// when
+		_ = await store.send(.internal(.user(.removeWallet)))
+
+		// then
+		await store.receive(.internal(.system(.removedWallet)))
+		await store.receive(.coordinate(.removedWallet))
 	}
 
-	/*
-	 func testDisplaySettings() {
-	 	let store = TestStore(
-	 initialState: Main.State(home: .placeholder),
-	 		reducer: Main.reducer,
-	 		environment: .noop
-	 	)
+	func test_displaySettings_whenCoordinatedToDispaySettings_thenDisplaySettings() async {
+		// given
+		let store = TestStore(
+			initialState: Main.State(home: .placeholder),
+			reducer: Main.reducer,
+			environment: .noop
+		)
 
-	 	store.send(.home(.coordinate(.displaySettings))) {
-	 		$0.settings = .init()
-	 	}
-	 }
+		// when
+		_ = await store.send(.home(.coordinate(.displaySettings))) {
+			// then
+			$0.settings = .init()
+		}
+	}
 
-	 func testDismissSettings() {
-	 	let store = TestStore(
-	 initialState: Main.State(home: .placeholder, settings: .init()),
-	 		reducer: Main.reducer,
-	 		environment: .noop
-	 	)
+	func test_dismissSettings_whenCoordinatedToDismissSettings_thenDismissSettings() async {
+		// given
+		let store = TestStore(
+			initialState: Main.State(home: .placeholder, settings: .init()),
+			reducer: Main.reducer,
+			environment: .noop
+		)
 
-	 	store.send(.settings(.internal(.user(.dismissSettings))))
-	 	store.receive(.settings(.coordinate(.dismissSettings))) {
-	 		$0.settings = nil
-	 	}
-	 }
-
-	 func testDisplayVisitHub() {
-	 	let store = TestStore(
-	 initialState: Main.State(home: .placeholder),
-	 		reducer: Main.reducer,
-	 		environment: .noop
-	 	)
-
-	 	store.send(.home(.coordinate(.displayVisitHub)))
-	 }
-
-	 func testDisplayCreateAccount() {
-	 	let store = TestStore(
-	 		initialState: Main.State(),
-	 		reducer: Main.reducer,
-	 		environment: .noop
-	 	)
-
-	 	store.send(.home(.coordinate(.displayCreateAccount))) {
-	 		$0.createAccount = .init()
-	 	}
-	 }
-
-	 func testDismissCreateAccount() {
-	 	let store = TestStore(
-	 		initialState: Main.State(),
-	 		reducer: Main.reducer,
-	 		environment: .noop
-	 	)
-
-	 	store.send(.createAccount(.coordinate(.dismissCreateAccount))) {
-	 		$0.createAccount = nil
-	 	}
-	 }
-	 */
+		// when
+		_ = await store.send(.settings(.coordinate(.dismissSettings))) {
+			// then
+			$0.settings = nil
+		}
+	}
 }
