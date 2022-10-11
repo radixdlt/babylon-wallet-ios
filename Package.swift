f2c5a2e7e388524f699a255953b0c98ae5c33cf1
Nikola Milicevic <12729242+nikola-milicevic@users.noreply.github.com>
2022-10-11 16:14:42 +0200

    Chore/ABW-487 Update Libraries (#39)

--- Package.swift
+++ Package.swift
@@ -30,12 +30,9 @@ let package = Package(
 	],
 	dependencies: [
 		// TCA - ComposableArchitecture used as architecture
-		.package(
-			url: "https://github.com/pointfreeco/swift-composable-architecture",
-			branch: "protocol-beta"
-		),
+		.package(url: "https://github.com/pointfreeco/swift-composable-architecture", from: "0.41.2"),
 		// Format code
-		.package(url: "https://github.com/nicklockwood/SwiftFormat", from: "0.50.0"),
+		.package(url: "https://github.com/nicklockwood/SwiftFormat", from: "0.50.1"),
 	],
 	targets: [
 		// Targets sorted lexicographically, placing `testTarget` just after `target`.
