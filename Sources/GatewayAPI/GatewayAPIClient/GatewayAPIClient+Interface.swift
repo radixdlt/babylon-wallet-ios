c94556cc1661a9c69e9600749836dcc8f8cda9ce
Alexander Cyon <alex.cyon@gmail.com>
2022-10-21 23:17:11 +0200

    WIP NOT WORKING first fetch resources for entity impl

--- Sources/GatewayAPI/GatewayAPIClient/GatewayAPIClient+Interface.swift
+++ Sources/GatewayAPI/GatewayAPIClient/GatewayAPIClient+Interface.swift
@@ -35,19 +35,62 @@ public extension GatewayAPIClient {
 // MARK: - Date + Sendable
 extension Date: @unchecked Sendable {}
 
+// MARK: - ExpectedHTTPURLResponse
+struct ExpectedHTTPURLResponse: Swift.Error {}
+
+// MARK: - BadHTTPResponseCode
+struct BadHTTPResponseCode: Swift.Error {
+	let got: Int
+	let butExpected = Self.expected
+	static let expected = 200
+}
+
 public extension GatewayAPIClient {
-	static let live: Self = .init(
-		accountResourcesByAddress: { accountAddress in
-			try await Self.mock().accountResourcesByAddress(accountAddress)
-		},
-		resourceDetailsByResourceIdentifier: { resourceAddress in
-			try await Self.mock().resourceDetailsByResourceIdentifier(resourceAddress)
-		},
-		submitTransaction: { transactionSubmitRequest in
-			try await Self.mock().submitTransaction(transactionSubmitRequest)
-		},
-		transactionStatus: { transactionStatusRequest in
-			try await Self.mock().transactionStatus(transactionStatusRequest)
-		}
-	)
+	static func live(
+		baseURL: URL = .init(string: "https://adapanet-gateway.radixdlt.com")!,
+		urlSession: URLSession = .shared,
+		jsonEncoder: JSONEncoder = .init(),
+		jsonDecoder: JSONDecoder = .init()
+	) -> Self {
+		Self(
+			accountResourcesByAddress: { accountAddress in
+
+				let request = EntityResourcesRequest(
+					address: accountAddress.address,
+					atStateIdentifier: PartialLedgerStateIdentifier(stateVersion: 7588, timestamp: Date(), epoch: 0, round: 0)
+				)
+
+				let url = baseURL
+					.appendingPathComponent("entity")
+					.appendingPathComponent("resources")
+
+				var urlRequest = URLRequest(url: url)
+				urlRequest.httpMethod = "POST"
+				urlRequest.httpBody = try jsonEncoder.encode(request)
+
+				let (data, urlResponse) = try await urlSession.data(for: urlRequest)
+
+				guard let httpURLResponse = urlResponse as? HTTPURLResponse else {
+					throw ExpectedHTTPURLResponse()
+				}
+
+				guard httpURLResponse.statusCode == BadHTTPResponseCode.expected else {
+					throw BadHTTPResponseCode(got: httpURLResponse.statusCode)
+				}
+
+				let response = try jsonDecoder.decode(EntityResourcesResponse.self, from: data)
+
+				return response
+			},
+			resourceDetailsByResourceIdentifier: { resourceAddress in
+				try await Self.mock().resourceDetailsByResourceIdentifier(resourceAddress)
+			},
+			submitTransaction: { transactionSubmitRequest in
+				try await Self.mock().submitTransaction(transactionSubmitRequest)
+			},
+			transactionStatus: { transactionStatusRequest in
+				try await Self.mock().transactionStatus(transactionStatusRequest)
+			}
+		)
+	}
 }
