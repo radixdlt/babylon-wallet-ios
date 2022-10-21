0fd976bfd669647e96c42e2a16e07091a9e241a4
Alexander Cyon <alex.cyon@gmail.com>
2022-10-21 22:19:56 +0200

    Fix unit tests for onboarding

--- Tests/OnboardingFeatureTests/OnboardingFeatureTests.swift
+++ Tests/OnboardingFeatureTests/OnboardingFeatureTests.swift
@@ -7,12 +7,12 @@ import TestUtils
 import UserDefaultsClient
 
 @MainActor
-final class OnboardingFeatureTests: TestCase {
-	func test_createWallet_whenTappedOnCreateWalletButton_thenCreateWallet() async throws {
+final class OnboardingNewProfileFeatureTests: TestCase {
+	func test__GIVEN__no_profile__WHEN__new_profile_button_tapped__THEN__user_is_onboarded_with_new_profile() async throws {
 		// given
 		var keychainClient: KeychainClient = .unimplemented
 
-		let expectActorIsolated = expectation(description: "ActorIsolated<Profile?> to have set value")
+		let setDataForProfileSnapshotExpectation = expectation(description: "setDataForKey for ProfileSnapshot should have been called")
 		let profileSavedToKeychain = ActorIsolated<Profile?>(nil)
 
 		keychainClient.setDataDataForKey = { data, key in
@@ -21,40 +21,44 @@ final class OnboardingFeatureTests: TestCase {
 					let profile = try? Profile(snapshot: snapshot)
 					Task {
 						await profileSavedToKeychain.setValue(profile)
-						expectActorIsolated.fulfill()
+						setDataForProfileSnapshotExpectation.fulfill()
 					}
 				}
 			}
 		}
-		let environment = Onboarding.Environment(
-			backgroundQueue: .unimplemented,
-			keychainClient: keychainClient,
-			mainQueue: .unimplemented
-		)
 
-		let nameOfFirstAccount = "Profile"
-		let canProceed = true
 		let store = TestStore(
-			initialState: Onboarding.State(
-				nameOfFirstAccount: nameOfFirstAccount,
-				canProceed: canProceed
-			),
-			reducer: Onboarding.reducer,
-			environment: environment
+			initialState: NewProfile.State(canProceed: true),
+			reducer: NewProfile()
 		)
+		store.dependencies.keychainClient = keychainClient
+		let mnemonic = try Mnemonic(phrase: "zoo zoo zoo zoo zoo zoo zoo zoo zoo zoo zoo wrong", language: .english)
+		let generateMnemonicCalled = ActorIsolated<Bool>(false)
+
+		let mnemonicGeneratorExpectation = expectation(description: "Generate Mnemonic should have been called")
+		store.dependencies.mnemonicGenerator = { _, _ in
+			Task {
+				await generateMnemonicCalled.setValue(true)
+				mnemonicGeneratorExpectation.fulfill()
+			}
+			return mnemonic
+		}
 
 		// when
 		_ = await store.send(.internal(.user(.createProfile)))
 
 		// then
-		await store.receive(.internal(.system(.createProfile)))
+		_ = await store.receive(.internal(.system(.createProfile)))
 
 		waitForExpectations(timeout: 1)
 		await profileSavedToKeychain.withValue {
 			if let profile = $0 {
 				await store.receive(.internal(.system(.createdProfile(profile))))
-				await store.receive(.coordinate(.onboardedWithProfile(profile)))
+				await store.receive(.coordinate(.finishedCreatingNewProfile(profile)))
 			}
+		}
+		await generateMnemonicCalled.withValue {
+			XCTAssertTrue($0)
 		}
 	}
 }
