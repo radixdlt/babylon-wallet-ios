1d1295b5735e6ef041c7b1f81a7d5eccfe1c7e3b
David Roman <2538074+davdroman@users.noreply.github.com>
2022-11-08 12:43:10 +0000

    flatten switch

--- Sources/Features/TransactionSigningFeature/TransactionSigning+Reducer.swift
+++ Sources/Features/TransactionSigningFeature/TransactionSigning+Reducer.swift
@@ -13,45 +13,43 @@ public struct TransactionSigning: ReducerProtocol {
 public extension TransactionSigning {
 	func reduce(into state: inout State, action: Action) -> EffectTask<Action> {
 		switch action {
+		case .internal(.view(.signTransactionButtonTapped)):
+			state.isSigningTX = true
+			return .run { [transactionManifest = state.transactionManifest, addressOfSigner = state.addressOfSigner] send in
+				await send(.internal(.signTransactionResult(TaskResult {
+					try await profileClient.signTransaction(
+						manifest: transactionManifest,
+						addressOfSigner: addressOfSigner
+					)
+				})))
+			}
+
+		case let .internal(.signTransactionResult(result)):
+			state.isSigningTX = false
+			switch result {
+			case let .success(txid):
+				return .run { [incomingMessageFromBrowser = state.incomingMessageFromBrowser] send in
+					await send(.delegate(
+						.signedTXAndSubmittedToGateway(
+							txid,
+							incomingMessageFromBrowser: incomingMessageFromBrowser
+						)
+					))
+				}
+			case let .failure(error):
+				state.errorAlert = .init(title: .init("An error ocurred"), message: .init(error.localizedDescription))
+			}
+			return .none
+
+		case .internal(.view(.errorAlertDismissButtonTapped)):
+			state.errorAlert = nil
+			return .none
+
+		case .internal(.view(.closeButtonTapped)):
+			return .run { send in await send(.delegate(.dismissView)) }
+
 		case .delegate:
 			return .none
-		case let .internal(action):
-			switch action {
-			case .view(.signTransactionButtonTapped):
-				state.isSigningTX = true
-				return .run { [transactionManifest = state.transactionManifest, addressOfSigner = state.addressOfSigner] send in
-					await send(.internal(.signTransactionResult(TaskResult {
-						try await profileClient.signTransaction(
-							manifest: transactionManifest,
-							addressOfSigner: addressOfSigner
-						)
-					})))
-				}
-
-			case let .signTransactionResult(result):
-				state.isSigningTX = false
-				switch result {
-				case let .success(txid):
-					return .run { [incomingMessageFromBrowser = state.incomingMessageFromBrowser] send in
-						await send(.delegate(
-							.signedTXAndSubmittedToGateway(
-								txid,
-								incomingMessageFromBrowser: incomingMessageFromBrowser
-							)
-						))
-					}
-				case let .failure(error):
-					state.errorAlert = .init(title: .init("An error ocurred"), message: .init(error.localizedDescription))
-				}
-				return .none
-
-			case .view(.errorAlertDismissButtonTapped):
-				state.errorAlert = nil
-				return .none
-
-			case .view(.closeButtonTapped):
-				return .run { send in await send(.delegate(.dismissView)) }
-			}
 		}
 	}
 }
