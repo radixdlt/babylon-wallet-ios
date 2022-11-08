994c457b94e1d3e86e2ce2344eae2b3a2c80ee97
David Roman <2538074+davdroman@users.noreply.github.com>
2022-11-08 11:32:18 +0000

    wip

--- Sources/Features/TransactionSigningFeature/TransactionSigning+Action.swift
+++ Sources/Features/TransactionSigningFeature/TransactionSigning+Action.swift
@@ -7,7 +7,7 @@ import ProfileClient
 // MARK: - TransactionSigning.Action
 public extension TransactionSigning {
 	enum Action: Equatable {
-		case view(ViewAction)
+		public static func view(_ action: ViewAction) -> Self { .internal(.view(action)) }
 		case `internal`(InternalAction)
 		case delegate(DelegateAction)
 	}
@@ -25,6 +25,7 @@ public extension TransactionSigning.Action {
 // MARK: - TransactionSigning.Action.InternalAction
 public extension TransactionSigning.Action {
 	enum InternalAction: Equatable {
+		case view(ViewAction)
 		case signTransactionResult(TaskResult<TransactionIntent.TXID>)
 	}
 }
