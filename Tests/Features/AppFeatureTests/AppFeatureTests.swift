a3866aae80f982bd7fbd23595a1e33e1117f552c
David Roman <2538074+davdroman@users.noreply.github.com>
2022-11-10 15:09:29 +0000

    wip

--- Tests/Features/AppFeatureTests/AppFeatureTests.swift
+++ Tests/Features/AppFeatureTests/AppFeatureTests.swift
@@ -24,8 +24,7 @@ final class AppFeatureTests: TestCase {
 		)
 
 		// when
-		_ = await store.send(.child(.main(.delegate(.removedWallet))))
-		_ = await store.receive(.internal(.system(.goToOnboarding))) {
+		_ = await store.send(.child(.main(.delegate(.removedWallet)))) {
 			// then
 			$0 = .onboarding(.init())
 		}
@@ -33,27 +32,25 @@ final class AppFeatureTests: TestCase {
 
 	func test_onboaring__GIVEN__no_profile__WHEN__new_profile_created__THEN__it_is_injected_into_profileClient_and_we_navigate_to_main() async throws {
 		let store = TestStore(
-			// GIVEN: No profile (Onboarding)
 			initialState: .onboarding(Onboarding.State(newProfile: .init())),
 			reducer: App()
 		)
 		let newProfile = try await Profile.new(networkID: networkID, mnemonic: .generate())
+		let expectation = expectation(description: "Profile injected")
 		store.dependencies.profileClient.injectProfile = { injected, _ in
-			XCTAssertEqual(injected, newProfile) // assert correct profile is injected
+			XCTAssertEqual(injected, newProfile)
+			expectation.fulfill()
 		}
 
-		// WHEN: a new profile is created
+		// when
 		_ = await store.send(.child(.onboarding(.child(.newProfile(.delegate(.finishedCreatingNewProfile(newProfile)))))))
-		_ = await store.receive(.child(.onboarding(.delegate(.onboardedWithProfile(newProfile, isNew: true)))))
 
-		// THEN: it is injected into ProfileClient...
-		_ = await store.receive(.internal(.system(.injectProfileIntoProfileClient(newProfile, persistIntoKeychain: true))))
-
-		// THEN: ... and we navigate to main
-		await store.receive(.internal(.system(.injectProfileIntoProfileClientResult(.success(newProfile)))))
-		await store.receive(.internal(.system(.goToMain))) {
+		// then
+		_ = await store.receive(.child(.onboarding(.delegate(.onboardedWithProfile(newProfile, isNew: true))))) {
 			$0 = .main(.init())
 		}
+
+		wait(for: [expectation], timeout: 0)
 	}
 
 	func test_splash__GIVEN__an_existing_profile__WHEN__existing_profile_loaded__THEN__it_is_injected_into_profileClient_and_we_navigate_to_main() async throws {
@@ -73,16 +70,11 @@ final class AppFeatureTests: TestCase {
 		// WHEN: existing profile is loaded
 		_ = await store.send(.child(.splash(.internal(.system(.loadProfileResult(.success(existingProfile)))))))
 
+		// then
 		_ = await store.receive(.child(.splash(.internal(.coordinate(.loadProfileResult(.profileLoaded(existingProfile)))))))
 
 		await testScheduler.advance(by: .milliseconds(110))
-		_ = await store.receive(.child(.splash(.delegate(.loadProfileResult(.profileLoaded(existingProfile))))))
-
-		// THEN: it is injected into ProfileClient...
-		_ = await store.receive(.internal(.system(.injectProfileIntoProfileClient(existingProfile, persistIntoKeychain: false))))
-		// THEN: ... and we navigate to main
-		await store.receive(.internal(.system(.injectProfileIntoProfileClientResult(.success(existingProfile)))))
-		await store.receive(.internal(.system(.goToMain))) {
+		_ = await store.receive(.child(.splash(.delegate(.loadProfileResult(.profileLoaded(existingProfile)))))) {
 			$0 = .main(.init())
 		}
 	}
@@ -101,7 +93,7 @@ final class AppFeatureTests: TestCase {
 		_ = await store.send(.child(.splash(.delegate(.loadProfileResult(loadProfileResult)))))
 
 		// then
-		_ = await store.receive(.internal(.system(.failedToCreateOrImportProfile(reason: "Failed to decode profile: FAIL_FROM_TEST"))))
+		// TODO: does nothing for now (prints error)
 	}
 
 	func test_loadWalletResult_whenWalletLoadingFailedBecauseNoWalletFound_thenNavigateToOnboarding() async {
@@ -115,10 +107,8 @@ final class AppFeatureTests: TestCase {
 		)
 
 		// when
-		_ = await store.send(.child(.splash(.delegate(.loadProfileResult(loadProfileResult)))))
-
-		// then
-		await store.receive(.internal(.system(.goToOnboarding))) {
+		_ = await store.send(.child(.splash(.delegate(.loadProfileResult(loadProfileResult))))) {
+			// then
 			$0 = .onboarding(.init())
 		}
 	}
