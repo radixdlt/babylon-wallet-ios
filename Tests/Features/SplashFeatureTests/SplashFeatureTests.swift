fd1f2057765d5e76d80a13d5cea90f8c4c06dd87
Ghenadie Vasiliev-Pusca <ghenadie.vasiliev-pusca@rdx.works>
2022-12-09 09:34:59 +0200

    Update tests

--- Tests/Features/SplashFeatureTests/SplashFeatureTests.swift
+++ Tests/Features/SplashFeatureTests/SplashFeatureTests.swift
@@ -11,6 +11,7 @@ final class SplashFeatureTests: TestCase {
 	func test__GIVEN__splash_appeared__WHEN__no_biometrics_config__THEN__alert_is_shown() async throws {
 		let authBiometricsConfig = LocalAuthenticationConfig.neitherBiometricsNorPasscodeSetUp
 
+		let testScheduler = DispatchQueue.test
 		let store = TestStore(
 			initialState: Splash.State(),
 			reducer: Splash()
@@ -20,10 +21,22 @@ final class SplashFeatureTests: TestCase {
 			authBiometricsConfig
 		}
 
+		store.dependencies.mainQueue = testScheduler.eraseToAnyScheduler()
+
+		let newProfile = try await Profile.new(networkAndGateway: .hammunet, mnemonic: .generate())
+		store.dependencies.profileLoader = ProfileLoader(loadProfile: {
+			.success(newProfile)
+		})
+
 		// when
 		await store.send(.internal(.view(.viewAppeared)))
 
 		// then
+		await store.receive(.internal(.system(.loadProfile)))
+		await store.receive(.internal(.system(.loadProfileResult(.success(newProfile))))) {
+			$0.profileResult = .success(newProfile)
+		}
+		await testScheduler.advance(by: .seconds(0.2))
 		await store.receive(.internal(.system(.verifyBiometrics)))
 		await store.receive(.internal(.system(.biometricsConfigResult(.success(authBiometricsConfig))))) {
 			$0.alert = .init(
@@ -57,11 +70,13 @@ final class SplashFeatureTests: TestCase {
 		await store.send(.internal(.view(.viewAppeared)))
 
 		// then
+		await store.receive(.internal(.system(.loadProfile)))
+		await store.receive(.internal(.system(.loadProfileResult(.success(newProfile))))) {
+			$0.profileResult = .success(newProfile)
+		}
+		await testScheduler.advance(by: .seconds(0.2))
 		await store.receive(.internal(.system(.verifyBiometrics)))
 		await store.receive(.internal(.system(.biometricsConfigResult(.success(authBiometricsConfig)))))
-		await store.receive(.internal(.system(.loadProfile)))
-		await store.receive(.internal(.system(.loadProfileResult(.success(newProfile)))))
-		await testScheduler.advance(by: .seconds(0.2))
 		await store.receive(.delegate(.profileResultLoaded(.success(newProfile))))
 	}
 }
