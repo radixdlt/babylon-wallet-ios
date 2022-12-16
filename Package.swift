071ad93b3fb91a6eaebede6d64e676bed8f77fc8
Alexander Cyon <116169792+CyonAlexRDX@users.noreply.github.com>
2022-12-16 15:01:21 +0100

    update converse to PyrusPyrifolia

--- Package.swift
+++ Package.swift
@@ -17,7 +17,7 @@ let package = Package(
 package.dependencies += [
 	// RDX Works dependencies
 	.package(url: "git@github.com:radixdlt/Bite.git", from: "0.0.1"),
-	.package(url: "git@github.com:radixdlt/Converse.git", from: "0.2.1"),
+	.package(url: "git@github.com:radixdlt/Converse.git", branch: "abw_659_pyrus_pyrifolia"),
 	.package(url: "git@github.com:radixdlt/swift-engine-toolkit.git", from: "0.1.10"),
 	.package(url: "git@github.com:radixdlt/swift-profile.git", from: "0.0.55"),
 
