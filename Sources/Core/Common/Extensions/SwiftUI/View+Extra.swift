9d5143ab3501fb1d93ab0800ebf897f9700c68e3
David Roman <2538074+davdroman@users.noreply.github.com>
2022-12-26 12:20:28 +0000

    wip

--- Sources/Core/Common/Extensions/SwiftUI/View+Extra.swift
+++
@@ -1,9 +0,0 @@
-import SwiftUI
-
-public extension View {
-	@available(*, deprecated, message: "Use 'controlState(.enabled/.disabled)' instead.")
-	@inlinable
-	func enabled(_ enabled: @autoclosure () -> Bool) -> some View {
-		disabled(!enabled())
-	}
-}
