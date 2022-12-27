6ada421fa4426bdfa6670266233c73fb38cf0e17
David Roman <2538074+davdroman@users.noreply.github.com>
2022-12-27 15:30:43 +0000

    wip

--- Sources/Clients/ProfileClient/ProfileClient+Test.swift
+++ Sources/Clients/ProfileClient/ProfileClient+Test.swift
@@ -18,42 +18,7 @@ public extension DependencyValues {
 
 // MARK: - ProfileClient + TestDependencyKey
 extension ProfileClient: TestDependencyKey {
-	// TODO: make every endpoint no-op
-	public static let previewValue = Self(
-		getCurrentNetworkID: { NetworkID.nebunet },
-		getGatewayAPIEndpointBaseURL: { URL(string: "example.com")! },
-		getNetworkAndGateway: { AppPreferences.NetworkAndGateway.nebunet },
-		setNetworkAndGateway: { _ in },
-		createNewProfile: { req in
-			try! await Profile.new(
-				networkAndGateway: req.networkAndGateway,
-				mnemonic: req.curve25519FactorSourceMnemonic
-			)
-		},
-		injectProfile: { _ in /* Noop */ },
-		extractProfileSnapshot: { fatalError("Impl me") },
-		deleteProfileAndFactorSources: { /* Noop */ },
-		hasAccountOnNetwork: { _ in false },
-		getAccounts: {
-			let accounts: [OnNetwork.Account] = [.placeholder0, .placeholder1]
-			return NonEmpty(rawValue: OrderedSet(accounts))!
-		},
-		getP2PClients: unimplemented("\(Self.self).getP2PClients"),
-		addP2PClient: unimplemented("\(Self.self).addP2PClient"),
-		deleteP2PClientByID: unimplemented("\(Self.self).deleteP2PClientByID"),
-		getAppPreferences: unimplemented("\(Self.self).getAppPreferences"),
-		setDisplayAppPreferences: unimplemented("\(Self.self).setDisplayAppPreferences"),
-		createVirtualAccount: unimplemented("\(Self.self).createVirtualAccount"),
-		lookupAccountByAddress: { _ in
-			.placeholder0
-		},
-		signersForAccountsGivenAddresses: { _ in
-			struct MockError: LocalizedError {
-				let errorDescription: String? = "Failed to get signers for addresses"
-			}
-			throw MockError()
-		}
-	)
+	public static let previewValue = Self.noop
 
 	public static let testValue = Self(
 		getCurrentNetworkID: unimplemented("\(Self.self).getCurrentNetworkID"),
@@ -74,6 +39,44 @@ extension ProfileClient: TestDependencyKey {
 		createVirtualAccount: unimplemented("\(Self.self).createVirtualAccount"),
 		lookupAccountByAddress: unimplemented("\(Self.self).lookupAccountByAddress"),
 		signersForAccountsGivenAddresses: unimplemented("\(Self.self).signersForAccountsGivenAddresses")
+	)
+}
+
+public extension ProfileClient {
+	static let noop = Self(
+		getCurrentNetworkID: { NetworkID.nebunet },
+		getGatewayAPIEndpointBaseURL: { URL(string: "example.com")! },
+		getNetworkAndGateway: { AppPreferences.NetworkAndGateway.nebunet },
+		setNetworkAndGateway: { _ in },
+		createNewProfile: { req in
+			try! await Profile.new(
+				networkAndGateway: req.networkAndGateway,
+				mnemonic: req.curve25519FactorSourceMnemonic
+			)
+		},
+		injectProfile: { _ in },
+		extractProfileSnapshot: { throw CancellationError() },
+		deleteProfileAndFactorSources: {},
+		hasAccountOnNetwork: { _ in false },
+		getAccounts: {
+			let accounts: [OnNetwork.Account] = [.placeholder0, .placeholder1]
+			return NonEmpty(rawValue: OrderedSet(accounts))!
+		},
+		getP2PClients: { throw CancellationError() },
+		addP2PClient: { _ in throw CancellationError() },
+		deleteP2PClientByID: { _ in throw CancellationError() },
+		getAppPreferences: { throw CancellationError() },
+		setDisplayAppPreferences: { _ in throw CancellationError() },
+		createVirtualAccount: { _ in throw CancellationError() },
+		lookupAccountByAddress: { _ in
+			.placeholder0
+		},
+		signersForAccountsGivenAddresses: { _ in
+			struct MockError: LocalizedError {
+				let errorDescription: String? = "Failed to get signers for addresses"
+			}
+			throw MockError()
+		}
 	)
 }
 #endif
