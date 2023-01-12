fe68412a465217952acdb2221574b57bb885e9b5
David Roman <2538074+davdroman@users.noreply.github.com>
2023-01-12 18:24:12 +0100

    wip

--- App/Shared/WalletApp.swift
+++ App/Shared/WalletApp.swift
@@ -1,7 +1,6 @@
 import AppFeature
 import FeaturePrelude
 import ProfileClient
-import UserDefaultsClient
 
 // MARK: - WalletApp
 @main
--- Tests/Features/OnboardingFeatureTests/OnboardingFeatureTests.swift
+++ Tests/Features/OnboardingFeatureTests/OnboardingFeatureTests.swift
@@ -2,7 +2,6 @@ import FeaturePrelude
 @testable import OnboardingFeature
 import Profile
 import TestingPrelude
-import UserDefaultsClient
 
 @MainActor
 final class OnboardingNewProfileFeatureTests: TestCase {}
