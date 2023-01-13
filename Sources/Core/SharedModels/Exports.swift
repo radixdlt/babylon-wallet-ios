f39f7cfebe3e7fc4bf169097125f481a5ca19282
David Roman <2538074+davdroman@users.noreply.github.com>
2023-01-13 10:58:12 +0100

    wip

--- Sources/Core/SharedModels/Exports.swift
+++ Sources/Core/SharedModels/Exports.swift
@@ -1,3 +1,4 @@
 @_exported import EngineToolkitModels
+@_exported import P2PConnection // FIXME: remove dependency on this, rely only on P2PModels
 @_exported import P2PModels
 @_exported import ProfileModels
