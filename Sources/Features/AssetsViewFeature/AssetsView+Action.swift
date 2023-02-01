1d60bb5bab38a98db315469b142c7d5f1319190d
David Roman <2538074+davdroman@users.noreply.github.com>
2023-02-01 23:33:53 +0000

    more cleanup

--- Sources/Features/AssetsViewFeature/AssetsView+Action.swift
+++ Sources/Features/AssetsViewFeature/AssetsView+Action.swift
@@ -3,18 +3,12 @@ import FungibleTokenListFeature
 import NonFungibleTokenListFeature
 
 public extension AssetsView {
-	typealias Action = ActionOf<Self>
-
 	enum ViewAction: Sendable, Equatable {
 		case listSelectorTapped(AssetsView.AssetsViewType)
 	}
-
-	typealias InternalAction = Never
 
 	enum ChildAction: Sendable, Equatable {
 		case fungibleTokenList(FungibleTokenList.Action)
 		case nonFungibleTokenList(NonFungibleTokenList.Action)
 	}
-
-	typealias DelegateAction = Never
 }
