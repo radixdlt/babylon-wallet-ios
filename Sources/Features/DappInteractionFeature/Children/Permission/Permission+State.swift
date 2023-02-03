e924ffb5fbbe4d5953340fcbb98930ea31c61d16
Nikola Milicevic <12729242+nikola-milicevic@users.noreply.github.com>
2023-02-03 12:48:48 +0100

    Implement basic permission UI

--- Sources/Features/DappInteractionFeature/Children/Permission/Permission+State.swift
+++ Sources/Features/DappInteractionFeature/Children/Permission/Permission+State.swift
@@ -3,12 +3,32 @@ import FeaturePrelude
 // MARK: - Permission.State
 public extension Permission {
 	struct State: Sendable, Hashable {
-		public init() {}
+		public let permissionKind: Kind
+		public let dappMetadata: DappMetadata
+
+		public init(
+			permissionKind: Kind,
+			dappMetadata: DappMetadata
+		) {
+			self.permissionKind = permissionKind
+			self.dappMetadata = dappMetadata
+		}
+	}
+}
+
+// MARK: - Permission.Kind
+public extension Permission {
+	enum Kind: Sendable, Equatable {
+		case account
+		case personalData
 	}
 }
 
 #if DEBUG
 public extension Permission.State {
-	static let previewValue: Self = .init()
+	static let previewValue: Self = .init(
+		permissionKind: .account,
+		dappMetadata: .previewValue
+	)
 }
 #endif
--- Sources/Features/DappInteractionFeature/Children/Permission/Permission+View.swift
+++ Sources/Features/DappInteractionFeature/Children/Permission/Permission+View.swift
@@ -19,11 +19,115 @@ public extension Permission.View {
 			observe: ViewState.init(state:),
 			send: { .view($0) }
 		) { viewStore in
-			// TODO: implement
-			Text("Implement: Permission")
-				.background(Color.yellow)
-				.foregroundColor(.red)
-				.onAppear { viewStore.send(.appeared) }
+			ForceFullScreen {
+				VStack(spacing: .zero) {
+					NavigationBar(
+						leadingItem: CloseButton {
+//							viewStore.send(.dismissButtonTapped)
+						}
+					)
+					.foregroundColor(.app.gray1)
+					.padding([.horizontal, .top], .medium3)
+
+					Spacer()
+						.frame(height: .small2)
+
+					ScrollView {
+						VStack(spacing: .medium2) {
+							VStack(spacing: .medium2) {
+								dappImage
+
+								Text(titleText(with: viewStore))
+									.foregroundColor(.app.gray1)
+									.textStyle(.sheetTitle)
+
+								subtitle(
+									dappName: viewStore.dappName,
+									message: subtitleText(with: viewStore)
+								)
+								.textStyle(.secondaryHeader)
+								.multilineTextAlignment(.center)
+							}
+							.padding(.bottom, .medium2)
+
+							VStack {
+								HStack {
+									Text("• 2 or more accounts")
+										.foregroundColor(.app.gray1)
+										.textStyle(.body1Regular)
+										.padding([.horizontal, .vertical], .medium1)
+
+									Spacer()
+								}
+								.background(Color.app.gray5)
+								.cornerRadius(.medium3)
+
+								Spacer()
+									.frame(height: .large1 * 1.5)
+
+								Text("You can update this permission in your settings at any time.")
+									.foregroundColor(.app.gray2)
+									.textStyle(.body1Regular)
+									.multilineTextAlignment(.center)
+									.padding(.horizontal, .medium3)
+							}
+							.padding(.horizontal, .medium3)
+
+							Spacer()
+								.frame(height: .large1 * 1.5)
+						}
+						.padding(.horizontal, .medium1)
+					}
+
+					ConfirmationFooter(
+						title: L10n.DApp.LoginRequest.continueButtonTitle,
+						isEnabled: true,
+						action: {}
+					)
+				}
+			}
+		}
+	}
+}
+
+// MARK: - Permission.View.PermissionViewStore
+private extension Permission.View {
+	typealias PermissionViewStore = ComposableArchitecture.ViewStore<Permission.View.ViewState, Permission.ViewAction>
+}
+
+private extension Permission.View {
+	var dappImage: some View {
+		// NOTE: using placeholder until API is available
+		Color.app.gray4
+			.frame(.medium)
+			.cornerRadius(.medium3)
+	}
+
+	func titleText(with viewStore: PermissionViewStore) -> String {
+		switch viewStore.permissionKind {
+		case .account:
+			return "Account Permission"
+		case .personalData:
+			return "Personal Data Permission"
+		}
+	}
+
+	func subtitle(dappName: String, message: String) -> some View {
+		var component1 = AttributedString(dappName)
+		component1.foregroundColor = .app.gray1
+
+		var component2 = AttributedString(message)
+		component2.foregroundColor = .app.gray2
+
+		return Text(component1 + component2)
+	}
+
+	func subtitleText(with viewStore: PermissionViewStore) -> String {
+		switch viewStore.permissionKind {
+		case .account:
+			return " is requesting permission to always be able to view account information when you login with this Persona."
+		case .personalData:
+			return " is requesting permission to always be able to view the following personal data when you login with this Persona."
 		}
 	}
 }
@@ -31,8 +135,12 @@ public extension Permission.View {
 // MARK: - Permission.View.ViewState
 extension Permission.View {
 	struct ViewState: Equatable {
+		let permissionKind: Permission.Kind
+		let dappName: String
+
 		init(state: Permission.State) {
-			// TODO: implement
+			permissionKind = state.permissionKind
+			dappName = state.dappMetadata.name
 		}
 	}
 }
