818fd6ff18c462bca77d467becbfcf09ec8fe135
David Roman <116723827+davdroman-rdx@users.noreply.github.com>
2023-02-07 16:18:29 +0000

    wip

--- Tests/Features/TransactionSigningFeatureTests/TransactionSigningFeatureTests.swift
+++ Tests/Features/TransactionSigningFeatureTests/TransactionSigningFeatureTests.swift
@@ -3,11 +3,30 @@ import FeatureTestingPrelude
 
 @MainActor
 final class TransactionSigningFeatureTests: TestCase {
+	let mockManifestWithoutLockFee = TransactionManifest(instructions: .string(
+		"""
+		# Withdraw XRD from account
+		CALL_METHOD ComponentAddress("account_sim1q02r73u7nv47h80e30pc3q6ylsj7mgvparm3pnsm780qgsy064") "withdraw_by_amount" Decimal("5.0") ResourceAddress("resource_sim1qqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqzqu57yag");
+		"""
+	))
+
+	let mockManifestWithLockFee = TransactionManifest(instructions: .string(
+		"""
+		CALL_METHOD
+		 ComponentAddress("{this_account_component_address}")
+		 "lock_fee"
+		 Decimal("10");
+
+		# Withdraw XRD from account
+		CALL_METHOD ComponentAddress("account_sim1q02r73u7nv47h80e30pc3q6ylsj7mgvparm3pnsm780qgsy064") "withdraw_by_amount" Decimal("5.0") ResourceAddress("resource_sim1qqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqzqu57yag");
+		"""
+	))
+
 	func testAddLockFeeToManifestOnAppearFailure() async {
 		// given
 		let store = TestStore(
 			initialState: TransactionSigning.State(
-				transactionManifestWithoutLockFee: .mock
+				transactionManifestWithoutLockFee: mockManifestWithoutLockFee
 			),
 			reducer: TransactionSigning()
 		) {
@@ -27,15 +46,14 @@ final class TransactionSigningFeatureTests: TestCase {
 	func testSignTransaction() async {
 		let store = TestStore(
 			initialState: TransactionSigning.State(
-				transactionManifestWithoutLockFee: .mock
+				transactionManifestWithoutLockFee: mockManifestWithoutLockFee
 			),
 			reducer: TransactionSigning()
 		) {
 			$0.profileClient.getCurrentNetworkID = { .nebunet }
-			$0.transactionClient.addLockFeeInstructionToManifest = { _ in .mock }
+			$0.transactionClient.addLockFeeInstructionToManifest = { [self] _ in mockManifestWithLockFee }
 			$0.errorQueue.schedule = { _ in }
 		}
-		store.exhaustivity = .off(showSkippedAssertions: true)
 
 		// Unhappy path - sign TX error
 		store.dependencies.transactionClient.signAndSubmitTransaction = { @Sendable _ in
@@ -43,8 +61,32 @@ final class TransactionSigningFeatureTests: TestCase {
 		}
 
 		await store.send(.view(.appeared))
-		await store.receive(/TransactionSigning.Action.internal .. /TransactionSigning.InternalAction.addLockFeeInstructionToManifestResult .. TaskResult.success) {
-			$0.transactionWithLockFee = .mock
+		await store.receive(
+			/TransactionSigning.Action.internal .. /TransactionSigning.InternalAction.addLockFeeInstructionToManifestResult .. TaskResult.success
+		) { [self] in
+			$0.transactionWithLockFee = mockManifestWithLockFee
+			$0.transactionWithLockFeeString = String(
+				"""
+				CALL_METHOD
+
+					ComponentAddress("{this_account_component_address}")
+
+					"lock_fee"
+
+					Decimal("10");
+
+				#
+					Withdraw
+					XRD
+					from
+					account
+				CALL_METHOD
+					ComponentAddress("account_sim1q02r73u7nv47h80e30pc3q6ylsj7mgvparm3pnsm780qgsy064")
+					"withdraw_by_amount"
+					Decimal("5.0")
+					ResourceAddress("resource_sim1qqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqzqu57yag");
+				"""
+			)
 		}
 
 		await store.send(.view(.signTransactionButtonTapped)) {
@@ -65,5 +107,6 @@ final class TransactionSigningFeatureTests: TestCase {
 		await store.receive(.internal(.signTransactionResult(.success("MOCKED_TX_ID")))) {
 			$0.isSigningTX = false
 		}
+		await store.receive(.delegate(.signedTXAndSubmittedToGateway("MOCKED_TX_ID")))
 	}
 }
