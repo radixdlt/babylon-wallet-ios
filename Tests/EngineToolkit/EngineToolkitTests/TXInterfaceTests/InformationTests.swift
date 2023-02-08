2bb90c42cc8135ae72b99267a69182bf3d83b66f
Omar <OmarAbdulla7@hotmail.com>
2023-02-08 17:45:23 +0300

    Formatting

--- Tests/EngineToolkit/EngineToolkitTests/TXInterfaceTests/InformationTests.swift
+++ Tests/EngineToolkit/EngineToolkitTests/TXInterfaceTests/InformationTests.swift
@@ -1,14 +1,14 @@
 import Prelude
 
 final class InformationTests: TestCase {
-    func test__information() throws {
-        let information = try sut.information().get()
-        XCTAssertNoDifference(
-            information,
-            .init(
-                packageVersion: "0.8.0",
-                gitHash: "335fdc5547ba86f8d0fc2a1a35dd9e84409c78a0"
-            )
-        )
-    }
+	func test__information() throws {
+		let information = try sut.information().get()
+		XCTAssertNoDifference(
+			information,
+			.init(
+				packageVersion: "0.8.0",
+				gitHash: "335fdc5547ba86f8d0fc2a1a35dd9e84409c78a0"
+			)
+		)
+	}
 }
