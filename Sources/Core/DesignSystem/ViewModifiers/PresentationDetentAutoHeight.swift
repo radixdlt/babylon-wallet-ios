6f8f1ef88208eb5445e064fbfe163f62136645b7
David Roman <116723827+davdroman-rdx@users.noreply.github.com>
2023-02-08 15:35:31 +0000

    wip

--- Sources/Core/DesignSystem/ViewModifiers/PresentationDetentAutoHeight.swift
+++ Sources/Core/DesignSystem/ViewModifiers/PresentationDetentAutoHeight.swift
@@ -1,29 +1,29 @@
 import SwiftUI
 
 public extension View {
-	func presentationDetentAutoHeight() -> some View {
-		self.modifier(FixedInnerHeightViewModifier())
+	func presentationDetentIntrinsicHeight() -> some View {
+		self.modifier(PresentationDetentIntrinsicHeight())
 	}
 }
 
-// MARK: - FixedInnerHeightViewModifier
-private struct FixedInnerHeightViewModifier: ViewModifier {
+// MARK: - PresentationDetentIntrinsicHeight
+private struct PresentationDetentIntrinsicHeight: ViewModifier {
 	@State private var sheetHeight: CGFloat = .zero
 
 	func body(content: Content) -> some View {
 		content
 			.overlay {
 				GeometryReader { proxy in
-					Color.clear.preference(key: InnerHeightPreferenceKey.self, value: proxy.size.height)
+					Color.clear.preference(key: IntrinsicHeightPreferenceKey.self, value: proxy.size.height)
 				}
 			}
-			.onPreferenceChange(InnerHeightPreferenceKey.self) { newHeight in sheetHeight = newHeight }
+			.onPreferenceChange(IntrinsicHeightPreferenceKey.self) { newHeight in sheetHeight = newHeight }
 			.presentationDetents([.height(sheetHeight)])
 	}
 }
 
-// MARK: - InnerHeightPreferenceKey
-private struct InnerHeightPreferenceKey: PreferenceKey {
+// MARK: - IntrinsicHeightPreferenceKey
+private struct IntrinsicHeightPreferenceKey: PreferenceKey {
 	static var defaultValue: CGFloat = .zero
 
 	static func reduce(value: inout CGFloat, nextValue: () -> CGFloat) {
