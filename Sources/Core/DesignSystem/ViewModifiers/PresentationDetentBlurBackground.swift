cad8d4f5523d95c78cacd75db2f2a75dfcaad972
David Roman <116723827+davdroman-rdx@users.noreply.github.com>
2023-02-09 13:55:33 +0000

    wip

--- Sources/Core/DesignSystem/ViewModifiers/PresentationDetentBlurBackground.swift
+++ Sources/Core/DesignSystem/ViewModifiers/PresentationDetentBlurBackground.swift
@@ -14,61 +14,121 @@ public extension View {
 // MARK: - PresentationDetentBlurBackgroundModifier
 private struct PresentationDetentBlurBackgroundModifier: ViewModifier {
 	let style: UIBlurEffect.Style
-	let animationDuration = 0.35
-
-	@Weak var blurEffectView: UIView?
 
 	func body(content: Content) -> some View {
 		content
 			.introspectViewController { viewController in
 				guard
-					let containerView = viewController.sheetPresentationController?.containerView
+					let sheetPresentationController = viewController.sheetPresentationController,
+					let containerView = sheetPresentationController.containerView
 				else {
 					return
 				}
-				self.blurEffectView = with(UIVisualEffectView(effect: UIBlurEffect(style: style))) {
+
+				sheetPresentationController.largestUndimmedDetentIdentifier = .large
+
+				let blurEffectView = with(UIVisualEffectView(effect: UIBlurEffect(style: style))) {
 					$0.frame = containerView.bounds
 					$0.autoresizingMask = [.flexibleWidth, .flexibleHeight]
-					$0.alpha = 0
-
-					containerView.backgroundColor = .clear
-					containerView.insertSubview($0, at: 0)
-
-					UIView.animate(
-						withDuration: animationDuration,
-						delay: 0,
-						options: [.curveEaseInOut],
-						animations: { [blurEffectView = $0] in
-							blurEffectView.alpha = 1
-						}
-					)
 				}
-			}
-			.onWillDisappear {
+				containerView.insertSubview(blurEffectView, at: 0)
+
+				// TODO: find a way to make this work
+//				sheetPresentationController.swizzle(
+//					original: #selector(UIPresentationController.presentationTransitionWillBegin),
+//					swizzled: #selector(UIPresentationController.swizzled_presentationTransitionWillBegin)
+//				)
+//				sheetPresentationController.additionalPresentationAnimation = {
+//					viewController.transitionCoordinator?.animate(alongsideTransition: { context in
+//						blurEffectView.alpha = 1
+//					})
+//				}
+				// Workaround for now
+				blurEffectView.alpha = 0
 				UIView.animate(
-					withDuration: animationDuration,
+					withDuration: 0.35,
 					delay: 0,
 					options: [.curveEaseInOut],
-					animations: {
-						blurEffectView?.alpha = 0
-					}
+					animations: { blurEffectView.alpha = 1 }
 				)
+
+				sheetPresentationController.swizzle(
+					original: #selector(UIPresentationController.dismissalTransitionWillBegin),
+					swizzled: #selector(UIPresentationController.swizzled_dismissalTransitionWillBegin)
+				)
+				sheetPresentationController.additionalDismissalAnimation = {
+					viewController.transitionCoordinator?.animate(alongsideTransition: { _ in
+						blurEffectView.alpha = 0
+					})
+				}
 			}
 	}
 }
 
-// MARK: - Weak
-@propertyWrapper
-private final class Weak<T: AnyObject> {
-	var wrappedValue: T? {
-		get { weakValue }
-		set { weakValue = newValue }
+private extension UIPresentationController {
+	@objc dynamic func swizzled_dismissalTransitionWillBegin() {
+		swizzled_dismissalTransitionWillBegin()
+		additionalDismissalAnimation?()
 	}
 
-	weak var weakValue: T?
+	@objc dynamic func swizzled_presentationTransitionWillBegin() {
+		swizzled_presentationTransitionWillBegin()
+		additionalPresentationAnimation?()
+	}
+}
 
-	init(wrappedValue: T?) {
-		self.weakValue = wrappedValue
+private extension UIPresentationController {
+	var additionalPresentationAnimation: (() -> Void)? {
+		get {
+			let key = unsafeBitCast(Selector(#function), to: UnsafeRawPointer.self)
+			return objc_getAssociatedObject(self, key) as? () -> Void
+		}
+		set {
+			let key = unsafeBitCast(Selector(#function), to: UnsafeRawPointer.self)
+			objc_setAssociatedObject(self, key, newValue, .OBJC_ASSOCIATION_RETAIN_NONATOMIC)
+		}
+	}
+
+	var additionalDismissalAnimation: (() -> Void)? {
+		get {
+			let key = unsafeBitCast(Selector(#function), to: UnsafeRawPointer.self)
+			return objc_getAssociatedObject(self, key) as? () -> Void
+		}
+		set {
+			let key = unsafeBitCast(Selector(#function), to: UnsafeRawPointer.self)
+			objc_setAssociatedObject(self, key, newValue, .OBJC_ASSOCIATION_RETAIN_NONATOMIC)
+		}
+	}
+}
+
+private extension UIPresentationController {
+	static var swizzledSelectors: [Selector: Void] = [:]
+
+	func swizzle(
+		original originalSelector: Selector,
+		swizzled swizzledSelector: Selector
+	) {
+		guard Self.swizzledSelectors[originalSelector] == nil else {
+			return
+		}
+
+		guard let myClass = object_getClass(self) else {
+			return
+		}
+
+		guard let swizzledMethod = class_getInstanceMethod(UIPresentationController.self, swizzledSelector) else {
+			return
+		}
+
+		if let originalMethod = class_getInstanceMethod(myClass, originalSelector) {
+			// exchange implementation
+			method_exchangeImplementations(originalMethod, swizzledMethod)
+		} else {
+			// add implementation
+			class_addMethod(myClass, originalSelector, method_getImplementation(swizzledMethod), method_getTypeEncoding(swizzledMethod))
+		}
+
+		Self.swizzledSelectors[originalSelector] = ()
 	}
 }
 #endif
