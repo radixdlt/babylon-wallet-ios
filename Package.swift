6f059faaff13338ac8ee4a27560aabec936b370c
kugel3 <gustaf.kugelberg@rdx.works>
2023-02-09 20:19:41 +0100

    Optional: Remove convenience version of isXRD

--- Package.swift
+++ Package.swift
@@ -249,7 +249,6 @@ package.addModules([
 			"Cryptography",
 			"EngineToolkit",
 			"Profile",
-			"ProfileClient",
 		],
 		tests: .yes()
 	),
--- Sources/Clients/EngineToolkitClient/EngineToolKitClient+XRD.swift
+++ Sources/Clients/EngineToolkitClient/EngineToolKitClient+XRD.swift
@@ -1,15 +1,7 @@
 import ClientPrelude
 import EngineToolkit
-import ProfileClient
 
 public extension EngineToolkitClient {
-	func isXRD(component: ComponentAddress) async throws -> Bool {
-		@Dependency(\.profileClient.getCurrentNetworkID) var getCurrentNetworkID
-		let networkID = await getCurrentNetworkID()
-
-		return try isXRD(component: component, on: networkID)
-	}
-
 	func isXRD(component: ComponentAddress, on networkID: NetworkID) throws -> Bool {
 		try isXRD(address: component.address, on: networkID)
 	}
