61bce466f62e1af57793e7cb551603751504880c
Omar <OmarAbdulla7@hotmail.com>
2023-02-13 17:24:28 +0300

    Updated RET binaries

--- Tests/EngineToolkit/EngineToolkitTests/TXInterfaceTests/InformationTests.swift
+++ Tests/EngineToolkit/EngineToolkitTests/TXInterfaceTests/InformationTests.swift
@@ -1,14 +1,14 @@
 import Prelude
 
 final class InformationTests: TestCase {
-	func test__information() throws {
-		let information = try sut.information().get()
-		XCTAssertNoDifference(
-			information,
-			.init(
-				packageVersion: "0.8.0",
-				gitHash: "335fdc5547ba86f8d0fc2a1a35dd9e84409c78a0"
-			)
-		)
-	}
+    func test__information() throws {
+        let information = try sut.information().get()
+        XCTAssertNoDifference(
+            information,
+            .init(
+                packageVersion: "0.8.0",
+                gitHash: "d6dbcead25e6e7cc2fb229e3b1ef79ea6371f423"
+            )
+        )
+    }
 }
