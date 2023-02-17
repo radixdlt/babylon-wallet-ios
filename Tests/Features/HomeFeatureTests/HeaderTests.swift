c536f4e1457d1bffff47e7bcd6e9500c9b9319d4
Nikola Milicevic <12729242+nikola-milicevic@users.noreply.github.com>
2023-02-17 11:15:24 +0100

    Fix tests

--- Tests/Features/HomeFeatureTests/HeaderTests.swift
+++ Tests/Features/HomeFeatureTests/HeaderTests.swift
@@ -5,11 +5,11 @@ import FeatureTestingPrelude
 final class HeaderTests: TestCase {
 	func testSettingsButtonTapped() async {
 		let store = TestStore(
-			initialState: Home.Header.State(),
-			reducer: Home.Header()
+			initialState: Header.State(),
+			reducer: Header()
 		)
 
-		await store.send(.internal(.view(.settingsButtonTapped)))
+		await store.send(.view(.settingsButtonTapped))
 		await store.receive(.delegate(.displaySettings))
 	}
 }
