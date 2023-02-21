7fc07ea739f69ab4b165f773a7541262b949a3b4
kugel3 <gustaf.kugelberg@rdx.works>
2023-02-21 13:10:30 +0100

    PersonaDetails - the bottom layer of the feature

---
+++ Sources/Features/ConnectedDAppsFeature/PersonaDetails/PersonaDetails+View.swift
@@ -0,0 +1,206 @@
+import FeaturePrelude
+
+// MARK: - View
+
+extension PersonaDetails {
+	@MainActor
+	public struct View: SwiftUI.View {
+		let store: Store
+
+		public init(store: Store) {
+			self.store = store
+		}
+	}
+
+	struct ViewState: Equatable {
+		let personaName: String
+	}
+}
+
+// MARK: - Body
+
+public extension PersonaDetails.View {
+	var body: some View {
+		WithViewStore(store, observe: \.viewState, send: { .view($0) }) { viewStore in
+			ScrollView(showsIndicators: false) {
+				VStack(spacing: 0) {
+					PersonaThumbnail(.placeholder, size: .veryLarge)
+						.padding(.vertical, .large2)
+
+					InfoSection(store: store.actionless)
+
+					Button(L10n.PersonaDetails.editPersona) {
+						viewStore.send(.editPersonaTapped)
+					}
+					.buttonStyle(.secondaryRectangular)
+					.padding(.vertical, .large3)
+
+					AccountSection(store: store)
+						.background(.app.gray5)
+
+					Button(L10n.PersonaDetails.disconnectPersona) {
+						viewStore.send(.disconnectPersonaTapped)
+					}
+					.buttonStyle(.primaryRectangular(isDestructive: true))
+					.padding([.horizontal, .top], .medium3)
+					.padding(.bottom, .large2)
+				}
+			}
+			.navigationTitle(viewStore.personaName)
+		}
+	}
+}
+
+// MARK: - Extensions
+
+private extension PersonaDetails.State {
+	var viewState: PersonaDetails.ViewState {
+		.init(personaName: persona.displayName.rawValue)
+	}
+}
+
+// MARK: - PersonaDetails.View.InfoSection
+extension PersonaDetails.View {
+	@MainActor
+	struct InfoSection: View {
+		struct ViewState: Equatable {
+			let dAppName: String
+			let personaName: String
+			let firstName: String?
+			let lastName: String?
+			let email: String?
+			let zipCode: String?
+			let personalIdentificationNumber: String?
+
+			var isSharingSomething: Bool {
+				firstName != nil || lastName != nil || email != nil || zipCode != nil || personalIdentificationNumber != nil
+			}
+		}
+
+		let store: Store<PersonaDetails.State, Never>
+
+		var body: some View {
+			WithViewStore(store, observe: \.infoSectionViewState) { viewStore in
+				VStack(alignment: .leading, spacing: .medium1) {
+					InfoPair(heading: L10n.PersonaDetails.personaNameHeading,
+					         item: viewStore.personaName)
+
+					Separator()
+
+					if viewStore.isSharingSomething {
+						Text(L10n.PersonaDetails.personalDataSharingDescription(viewStore.dAppName))
+							.textBlock
+					} else {
+						Text(L10n.PersonaDetails.notSharingAnything(viewStore.dAppName))
+							.textBlock
+					}
+
+					if let firstName = viewStore.firstName {
+						InfoPair(heading: L10n.PersonaDetails.firstNameHeading, item: firstName)
+					}
+
+					if let lastName = viewStore.lastName {
+						InfoPair(heading: L10n.PersonaDetails.secondNameHeading, item: lastName)
+					}
+
+					if let email = viewStore.email {
+						InfoPair(heading: L10n.PersonaDetails.emailHeading, item: email)
+					}
+
+					if let zipCode = viewStore.zipCode {
+						InfoPair(heading: L10n.PersonaDetails.zipCodeHeading, item: zipCode)
+					}
+				}
+				.padding(.horizontal, .medium1)
+			}
+		}
+	}
+}
+
+private extension PersonaDetails.State {
+	var infoSectionViewState: PersonaDetails.View.InfoSection.ViewState {
+		.init(dAppName: dAppName,
+		      personaName: persona.displayName.rawValue,
+		      firstName: persona.fields[kind: .firstName]?.rawValue,
+		      lastName: persona.fields[kind: .lastName]?.rawValue,
+		      email: persona.fields[kind: .email]?.rawValue,
+		      zipCode: persona.fields[kind: .zipCode]?.rawValue,
+		      personalIdentificationNumber: persona.fields[kind: .personalIdentificationNumber]?.rawValue)
+	}
+}
+
+// MARK: - PersonaDetails.View.AccountSection
+extension PersonaDetails.View {
+	@MainActor
+	struct AccountSection: View {
+		struct ViewState: Equatable {
+			let dAppName: String
+			let sharingAccounts: OrderedSet<OnNetwork.AccountForDisplay>
+		}
+
+		let store: StoreOf<PersonaDetails>
+
+		var body: some View {
+			WithViewStore(store, observe: \.accountSectionViewState, send: { .view($0) }) { viewStore in
+				VStack(spacing: 0) {
+					Text(L10n.PersonaDetails.accountSharingDescription(viewStore.dAppName))
+						.textBlock
+						.flushedLeft
+						.padding(.vertical, .medium2)
+						.padding(.horizontal, .medium1)
+
+					VStack(spacing: .medium3) {
+						ForEach(viewStore.sharingAccounts) { account in
+							AccountButton(account.label.rawValue,
+							              address: account.address.address,
+							              gradient: .init(account.appearanceID)) {
+								viewStore.send(.accountTapped(account.address))
+							}
+						}
+					}
+					.padding(.horizontal, .medium3)
+
+					Button(L10n.PersonaDetails.editAccountSharing) {
+						viewStore.send(.editAccountSharingTapped)
+					}
+					.buttonStyle(.secondaryRectangular)
+					.padding(.vertical, .large3)
+				}
+			}
+		}
+	}
+}
+
+private extension PersonaDetails.State {
+	var accountSectionViewState: PersonaDetails.View.AccountSection.ViewState {
+		.init(dAppName: dAppName, sharingAccounts: persona.simpleAccounts ?? [])
+	}
+}
+
+// MARK: - InfoPair
+public struct InfoPair: View {
+	let heading: String
+	let item: String
+
+	public init(heading: String, item: String) {
+		self.heading = heading
+		self.item = item
+	}
+
+	public var body: some View {
+		VStack(alignment: .leading, spacing: .small2) {
+			Text(heading)
+				.sectionHeading
+			Text(item)
+				.infoItem
+		}
+	}
+}
+
+// MARK: Extensions
+
+extension IdentifiedArrayOf<OnNetwork.Persona.Field> {
+	subscript(kind kind: OnNetwork.Persona.Field.Kind) -> OnNetwork.Persona.Field.Value? {
+		first { $0.kind == kind }?.value
+	}
+}
---
+++ Sources/Features/ConnectedDAppsFeature/PersonaDetails/PersonaDetails.swift
@@ -0,0 +1,63 @@
+import FeaturePrelude
+import ProfileClient
+
+// MARK: - PersonaDetails
+public struct PersonaDetails: Sendable, FeatureReducer {
+	@Dependency(\.profileClient) var profileClient
+
+	public typealias Store = StoreOf<Self>
+
+	public init() {}
+
+	// MARK: - State
+
+	public struct State: Sendable, Hashable {
+		public let dAppName: String
+		public let dAppID: OnNetwork.ConnectedDapp.ID
+		public let networkID: NetworkID
+		public let persona: OnNetwork.AuthorizedPersonaDetailed
+
+		public init(dAppName: String,
+		            dAppID: OnNetwork.ConnectedDapp.ID,
+		            networkID: NetworkID,
+		            persona: OnNetwork.AuthorizedPersonaDetailed)
+		{
+			self.dAppName = dAppName
+			self.dAppID = dAppID
+			self.networkID = networkID
+			self.persona = persona
+		}
+	}
+
+	// MARK: - Action
+
+	public enum ViewAction: Sendable, Equatable {
+		case editPersonaTapped
+		case accountTapped(AccountAddress)
+		case editAccountSharingTapped
+		case disconnectPersonaTapped
+	}
+
+	public enum DelegateAction: Sendable, Equatable {
+		case personaDisconnected
+	}
+
+	// MARK: - Reducer
+
+	public func reduce(into state: inout State, viewAction: ViewAction) -> EffectTask<Action> {
+		switch viewAction {
+		case .editPersonaTapped:
+			return .none
+		case let .accountTapped(address):
+			return .none
+		case .editAccountSharingTapped:
+			return .none
+		case .disconnectPersonaTapped:
+			let (personaID, dAppID, networkID) = (state.persona.id, state.dAppID, state.networkID)
+			return .task {
+				try await profileClient.disconnectPersonaFromDapp(personaID, dAppID, networkID)
+				return .delegate(.personaDisconnected)
+			}
+		}
+	}
+}
