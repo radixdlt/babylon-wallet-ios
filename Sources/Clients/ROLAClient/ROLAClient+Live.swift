37bd9084ae39633138e6c65f26a953ce2f0bcc8e
Nikola Milicevic <12729242+nikola-milicevic@users.noreply.github.com>
2023-02-21 08:48:17 +0100

    Implement file check

--- Sources/Clients/ROLAClient/ROLAClient+Live.swift
+++ Sources/Clients/ROLAClient/ROLAClient+Live.swift
@@ -6,26 +6,59 @@ extension ROLAClient {
 			@Dependency(\.urlSession) var urlSession
 
 			guard let originURL = URL(string: interaction.metadata.origin.rawValue) else {
-				throw WellKnownCheckError.invalidURL
+				throw WellKnownFileCheckError.invalidOriginURL
 			}
 			let wellKnownFilePath = ".well-known/radix.json"
 			let url = originURL.appending(path: wellKnownFilePath)
 
-			print("\(url)")
-			let (data, response) = try await urlSession.data(from: url)
-			print("🟢🟢🟢🟢🟢🟢")
-			print(data)
-			print(try JSONSerialization.jsonObject(with: data))
-			print("=============================================")
-			print(response)
-			print("🟣🟣🟣🟣🟣🟣")
+			let (data, urlResponse) = try await urlSession.data(from: url)
+
+			guard let httpURLResponse = urlResponse as? HTTPURLResponse else {
+				throw ExpectedHTTPURLResponse()
+			}
+
+			guard httpURLResponse.statusCode == BadHTTPResponseCode.expected else {
+				throw BadHTTPResponseCode(got: httpURLResponse.statusCode)
+			}
+
+			let response: WellKnownFileResponse
+			do {
+				response = try JSONDecoder().decode(WellKnownFileResponse.self, from: data)
+			} catch {
+				throw WellKnownFileCheckError.invalidOriginURL
+			}
+
+			let dAppDefinitionAddresses = response.dApps.map(\.dAppDefinitionAddress)
+			guard dAppDefinitionAddresses.contains(interaction.metadata.dAppDefinitionAddress.address) else {
+				throw WellKnownFileCheckError.unknownDappDefinitionAddress
+			}
 		}
 	)
+
+	struct WellKnownFileResponse: Decodable {
+		let dApps: [Item]
+
+		struct Item: Decodable {
+			let dAppDefinitionAddress: String
+		}
+	}
 }
 
-// MARK: - ROLAClient.WellKnownCheckError
+// MARK: - ROLAClient.WellKnownFileCheckError
 extension ROLAClient {
-	enum WellKnownCheckError: Error, LocalizedError {
-		case invalidURL
+	enum WellKnownFileCheckError: Error, LocalizedError {
+		case invalidOriginURL
+		case invalidWellKnownFileStructure
+		case unknownDappDefinitionAddress
 	}
+}
+
+// MARK: - ExpectedHTTPURLResponse
+struct ExpectedHTTPURLResponse: Swift.Error {}
+
+// MARK: - BadHTTPResponseCode
+public struct BadHTTPResponseCode: Swift.Error {
+	public let got: Int
+	public let butExpected = Self.expected
+	static let expected = 200
 }
