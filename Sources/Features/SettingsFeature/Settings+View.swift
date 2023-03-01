dec5814fed82a44000cdde4868239d0cf78a9c63
David Roman <116723827+davdroman-rdx@users.noreply.github.com>
2023-03-01 15:19:04 +0000

    wip

--- Sources/Features/SettingsFeature/Settings+View.swift
+++ Sources/Features/SettingsFeature/Settings+View.swift
@@ -21,7 +21,7 @@ extension AppSettings {
 		}
 	}
 
-	public struct ViewState: Equatable {
+	struct ViewState: Equatable {
 		#if DEBUG
 		let isDebugProfileViewSheetPresented: Bool
 		let profileToInspect: Profile?
