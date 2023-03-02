e5876a694736bc77263c539b99c354fcad595fed
kugel3 <gustaf.kugelberg@rdx.works>
2023-03-02 13:35:30 +0100

    Make Card more powerful

--- Sources/Core/DesignSystem/Components/Card.swift
+++ Sources/Core/DesignSystem/Components/Card.swift
@@ -3,14 +3,22 @@ import SwiftUI
 // MARK: - Card
 public struct Card<Contents: View>: View {
 	let contents: Contents
+	let insetContents: Bool
 
-	public init(@ViewBuilder contents: () -> Contents) {
+	public init(insetContents: Bool = false, @ViewBuilder contents: () -> Contents) {
+		self.insetContents = insetContents
 		self.contents = contents()
 	}
 
 	public var body: some View {
 		HStack(spacing: 0) {
-			contents
+			if insetContents {
+				contents
+					.clipShape(RoundedRectangle(cornerRadius: .small1))
+					.padding(.small1)
+			} else {
+				contents
+			}
 		}
 		.cardStyle
 	}
@@ -19,7 +27,7 @@ public struct Card<Contents: View>: View {
 extension View {
 	public var cardStyle: some View {
 		background {
-			RoundedRectangle(cornerRadius: .small1)
+			RoundedRectangle(cornerRadius: .medium3)
 				.fill(.white)
 				.cardShadow
 		}
