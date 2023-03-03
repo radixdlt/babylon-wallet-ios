f9a112e7ec38e80d339b871abdbacde6891b3c6b
David Roman <116723827+davdroman-rdx@users.noreply.github.com>
2023-03-03 15:58:05 +0000

    wip

--- Sources/Features/CreateEntityFeature/Coordinator/CreateEntityCoordinator+View.swift
+++ Sources/Features/CreateEntityFeature/Coordinator/CreateEntityCoordinator+View.swift
@@ -19,46 +19,44 @@ extension CreateEntityCoordinator {
 		public init(store: StoreOf<CreateEntityCoordinator>) {
 			self.store = store
 		}
-	}
-}
 
-extension CreateEntityCoordinator.View {
-	public var body: some View {
-		WithViewStore(store, observe: \.viewState) { viewStore in
-			NavigationStack {
-				SwitchStore(store.scope(state: \.step)) {
-					CaseLet(
-						state: /CreateEntityCoordinator.State.Step.step0_nameNewEntity,
-						action: { CreateEntityCoordinator.Action.child(.step0_nameNewEntity($0)) },
-						then: { NameNewEntity.View(store: $0) }
-					)
-					CaseLet(
-						state: /CreateEntityCoordinator.State.Step.step1_selectGenesisFactorSource,
-						action: { CreateEntityCoordinator.Action.child(.step1_selectGenesisFactorSource($0)) },
-						then: { SelectGenesisFactorSource.View(store: $0) }
-					)
-					CaseLet(
-						state: /CreateEntityCoordinator.State.Step.step2_creationOfEntity,
-						action: { CreateEntityCoordinator.Action.child(.step2_creationOfEntity($0)) },
-						then: { CreationOfEntity.View(store: $0) }
-					)
-					CaseLet(
-						state: /CreateEntityCoordinator.State.Step.step3_completion,
-						action: { CreateEntityCoordinator.Action.child(.step3_completion($0)) },
-						then: { NewEntityCompletion.View(store: $0) }
-					)
-				}
-				#if os(iOS)
-				.toolbar {
-					if viewStore.shouldDisplayNavBar {
-						ToolbarItem(placement: .navigationBarLeading) {
-							CloseButton {
-								viewStore.send(.view(.dismiss))
+		public var body: some SwiftUI.View {
+			WithViewStore(store, observe: \.viewState) { viewStore in
+				NavigationStack {
+					SwitchStore(store.scope(state: \.step)) {
+						CaseLet(
+							state: /CreateEntityCoordinator.State.Step.step0_nameNewEntity,
+							action: { CreateEntityCoordinator.Action.child(.step0_nameNewEntity($0)) },
+							then: { NameNewEntity.View(store: $0) }
+						)
+						CaseLet(
+							state: /CreateEntityCoordinator.State.Step.step1_selectGenesisFactorSource,
+							action: { CreateEntityCoordinator.Action.child(.step1_selectGenesisFactorSource($0)) },
+							then: { SelectGenesisFactorSource.View(store: $0) }
+						)
+						CaseLet(
+							state: /CreateEntityCoordinator.State.Step.step2_creationOfEntity,
+							action: { CreateEntityCoordinator.Action.child(.step2_creationOfEntity($0)) },
+							then: { CreationOfEntity.View(store: $0) }
+						)
+						CaseLet(
+							state: /CreateEntityCoordinator.State.Step.step3_completion,
+							action: { CreateEntityCoordinator.Action.child(.step3_completion($0)) },
+							then: { NewEntityCompletion.View(store: $0) }
+						)
+					}
+					#if os(iOS)
+					.toolbar {
+						if viewStore.shouldDisplayNavBar {
+							ToolbarItem(placement: .navigationBarLeading) {
+								CloseButton {
+									viewStore.send(.view(.dismiss))
+								}
 							}
 						}
 					}
+					#endif
 				}
-				#endif
 			}
 		}
 	}
