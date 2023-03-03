d02b4e7950694407fac95958820a4f887efe8a0c
David Roman <116723827+davdroman-rdx@users.noreply.github.com>
2023-03-03 15:45:40 +0000

    wip

--- Sources/Features/CreateEntityFeature/Children/NewEntityCompletion/NewEntityCompletion+View.swift
+++ Sources/Features/CreateEntityFeature/Children/NewEntityCompletion/NewEntityCompletion+View.swift
@@ -1,69 +1,116 @@
 import FeaturePrelude
 
+extension NewEntityCompletion.State {
+	var viewState: NewEntityCompletion.ViewState {
+		.init(state: self)
+	}
+}
+
 // MARK: - NewEntityCompletion.View
 extension NewEntityCompletion {
+	public struct ViewState: Equatable {
+		let entityKind: String
+		let entityName: String
+		let entityIndex: Int
+		let destinationDisplayText: String
+		let isFirstOnNetwork: Bool
+		let explaination: String
+
+		// Account only
+		struct WhenAccount: Equatable {
+			let accountAddress: AddressView.ViewState
+			let appearanceID: OnNetwork.Account.AppearanceID
+		}
+
+		let whenAccount: WhenAccount?
+
+		init(state: NewEntityCompletion.State) {
+			let entityKind = state.entity.kind == .account ? L10n.Common.Account.kind : L10n.Common.Persona.kind
+			self.entityKind = entityKind
+			entityName = state.entity.displayName.rawValue
+			entityIndex = state.entity.index
+
+			destinationDisplayText = {
+				switch state.navigationButtonCTA {
+				case .goHome:
+					return L10n.CreateEntity.Completion.Destination.home
+				case .goBackToChooseEntities:
+					return L10n.CreateEntity.Completion.Destination.chooseEntities(entityKind)
+				case .goBackToPersonaList:
+					return L10n.CreateEntity.Completion.Destination.settingsPersonaList
+				}
+			}()
+
+			isFirstOnNetwork = state.isFirstOnNetwork
+
+			if let account = state.entity as? OnNetwork.Account {
+				self.whenAccount = WhenAccount(
+					accountAddress: .init(address: account.address.address, format: .default),
+					appearanceID: account.appearanceID
+				)
+				self.explaination = L10n.CreateEntity.Completion.Explanation.Specific.account
+			} else {
+				self.explaination = L10n.CreateEntity.Completion.Explanation.Specific.persona
+				self.whenAccount = nil
+			}
+		}
+	}
+
 	@MainActor
 	public struct View: SwiftUI.View {
-		public typealias Store = ComposableArchitecture.StoreOf<NewEntityCompletion>
-		private let store: Store
+		private let store: StoreOf<NewEntityCompletion>
 
-		public init(store: Store) {
+		public init(store: StoreOf<NewEntityCompletion>) {
 			self.store = store
 		}
-	}
-}
 
-extension NewEntityCompletion.View {
-	public var body: some View {
-		WithViewStore(
-			store,
-			observe: ViewState.init(state:),
-			send: { .view($0) }
-		) { viewStore in
-			VStack(spacing: .medium2) {
-				if let whenAccount = viewStore.whenAccount {
-					Spacer()
-					accountsStackView(with: viewStore, for: whenAccount)
-					Spacer()
-				} else {
+		public var body: some SwiftUI.View {
+			WithViewStore(store, observe: \.viewState, send: { .view($0) }) { viewStore in
+				VStack(spacing: .medium2) {
+					if let whenAccount = viewStore.whenAccount {
+						Spacer()
+						accountsStackView(with: viewStore, for: whenAccount)
+						Spacer()
+					} else {
+						Spacer()
+					}
+
+					VStack(spacing: .medium1) {
+						Text(L10n.CreateEntity.Completion.title)
+							.foregroundColor(.app.gray1)
+							.textStyle(.sheetTitle)
+
+						Text(subtitleText(with: viewStore))
+							.foregroundColor(.app.gray1)
+							.textStyle(.body1Regular)
+
+						Text(viewStore.explaination)
+							.foregroundColor(.app.gray1)
+							.textStyle(.body1Regular)
+							.multilineTextAlignment(.center)
+					}
+					.padding(.horizontal, .small1)
+
 					Spacer()
 				}
-
-				VStack(spacing: .medium1) {
-					Text(L10n.CreateEntity.Completion.title)
-						.foregroundColor(.app.gray1)
-						.textStyle(.sheetTitle)
-
-					Text(subtitleText(with: viewStore))
-						.foregroundColor(.app.gray1)
-						.textStyle(.body1Regular)
-
-					Text(viewStore.explaination)
-						.foregroundColor(.app.gray1)
-						.textStyle(.body1Regular)
-						.multilineTextAlignment(.center)
-				}
-				.padding(.horizontal, .small1)
-
-				Spacer()
-			}
-			.padding(.medium1)
-			.safeAreaInset(edge: .bottom, spacing: 0) {
-				Button(L10n.CreateEntity.Completion.goToDestination(viewStore.destinationDisplayText)) {
-					viewStore.send(.goToDestination)
-				}
-				.buttonStyle(.primaryRectangular)
 				.padding(.medium1)
+				.safeAreaInset(edge: .bottom, spacing: 0) {
+					Button(L10n.CreateEntity.Completion.goToDestination(viewStore.destinationDisplayText)) {
+						viewStore.send(.goToDestination)
+					}
+					.buttonStyle(.primaryRectangular)
+					.padding(.medium1)
+				}
 			}
 		}
 	}
 }
 
-extension NewEntityCompletion.View {
+private extension NewEntityCompletion.View {
 	@ViewBuilder
-	fileprivate func accountsStackView(
-		with viewStore: CompletionViewStore,
-		for whenAccount: ViewState.WhenAccount
+	func accountsStackView(
+		with viewStore: ViewStoreOf<NewEntityCompletion>,
+		for whenAccount: NewEntityCompletion.ViewState.WhenAccount
 	) -> some View {
 		ZStack {
 			VStack(spacing: .small2) {
@@ -95,19 +142,19 @@ extension NewEntityCompletion.View {
 		}
 	}
 
-	fileprivate func scale(index: Int) -> CGFloat {
+	func scale(index: Int) -> CGFloat {
 		1 - (CGFloat(index + 1) * 0.05)
 	}
 
-	fileprivate func reversedZIndex(count: Int, index: Int) -> Double {
+	func reversedZIndex(count: Int, index: Int) -> Double {
 		Double(count - index)
 	}
 
-	fileprivate func nextAppearanceId(from accountIndex: OnNetwork.Account.Index) -> OnNetwork.Account.AppearanceID {
+	func nextAppearanceId(from accountIndex: OnNetwork.Account.Index) -> OnNetwork.Account.AppearanceID {
 		OnNetwork.Account.AppearanceID.fromIndex(accountIndex + 1)
 	}
 
-	fileprivate func subtitleText(with viewStore: CompletionViewStore) -> String {
+	func subtitleText(with viewStore: ViewStoreOf<NewEntityCompletion>) -> String {
 		if viewStore.isFirstOnNetwork {
 			return L10n.CreateEntity.Completion.Subtitle.first(viewStore.entityKind)
 		} else {
@@ -121,63 +168,6 @@ private enum Constants {
 	static let cardFrame: CGSize = .init(width: 277, height: 85)
 	static let transparentCardsCount: Int = 3
 	static let transparentCardOffset: CGFloat = .small1
-}
-
-// MARK: - NewEntityCompletion.View.CompletionViewStore
-extension NewEntityCompletion.View {
-	fileprivate typealias CompletionViewStore = ViewStore<NewEntityCompletion.View.ViewState, NewEntityCompletion.Action.ViewAction>
-}
-
-// MARK: - NewEntityCompletion.View.ViewState
-extension NewEntityCompletion.View {
-	// MARK: ViewState
-	struct ViewState: Equatable {
-		let entityKind: String
-		let entityName: String
-		let entityIndex: Int
-		let destinationDisplayText: String
-		let isFirstOnNetwork: Bool
-		let explaination: String
-
-		// Account only
-		struct WhenAccount: Equatable {
-			let accountAddress: AddressView.ViewState
-			let appearanceID: OnNetwork.Account.AppearanceID
-		}
-
-		let whenAccount: WhenAccount?
-
-		init(state: NewEntityCompletion.State) {
-			let entityKind = state.entity.kind == .account ? L10n.Common.Account.kind : L10n.Common.Persona.kind
-			self.entityKind = entityKind
-			entityName = state.displayName
-			entityIndex = state.index
-
-			destinationDisplayText = {
-				switch state.navigationButtonCTA {
-				case .goHome:
-					return L10n.CreateEntity.Completion.Destination.home
-				case .goBackToChooseEntities:
-					return L10n.CreateEntity.Completion.Destination.chooseEntities(entityKind)
-				case .goBackToPersonaList:
-					return L10n.CreateEntity.Completion.Destination.settingsPersonaList
-				}
-			}()
-
-			isFirstOnNetwork = state.isFirstOnNetwork
-
-			if let account = state.entity as? OnNetwork.Account {
-				self.whenAccount = WhenAccount(
-					accountAddress: .init(address: account.address.address, format: .default),
-					appearanceID: account.appearanceID
-				)
-				self.explaination = L10n.CreateEntity.Completion.Explanation.Specific.account
-			} else {
-				self.explaination = L10n.CreateEntity.Completion.Explanation.Specific.persona
-				self.whenAccount = nil
-			}
-		}
-	}
 }
 
 #if DEBUG
