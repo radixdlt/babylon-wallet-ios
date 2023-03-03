c0e9e08daf25b048873c1679a30e8d7789c89902
David Roman <116723827+davdroman-rdx@users.noreply.github.com>
2023-03-03 12:22:56 +0000

    wip

--- Sources/Features/AggregatedValueFeature/AggregatedValue+Reducer.swift
+++ Sources/Features/AggregatedValueFeature/AggregatedValue+Reducer.swift
@@ -1,16 +1,38 @@
 import FeaturePrelude
 
-public struct AggregatedValue: Sendable, ReducerProtocol {
+public struct AggregatedValue: Sendable, FeatureReducer {
+	public struct State: Sendable, Hashable {
+		public var value: BigDecimal?
+
+		// MARK: - AppSettings properties
+		public var currency: FiatCurrency
+		public var isCurrencyAmountVisible: Bool
+
+		public init(
+			value: BigDecimal? = nil,
+			currency: FiatCurrency = .usd,
+			isCurrencyAmountVisible: Bool = false
+		) {
+			self.value = value
+			self.currency = currency
+			self.isCurrencyAmountVisible = isCurrencyAmountVisible
+		}
+	}
+
+	public enum ViewAction: Sendable, Equatable {
+		case toggleVisibilityButtonTapped
+	}
+
+	public enum DelegateAction: Sendable, Equatable {
+		case toggleIsCurrencyAmountVisible
+	}
+
 	public init() {}
 
-	public func reduce(into state: inout State, action: Action) -> EffectTask<Action> {
-		switch action {
-		case .internal(.view(.toggleVisibilityButtonTapped)):
-			return .run { send in
-				await send(.delegate(.toggleIsCurrencyAmountVisible))
-			}
-		case .delegate:
-			return .none
+	public func reduce(into state: inout State, viewAction: ViewAction) -> EffectTask<Action> {
+		switch viewAction {
+		case .toggleVisibilityButtonTapped:
+			return .send(.delegate(.toggleIsCurrencyAmountVisible))
 		}
 	}
 }
