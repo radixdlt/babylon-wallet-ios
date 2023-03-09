27f6350e34d83a578852c911e778a25a0b711b24
kugel3 <gustaf.kugelberg@rdx.works>
2023-03-09 09:39:13 +0100

    Remove curves from Value_ system

--- Sources/EngineToolkit/EngineToolkitModels/Models/Value/EcdsaSecp256k1Signature.swift
+++ Sources/EngineToolkit/EngineToolkitModels/Models/Value/EcdsaSecp256k1Signature.swift
@@ -1,12 +1,10 @@
 import Foundation
 
 // MARK: - EcdsaSecp256k1Signature
-public struct EcdsaSecp256k1Signature: ValueProtocol, Sendable, Codable, Hashable {
-	// Type name, used as a discriminator
-	public static let kind: ValueKind = .ecdsaSecp256k1Signature
-	public func embedValue() -> Value_ {
-		.ecdsaSecp256k1Signature(self)
-	}
+public struct EcdsaSecp256k1Signature: Sendable, Codable, Hashable {
+	// Curve name and key type, used as a discriminators
+	public static let curve: CurveDiscriminator = .ecdsaSecp256k1
+	public static let keyType: CurveKeyType = .signature
 
 	// MARK: Stored properties
 	public let bytes: [UInt8]
@@ -31,18 +29,15 @@ extension EcdsaSecp256k1Signature {
 	// MARK: Codable
 	public func encode(to encoder: Encoder) throws {
 		var container = encoder.container(keyedBy: CodingKeys.self)
-		try container.encode(Self.kind, forKey: .type)
-
+		try container.encode(Self.curve, forKey: .type)
 		try container.encode(bytes.hex(), forKey: .signature)
 	}
 
 	public init(from decoder: Decoder) throws {
 		// Checking for type discriminator
 		let container = try decoder.container(keyedBy: CodingKeys.self)
-		let kind: ValueKind = try container.decode(ValueKind.self, forKey: .type)
-		if kind != Self.kind {
-			throw InternalDecodingFailure.valueTypeDiscriminatorMismatch(expected: Self.kind, butGot: kind)
-		}
+		let type = try container.decode(String.self, forKey: .type)
+		try type.confirmCurveDiscriminator(curve: Self.curve, keyType: Self.keyType)
 
 		// Decoding `signature`
 		try self.init(hex: container.decode(String.self, forKey: .signature))
