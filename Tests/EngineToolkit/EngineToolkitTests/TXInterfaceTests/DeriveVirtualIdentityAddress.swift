fd575cd037f1b3cb7d989edd4363e1b09bb689b8
Omar <97489629+0xOmarA@users.noreply.github.com>
2023-03-10 01:51:58 +0300

    Analyze manifest with preview context (#355)

--- Tests/EngineToolkit/EngineToolkitTests/TXInterfaceTests/DeriveVirtualIdentityAddress.swift
+++ Tests/EngineToolkit/EngineToolkitTests/TXInterfaceTests/DeriveVirtualIdentityAddress.swift
@@ -2,49 +2,47 @@
 import Prelude
 
 // MARK: - DeriveVirtualIdentityAddressRequestTests
-
 final class DeriveVirtualIdentityAddressRequestTests: TestCase {
-    override func setUp() {
-        debugPrint = false
-        super.setUp()
-        continueAfterFailure = false
-    }
+	override func setUp() {
+		debugPrint = false
+		super.setUp()
+		continueAfterFailure = false
+	}
 
-    func test__encodeDecodeAddressRequest() throws {
-        try TestSuite.vectors.forEach { try doTest(vector: $0) }
-    }
+	func test__encodeDecodeAddressRequest() throws {
+		try TestSuite.vectors.forEach { try doTest(vector: $0) }
+	}
 }
 
 extension DeriveVirtualIdentityAddressRequestTests {
-    private func doTest(
-        vector: DeriveVirtualIdentityAddressTestVectors.Vector,
-        networkID: NetworkID = .simulator,
-        line: UInt = #line
-    ) throws {
-        let derivedVirtualIdentityAddress = try sut.deriveVirtualIdentityAddressRequest(
-            request: DeriveVirtualIdentityAddressRequest(
-                publicKey: vector.publicKey,
-                networkId: NetworkID(0xF2)
-            )
-        ).get().virtualIdentityAddress
-        XCTAssertNoDifference(
-            derivedVirtualIdentityAddress,
-            vector.virtualIdentityComponentAddress,
-            line: line
-        )
-    }
+	private func doTest(
+		vector: DeriveVirtualIdentityAddressTestVectors.Vector,
+		networkID: NetworkID = .simulator,
+		line: UInt = #line
+	) throws {
+		let derivedVirtualIdentityAddress = try sut.deriveVirtualIdentityAddressRequest(
+			request: DeriveVirtualIdentityAddressRequest(
+				publicKey: vector.publicKey,
+				networkId: NetworkID(0xF2)
+			)
+		).get().virtualIdentityAddress
+		XCTAssertNoDifference(
+			derivedVirtualIdentityAddress,
+			vector.virtualIdentityComponentAddress,
+			line: line
+		)
+	}
 
-    fileprivate typealias TestSuite = DeriveVirtualIdentityAddressTestVectors
+	fileprivate typealias TestSuite = DeriveVirtualIdentityAddressTestVectors
 }
 
 // MARK: - DeriveVirtualIdentityAddressTestVectors
-
 enum DeriveVirtualIdentityAddressTestVectors {
-    typealias Vector = (publicKey: Engine.PublicKey, virtualIdentityComponentAddress: ComponentAddress)
-    static let vectors: [Vector] = [
-        (
-            publicKey: try! Engine.PublicKey.eddsaEd25519(Engine.EddsaEd25519PublicKey(hex: "1262bc6d5408a3c4e025aa0c15e64f69197cdb38911be5ad344a949779df3da6")),
-            virtualIdentityComponentAddress: ComponentAddress(address: "identity_sim1pv4zv7pqlfq8tqqns9qqreegtct6r3n8kcq0ag3q7v7sujmz2r")
-        ),
-    ]
+	typealias Vector = (publicKey: Engine.PublicKey, virtualIdentityComponentAddress: ComponentAddress)
+	static let vectors: [Vector] = [
+		(
+			publicKey: try! Engine.PublicKey.eddsaEd25519(Engine.EddsaEd25519PublicKey(hex: "1262bc6d5408a3c4e025aa0c15e64f69197cdb38911be5ad344a949779df3da6")),
+			virtualIdentityComponentAddress: ComponentAddress(address: "identity_sim1pv4zv7pqlfq8tqqns9qqreegtct6r3n8kcq0ag3q7v7sujmz2r")
+		),
+	]
 }
