80526f604b764d491d153ac3755b88b3d8fadef7
kugel3 <gustaf.kugelberg@rdx.works>
2023-03-09 22:58:31 +0100

    Restore Map and fix tests

--- Sources/EngineToolkit/EngineToolkitModels/Models/Value/Map.swift
+++ Sources/EngineToolkit/EngineToolkitModels/Models/Value/Map.swift
@@ -1,76 +1,75 @@
 import Foundation
 
 // MARK: - Map_
-
 public struct Map_: ValueProtocol, Sendable, Codable, Hashable {
-    // Type name, used as a discriminator
-    public static let kind: ValueKind = .map
-    public func embedValue() -> Value_ {
-        .map(self)
-    }
+	// Type name, used as a discriminator
+	public static let kind: ValueKind = .map
+	public func embedValue() -> Value_ {
+		.map(self)
+	}
 
-    // MARK: Stored properties
+	// MARK: Stored properties
 
-    public let keyValueKind: ValueKind
-    public let valueValueKind: ValueKind
-    public let entries: [[Value_]]
+	public let keyValueKind: ValueKind
+	public let valueValueKind: ValueKind
+	public let entries: [[Value_]]
 
-    // MARK: Init
+	// MARK: Init
 
-    public init(
-        keyValueKind: ValueKind,
-        valueValueKind: ValueKind,
-        entries: [[Value_]]
-    ) {
-        self.keyValueKind = keyValueKind
-        self.valueValueKind = valueValueKind
-        self.entries = entries
-    }
+	public init(
+		keyValueKind: ValueKind,
+		valueValueKind: ValueKind,
+		entries: [[Value_]]
+	) {
+		self.keyValueKind = keyValueKind
+		self.valueValueKind = valueValueKind
+		self.entries = entries
+	}
 }
 
 // MARK: Map_.Error
-
 public extension Map_ {
-    enum Error: String, Swift.Error, Sendable, Hashable {
-        case homogeneousMapRequired
-    }
+	enum Error: String, Swift.Error, Sendable, Hashable {
+		case homogeneousMapRequired
+	}
 }
 
-public extension Map_ {
-    // MARK: CodingKeys
+extension Map_ {
+	// MARK: CodingKeys
 
-    private enum CodingKeys: String, CodingKey {
-        case type
-        case entries
-        case keyValueKind = "key_value_kind"
-        case valueValueKind = "value_value_kind"
-    }
+	private enum CodingKeys: String, CodingKey {
+		case type
+		case entries
+		case keyValueKind = "key_value_kind"
+		case valueValueKind = "value_value_kind"
+	}
 
-    // MARK: Codable
+	// MARK: Codable
 
-    func encode(to encoder: Encoder) throws {
-        var container = encoder.container(keyedBy: CodingKeys.self)
-        try container.encode(Self.kind, forKey: .type)
+	public func encode(to encoder: Encoder) throws {
+		var container = encoder.container(keyedBy: CodingKeys.self)
+		try container.encode(Self.kind, forKey: .type)
 
-        try container.encode(keyValueKind, forKey: .keyValueKind)
-        try container.encode(valueValueKind, forKey: .valueValueKind)
-        try container.encode(entries, forKey: .entries) // TODO: Fix map
-    }
+		try container.encode(keyValueKind, forKey: .keyValueKind)
+		try container.encode(valueValueKind, forKey: .valueValueKind)
 
-    init(from decoder: Decoder) throws {
-        // Checking for type discriminator
-        let container = try decoder.container(keyedBy: CodingKeys.self)
-        let kind: ValueKind = try container.decode(ValueKind.self, forKey: .type)
-        if kind != Self.kind {
-            throw InternalDecodingFailure.valueTypeDiscriminatorMismatch(expected: Self.kind, butGot: kind)
-        }
+		try container.encode(entries, forKey: .entries) // TODO: Fix map
+	}
 
-        let entries = try container.decode([[Value_]].self, forKey: .entries)
-        let keyValueKind = try container.decode(ValueKind.self, forKey: .keyValueKind)
-        let valueValueKind = try container.decode(ValueKind.self, forKey: .valueValueKind)
+	public init(from decoder: Decoder) throws {
+		// Checking for type discriminator
+		let container = try decoder.container(keyedBy: CodingKeys.self)
+		let kind: ValueKind = try container.decode(ValueKind.self, forKey: .type)
+		if kind != Self.kind {
+			throw InternalDecodingFailure.valueTypeDiscriminatorMismatch(expected: Self.kind, butGot: kind)
+		}
 
-        self.init(
-            keyValueKind: keyValueKind, valueValueKind: valueValueKind, entries: entries
-        )
-    }
+		let entries = try container.decode([[Value_]].self, forKey: .entries)
+		let keyValueKind = try container.decode(ValueKind.self, forKey: .keyValueKind)
+		let valueValueKind = try container.decode(ValueKind.self, forKey: .valueValueKind)
+
+		self.init(
+			keyValueKind: keyValueKind, valueValueKind: valueValueKind, entries: entries
+		)
+	}
 }
