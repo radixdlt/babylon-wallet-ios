fb430d945e1210fce65227640261a4f0d29ef862
Omar <OmarAbdulla7@hotmail.com>
2023-03-09 21:33:59 +0300

    fix information test

--- Tests/EngineToolkit/EngineToolkitTests/TXInterfaceTests/InformationTests.swift
+++ Tests/EngineToolkit/EngineToolkitTests/TXInterfaceTests/InformationTests.swift
@@ -1,14 +1,14 @@
 import Prelude
 
 final class InformationTests: TestCase {
-	func test__information() throws {
-		let information = try sut.information().get()
-		XCTAssertNoDifference(
-			information,
-			.init(
-				packageVersion: "0.8.0",
-				gitHash: "d6dbcead25e6e7cc2fb229e3b1ef79ea6371f423"
-			)
-		)
-	}
+    func test__information() throws {
+        let information = try sut.information().get()
+        XCTAssertNoDifference(
+            information,
+            .init(
+                packageVersion: "0.9.0",
+                lastCommitHash: "4a47287fee8938edea8adf5c3fb43c5ff7d1a9bb"
+            )
+        )
+    }
 }
