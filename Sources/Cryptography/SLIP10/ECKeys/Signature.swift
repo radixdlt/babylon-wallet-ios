70db7b0aa92e633cc0bd6dcf81073b49ea21a4ae
David Roman <116723827+davdroman-rdx@users.noreply.github.com>
2023-03-10 12:02:20 +0000

    fix?

--- Sources/Cryptography/SLIP10/ECKeys/Signature.swift
+++ Sources/Cryptography/SLIP10/ECKeys/Signature.swift
@@ -7,16 +7,14 @@ extension SLIP10 {
 	public enum Signature: Sendable, Hashable {
 		case ecdsaSecp256k1(ECDSASignatureRecoverable)
 		case eddsaEd25519(EdDSASignature)
-	}
-}
 
-extension SLIP10.Signature {
-	public var rawRepresentation: Data {
-		switch self {
-		case let .ecdsaSecp256k1(secp256k1):
-			return secp256k1.rawRepresentation
-		case let .eddsaEd25519(curve25519):
-			return curve25519
+		public var rawRepresentation: Data {
+			switch self {
+			case let .ecdsaSecp256k1(secp256k1):
+				return secp256k1.rawRepresentation
+			case let .eddsaEd25519(curve25519):
+				return curve25519
+			}
 		}
 	}
 }
