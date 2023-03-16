e13282225b0f1f6c43e6a44bede2d78468168966
kugel3 <gustaf.kugelberg@rdx.works>
2023-03-16 14:13:27 +0100

    Use RET for involvedAccounts and accountsRequiredToSign

--- Sources/EngineToolkit/EngineToolkit/TransactionManifest+InvolvedAccounts.swift
+++ Sources/EngineToolkit/EngineToolkit/TransactionManifest+InvolvedAccounts.swift
@@ -7,73 +7,19 @@ extension TransactionManifest {
 		networkId: NetworkID,
 		callMethodFilter: (CallMethod) -> Bool = { _ in true }
 	) throws -> Set<ComponentAddress> {
-		let convertedManifest = try EngineToolkit().convertManifest(request: ConvertManifestRequest(
-			manifest: self,
-			outputFormat: .parsed,
-			networkId: networkId
-		)).get()
-
-		switch convertedManifest.instructions {
-		case let .parsed(instructions):
-			var accountsRequiredToSign: Set<ComponentAddress> = []
-			for instruction in instructions {
-				switch instruction {
-				case let .callMethod(callMethodInstruction):
-					guard
-						let accountAddress = callMethodInstruction.receiverAsAccountComponentAddress,
-						callMethodFilter(callMethodInstruction)
-					else {
-						continue
-					}
-					accountsRequiredToSign.insert(accountAddress)
-				case let .setMetadata(setMetadataInstruction):
-					if let accountAddress = setMetadataInstruction.entityAddress.asAccountComponentAddress {
-						accountsRequiredToSign.insert(accountAddress)
-					}
-				case let .setMethodAccessRule(setMethodAccessRuleInstruction):
-					if let accountAddress = setMethodAccessRuleInstruction.entityAddress.asAccountComponentAddress {
-						accountsRequiredToSign.insert(accountAddress)
-					}
-				case let .setComponentRoyaltyConfig(setCompœonentRoyaltyConfigInstruction):
-					if let accountAddress = setCompœonentRoyaltyConfigInstruction.componentAddress.asAccountComponentAddress {
-						accountsRequiredToSign.insert(accountAddress)
-					}
-				case let .claimComponentRoyalty(claimComponentRoyaltyInstruction):
-					if let accountAddress = claimComponentRoyaltyInstruction.componentAddress.asAccountComponentAddress {
-						accountsRequiredToSign.insert(accountAddress)
-					}
-				default:
-					break
-				}
-			}
-
-			return accountsRequiredToSign
-		case .string:
-			fatalError("Converted the manifest to Parsed by instead received a string manifest!")
-		}
+		let analysis = try EngineToolkit()
+			.analyzeManifest(request: .init(manifest: self, networkId: networkId))
+			.get()
+		return Set(analysis.accountAddresses)
 	}
 
 	public func accountsRequiredToSign(
 		networkId: NetworkID
 	) throws -> Set<ComponentAddress> {
-		try involvedAccounts(
-			networkId: networkId,
-			callMethodFilter: {
-				[
-					"lock_fee",
-					"lock_contingent_fee",
-					"withdraw",
-					"withdraw_by_amount",
-					"withdraw_by_ids",
-					"lock_fee_and_withdraw",
-					"lock_fee_and_withdraw_by_amount",
-					"lock_fee_and_withdraw_by_ids",
-					"create_proof",
-					"create_proof_by_amount",
-					"create_proof_by_ids",
-				].contains($0.methodName)
-			}
-		)
+		let analysis = try EngineToolkit()
+			.analyzeManifest(request: .init(manifest: self, networkId: networkId))
+			.get()
+		return Set(analysis.accountsRequiringAuth)
 	}
 
 	public func accountsSuitableToPayTXFee(networkId: NetworkID) throws -> Set<ComponentAddress> {
