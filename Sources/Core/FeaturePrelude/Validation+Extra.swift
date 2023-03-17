e19fef8e3fbab794bd2e08c166e64b6029a2fcf8
David Roman <116723827+davdroman-rdx@users.noreply.github.com>
2023-03-17 14:40:39 +0000

    wip

--- Sources/Core/FeaturePrelude/Validation+Extra.swift
+++ Sources/Core/FeaturePrelude/Validation+Extra.swift
@@ -31,39 +31,38 @@ extension Binding {
 
 #if DEBUG
 struct VanillaValidationPreview: PreviewProvider {
-	@MainActor
-	struct VanillaValidationView: View {
-		@State
-		@Validation<String, String>(
-			onNil: "Cannot be nil",
-			rules: [
-				.if(\.isEmpty, error: "Cannot be empty"),
-				.if(\.isBlank, error: "Cannot be blank"),
-			]
-		)
-		var name: String? = nil
-
-		var body: some View {
-			VStack(alignment: .leading) {
-				TextField(
-					"Name",
-					text: .validation($name),
-					prompt: Text("Name")
-				)
-				.textFieldStyle(.roundedBorder)
-
-				if let error = $name.errors?.first {
-					Text(error)
-						.foregroundColor(.red)
-						.font(.footnote)
-				}
-			}
-			.padding()
-		}
-	}
-
 	static var previews: some View {
 		VanillaValidationView()
+	}
+}
+
+struct VanillaValidationView: View {
+	@State
+	@Validation<String, String>(
+		onNil: "Cannot be nil",
+		rules: [
+			.if(\.isEmpty, error: "Cannot be empty"),
+			.if(\.isBlank, error: "Cannot be blank"),
+		]
+	)
+	var name: String? = nil
+
+	var body: some View {
+		VStack(alignment: .leading) {
+			TextField(
+				"Name",
+				text: .validation($name),
+				prompt: Text("Name")
+			)
+			.textFieldStyle(.roundedBorder)
+
+			if let error = $name.errors?.first {
+				Text(error)
+					.foregroundColor(.red)
+					.font(.footnote)
+			}
+		}
+		.padding()
 	}
 }
 #endif
@@ -73,6 +72,7 @@ struct VanillaValidationPreview: PreviewProvider {
 import ComposableArchitecture
 
 extension ViewStore {
+	@_disfavoredOverload
 	public func validation<Value, Error>(
 		get: @escaping (ViewState) -> Validation<Value, Error>,
 		send valueToAction: @escaping (Value?) -> ViewAction
@@ -90,71 +90,6 @@ extension ViewStore {
 
 #if DEBUG
 struct TCAValidationPreview: PreviewProvider {
-	struct TCAValidation: Sendable, ReducerProtocol {
-		struct State: Hashable {
-			@Validation<String, String>(
-				onNil: "Cannot be nil",
-				rules: [
-					.if(\.isEmpty, error: "Cannot be empty"),
-					.if(\.isBlank, error: "Cannot be blank"),
-				]
-			)
-			var name: String? = nil
-		}
-
-		enum Action: Equatable {
-			case nameTextFieldChanged(String)
-		}
-
-		struct ViewState: Equatable {
-			@Validation<String, String> var name: String?
-
-			init(state: State) {
-				self._name = state.$name
-			}
-		}
-
-		func reduce(into state: inout State, action: Action) -> EffectTask<Action> {
-			switch action {
-			case let .nameTextFieldChanged(name):
-				state.name = name
-				return .none
-			}
-		}
-
-		@MainActor
-		struct View: SwiftUI.View {
-			private let store: StoreOf<TCAValidation>
-
-			init(store: StoreOf<TCAValidation>) {
-				self.store = store
-			}
-
-			var body: some SwiftUI.View {
-				WithViewStore(store, observe: ViewState.init(state:)) { viewStore in
-					VStack(alignment: .leading) {
-						TextField(
-							"Name",
-							text: viewStore.validation(
-								get: \.$name,
-								send: { .nameTextFieldChanged($0) }
-							),
-							prompt: Text("Name")
-						)
-						.textFieldStyle(.roundedBorder)
-
-						if let error = viewStore.$name.errors?.first {
-							Text(error)
-								.foregroundColor(.red)
-								.font(.footnote)
-						}
-					}
-					.padding()
-				}
-			}
-		}
-	}
-
 	static var previews: some View {
 		TCAValidation.View(
 			store: Store(
@@ -162,6 +97,66 @@ struct TCAValidationPreview: PreviewProvider {
 				reducer: TCAValidation()
 			)
 		)
+	}
+}
+
+struct TCAValidation: ReducerProtocol {
+	struct State: Hashable {
+		@Validation<String, String>(
+			onNil: "Cannot be nil",
+			rules: [
+				.if(\.isEmpty, error: "Cannot be empty"),
+				.if(\.isBlank, error: "Cannot be blank"),
+			]
+		)
+		var name: String? = nil
+	}
+
+	enum Action: Equatable {
+		case nameTextFieldChanged(String)
+	}
+
+	func reduce(into state: inout State, action: Action) -> EffectTask<Action> {
+		switch action {
+		case let .nameTextFieldChanged(name):
+			state.name = name
+			return .none
+		}
+	}
+
+	struct ViewState: Equatable {
+		@Validation<String, String> var name: String?
+
+		init(state: State) {
+			self._name = state.$name
+		}
+	}
+
+	struct View: SwiftUI.View {
+		let store: StoreOf<TCAValidation>
+
+		var body: some SwiftUI.View {
+			WithViewStore(store, observe: ViewState.init(state:)) { viewStore in
+				VStack(alignment: .leading) {
+					TextField(
+						"Name",
+						text: viewStore.validation(
+							get: \.$name,
+							send: { .nameTextFieldChanged($0) }
+						),
+						prompt: Text("Name")
+					)
+					.textFieldStyle(.roundedBorder)
+
+					if let error = viewStore.$name.errors?.first {
+						Text(error)
+							.foregroundColor(.red)
+							.font(.footnote)
+					}
+				}
+				.padding()
+			}
+		}
 	}
 }
 #endif
