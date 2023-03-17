bdd76df1d0845fde9e66410c1e480e5f6eb09003
Ghenadie Vasiliev-Pusca <ghenadie.vasiliev-pusca@rdx.works>
2023-03-17 15:50:29 +0200

    wip

--- Sources/Features/DappInteractionFeature/Interactor/DappInteractor.swift
+++ Sources/Features/DappInteractionFeature/Interactor/DappInteractor.swift
@@ -11,7 +11,6 @@ struct DappInteractor: Sendable, FeatureReducer {
 
 		@PresentationState
 		var currentModal: Destinations.State?
-		var currentModalIsActuallyPresented = false
 		@PresentationState
 		var responseFailureAlert: AlertState<ViewAction.ResponseFailureAlertAction>?
 	}
@@ -81,53 +80,14 @@ struct DappInteractor: Sendable, FeatureReducer {
 	func reduce(into state: inout State, viewAction: ViewAction) -> EffectTask<Action> {
 		switch viewAction {
 		case .task:
-			return .run { send in
-				await radixConnectClient.loadFromProfileAndConnectAll()
-				let currentNetworkID = await gatewaysClient.getCurrentNetworkID()
-
-				for try await incomingMessageResult in await radixConnectClient.receiveMessages() {
-					guard !Task.isCancelled else {
-						return
-					}
-
-					do {
-						let interactionMessage = try incomingMessageResult.unwrapResult()
-						let interaction = interactionMessage.peerMessage.content
-						guard interaction.metadata.networkId == currentNetworkID else {
-							let incomingRequestNetwork = try Network.lookupBy(id: interaction.metadata.networkId)
-							let currentNetwork = try Network.lookupBy(id: currentNetworkID)
-							let outMessage = interactionMessage.toOutgoingMessage(.failure(.init(
-								interactionId: interaction.id,
-								errorType: .wrongNetwork,
-								message: L10n.DApp.Request.wrongNetworkError(incomingRequestNetwork.name, currentNetwork.name)
-							)))
-
-							try await radixConnectClient.sendMessage(outMessage)
-							return
-						}
-
-						// TODO: uncomment and enable / disable based on developer mode preference
-						/*
-						 try await rolaClient.performDappDefinitionVerification(request.interaction.metadata)
-						 try await rolaClient.performWellKnownFileCheck(request.interaction.metadata)
-						 */
-						await send(.internal(.receivedRequestFromDapp(interactionMessage)))
-					} catch {
-						loggerGlobal.error("Received message contans error: \(error.localizedDescription)")
-						errorQueue.schedule(error)
-					}
-				}
-			} catch: { error, _ in
-				errorQueue.schedule(error)
-			}
-
+			return handleIncommintRequests()
 		case let .responseFailureAlert(action):
 			switch action {
 			case .dismiss:
 				return .none
 			case let .presented(.cancelButtonTapped(request)):
 				dismissCurrentModalAndRequest(request, for: &state)
-				return delayedEffect(for: .internal(.presentQueuedRequestIfNeeded))
+				return .send(.internal(.presentQueuedRequestIfNeeded))
 			case let .presented(.retryButtonTapped(response, request, dappMetadata)):
 				return sendResponseToDappEffect(response, for: request, dappMetadata: dappMetadata)
 			}
@@ -184,17 +144,6 @@ struct DappInteractor: Sendable, FeatureReducer {
 		}
 	}
 
-	func presentQueuedRequestIfNeededEffect(
-		for state: inout State
-	) -> EffectTask<Action> {
-		guard let next = state.requestQueue.first, state.currentModal == nil else {
-			return .none
-		}
-
-		state.currentModal = .dappInteraction(.relayed(next, with: .init(interaction: next.peerMessage.content)))
-		return .none
-	}
-
 	func reduce(into state: inout State, childAction: ChildAction) -> EffectTask<Action> {
 		switch childAction {
 		case let .modal(.presented(.dappInteraction(.relay(request, .delegate(.submitAndDismiss(responseToDapp, dappMetadata)))))):
@@ -204,15 +153,20 @@ struct DappInteractor: Sendable, FeatureReducer {
 		case .modal(.presented(.dappInteractionCompletion(.delegate(.dismiss)))):
 			state.currentModal = nil
 			return .send(.internal(.presentQueuedRequestIfNeeded))
-		case
-			.modal(.presented(.dappInteraction(.relay(_, .delegate(.presented))))),
-			.modal(.presented(.dappInteractionCompletion(.delegate(.presented)))):
-			state.currentModalIsActuallyPresented = true
-			return .none
-
 		default:
 			return .none
 		}
+	}
+
+	func presentQueuedRequestIfNeededEffect(
+		for state: inout State
+	) -> EffectTask<Action> {
+		guard let next = state.requestQueue.first, state.currentModal == nil else {
+			return .none
+		}
+
+		state.currentModal = .dappInteraction(.relayed(next, with: .init(interaction: next.peerMessage.content)))
+		return .none
 	}
 
 	func sendResponseToDappEffect(
@@ -233,13 +187,45 @@ struct DappInteractor: Sendable, FeatureReducer {
 		state.currentModal = nil
 	}
 
-	func delayedEffect(
-		delay: Duration = .seconds(0.75),
-		for action: Action
-	) -> EffectTask<Action> {
+	func handleIncommintRequests() -> EffectTask<Action> {
 		.run { send in
-			try await clock.sleep(for: delay)
-			await send(action)
+			await radixConnectClient.loadFromProfileAndConnectAll()
+			let currentNetworkID = await gatewaysClient.getCurrentNetworkID()
+
+			for try await incomingMessageResult in await radixConnectClient.receiveMessages() {
+				guard !Task.isCancelled else {
+					return
+				}
+
+				do {
+					let interactionMessage = try incomingMessageResult.unwrapResult()
+					let interaction = interactionMessage.peerMessage.content
+					guard interaction.metadata.networkId == currentNetworkID else {
+						let incomingRequestNetwork = try Network.lookupBy(id: interaction.metadata.networkId)
+						let currentNetwork = try Network.lookupBy(id: currentNetworkID)
+						let outMessage = interactionMessage.toOutgoingMessage(.failure(.init(
+							interactionId: interaction.id,
+							errorType: .wrongNetwork,
+							message: L10n.DApp.Request.wrongNetworkError(incomingRequestNetwork.name, currentNetwork.name)
+						)))
+
+						try await radixConnectClient.sendMessage(outMessage)
+						return
+					}
+
+					// TODO: uncomment and enable / disable based on developer mode preference
+					/*
+					 try await rolaClient.performDappDefinitionVerification(request.interaction.metadata)
+					 try await rolaClient.performWellKnownFileCheck(request.interaction.metadata)
+					 */
+					await send(.internal(.receivedRequestFromDapp(interactionMessage)))
+				} catch {
+					loggerGlobal.error("Received message contans error: \(error.localizedDescription)")
+					errorQueue.schedule(error)
+				}
+			}
+		} catch: { error, _ in
+			errorQueue.schedule(error)
 		}
 	}
 }
