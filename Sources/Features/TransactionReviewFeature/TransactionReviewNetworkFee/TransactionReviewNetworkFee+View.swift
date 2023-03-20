0201921b0ce20a771239167f3e20125d4f4b8e24
kugel3 <gustaf.kugelberg@rdx.works>
2023-03-20 17:03:34 +0100

    NetworkFee

--- Sources/Features/TransactionReviewFeature/TransactionReviewNetworkFee/TransactionReviewNetworkFee+View.swift
+++ Sources/Features/TransactionReviewFeature/TransactionReviewNetworkFee/TransactionReviewNetworkFee+View.swift
@@ -2,10 +2,6 @@ import FeaturePrelude
 
 // MARK: - TransactionReviewDappsUsed.View
 extension TransactionReviewNetworkFee {
-	public struct ViewState: Equatable {
-		// TODO: declare some properties
-	}
-
 	@MainActor
 	public struct View: SwiftUI.View {
 		let store: StoreOf<TransactionReviewNetworkFee>
@@ -15,32 +11,43 @@ extension TransactionReviewNetworkFee {
 		}
 
 		public var body: some SwiftUI.View {
-			WithViewStore(store, observe: \.viewState, send: { .view($0) }) { _ in
-				// TODO: implement
-				Text(L10n.TransactionReview.networkFeeText)
-					.background(Color.yellow)
-					.foregroundColor(.red)
+			WithViewStore(store, observe: { $0 }, send: { .view($0) }) { viewStore in
+
+				VStack(alignment: .leading, spacing: .small2) {
+					HStack {
+						Text(L10n.TransactionReview.NetworkFee.heading)
+							.sectionHeading
+
+						Button {
+							viewStore.send(.infoTapped)
+						} label: {
+							Image(asset: AssetResource.info)
+								.renderingMode(.template)
+								.foregroundColor(.app.gray3)
+						}
+
+						Spacer(minLength: 0)
+
+						Text("XRD \(viewStore.fee.format(maxPlaces: 1))") // TODO:  Revisit
+							.textStyle(.body1HighImportance)
+							.foregroundColor(.app.gray1)
+					}
+
+					if viewStore.isCongested {
+						Text(L10n.TransactionReview.NetworkFee.congestedText)
+							.textStyle(.body1Regular)
+							.foregroundColor(.app.alert)
+					}
+
+					Button(L10n.TransactionReview.NetworkFee.customizeButtonTitle) {
+						viewStore.send(.customizeTapped)
+					}
+					.textStyle(.body1StandaloneLink)
+					.foregroundColor(.app.blue2)
+				}
 			}
+			.padding(.horizontal, .medium3)
+			.padding(.bottom, .medium1)
 		}
 	}
 }
-
-// #if DEBUG
-// import SwiftUI // NB: necessary for previews to appear
-//
-//// MARK: - TransactionReviewDappsUsed_Preview
-// struct TransactionReviewDappsUsed_Preview: PreviewProvider {
-//	static var previews: some View {
-//		TransactionReviewDappsUsed.View(
-//			store: .init(
-//				initialState: .previewValue,
-//				reducer: TransactionReviewDappsUsed()
-//			)
-//		)
-//	}
-// }
-//
-// extension TransactionReviewDappsUsed.State {
-//	public static let previewValue = Self()
-// }
-// #endif
