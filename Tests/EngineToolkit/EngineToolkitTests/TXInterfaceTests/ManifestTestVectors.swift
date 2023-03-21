9b5724848e5773806d6cfb9ff5de5694d5dc64f3
kugel3 <gustaf.kugelberg@rdx.works>
2023-03-21 17:54:19 +0100

    Update replacements

--- Tests/EngineToolkit/EngineToolkitTests/TXInterfaceTests/ManifestTestVectors.swift
+++ Tests/EngineToolkit/EngineToolkitTests/TXInterfaceTests/ManifestTestVectors.swift
@@ -97,26 +97,46 @@ public func manifestTestVectors() throws -> [(manifest: String, blobs: [[UInt8]]
 	for (index, _) in testVectors.enumerated() {
 		testVectors[index].manifest = try testVectors[index]
 			.manifest
-			.replacingOccurrences(of: "${", with: "{")
-			.replacingOccurrences(of: "{xrd_resource_address}", with: "resource_sim1qzkcyv5dwq3r6kawy6pxpvcythx8rh8ntum6ws62p95sqjjpwr")
-			.replacingOccurrences(of: "{faucet_component_address}", with: "component_sim1qftacppvmr9ezmekxqpq58en0nk954x0a7jv2zz0hc7q8utaxr")
-			.replacingOccurrences(of: "{this_account_component_address}", with: "account_sim1qwskd4q5jdywfw6f7jlwmcyp2xxq48uuwruc003x2kcskxh3na")
-			.replacingOccurrences(of: "{account_component_address}", with: "account_sim1qwskd4q5jdywfw6f7jlwmcyp2xxq48uuwruc003x2kcskxh3na")
-			.replacingOccurrences(of: "{other_account_component_address}", with: "account_sim1qdy4jqfpehf8nv4n7680cw0vhxqvhgh5lf3ae8jkjz6q5hmzed")
-			.replacingOccurrences(of: "{account_a_component_address}", with: "account_sim1qwssydet6r0wen92wzs3nex8x9ch5ye0uz9tzgq5nchq86xmpm")
-			.replacingOccurrences(of: "{account_b_component_address}", with: "account_sim1qdxpdrpjtsqmumccye045u4cfw2fqa3a9gujh6qvdresgnl2nh")
-			.replacingOccurrences(of: "{account_c_component_address}", with: "account_sim1qd4jtjgqxtmk2m7ze0cpa6ugae8jwfhgxqenvw6m6uwqgqmp4q")
-			.replacingOccurrences(of: "{owner_badge_resource_address}", with: "resource_sim1qrtkj5zx7tcpuhwjxerhhnmwv58k9v5yyjqgqt7rtnxsnqyl3s")
-			.replacingOccurrences(of: "{minter_badge_resource_address}", with: "resource_sim1qp075qmn6389pkq30ppzzsuadd55ry04mjx69v86r4wq0feh02")
-			.replacingOccurrences(of: "{auth_badge_resource_address}", with: "resource_sim1qp075qmn6389pkq30ppzzsuadd55ry04mjx69v86r4wq0feh02")
-			.replacingOccurrences(of: "{mintable_resource_address}", with: "resource_sim1qqgvpz8q7ypeueqcv4qthsv7ezt8h9m3depmqqw7pc4sfmucfx")
-			.replacingOccurrences(of: "{owner_badge_non_fungible_local_id}", with: "#1#")
-			.replacingOccurrences(of: "{auth_badge_non_fungible_local_id}", with: "#1#")
-			.replacingOccurrences(of: "{code_blob_hash}", with: blake2b(data: Data([10])).hex)
-			.replacingOccurrences(of: "{schema_blob_hash}", with: blake2b(data: Data([10])).hex)
-			.replacingOccurrences(of: "{initial_supply}", with: "12")
-			.replacingOccurrences(of: "{mint_amount}", with: "12")
-			.replacingOccurrences(of: "{non_fungible_local_id}", with: "#1#")
+			.replacingOccurrences(of: "${",
+			                      with: "{")
+			.replacingOccurrences(of: "{xrd_resource_address}",
+			                      with: "resource_sim1qyqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqs6d89k")
+			.replacingOccurrences(of: "{faucet_component_address}",
+			                      with: "component_sim1q0kryz5scup945usk39qjc2yjh6l5zsyuh8t7v5pk0tshjs68x")
+			.replacingOccurrences(of: "{this_account_component_address}",
+			                      with: "account_sim1qspjlnwx4gdcazhral74rjgzgysrslf8ngrfmprecrrss3p9md")
+			.replacingOccurrences(of: "{account_component_address}",
+			                      with: "account_sim1qspjlnwx4gdcazhral74rjgzgysrslf8ngrfmprecrrss3p9md")
+			.replacingOccurrences(of: "{other_account_component_address}",
+			                      with: "account_sim1qspjlnwx4gdcazhral74rjgzgysrslf8ngrfmprecrrss3p9md")
+			.replacingOccurrences(of: "{account_a_component_address}",
+			                      with: "account_sim1qspjlnwx4gdcazhral74rjgzgysrslf8ngrfmprecrrss3p9md")
+			.replacingOccurrences(of: "{account_b_component_address}",
+			                      with: "account_sim1qspjlnwx4gdcazhral74rjgzgysrslf8ngrfmprecrrss3p9md")
+			.replacingOccurrences(of: "{account_c_component_address}",
+			                      with: "account_sim1qspjlnwx4gdcazhral74rjgzgysrslf8ngrfmprecrrss3p9md")
+			.replacingOccurrences(of: "{owner_badge_resource_address}",
+			                      with: "resource_sim1qyqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqs6d89k")
+			.replacingOccurrences(of: "{minter_badge_resource_address}",
+			                      with: "resource_sim1qyqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqs6d89k")
+			.replacingOccurrences(of: "{auth_badge_resource_address}",
+			                      with: "resource_sim1qyqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqs6d89k")
+			.replacingOccurrences(of: "{mintable_resource_address}",
+			                      with: "resource_sim1qyqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqs6d89k")
+			.replacingOccurrences(of: "{owner_badge_non_fungible_local_id}",
+			                      with: "#1#")
+			.replacingOccurrences(of: "{auth_badge_non_fungible_local_id}",
+			                      with: "#1#")
+			.replacingOccurrences(of: "{code_blob_hash}",
+			                      with: blake2b(data: Data([10])).hex)
+			.replacingOccurrences(of: "{schema_blob_hash}",
+			                      with: blake2b(data: Data([10])).hex)
+			.replacingOccurrences(of: "{initial_supply}",
+			                      with: "12")
+			.replacingOccurrences(of: "{mint_amount}",
+			                      with: "12")
+			.replacingOccurrences(of: "{non_fungible_local_id}",
+			                      with: "#1#")
 	}
 
 	return testVectors
