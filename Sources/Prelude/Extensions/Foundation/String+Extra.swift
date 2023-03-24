3aa7b5a53de034d5eae7dde2ecf620f46c32fb8d
David Roman <116723827+davdroman-rdx@users.noreply.github.com>
2023-03-24 15:55:08 +0000

    wip

--- Sources/Prelude/Extensions/Foundation/String+Extra.swift
+++ Sources/Prelude/Extensions/Foundation/String+Extra.swift
@@ -23,3 +23,39 @@ extension Optional where Wrapped: StringProtocol {
 		self == nil || self?.isBlank == true
 	}
 }
+
+extension String {
+	public var isEmailAddress: Bool {
+		// Adapted from https://www.swiftbysundell.com/articles/validating-email-addresses
+
+		let detector = try? NSDataDetector(
+			types: NSTextCheckingResult.CheckingType.link.rawValue
+		)
+
+		let range = NSRange(
+			self.startIndex ..< self.endIndex,
+			in: self
+		)
+
+		let matches = detector?.matches(
+			in: self,
+			options: [],
+			range: range
+		)
+
+		// We only want our string to contain a single email
+		// address, so if multiple matches were found, then
+		// we fail our validation process and return nil:
+		guard let match = matches?.first, matches?.count == 1 else {
+			return false
+		}
+
+		// Verify that the found link points to an email address,
+		// and that its range covers the whole input string:
+		guard match.url?.scheme == "mailto", match.range == range else {
+			return false
+		}
+
+		return true
+	}
+}
