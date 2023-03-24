583df8e6a9ddf7124c363c537c8e16cb5e01ea2b
kugel3 <gustaf.kugelberg@rdx.works>
2023-03-24 07:34:13 +0100

    wip

--- Sources/Features/TransactionReviewFeature/TransactionReviewGuarantees/TransactionReviewGuarantees.swift
+++ Sources/Features/TransactionReviewFeature/TransactionReviewGuarantees/TransactionReviewGuarantees.swift
@@ -3,16 +3,22 @@ import FeaturePrelude
 // MARK: - TransactionReviewGuarantees
 public struct TransactionReviewGuarantees: Sendable, FeatureReducer {
 	public struct State: Sendable, Hashable {
-		public var transfers: IdentifiedArrayOf<TransactionReview.Transfer>
+		public var transferAccounts: IdentifiedArrayOf<TransactionReviewAccount.State>
 
-		public init(transfers: IdentifiedArrayOf<TransactionReview.Transfer>) {
-			self.transfers = transfers
+		public init(transferAccounts: IdentifiedArrayOf<TransactionReviewAccount.State>) {
+			self.transferAccounts = transferAccounts
 		}
 	}
 
 	public enum ViewAction: Sendable, Equatable {
 		case infoTapped
-		case dAppTapped(id: TransactionReview.Dapp.ID)
+		case closeTapped
+		case increaseTapped(id: TransactionReview.Transfer.ID)
+		case decreaseTapped(id: TransactionReview.Transfer.ID)
+	}
+
+	public enum DelegateAction: Sendable, Equatable {
+		case dismiss
 	}
 
 	public init() {}
@@ -21,7 +27,12 @@ public struct TransactionReviewGuarantees: Sendable, FeatureReducer {
 		switch viewAction {
 		case .infoTapped:
 			return .none
-		case let .dAppTapped(id):
+		case .closeTapped:
+			return .send(.delegate(.dismiss))
+		case let .increaseTapped(id: id):
+//			guard let account =
+			return .none
+		case let .decreaseTapped(id: id):
 			return .none
 		}
 	}
