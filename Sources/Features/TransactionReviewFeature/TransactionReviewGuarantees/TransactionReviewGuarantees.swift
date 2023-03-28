0b7eeb64dcc01dd08fc75d86ef10969350208195
kugel3 <gustaf.kugelberg@rdx.works>
2023-03-28 18:36:27 +0200

    wip

--- Sources/Features/TransactionReviewFeature/TransactionReviewGuarantees/TransactionReviewGuarantees.swift
+++ Sources/Features/TransactionReviewFeature/TransactionReviewGuarantees/TransactionReviewGuarantees.swift
@@ -3,24 +3,65 @@ import FeaturePrelude
 // MARK: - TransactionReviewGuarantees
 public struct TransactionReviewGuarantees: Sendable, FeatureReducer {
 	public struct State: Sendable, Hashable {
-		public var transfers: IdentifiedArrayOf<FungibleTransfer>
+		public var guarantees: IdentifiedArrayOf<TransactionReviewGuarantee.State>
 
-		public init(transfers: IdentifiedArrayOf<FungibleTransfer>) {
-			self.transfers = transfers
-		}
-
-		public struct FungibleTransfer: Identifiable, Sendable, Hashable {
-			public var id: AccountAction { transfer.id }
-			public let account: TransactionReview.Account
-			public let transfer: TransactionReview.Transfer
+		public init(guarantees: IdentifiedArrayOf<TransactionReviewGuarantee.State>) {
+			self.guarantees = guarantees
 		}
 	}
 
 	public enum ViewAction: Sendable, Equatable {
 		case infoTapped
+		case applyTapped
 		case closeTapped
-		case increaseTapped(id: TransactionReview.Transfer.ID)
-		case decreaseTapped(id: TransactionReview.Transfer.ID)
+	}
+
+	public enum ChildAction: Sendable, Equatable {
+		case guarantee(id: TransactionReviewGuarantee.State.ID, action: TransactionReviewGuarantee.Action)
+	}
+
+	public enum DelegateAction: Sendable, Equatable {
+		case dismiss(apply: Bool)
+	}
+
+	public init() {}
+
+	public var body: some ReducerProtocolOf<TransactionReviewGuarantees> {
+		Reduce(core)
+			.forEach(\.guarantees, action: /Action.child .. /ChildAction.guarantee) {
+				TransactionReviewGuarantee()
+			}
+	}
+
+	public func reduce(into state: inout State, viewAction: ViewAction) -> EffectTask<Action> {
+		switch viewAction {
+		case .infoTapped:
+			// TODO: Show some info
+			return .none
+
+		case .applyTapped:
+			return .send(.delegate(.dismiss(apply: true)))
+
+		case .closeTapped:
+			return .send(.delegate(.dismiss(apply: false)))
+		}
+	}
+}
+
+// MARK: - TransactionReviewGuarantee
+public struct TransactionReviewGuarantee: Sendable, FeatureReducer {
+	public struct State: Identifiable, Sendable, Hashable {
+		public var id: AccountAction { transfer.id }
+		public let account: TransactionReview.Account
+		public let accountIfVisible: TransactionReview.Account?
+
+		public var transfer: TransactionReview.Transfer
+		public var minimumPercentage: Double
+	}
+
+	public enum ViewAction: Sendable, Equatable {
+		case increaseTapped
+		case decreaseTapped
 	}
 
 	public enum DelegateAction: Sendable, Equatable {
@@ -31,15 +72,24 @@ public struct TransactionReviewGuarantees: Sendable, FeatureReducer {
 
 	public func reduce(into state: inout State, viewAction: ViewAction) -> EffectTask<Action> {
 		switch viewAction {
-		case .infoTapped:
+		case .increaseTapped:
+			state.updateMinimumPercentage(with: state.minimumPercentage + percentageDelta)
 			return .none
-		case .closeTapped:
-			return .send(.delegate(.dismiss))
-		case let .increaseTapped(id: id):
-//			guard let account =
-			return .none
-		case let .decreaseTapped(id: id):
+		case .decreaseTapped:
+			state.updateMinimumPercentage(with: state.minimumPercentage - percentageDelta)
 			return .none
 		}
+	}
+
+	private let percentageDelta: Double = 5
+}
+
+extension TransactionReviewGuarantee.State {
+	mutating func updateMinimumPercentage(with newPercentage: Double) {
+		let newMinimum = max(min(newPercentage * 0.01, 1), 0)
+		guard let newMinimumDecimal = BigDecimal(newMinimum) else { return } // TODO: Handle?
+
+		minimumPercentage = newPercentage
+		transfer.metadata.guarantee?.amount = newMinimumDecimal * transfer.action.amount
 	}
 }
