2a6c9dacd7e75eaf422a9cd309fe795a3aa9328f
Nikola Milicevic <12729242+nikola-milicevic@users.noreply.github.com>
2023-04-07 00:24:07 +0200

    Remove do catch block

--- Sources/Clients/DiskPersistenceClient/DiskPersistenceClient+Live.swift
+++ Sources/Clients/DiskPersistenceClient/DiskPersistenceClient+Live.swift
@@ -11,12 +11,8 @@ extension DiskPersistenceClient: DependencyKey {
 
 		@Sendable
 		func createDirectoryIfNeeded(at url: URL) throws {
-			do {
-				guard !fileManager.fileExists(atPath: url.path) else { return }
-				try fileManager.createDirectory(at: url.deletingLastPathComponent(), withIntermediateDirectories: true, attributes: nil)
-			} catch {
-				throw error
-			}
+			guard !fileManager.fileExists(atPath: url.path) else { return }
+			try fileManager.createDirectory(at: url.deletingLastPathComponent(), withIntermediateDirectories: true, attributes: nil)
 		}
 
 		enum Error: Swift.Error {
@@ -25,47 +21,31 @@ extension DiskPersistenceClient: DependencyKey {
 
 		return Self(
 			save: { encodable, path in
-				do {
-					guard let url = cachesDirectoryURL?.appendingPathComponent(path) else {
-						throw Error.noCachesDirectoryFound
-					}
-					try createDirectoryIfNeeded(at: url)
-					let data = try JSONEncoder().encode(encodable)
-					try data.write(to: url, options: .atomic)
-				} catch {
-					throw error
+				guard let url = cachesDirectoryURL?.appendingPathComponent(path) else {
+					throw Error.noCachesDirectoryFound
 				}
+				try createDirectoryIfNeeded(at: url)
+				let data = try JSONEncoder().encode(encodable)
+				try data.write(to: url, options: .atomic)
 			}, load: { decodable, path in
-				do {
-					guard let url = cachesDirectoryURL?.appendingPathComponent(path) else {
-						throw Error.noCachesDirectoryFound
-					}
-					let data = try Data(contentsOf: url)
-					let value = try JSONDecoder().decode(decodable, from: data)
-					return value
-				} catch {
-					throw error
+				guard let url = cachesDirectoryURL?.appendingPathComponent(path) else {
+					throw Error.noCachesDirectoryFound
 				}
+				let data = try Data(contentsOf: url)
+				let value = try JSONDecoder().decode(decodable, from: data)
+				return value
 			}, remove: { path in
-				do {
-					guard let url = cachesDirectoryURL?.appendingPathComponent(path) else {
-						throw Error.noCachesDirectoryFound
-					}
-					try fileManager.removeItem(at: url)
-				} catch {
-					throw error
+				guard let url = cachesDirectoryURL?.appendingPathComponent(path) else {
+					throw Error.noCachesDirectoryFound
 				}
+				try fileManager.removeItem(at: url)
 			}, removeAll: {
-				do {
-					guard let caches = cachesDirectoryURL else {
-						throw Error.noCachesDirectoryFound
-					}
-					let contents = try fileManager.contentsOfDirectory(at: caches, includingPropertiesForKeys: nil, options: [])
-					for fileUrl in contents {
-						try fileManager.removeItem(at: fileUrl)
-					}
-				} catch {
-					throw error
+				guard let caches = cachesDirectoryURL else {
+					throw Error.noCachesDirectoryFound
+				}
+				let contents = try fileManager.contentsOfDirectory(at: caches, includingPropertiesForKeys: nil, options: [])
+				for fileUrl in contents {
+					try fileManager.removeItem(at: fileUrl)
 				}
 			}
 		)
