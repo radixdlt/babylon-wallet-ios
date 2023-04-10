3e7463f5ddf635a69638f07cced487a606a8843f
Alexander Cyon <alex.cyon@rdx.works>
2023-04-10 15:12:49 +0200

    WIP

--- Tests/Clients/ImportLegacyWalletClientTests/ImportLegacyWalletClientTests.swift
+++ Tests/Clients/ImportLegacyWalletClientTests/ImportLegacyWalletClientTests.swift
@@ -23,7 +23,7 @@ extension Olympia {
 	public enum Export {}
 	public struct Parsed: Sendable, Hashable {
 		let mnemonicWordCount: BIP39.WordCount
-		let accounts: OrderedSet<OlympiaAccountToMigrate>
+		let accounts: OrderedSet<Olympia.Parsed.Account>
 		public struct Account: Sendable, Hashable {
 			public let accountType: Olympia.AccountType
 			public let publicKey: K1.PublicKey
@@ -36,12 +36,13 @@ extension Olympia {
 
 extension Olympia.Export {
 	public static let accountNameForbiddenCharReplacement = " "
+	public static let accountNameMaxLength = 30 // sync with profile!
 	public enum Separator: Sendable, Hashable, CaseIterable {
 		static let inter = "~"
 		static let intra = "|"
 		static let headerEnd = "^"
 		public static let allCases: [String] = [
-			Self.inter, Self.inter, Self.headerEnd,
+			Self.inter, Self.intra, Self.headerEnd,
 		]
 	}
 
@@ -56,35 +57,9 @@ extension Olympia.Export {
 		}
 
 		public struct Contents: Sendable, Hashable {
-			public let accounts: OrderedSet<Account>
+			public let accounts: OrderedSet<Olympia.Parsed.Account>
 			public let rest: NonEmptyString?
 		}
-
-		public enum Account: Sendable, Hashable {
-			case raw(Raw)
-			case parsed(Olympia.Parsed.Account)
-			public struct Raw: Sendable, Hashable, Codable {
-				public let accountType: Olympia.AccountType
-				public let publicKeyCompressedBase64: String
-				public let addressIndex: Int
-				public let name: String?
-			}
-		}
-	}
-}
-
-// MARK: - Olympia.Export.Payload.Account + Codable
-extension Olympia.Export.Payload.Account: Codable {
-	public func encode(to encoder: Encoder) throws {
-		switch self {
-		case .parsed: fatalError()
-		case let .raw(raw):
-			try raw.encode(to: encoder)
-		}
-	}
-
-	public init(from decoder: Decoder) throws {
-		fatalError()
 	}
 }
 
@@ -96,11 +71,17 @@ struct TestVector: Codable, Sendable, Hashable {
 	let testID: Int
 	let olympiaWallet: OlympiaWallet
 	let payloadSizeThreshold: Int
-	let payloads: [String]
+	let payloads: NonEmpty<OrderedSet<NonEmptyString>>
 
 	struct OlympiaWallet: Codable, Hashable {
 		let mnemonic: String
-		let accounts: [Olympia.Export.Payload.Account.Raw]
+		let accounts: [TestVector.OlympiaWallet.Account]
+		struct Account: Sendable, Hashable, Codable {
+			public let accountType: Olympia.AccountType
+			public let publicKeyCompressedBase64: String
+			public let addressIndex: Int
+			public let name: NonEmptyString?
+		}
 	}
 }
 
@@ -130,7 +111,7 @@ enum CAP33 {
 	private static func deserializeHeaderAndContent(
 		payload payloadNonEmpty: NonEmptyString
 	) throws -> (header: Olympia.Export.Payload.Header, content: NonEmptyString) {
-		var payload = payloadNonEmpty.rawValue
+		let payload = payloadNonEmpty.rawValue
 
 		guard payload.contains(Olympia.Export.Separator.headerEnd) else {
 			throw Error.failedToParseHeaderDoesNotContainEndSeparator
@@ -138,7 +119,8 @@ enum CAP33 {
 
 		guard
 			case let components = payload.split(
-				separator: Olympia.Export.Separator.headerEnd
+				separator: Olympia.Export.Separator.headerEnd,
+				omittingEmptySubsequences: true
 			),
 			components.count == 2,
 			components.allSatisfy({ !$0.isEmpty }),
@@ -160,7 +142,10 @@ enum CAP33 {
 		}
 
 		guard
-			case let headerComponents = headerComponent.split(separator: Olympia.Export.Separator.intra),
+			case let headerComponents = headerComponent.split(
+				separator: Olympia.Export.Separator.intra,
+				omittingEmptySubsequences: true
+			),
 			headerComponents.count == 3,
 			headerComponents.allSatisfy({ !$0.isEmpty })
 		else {
@@ -192,7 +177,11 @@ enum CAP33 {
 	private static func _deserializeAccount(
 		accountComponent: String
 	) throws -> AccountOrRest? {
-		let components = accountComponent.split(separator: Olympia.Export.Separator.intra)
+		let components = accountComponent.split(
+			separator: Olympia.Export.Separator.intra,
+			omittingEmptySubsequences: false // if account name is empty, we wanna know.
+		)
+
 		guard components.count == 4 else {
 			if let rest = NonEmptyString(rawValue: accountComponent) {
 				return .rest(rest)
@@ -229,8 +218,6 @@ enum CAP33 {
 
 		let maybeName = NonEmptyString(rawValue: String(components[3]))
 
-		let path = try LegacyOlympiaBIP44LikeDerivationPath(index: bip44LikeAddressIndex)
-
 		let account = Olympia.Parsed.Account(
 			accountType: accountType,
 			publicKey: publicKey,
@@ -241,7 +228,10 @@ enum CAP33 {
 		return .account(account)
 	}
 
-	private static func deserialize(payload: NonEmptyString, rest maybeRest: NonEmptyString?) throws -> Olympia.Export.Payload {
+	private static func deserialize(
+		payload: NonEmptyString,
+		rest maybeRest: NonEmptyString?
+	) throws -> Olympia.Export.Payload {
 		let (header, contentNonEmpty) = try deserializeHeaderAndContent(payload: payload)
 		let contentSuffix = contentNonEmpty.rawValue
 		let content: String = {
@@ -255,14 +245,15 @@ enum CAP33 {
 
 		guard
 			case let accountComponentsAndMaybeRest = content.split(
-				separator: Olympia.Export.Separator.inter
+				separator: Olympia.Export.Separator.inter,
+				omittingEmptySubsequences: true
 			),
 			accountComponentsAndMaybeRest.allSatisfy({ !$0.isEmpty })
 		else {
 			throw Error.failedToParsePayloadDidNotContainHeaderAndContent
 		}
 
-		var accounts: OrderedSet<Olympia.Export.Payload.Account> = .init()
+		var accounts: OrderedSet<Olympia.Parsed.Account> = .init()
 		var rest: NonEmptyString?
 		for accountComponent in accountComponentsAndMaybeRest {
 			guard let accountOrRest = try _deserializeAccount(accountComponent: String(accountComponent)) else {
@@ -270,25 +261,35 @@ enum CAP33 {
 			}
 			switch accountOrRest {
 			case let .account(account):
-				accounts.append(.parsed(account))
+				accounts.append(account)
 			case let .rest(_rest):
 				rest = _rest
 			}
 		}
-
 		return .init(header: header, contents: .init(accounts: accounts, rest: rest))
 	}
 
-	public static func deserialize(payloads payloadStrings: NonEmpty<[NonEmptyString]>) throws -> Olympia.Parsed {
-		var accounts: OrderedSet<OlympiaAccountToMigrate> = .init()
+	internal static func _deserialize(payloadsStrings: [String]) throws -> Olympia.Parsed {
+		try Self.deserialize(
+			payloads: .init(rawValue: OrderedSet(
+				uncheckedUniqueElements: payloadsStrings.compactMap { NonEmptyString(rawValue: $0) }
+			))!
+		)
+	}
+
+	public static func deserialize(
+		payloads payloadStrings: NonEmpty<OrderedSet<NonEmptyString>>
+	) throws -> Olympia.Parsed {
+		var accounts: OrderedSet<Olympia.Parsed.Account> = .init()
 		var mnemonicWordCount: BIP39.WordCount?
 
 		var rest: NonEmptyString?
-		for payloadString in payloadStrings.rawValue {
+		for payloadString in payloadStrings.rawValue.elements {
 			let payload = try Self.deserialize(payload: payloadString, rest: rest)
 			if mnemonicWordCount == nil {
 				mnemonicWordCount = BIP39.WordCount(wordCount: payload.header.mnemonicWordCount)!
 			}
+			accounts.append(contentsOf: payload.contents.accounts)
 			rest = payload.contents.rest
 		}
 
@@ -298,32 +299,29 @@ enum CAP33 {
 		)
 	}
 
+	internal static func _sanitize(name: NonEmptyString?) -> String {
+		guard let name = name else { return "" }
+		var truncated = String(name.rawValue.prefix(30))
+		let forbiddenCharacters: [String] = Olympia.Export.Separator.allCases
+		for forbiddenChar in forbiddenCharacters {
+			truncated = truncated.replacingOccurrences(of: forbiddenChar, with: Olympia.Export.accountNameForbiddenCharReplacement)
+		}
+		return truncated
+	}
+
 	public static func serialize(
 		wordCount: Int,
-		accounts: [Olympia.Export.Payload.Account.Raw],
+		accounts: [TestVector.OlympiaWallet.Account],
 		payloadSizeThreshold: Int
-	) throws -> [String] {
+	) throws -> NonEmpty<OrderedSet<NonEmptyString>> {
 		// 1. First serialize each account
-		let serializedAccounts: [String] = accounts.map { (account: Olympia.Export.Payload.Account.Raw) -> String in
+		let serializedAccounts: [String] = accounts.map { (account: TestVector.OlympiaWallet.Account) -> String in
 
-			let sanitizedAccountName = {
-				if let name = account.name {
-					var truncated = String(name.prefix(30))
-					let forbiddenCharacters: [String] = Olympia.Export.Separator.allCases
-					for forbiddenChar in forbiddenCharacters {
-						truncated = truncated.replacingOccurrences(of: forbiddenChar, with: Olympia.Export.accountNameForbiddenCharReplacement)
-					}
-					return truncated
-				} else {
-					return ""
-				}
-			}()
-
-			return [
+			[
 				account.accountType.rawValue,
 				account.publicKeyCompressedBase64,
 				"\(account.addressIndex)",
-				sanitizedAccountName,
+				Self._sanitize(name: account.name),
 			].joined(separator: Olympia.Export.Separator.intra)
 		}
 
@@ -338,88 +336,201 @@ enum CAP33 {
 		let payloadCount = contentsOfPayloads.count
 
 		// 5. Prepend each "contentOfPayload| with a header, with a unique header specifying the index of the resulting payload
-		let payloads: [String] = contentsOfPayloads.enumerated().map { payloadIndex, payloadContent in
-			//            let header = Olympia.Export.Payload.Header(
-			//                payloadCount: payloadCount,
-			//                payloadIndex: payloadIndex,
-			//                mnemonicWordCount: wordCount
-			//            )
-
+		let payloadsArray: [NonEmptyString] = contentsOfPayloads.enumerated().map { payloadIndex, payloadContent in
 			// Construct header, end it with `headerEnd` seperator
-			let header: String = [payloadCount, payloadIndex, wordCount].map { "\($0)" }.joined(separator: Olympia.Export.Separator.inter) + Olympia.Export.Separator.headerEnd
+			let header: String = [payloadCount, payloadIndex, wordCount].map { "\($0)" }.joined(separator: Olympia.Export.Separator.intra) + Olympia.Export.Separator.headerEnd
 
-			let payload: String = header + payloadContent
+			guard let payload = NonEmptyString(rawValue: header + payloadContent) else {
+				fatalError("Failed to serialize payload")
+			}
 			return payload
 		}
 
-		// return payloads, being an array of "\(header)\(payloadContent)"
-		return payloads
+		let payloadsOrderedSet = OrderedSet<NonEmptyString>(uncheckedUniqueElements: payloadsArray)
+
+		// 6. Return payloads, being an array of "\(header)\(payloadContent)"
+		guard let nonEmpty = NonEmpty<OrderedSet<NonEmptyString>>(rawValue: payloadsOrderedSet) else {
+			fatalError("Failed to create payloads")
+		}
+		return nonEmpty
+	}
+}
+
+extension K1.PublicKey {
+	var base64Encoded: String {
+		let publicKeyCompressed = compressedRepresentation
+		assert(publicKeyCompressed.count == 33)
+		assert(publicKeyCompressed[0] == 0x02 || publicKeyCompressed[0] == 0x03)
+		return publicKeyCompressed.base64EncodedString()
+	}
+}
+
+private func generateTestVectors(numberOfTestVectors: Int = 6) throws -> [TestVector] {
+	let payloadSizes: [Int] = .init(stride(from: 300, to: 3000, by: 100))
+	let accountNames: [String?] = [
+		String?.none,
+		"",
+		Olympia.Export.accountNameForbiddenCharReplacement,
+		"Main account.",
+		"Saving's account.",
+		"Olympia is a small town in Elis on the Peloponnese peninsula in Greece, famous for the nearby archaeological site of the same name",
+		"Alexandria is the second largest city in Egypt, and the largest city on the Mediterranean coast",
+		"Forbidden \(Olympia.Export.Separator.allCases.joined(separator: "-"))",
+		"OK +?-_,.\\)[]\\(!#$%&/*<>=OK",
+		"Numbers are allowed 0123456789",
+	]
+	let numberOfAccounts = [1, 5, 10, 15, 30, 50, 100]
+
+	let testVectors: [TestVector] = try (0 ..< numberOfTestVectors).map { (testID: Int) -> TestVector in
+		let wordCount = BIP39.WordCount.allCases[testID % BIP39.WordCount.allCases.count]
+		let mnemonic = try Mnemonic.generate(wordCount: wordCount)
+		let hdRoot = try mnemonic.hdRoot()
+		let payloadSizeThreshold = payloadSizes[testID % payloadSizes.count]
+		let numberOfAccounts = numberOfAccounts[testID % numberOfAccounts.count]
+
+		let accounts: [TestVector.OlympiaWallet.Account] = try (0 ..< numberOfAccounts).map { (accountIndex: Int) -> TestVector.OlympiaWallet.Account in
+
+			let accountRND = testID * accountIndex
+			let name: NonEmptyString? = accountNames[accountIndex % accountNames.count].map {
+				NonEmptyString(rawValue: $0)
+			} ?? nil
+			let accountType: Olympia.AccountType = (accountRND % 2) == 0 ? .software : .hardware
+			let path = try LegacyOlympiaBIP44LikeDerivationPath(index: Profile.Network.NextDerivationIndices.Index(accountIndex))
+			let publicKey = try hdRoot.derivePrivateKey(path: path.fullPath, curve: SECP256K1.self).publicKey
+
+			return TestVector.OlympiaWallet.Account(
+				accountType: accountType,
+				publicKeyCompressedBase64: publicKey.base64Encoded,
+				addressIndex: accountIndex,
+				name: name
+			)
+		}
+
+		let payloads = try CAP33.serialize(
+			wordCount: wordCount.rawValue,
+			accounts: accounts,
+			payloadSizeThreshold: payloadSizeThreshold
+		)
+
+		// Soundness check!
+		let parsed = try CAP33.deserialize(payloads: payloads)
+		XCTAssertEqual(parsed.mnemonicWordCount, wordCount)
+		//        XCTAssertEqual(parsed.accounts.elements.map {
+		//            $0.toTestVectorAccount()
+		//        }, accounts)
+		XCTAssertEqual(accounts.count, parsed.accounts.count)
+		for index in 0 ..< accounts.count {
+			let lhs = accounts[index].sanitizedName()
+			let rhs = parsed.accounts.elements[index].toTestVectorAccount()
+			XCTAssertEqual(lhs.name, rhs.name, "Name mismatch, unserialized: '\(String(describing: accounts[index].name))', parsed: \(String(describing: parsed.accounts.elements[index].displayName))")
+		}
+
+		return .init(
+			testID: testID, olympiaWallet: .init(
+				mnemonic: mnemonic.phrase,
+				accounts: accounts
+			), payloadSizeThreshold: payloadSizeThreshold,
+			payloads: payloads
+		)
+	}
+
+	assert(testVectors.count == numberOfTestVectors)
+
+	return testVectors
+}
+
+extension TestVector.OlympiaWallet.Account {
+	func sanitizedName() -> Self {
+		.init(
+			accountType: accountType,
+			publicKeyCompressedBase64: publicKeyCompressedBase64,
+			addressIndex: addressIndex,
+			name: NonEmptyString(rawValue: CAP33._sanitize(name: name))
+		)
+	}
+}
+
+extension Olympia.Parsed.Account {
+	func toTestVectorAccount() -> TestVector.OlympiaWallet.Account {
+		.init(
+			accountType: accountType,
+			publicKeyCompressedBase64: publicKey.base64Encoded,
+			addressIndex: Int(addressIndex),
+			name: displayName
+		)
 	}
 }
 
 // MARK: - ImportLegacyWalletClientTests
 final class ImportLegacyWalletClientTests: TestCase {
 	func test_generate_tests() throws {
-		let numberOfTests = 6
-		let payloadSizes: [Int] = .init(stride(from: 300, to: 3000, by: 100))
-		let accountNames: [String?] = [
-			String?.none,
-			"",
-			Olympia.Export.accountNameForbiddenCharReplacement,
-			"Main account.",
-			"Saving's account.",
-			"Olympia is a small town in Elis on the Peloponnese peninsula in Greece, famous for the nearby archaeological site of the same name",
-			"Alexandria is the second largest city in Egypt, and the largest city on the Mediterranean coast",
-			"Forbidden \(Olympia.Export.Separator.allCases.joined(separator: "-"))",
-			"OK +?-_,.\\)[]\\(!#$%&/*<>=OK",
-			"Numbers are allowed 0123456789",
-		]
-		let numberOfAccounts = [1, 5, 10, 15, 30, 50, 100]
-		let tests: [TestVector] = try (0 ..< numberOfTests).map { (testID: Int) -> TestVector in
-			let wordCount = BIP39.WordCount.allCases[testID % BIP39.WordCount.allCases.count]
-			let mnemonic = try Mnemonic.generate(wordCount: wordCount)
-			let hdRoot = try mnemonic.hdRoot()
-			let payloadSizeThreshold = payloadSizes[testID % payloadSizes.count]
-			let numberOfAccounts = numberOfAccounts[testID % numberOfAccounts.count]
-
-			let accounts: [Olympia.Export.Payload.Account.Raw] = try (0 ..< numberOfAccounts).map { (accountIndex: Int) -> Olympia.Export.Payload.Account.Raw in
-
-				let accountRND = testID * accountIndex
-				let name: String? = accountNames[accountIndex % accountNames.count]
-				let accountType: Olympia.AccountType = (accountRND % 2) == 0 ? .software : .hardware
-				let path = try LegacyOlympiaBIP44LikeDerivationPath(index: Profile.Network.NextDerivationIndices.Index(accountIndex))
-				let publicKey = try hdRoot.derivePrivateKey(path: path.fullPath, curve: SECP256K1.self).publicKey
-				let publicKeyCompressed = publicKey.compressedRepresentation
-				assert(publicKeyCompressed.count == 33)
-				assert(publicKeyCompressed[0] == 0x02 || publicKeyCompressed[0] == 0x03)
-				return Olympia.Export.Payload.Account.Raw(
-					accountType: accountType,
-					publicKeyCompressedBase64: publicKeyCompressed.base64EncodedString(),
-					addressIndex: accountIndex,
-					name: name
-				)
-			}
-
-			let payloads: [String] = try CAP33.serialize(
-				wordCount: wordCount.rawValue,
-				accounts: accounts,
-				payloadSizeThreshold: payloadSizeThreshold
-			)
-
-			return .init(
-				testID: testID, olympiaWallet: .init(
-					mnemonic: mnemonic.phrase,
-					accounts: accounts
-				), payloadSizeThreshold: payloadSizeThreshold,
-				payloads: payloads
-			)
-		}
-
+		let testVectors = try generateTestVectors(numberOfTestVectors: 6)
 		let jsonEncoder = JSONEncoder()
 		jsonEncoder.outputFormatting = .prettyPrinted
-		let testJSON = try jsonEncoder.encode(tests)
-		print("🔮 generated #\(tests.count) tests:")
+		let testJSON = try jsonEncoder.encode(testVectors)
+		print("🔮 generated #\(testVectors.count) tests:")
 		print(String(data: testJSON, encoding: .utf8)!)
 		print("✅ success")
+	}
+
+	func test_cap33_example() throws {
+		let payloadStrings: [String] = [
+			"2|0|12^S|AvZppDAk2Q/eaTUczFMCLC+GcI2bPEJpNkBzPFd4I12l|0|With forbidden char in name~H|A/YzLtwqoPA1w8VNdKOs7HbZtZherdzamVuX1BF3Bdez|1",
+			"2|1|12^|~H|A1STin2yF+LmEPU4mZarY6Bw+0QUZk350V4nWupv5JfG|2|Third account",
+		]
+		let parsed = try CAP33._deserialize(payloadsStrings: payloadStrings)
+		XCTAssertEqual(parsed.mnemonicWordCount, .twelve)
+		let accounts = parsed.accounts
+
+		XCTAssertEqual(accounts.count, 3)
+
+		let expectedAccounts: [Olympia.Parsed.Account] = try [
+			.init(
+				accountType: .software,
+				publicKey: K1.PublicKey(
+					compressedRepresentation: Data(
+						hex: "02f669a43024d90fde69351ccc53022c2f86708d9b3c42693640733c5778235da5"
+					)),
+				displayName: .init(rawValue: "With forbidden char in name"),
+				addressIndex: 0
+			),
+			.init(
+				accountType: .hardware,
+				publicKey: K1.PublicKey(
+					compressedRepresentation: Data(
+						hex: "03f6332edc2aa0f035c3c54d74a3acec76d9b5985eaddcda995b97d4117705d7b3"
+					)),
+				displayName: nil,
+				addressIndex: 1
+			),
+			.init(
+				accountType: .hardware,
+				publicKey: K1.PublicKey(
+					compressedRepresentation: Data(
+						hex: "0354938a7db217e2e610f5389996ab63a070fb4414664df9d15e275aea6fe497c6"
+					)),
+				displayName: .init(rawValue: "Third account"),
+				addressIndex: 2
+			),
+		]
+		XCTAssertEqual(accounts.elements, expectedAccounts)
+
+		let serialized = try CAP33.serialize(wordCount: 12, accounts: expectedAccounts.map { $0.toTestVectorAccount() }, payloadSizeThreshold: 125)
+		let parsedFromSerialized = try CAP33.deserialize(payloads: serialized)
+		XCTAssertEqual(parsedFromSerialized.accounts.elements, expectedAccounts)
+	}
+}
+
+// MARK: - K1.PublicKey + CustomDebugStringConvertible
+extension K1.PublicKey: CustomDebugStringConvertible {
+	public var debugDescription: String {
+		compressedRepresentation.hex
+	}
+}
+
+// MARK: - Olympia.AccountType + CustomDebugStringConvertible
+extension Olympia.AccountType: CustomDebugStringConvertible {
+	public var debugDescription: String {
+		description
 	}
 }
