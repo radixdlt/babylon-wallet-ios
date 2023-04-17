39f98d0a56f2a948b392c41a42e2ec51c75f4957
Alexander Cyon <alex.cyon@rdx.works>
2023-04-17 10:47:27 +0200

    WIP

--- Sources/Features/SettingsFeature/ManageFactorSourcesFeature/Childen/ImportOlympiaFactorSource/ImportOlympiaFactorSource.swift
+++ Sources/Features/SettingsFeature/ManageFactorSourcesFeature/Childen/ImportOlympiaFactorSource/ImportOlympiaFactorSource.swift
@@ -34,9 +34,9 @@ public struct ImportOlympiaFactorSource: Sendable, FeatureReducer {
 			self.passphrase = passphrase
 			self.selectedAccounts = selectedAccounts
 			#if DEBUG
-			if let new = (try? Mnemonic(phrase: "private sight rather cloud lock pelican barrel whisper spy more artwork crucial abandon among grow guilt control wrist memory group churn hen program sauce", language: .english))?.phrase {
-				self.mnemonic = new
-				self.expectedWordCount = .twentyFour
+			if let new = try? Mnemonic(phrase: "garage upgrade road wink twelve pink merit all festival keen puzzle twice", language: .english) {
+				self.mnemonic = new.phrase
+				self.expectedWordCount = new.wordCount
 			}
 			#endif
 		}
