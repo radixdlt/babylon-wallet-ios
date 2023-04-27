a0b6614096c656c11973a0485985d5379f939d56
kugel3 <gustaf.kugelberg@rdx.works>
2023-04-27 13:26:08 +0200

    fails to go live

--- Package.swift
+++ Package.swift
@@ -587,6 +587,18 @@ package.addModules([
 		],
 		tests: .no
 	),
+	.client(
+		name: "ImageServiceClient",
+		dependencies: [],
+		tests: .no
+	),
+	.client(
+		name: "ImageServiceClientLive",
+		dependencies: [
+			"ImageServiceClient",
+		],
+		tests: .no
+	),
 ])
 
 // MARK: - Core
@@ -615,6 +627,7 @@ package.addModules([
 	.core(
 		name: "DesignSystem",
 		dependencies: [
+			"ImageServiceClient",
 			.product(name: "Introspect", package: "SwiftUI-Introspect") {
 				.package(url: "https://github.com/siteline/SwiftUI-Introspect", from: "0.1.4")
 			},
