480eaf28edc2b5498203f3fe5f7c0d4f9d2aabed
Alexander Cyon <alex.cyon@rdx.works>
2023-04-27 08:21:03 +0200

    WIP

--- Sources/Clients/UseFactorSourceClient/UseFactorSourceClient+Interface.swift
+++ Sources/Clients/UseFactorSourceClient/UseFactorSourceClient+Interface.swift
@@ -68,76 +68,57 @@ struct FailedToDeviceFactorSourceForSigning: Swift.Error {}
 
 extension UseFactorSourceClient {
 	public func signUsingDeviceFactorSource(
-		deviceFactorSource: FactorSource? = nil,
+		deviceFactorSource: FactorSource,
 		of accounts: Set<Profile.Network.Account>,
-		unhashedDataToSign unhashed_: some DataProtocol
+		unhashedDataToSign: some DataProtocol
 	) async throws -> Set<AccountSignature> {
-		let cachedPrivateHDFactorSources: ActorIsolated<IdentifiedArrayOf<PrivateHDFactorSource>> = .init([])
 		@Dependency(\.factorSourcesClient) var factorSourcesClient
 		@Dependency(\.secureStorageClient) var secureStorageClient
-		let unhashedData = Data(unhashed_)
 
-		@Sendable func sign(
-			with account: Profile.Network.Account
-		) async throws -> AccountSignature {
+		let factorSourceID = deviceFactorSource.id
+
+		guard
+			let loadedMnemonicWithPassphrase = try await secureStorageClient.loadMnemonicByFactorSourceID(factorSourceID, .signTransaction)
+		else {
+			throw FailedToDeviceFactorSourceForSigning()
+		}
+		let hdRoot = try privateHDFactorSource.mnemonicWithPassphrase.hdRoot()
+
+		var signatures = Set<AccountSignature>()
+
+		loggerGlobal.debug("🔏 Signing data with device factor source label=\(privateHDFactorSource.hdOnDeviceFactorSource.label), description=\(privateHDFactorSource.hdOnDeviceFactorSource.description)")
+
+		for account in accounts {
 			switch account.securityState {
 			case let .unsecured(unsecuredControl):
 				let factorInstance = unsecuredControl.genesisFactorInstance
-				let factorSources = try await factorSourcesClient.getFactorSources()
-				let factorSourceID = deviceFactorSource?.id ?? factorInstance.factorSourceID
+				guard let derivationPath = factorInstance.derivationPath else {
+					let errMsg = "Expected derivation path on unsecured factorInstance"
+					loggerGlobal.critical(.init(stringLiteral: errMsg))
+					assertionFailure(errMsg)
+				}
 
 				if factorInstance.factorSourceID != factorSourceID {
 					let errMsg = "Discrepancy, you specified to use a device factor source you beleived to be the one controlling the entity, but it does not match the genesis factor source id."
 					loggerGlobal.critical(.init(stringLiteral: errMsg))
 					assertionFailure(errMsg)
 				}
+				let curve = factorInstance.publicKey.curve
 
-				let privateHDFactorSource: PrivateHDFactorSource = try await { @Sendable () async throws -> PrivateHDFactorSource in
-
-					let cache = await cachedPrivateHDFactorSources.value
-					if let cached = cache[id: factorSourceID] {
-						return cached
-					}
-
-					guard
-						let factorSource = deviceFactorSource ?? factorSources[id: factorSourceID],
-						let loadedMnemonicWithPassphrase = try await secureStorageClient.loadMnemonicByFactorSourceID(factorSourceID, .signTransaction)
-					else {
-						throw FailedToDeviceFactorSourceForSigning()
-					}
-
-					let privateHDFactorSource = try PrivateHDFactorSource(
-						mnemonicWithPassphrase: loadedMnemonicWithPassphrase,
-						hdOnDeviceFactorSource: .init(factorSource: factorSource)
-					)
-
-					await cachedPrivateHDFactorSources.setValue(cache.appending(privateHDFactorSource))
-
-					return privateHDFactorSource
-				}()
-
-				let hdRoot = try privateHDFactorSource.mnemonicWithPassphrase.hdRoot()
-				let curve = privateHDFactorSource.hdOnDeviceFactorSource.parameters.supportedCurves.last
-
-				loggerGlobal.debug("🔏 Signing data, with account=\(account.displayName), curve=\(curve), factorSourceKind=\(privateHDFactorSource.hdOnDeviceFactorSource.kind), factorSourceLabel=\(privateHDFactorSource.hdOnDeviceFactorSource.label), factorSourceDescription=\(privateHDFactorSource.hdOnDeviceFactorSource.description)")
+				loggerGlobal.debug("🔏 Signing data with device, with account=\(account.displayName), curve=\(curve)")
 
 				let signatureWithPublicKey = try await self.signatureFromOnDeviceHD(.init(
 					hdRoot: hdRoot,
-					derivationPath: factorInstance.derivationPath!,
+					derivationPath: derivationPath,
 					curve: curve,
-					unhashedData: unhashedData
+					unhashedData: Data(unhashedDataToSign)
 				))
-
-				let sig = Signature(signatureWithPublicKey: signatureWithPublicKey, derivationPath: factorInstance.derivationPath)
-				return try AccountSignature(entity: account, factorInstance: factorInstance, signature: sig)
+				let sigatureWithDerivationPath = Signature(signatureWithPublicKey: signatureWithPublicKey, derivationPath: factorInstance.derivationPath)
+				let accountSignature = try AccountSignature(entity: account, factorInstance: factorInstance, signature: sigatureWithDerivationPath)
+				sigs.insert(accountSignature)
 			}
 		}
 
-		let signatures = try await accounts.asyncMap { account in
-			try await sign(
-				with: account
-			)
-		}
-		return Set(signatures)
+		return signatures
 	}
 }
