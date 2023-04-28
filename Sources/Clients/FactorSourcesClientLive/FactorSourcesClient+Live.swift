fc70563c80b1a1e2688baef7b7bc423e4f664b7f
Alexander Cyon <alex.cyon@rdx.works>
2023-04-28 23:16:57 +0200

    add tests for FactorSourceClients getSigningFactors

--- Sources/Clients/FactorSourcesClientLive/FactorSourcesClient+Live.swift
+++ Sources/Clients/FactorSourcesClientLive/FactorSourcesClient+Live.swift
@@ -1,4 +1,3 @@
-import AccountsClient
 import ClientPrelude
 import FactorSourcesClient
 import ProfileStore
@@ -75,54 +74,60 @@ extension FactorSourcesClient: DependencyKey {
 			},
 			addOffDeviceFactorSource: addOffDeviceFactorSource,
 			getSigningFactors: { _, accounts in
-				@Dependency(\.accountsClient) var accountsClient
-
-				let allFactorSources = try await getFactorSources()
-
-				var signingFactors: [FactorSourceKind: NonEmpty<Set<SigningFactor>>] = [:]
-
-				for account in accounts {
-					switch account.securityState {
-					case let .unsecured(unsecuredEntityControl):
-						let factorInstance = unsecuredEntityControl.genesisFactorInstance
-						let id = factorInstance.factorSourceID
-						guard let factorSource = allFactorSources[id: id] else {
-							assertionFailure("Bad! factor source not found")
-							throw FactorSourceNotFound()
-						}
-						let signer = SigningFactor.Signer(account: account, factorInstancesRequiredToSign: [factorInstance])
-						let sigingFactor = SigningFactor(factorSource: factorSource, signers: .init(rawValue: [signer])!)
-						if let existing = signingFactors[factorSource.kind] {
-							// Complex case, this factor source kind already present in the dictionary => update `NonEmpty<Set<SigningFactor>>`
-
-							// we cannot mutate `rawValue` of `NonEmpty` :/ thus this complex dance
-							var unorderedSet = existing.rawValue // read out `Set<SigningFactor>` (which is non empty)
-							if var existingSigningFactorForThisFactorSource = unorderedSet.first(where: { $0.factorSource == factorSource }) {
-								// The most complex case, set `unorderedSet` contains this factor source... must update the `SigningFactor`
-								unorderedSet.remove(existingSigningFactorForThisFactorSource) // remove and dont forget to readd
-								var signers = existingSigningFactorForThisFactorSource.signers.rawValue
-								signers.insert(signer)
-								existingSigningFactorForThisFactorSource.signers = .init(rawValue: signers)!
-								unorderedSet.insert(existingSigningFactorForThisFactorSource) // dont forget to re-add the updated
-							} else {
-								// easy case
-								unorderedSet.insert(sigingFactor)
-							}
-
-							// Dont forget to update the dictionary!
-							signingFactors[factorSource.kind] = .init(rawValue: unorderedSet)!
-						} else {
-							// trivial case,
-							signingFactors[factorSource.kind] = .init(rawValue: [sigingFactor])!
-						}
-					}
-				}
-				return .init(uniqueKeysWithValues: signingFactors.sorted(by: \.key))
+				try await signingFactors(
+					for: accounts,
+					from: getFactorSources().rawValue
+				)
 			}
 		)
 	}
 
 	public static let liveValue = Self.live()
+}
+
+internal func signingFactors(
+	for accounts: some Collection<Profile.Network.Account>,
+	from allFactorSources: IdentifiedArrayOf<FactorSource>
+) throws -> SigningFactors {
+	var signingFactors: [FactorSourceKind: NonEmpty<Set<SigningFactor>>] = [:]
+
+	for account in accounts {
+		switch account.securityState {
+		case let .unsecured(unsecuredEntityControl):
+			let factorInstance = unsecuredEntityControl.genesisFactorInstance
+			let id = factorInstance.factorSourceID
+			guard let factorSource = allFactorSources[id: id] else {
+				assertionFailure("Bad! factor source not found")
+				throw FactorSourceNotFound()
+			}
+			let signer = SigningFactor.Signer(account: account, factorInstancesRequiredToSign: [factorInstance])
+			let sigingFactor = SigningFactor(factorSource: factorSource, signers: .init(rawValue: [signer])!)
+			if let existing = signingFactors[factorSource.kind] {
+				// Complex case, this factor source kind already present in the dictionary => update `NonEmpty<Set<SigningFactor>>`
+
+				// we cannot mutate `rawValue` of `NonEmpty` :/ thus this complex dance
+				var unorderedSet = existing.rawValue // read out `Set<SigningFactor>` (which is non empty)
+				if var existingSigningFactorForThisFactorSource = unorderedSet.first(where: { $0.factorSource == factorSource }) {
+					// The most complex case, set `unorderedSet` contains this factor source... must update the `SigningFactor`
+					unorderedSet.remove(existingSigningFactorForThisFactorSource) // remove and dont forget to readd
+					var signers = existingSigningFactorForThisFactorSource.signers.rawValue
+					signers.insert(signer)
+					existingSigningFactorForThisFactorSource.signers = .init(rawValue: signers)!
+					unorderedSet.insert(existingSigningFactorForThisFactorSource) // dont forget to re-add the updated
+				} else {
+					// easy case
+					unorderedSet.insert(sigingFactor)
+				}
+
+				// Dont forget to update the dictionary!
+				signingFactors[factorSource.kind] = .init(rawValue: unorderedSet)!
+			} else {
+				// trivial case,
+				signingFactors[factorSource.kind] = .init(rawValue: [sigingFactor])!
+			}
+		}
+	}
+	return .init(uniqueKeysWithValues: signingFactors.sorted(by: \.key))
 }
 
 // MARK: - FactorSourceNotFound
