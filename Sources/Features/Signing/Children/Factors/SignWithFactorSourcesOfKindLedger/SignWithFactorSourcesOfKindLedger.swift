29ab207c82feb5f8145e7190ec53f1979bfe1027
Alexander Cyon <alex.cyon@rdx.works>
2023-04-29 10:40:51 +0200

    WIP

--- Sources/Features/Signing/Children/Factors/SignWithFactorSourcesOfKindLedger/SignWithFactorSourcesOfKindLedger.swift
+++ Sources/Features/Signing/Children/Factors/SignWithFactorSourcesOfKindLedger/SignWithFactorSourcesOfKindLedger.swift
@@ -21,17 +21,19 @@ public struct SignWithFactorSourcesOfKindLedger: SignWithFactorSourcesOfKindRedu
 	public func reduce(into state: inout State, viewAction: ViewAction) -> EffectTask<Action> {
 		switch viewAction {
 		case .appeared:
-//			return .run { [signingFactors = state.signingFactors, data = state.dataToSign] send in
-//				let signatures = try await ledgerHardwareWalletClient.sign(
-//					ledger: signingFactor.factorSource,
-//					signers: Set(signingFactor.signers.map(\.account)),
-//					unhashedDataToSign: data
-//				)
-//				await send(.delegate(.done(signingFactor: signingFactor, signatures: signatures)))
-//			} catch: { _, _ in
-//				loggerGlobal.error("Failed to device sign")
-//			}
-			return .none
+			return .run { [signingFactors = state.signingFactors, data = state.dataToSign] send in
+				var allSignatures = Set<AccountSignature>()
+				for signingFactor in signingFactors {
+					let signatures = try await ledgerHardwareWalletClient.sign(
+						signingFactor: signingFactor,
+						unhashedDataToSign: data
+					)
+					allSignatures.append(contentsOf: signatures)
+				}
+				await send(.delegate(.done(signingFactors: signingFactors, signatures: allSignatures)))
+			} catch: { _, _ in
+				loggerGlobal.error("Failed to device sign")
+			}
 		}
 	}
 }
