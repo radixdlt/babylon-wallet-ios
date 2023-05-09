34b006072baadb52c65223a8335b62b2b7e22b27
Alexander Cyon <alex.cyon@rdx.works>
2023-05-09 22:25:58 +0200

    WIP

--- Sources/Features/Signing/Coordinator/Signing+View.swift
+++ Sources/Features/Signing/Coordinator/Signing+View.swift
@@ -23,11 +23,6 @@ extension Signing {
 		public var body: some SwiftUI.View {
 			SwitchStore(store.scope(state: \.step)) {
 				CaseLet(
-					state: /Signing.State.Step.prepare,
-					action: { Signing.Action.child(.prepare($0)) },
-					then: { PrepareForSigning.View(store: $0) }
-				)
-				CaseLet(
 					state: /Signing.State.Step.signWithDeviceFactors,
 					action: { Signing.Action.child(.signWithDeviceFactors($0)) },
 					then: { SignWithFactorSourcesOfKindDevice.View(store: $0) }
