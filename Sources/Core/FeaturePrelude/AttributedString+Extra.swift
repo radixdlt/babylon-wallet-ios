34303532f7a06eeb3a7cc4274eb39efa8b8be19f
Gustaf Kugelberg <123396602+kugel3@users.noreply.github.com>
2023-05-12 14:31:21 +0200

    Highlight Part of String (#492)

--- Sources/Core/FeaturePrelude/AttributedString+Extra.swift
+++ Sources/Core/FeaturePrelude/AttributedString+Extra.swift
@@ -7,4 +7,30 @@ extension AttributedString {
 	public init(_ string: some StringProtocol, foregroundColor: Color) {
 		self = with(AttributedString(string)) { $0.foregroundColor = foregroundColor }
 	}
+
+	public init(markdown: some StringProtocol, replaceItalicsWith italicsColor: Color) {
+		let string = String(markdown)
+		guard let attributed = try? AttributedString(markdown: string) else {
+			self.init(string)
+			return
+		}
+
+		self = attributed.replacingAttributes(.italics, with: .foregroundColor(italicsColor))
+	}
+}
+
+extension AttributeContainer {
+	public static let italics: AttributeContainer = intent(.emphasized)
+
+	public static func intent(_ intent: InlinePresentationIntent) -> AttributeContainer {
+		var result = AttributeContainer()
+		result.inlinePresentationIntent = intent
+		return result
+	}
+
+	public static func foregroundColor(_ color: Color) -> AttributeContainer {
+		var result = AttributeContainer()
+		result.foregroundColor = color
+		return result
+	}
 }
