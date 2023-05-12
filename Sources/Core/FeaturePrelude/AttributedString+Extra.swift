7a73538e6faa8b841ba9a7f68bf72150f9ec385c
kugel3 <gustaf.kugelberg@rdx.works>
2023-05-12 09:25:26 +0200

    Highlight

--- Sources/Core/FeaturePrelude/AttributedString+Extra.swift
+++ Sources/Core/FeaturePrelude/AttributedString+Extra.swift
@@ -7,4 +7,34 @@ extension AttributedString {
 	public init(_ string: some StringProtocol, foregroundColor: Color) {
 		self = with(AttributedString(string)) { $0.foregroundColor = foregroundColor }
 	}
+
+	public init(markdown: some StringProtocol, emphasizedColor: Color) {
+		let string = String(markdown)
+		guard let attributed = try? AttributedString(markdown: string) else {
+			self.init(string)
+			return
+		}
+
+		self = attributed.replacingAttributes(.italic, with: .foregroundColor(emphasizedColor))
+	}
+}
+
+extension AttributeContainer {
+	static let italic: AttributeContainer = {
+		var result = AttributeContainer()
+		result.inlinePresentationIntent = .emphasized
+		return result
+	}()
+
+	static let bold: AttributeContainer = {
+		var result = AttributeContainer()
+		result.inlinePresentationIntent = .stronglyEmphasized
+		return result
+	}()
+
+	static func foregroundColor(_ color: Color) -> AttributeContainer {
+		var result = AttributeContainer()
+		result.foregroundColor = color
+		return result
+	}
 }
