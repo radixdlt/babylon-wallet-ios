90532f6d65334b190f5d0c63a09b441c2b6fb0d1
Ghenadie Vasiliev-Pusca <ghenadie.vasiliev-pusca@rdx.works>
2023-05-16 12:30:55 +0300

    Increase the version

--- Sources/Profile/Snapshot/ProfileSnapshot+Header.swift
+++ Sources/Profile/Snapshot/ProfileSnapshot+Header.swift
@@ -142,7 +142,8 @@ extension ProfileSnapshot.Header.Version {
 	/// - 30: Fix critical bug where identity derivation path was used for account
 	/// - 31: Add `ledgerHQHardwareWalletSigningDisplayMode` to appPreferences.display
 	/// - 32: rename `genesisFactorInstance` -> `transactionSigning` and add `authSigning`
-	public static let minimum: Self = 32
+        /// - 33: Add snapshot `header`
+	public static let minimum: Self = 33
 }
 
 extension ProfileSnapshot.Header {
