479c22eaf2559bf5aa14e68ec00135fb60ee8a98
Alexander Cyon <alex.cyon@rdx.works>
2023-05-22 16:45:03 +0200

    WIP

--- App/Shared/WalletApp.swift
+++ App/Shared/WalletApp.swift
@@ -9,7 +9,7 @@ struct WalletApp: SwiftUI.App {
 			App.View(
 				store: Store(
 					initialState: App.State(),
-					reducer: App()._printChanges()
+					reducer: App()
 					#if targetEnvironment(simulator)
 						.dependency(\.localAuthenticationClient.queryConfig) { .biometricsAndPasscodeSetUp }
 					#endif
