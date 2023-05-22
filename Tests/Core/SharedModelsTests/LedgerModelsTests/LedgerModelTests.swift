d9d2f8c456daf599c0fd701db520a04fa44f0c77
Alexander Cyon <alex.cyon@rdx.works>
2023-05-22 13:46:48 +0200

    WIP

--- Tests/Core/SharedModelsTests/LedgerModelsTests/LedgerModelTests.swift
+++ Tests/Core/SharedModelsTests/LedgerModelsTests/LedgerModelTests.swift
@@ -2,15 +2,13 @@
 import TestingPrelude
 
 final class LedgerModelTests: TestCase {
-	func test_decode_derivePublicKey() throws {
+	func test_decode_derivePublicKeys() throws {
 		let json: JSON = [
 			"success": [
-				"publicKeys": [
-					[
-						"curve": "curve25519",
-						"derivationPath": "testPath",
-						"publicKey": "03e6e5f34b265cca342ac711e68b5df9d839bc722e0b004f471539867d179d57c8",
-					],
+				[
+					"curve": "curve25519",
+					"derivationPath": "testPath",
+					"publicKey": "03e6e5f34b265cca342ac711e68b5df9d839bc722e0b004f471539867d179d57c8",
 				],
 			],
 			"interactionId": "32b94cc3-2418-4964-9877-b3cd1d66a007",
