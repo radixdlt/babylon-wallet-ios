9803df9eff2b38fac0ed2eb40366b9ed03770276
kugel3 <gustaf.kugelberg@rdx.works>
2023-05-25 13:37:01 +0200

    Remove obsolete test

--- Tests/Features/HomeFeatureTests/HeaderTests.swift
+++ Tests/Features/HomeFeatureTests/HeaderTests.swift
@@ -2,14 +2,4 @@ import FeatureTestingPrelude
 @testable import HomeFeature
 
 @MainActor
-final class HeaderTests: TestCase {
-	func testSettingsButtonTapped() async {
-		let store = TestStore(
-			initialState: Header.State(accountRecoveryIsNeeded: false),
-			reducer: Header()
-		)
-
-		await store.send(.view(.settingsButtonTapped))
-		await store.receive(.delegate(.displaySettings))
-	}
-}
+final class HeaderTests: TestCase {}
