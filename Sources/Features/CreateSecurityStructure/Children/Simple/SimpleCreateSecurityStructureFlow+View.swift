9f155e6ef453458eb29358d944f6340d258e1310
Alexander Cyon <alex.cyon@rdx.works>
2023-06-03 12:59:05 +0200

    WIP

--- Sources/Features/CreateSecurityStructure/Children/Simple/SimpleCreateSecurityStructureFlow+View.swift
+++ Sources/Features/CreateSecurityStructure/Children/Simple/SimpleCreateSecurityStructureFlow+View.swift
@@ -21,14 +21,40 @@ extension SimpleCreateSecurityStructureFlow {
 		}
 
 		public var body: some SwiftUI.View {
-			WithViewStore(store, observe: \.viewState, send: { .view($0) }) { viewStore in
-				// TODO: implement
-				Text("Implement: SimpleCreateSecurityStructureFlow")
-					.background(Color.yellow)
-					.foregroundColor(.red)
-					.onAppear { viewStore.send(.appeared) }
+			WithViewStore(store, observe: \.viewState, send: { .view($0) }) { _ in
+				VStack {
+					SecurityStructureTutorialHeader()
+
+					Spacer(minLength: 0)
+				}
 			}
 		}
+	}
+}
+
+// MARK: - SecurityStructureTutorialHeader
+public struct SecurityStructureTutorialHeader: SwiftUI.View {
+	public let action: () -> Void
+	public init(
+		action: @escaping @Sendable () -> Void = { loggerGlobal.debug("MFA: How does it work? Button tapped") }
+	) {
+		self.action = action
+	}
+
+	public var body: some SwiftUI.View {
+		VStack(spacing: .medium1) {
+			Text("Multi-Factor Setup") // FIXME: Strings
+				.font(.app.sheetTitle)
+
+			Text("You can assign diffrent factors to different actions on Radix Accounts")
+				.font(.app.body2Regular)
+
+			Button("How does it work?", action: action)
+				.buttonStyle(.info)
+				.padding(.horizontal, .large2)
+				.padding(.bottom, .medium1)
+		}
+		.padding(.medium1)
 	}
 }
 
--- Sources/Features/CreateSecurityStructure/Children/Simple/SimpleCreateSecurityStructureFlow.swift
+++ Sources/Features/CreateSecurityStructure/Children/Simple/SimpleCreateSecurityStructureFlow.swift
@@ -1,9 +1,26 @@
 import FeaturePrelude
 
+// MARK: - NewStructure
+public struct NewStructure: Sendable, Hashable {
+	/// Confirmation role
+	public var newPhoneConfirmer: FactorSource?
+
+	/// Recovery role
+	public var lostPhoneHelper: FactorSource?
+
+	public init(newPhoneConfirmer: FactorSource? = nil, lostPhoneHelper: FactorSource? = nil) {
+		self.newPhoneConfirmer = newPhoneConfirmer
+		self.lostPhoneHelper = lostPhoneHelper
+	}
+}
+
 // MARK: - SimpleCreateSecurityStructureFlow
 public struct SimpleCreateSecurityStructureFlow: Sendable, FeatureReducer {
 	public struct State: Sendable, Hashable {
-		public init() {}
+		public var structure: NewStructure
+		public init(structure: NewStructure = .init()) {
+			self.structure = structure
+		}
 	}
 
 	public enum ViewAction: Sendable, Equatable {
