bdf837bc1bae3c3409813d34cf5be59d0133501e
Ghenadie Vasiliev-Pusca <ghenadie.vasiliev-pusca@rdx.works>
2023-06-07 11:18:48 +0300

    Disable navigation to empty screens

--- Sources/Features/AssetTransferFeature/Components/AssetTransferMessage/AssetTransferMessage+View.swift
+++ Sources/Features/AssetTransferFeature/Components/AssetTransferMessage/AssetTransferMessage+View.swift
@@ -26,16 +26,18 @@ extension AssetTransferMessage.View {
 
 				VStack(alignment: .leading, spacing: 0) {
 					HStack {
-						Button {
-							viewStore.send(.messageKindTapped)
-						} label: {
-							HStack {
-								Text(L10n.Common.public)
-								Image(asset: AssetResource.chevronDown)
-							}
-						}
-						.textStyle(.body1HighImportance)
-						.foregroundColor(.app.gray1)
+						Text(L10n.Common.public)
+						// FIXME: Enable once messageKind is implemented
+//						Button {
+//							viewStore.send(.messageKindTapped)
+//						} label: {
+//							HStack {
+						//                                                                Text(L10n.Common.public)
+//								Image(asset: AssetResource.chevronDown)
+//							}
+//						}
+//						.textStyle(.body1HighImportance)
+//						.foregroundColor(.app.gray1)
 
 						Spacer()
 
