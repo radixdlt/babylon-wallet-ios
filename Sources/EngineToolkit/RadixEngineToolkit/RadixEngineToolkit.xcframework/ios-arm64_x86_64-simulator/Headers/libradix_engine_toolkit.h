ae3364a490bf63aff56623407c48c6505a44538c
Ghenadie Vasiliev-Pusca <ghenadie.vasiliev-pusca@rdx.works>
2023-06-08 17:29:55 +0300

    update binary

--- Sources/EngineToolkit/RadixEngineToolkit/RadixEngineToolkit.xcframework/ios-arm64_x86_64-simulator/Headers/libradix_engine_toolkit.h
+++ Sources/EngineToolkit/RadixEngineToolkit/RadixEngineToolkit.xcframework/ios-arm64_x86_64-simulator/Headers/libradix_engine_toolkit.h
@@ -11,15 +11,21 @@ Pointer information(Pointer string_pointer);
 
 Pointer convert_manifest(Pointer string_pointer);
 
-Pointer analyze_manifest(Pointer string_pointer);
+Pointer extract_addresses_from_manifest(Pointer string_pointer);
 
-Pointer analyze_manifest_with_preview_context(Pointer string_pointer);
+Pointer analyze_transaction_execution(Pointer string_pointer);
 
 Pointer compile_transaction_intent(Pointer string_pointer);
 
 Pointer compile_signed_transaction_intent(Pointer string_pointer);
 
 Pointer compile_notarized_transaction(Pointer string_pointer);
+
+Pointer hash_transaction_intent(Pointer string_pointer);
+
+Pointer hash_signed_transaction_intent(Pointer string_pointer);
+
+Pointer hash_notarized_transaction(Pointer string_pointer);
 
 Pointer decompile_transaction_intent(Pointer string_pointer);
 
@@ -31,13 +37,11 @@ Pointer decompile_unknown_transaction_intent(Pointer string_pointer);
 
 Pointer derive_babylon_address_from_olympia_address(Pointer string_pointer);
 
+Pointer derive_olympia_address_from_public_key(Pointer string_pointer);
+
 Pointer derive_virtual_account_address(Pointer string_pointer);
 
 Pointer derive_virtual_identity_address(Pointer string_pointer);
-
-Pointer derive_non_fungible_global_id_from_public_key(Pointer string_pointer);
-
-Pointer derive_olympia_address_from_public_key(Pointer string_pointer);
 
 Pointer encode_address(Pointer string_pointer);
 
