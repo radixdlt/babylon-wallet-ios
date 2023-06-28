57a0194ec5504f2d543fac0b52e2d570ca46185e
Alexander Cyon <alex.cyon@rdx.works>
2023-06-28 10:48:25 +0200

    WIP

--- POC/Versioning/Tests/SingleVersionTests/SingleVersionTests.swift
+++ POC/Versioning/Tests/SingleVersionTests/SingleVersionTests.swift
@@ -2,79 +2,6 @@ import JSONTesting
 @testable import SingleVersion
 import XCTest
 
-// final class TrivialTests: XCTestCase {
-//	// MARK: Encoding
-//	func test_encoding() throws {
-//		let trivial2 = Trivial2(label: "test", foo: "encoding")
-//		try XCTAssertJSONEncoding(trivial2, [
-//			"version": 2,
-//			"label": "test",
-//			"foo": "encoding",
-//		])
-//	}
-//
-//	// MARK: Decoding
-//	func test_decoding_migration_1_to_2() throws {
-//		try assertSuccessfulDecoding(
-//			json: """
-//			{
-//				"version": 1,
-//				"label": "test"
-//			}
-//			"""
-//		) { (migrated: Trivial2) in
-//			XCTAssertEqual(migrated.version, Trivial2.currentVersion)
-//			XCTAssertEqual(migrated.foo, "MIGRATED_FROM_1")
-//		}
-//	}
-//
-//	func test_decoding_fail_no_version() {
-//		assertFailingDecoding(
-//			json: """
-//			{
-//				"label": "test"
-//			}
-//			""",
-//			type: Trivial2.self
-//		)
-//	}
-//
-//	func test_decoding_fail_version_unknown_too_low() {
-//		assertFailingDecoding(
-//			json: """
-//			{
-//				"version": 0,
-//				"label": "test"
-//			}
-//			""",
-//			type: Trivial2.self
-//		)
-//	}
-//
-//	func test_decoding_fail_version_unknown_too_high() {
-//		assertFailingDecoding(
-//			json: """
-//			{
-//				"version": 9,
-//				"label": "test"
-//			}
-//			""",
-//			type: Trivial2.self
-//		)
-//	}
-//
-//	func test_decoding_fail_correct_version_missing_other_property() {
-//		assertFailingDecoding(
-//			json: """
-//			{
-//				"version": 2
-//			}
-//			""",
-//			type: Trivial2.self
-//		)
-//	}
-// }
-
 final class NestedTests: XCTestCase {
 	// MARK: Encoding
 	func test_encoding() throws {
@@ -111,5 +38,51 @@ final class NestedTests: XCTestCase {
 			XCTAssertEqual(migrated.version, 2)
 			XCTAssertEqual(migrated.inner.bar, "MIGRATED_FROM_1")
 		}
+	}
+
+	func test_decoding_fail_no_version() {
+		assertFailingDecoding(
+			json: """
+			{
+				"label": "test"
+			}
+			""",
+			type: Nested2.self
+		)
+	}
+
+	func test_decoding_fail_version_unknown_too_low() {
+		assertFailingDecoding(
+			json: """
+			{
+				"version": 0,
+				"label": "test"
+			}
+			""",
+			type: Nested2.self
+		)
+	}
+
+	func test_decoding_fail_version_unknown_too_high() {
+		assertFailingDecoding(
+			json: """
+			{
+				"version": 9,
+				"label": "test"
+			}
+			""",
+			type: Nested2.self
+		)
+	}
+
+	func test_decoding_fail_correct_version_missing_other_property() {
+		assertFailingDecoding(
+			json: """
+			{
+				"version": 2
+			}
+			""",
+			type: Nested2.self
+		)
 	}
 }
