6cff0f7dba262bc3e92a318957b10a8c8c511fd4
Maciek Czarnik <maciek.czarnik@rdx.works>
2023-07-12 11:25:14 +0200

    Add the most basic implementation

--- App/Previews/PersonaDetailsPreview/PersonaDetailsPreviewApp.swift
+++ App/Previews/PersonaDetailsPreview/PersonaDetailsPreviewApp.swift
@@ -1,10 +1,36 @@
+import FeaturesPreviewerFeature
+import PersonaDetailsFeature
 import SwiftUI
 
+// MARK: - PersonaDetailsPreviewApp
 @main
 struct PersonaDetailsPreviewApp: App {
 	var body: some Scene {
-		WindowGroup {
-			ContentView()
+		FeaturesPreviewer<PersonaDetails>.delegateAction { _ in
+			nil
 		}
 	}
+}
+
+// MARK: - PersonaDetails + PreviewedFeature
+extension PersonaDetails: PreviewedFeature {
+	public typealias ResultFromFeature = Profile.Network.Account
+}
+
+// MARK: - PersonaDetails + EmptyInitializable
+extension PersonaDetails: EmptyInitializable {}
+
+// MARK: - PersonaDetails.State + EmptyInitializable
+extension PersonaDetails.State: EmptyInitializable {
+	public init() {
+		self.init(.general(
+			.previewValue0,
+			dApps: []
+		))
+	}
+}
+
+// MARK: - PersonaDetails.View + FeatureView
+extension PersonaDetails.View: FeatureView {
+	public typealias Feature = PersonaDetails
 }
