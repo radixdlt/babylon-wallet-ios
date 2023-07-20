cc3dda0c3963e54929e0b5531bed793aa6ec8ad0
kugel3 <gustaf.kugelberg@rdx.works>
2023-07-20 19:02:04 +0200

    Update request

--- Sources/Core/SharedModels/P2P/Dapp/Models/Interaction/Requests/PersonaDataRequestItem.swift
+++ Sources/Core/SharedModels/P2P/Dapp/Models/Interaction/Requests/PersonaDataRequestItem.swift
@@ -4,23 +4,13 @@ import Profile
 extension P2P.Dapp.Request {
 	public struct PersonaDataRequestItem: Sendable, Hashable, Decodable {
 		public let isRequestingName: Bool?
-		public let isRequestingDateOfBirth: Bool?
-		public let isRequestingCompanyName: Bool?
-		public let emailAddressesRequested: RequestedNumber?
-		public let phoneNumbersAddressesRequested: RequestedNumber?
-		public let urlsRequested: RequestedNumber?
-		public let postalAddressesRequested: RequestedNumber?
-		public let creditCardsRequested: RequestedNumber?
+		public let numberOfRequestedEmailAddresses: RequestedNumber?
+		public let numberOfRequestedPhoneNumbers: RequestedNumber?
 
 		public init(
-			isRequestingName: Bool? = nil,
-			isRequestingDateOfBirth: Bool? = nil,
-			isRequestingCompanyName: Bool? = nil,
-			emailAddressesRequested: RequestedNumber? = nil,
-			phoneNumbersAddressesRequested: RequestedNumber? = nil,
-			urlsRequested: RequestedNumber? = nil,
-			postalAddressesRequested: RequestedNumber? = nil,
-			creditCardsRequested: RequestedNumber? = nil
+			isRequestingName: Bool?,
+			numberOfRequestedEmailAddresses: RequestedNumber? = nil,
+			numberOfRequestedPhoneNumbers: RequestedNumber? = nil
 		) {
 			// The only purpose of this switch is to make sure we get a compilation error when we add a new PersonaData.Entry kind, so
 			// we do not forget to handle it here.
@@ -29,13 +19,8 @@ extension P2P.Dapp.Request {
 			}
 
 			self.isRequestingName = isRequestingName
-			self.isRequestingDateOfBirth = isRequestingDateOfBirth
-			self.isRequestingCompanyName = isRequestingCompanyName
-			self.emailAddressesRequested = emailAddressesRequested
-			self.phoneNumbersAddressesRequested = phoneNumbersAddressesRequested
-			self.urlsRequested = urlsRequested
-			self.postalAddressesRequested = postalAddressesRequested
-			self.creditCardsRequested = creditCardsRequested
+			self.numberOfRequestedEmailAddresses = numberOfRequestedEmailAddresses
+			self.numberOfRequestedPhoneNumbers = numberOfRequestedPhoneNumbers
 		}
 	}
 }
