000a88aee3be77fa70493ca6f858a7e8753d651e
Ghenadie Vasiliev-Pusca <ghenadie.vasiliev-pusca@rdx.works>
2023-07-23 00:08:21 +0300

    update test

--- Tests/Core/SharedModelsTests/AssetTests/AssetTests.swift
+++ Tests/Core/SharedModelsTests/AssetTests/AssetTests.swift
@@ -7,11 +7,13 @@ final class AssetTests: TestCase {
 		let rawLocalID = "#123#"
 		let localId = try nonFungibleLocalIdFromStr(string: rawLocalID)
 		let resourceAddress = try ResourceAddress(validatingAddress: "resource_tdx_d_1nfn7hdrua4wcxwq26qq2gpv2ew0p94k66ms3nmluev6rerr7sszc5z")
+
+		let globalId = try NonFungibleGlobalId.fromParts(resourceAddress: resourceAddress.intoEngine(), nonFungibleLocalId: localId)
 		let sut = AccountPortfolio.NonFungibleResource(
 			resourceAddress: resourceAddress,
-			tokens: [.init(id: localId, name: nil, description: nil, keyImageURL: nil, metadata: [])]
+			tokens: [.init(id: globalId, name: nil, description: nil, keyImageURL: nil, metadata: [])]
 		)
 		let expectedGlobalID = try NonFungibleGlobalId.fromParts(resourceAddress: resourceAddress.intoEngine(), nonFungibleLocalId: localId)
-		XCTAssertEqual(try sut.nftGlobalID(for: localId), expectedGlobalID)
+		XCTAssertEqual(globalId, expectedGlobalID)
 	}
 }
