fca32feab2fd21b6c913e6db61431a92244c0f0d
Maciek Czarnik <maciek.czarnik@rdx.works>
2023-07-27 15:42:25 +0200

    Update RET

--- Package.swift
+++ Package.swift
@@ -1003,7 +1003,7 @@ package.addModules([
 		dependencies: [
 			"Cryptography",
 			.product(name: "EngineToolkit", package: "swift-engine-toolkit") {
-				.package(url: "https://github.com/radixdlt/swift-engine-toolkit", exact: "0.11.0-e96dd29")
+				.package(url: "https://github.com/radixdlt/swift-engine-toolkit", exact: "0.11.0-b68ff8d")
 			},
 		],
 		tests: .no
