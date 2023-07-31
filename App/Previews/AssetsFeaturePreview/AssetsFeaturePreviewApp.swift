e6018a9692a948c0d7f3f4314b6637857138eed9
Maciek Czarnik <maciek.czarnik@rdx.works>
2023-07-31 15:51:46 +0200

    Add some initial preview

--- App/Previews/AssetsFeaturePreview/AssetsFeaturePreviewApp.swift
+++ App/Previews/AssetsFeaturePreview/AssetsFeaturePreviewApp.swift
@@ -1,10 +1,34 @@
+import AssetsFeature
+import FeaturesPreviewerFeature
+import Prelude
 import SwiftUI
 
+// MARK: - AssetsFeaturePreviewApp
 @main
 struct AssetsFeaturePreviewApp: App {
 	var body: some Scene {
-		WindowGroup {
-			ContentView()
+		FeaturesPreviewer<AssetsView>.action { _ in
+			nil
 		}
 	}
+}
+
+// MARK: - AssetsView + PreviewedFeature
+extension AssetsView: PreviewedFeature {
+	public typealias ResultFromFeature = Prelude.Unit
+}
+
+// MARK: - AssetsView + EmptyInitializable
+extension AssetsView: EmptyInitializable {}
+
+// MARK: - AssetsView.State + EmptyInitializable
+extension AssetsView.State: EmptyInitializable {
+	public init() {
+		self.init(account: .previewValue0, mode: .normal)
+	}
+}
+
+// MARK: - AssetsView.View + FeatureView
+extension AssetsView.View: FeatureView {
+	public typealias Feature = AssetsView
 }
--- Sources/Prelude/Extensions/Unit.swift
+++ Sources/Prelude/Extensions/Unit.swift
@@ -4,6 +4,6 @@ import Foundation
 /// Useful when writing features where actions semantically have associated value
 /// `TaskResult<Void>` which does not compile since `Void` is not `Equatable`. We
 /// can now use `TaskResult<Unit>`.
-public struct Unit: Sendable, Hashable, Codable {
+public struct Unit: Sendable, Hashable, Codable, Error {
 	public init() {}
 }
