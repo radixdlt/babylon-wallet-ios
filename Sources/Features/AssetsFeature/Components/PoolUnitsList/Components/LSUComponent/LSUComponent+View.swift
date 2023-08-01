61184327abc0a31134897a6ee9d1ab5b33137840
Maciek Czarnik <maciek.czarnik@rdx.works>
2023-08-01 12:33:28 +0200

    Add the lok for a single unit

--- Sources/Features/AssetsFeature/Components/PoolUnitsList/Components/LSUComponent/LSUComponent+View.swift
+++ Sources/Features/AssetsFeature/Components/PoolUnitsList/Components/LSUComponent/LSUComponent+View.swift
@@ -10,6 +10,154 @@ extension PoolUnitsList.LSUComponent.State {
 	}
 }
 
+// MARK: - StakeClaimNFTStatus
+enum StakeClaimNFTStatus: Equatable {
+	case unstaking
+	case readyToClaim
+
+	var localized: String {
+		switch self {
+		case .unstaking:
+			return "Unstaking"
+		case .readyToClaim:
+			return "Ready to Claim"
+		}
+	}
+}
+
+extension PoolUnitsList.LSUComponent {
+	struct LiquidStakeUnitViewState: Identifiable, Equatable {
+		var id: String {
+			symbol
+		}
+
+		let thumbnail: TokenThumbnail.Content
+		let symbol: String
+		let tokenAmount: String
+
+		let stakedAmmount: String
+	}
+
+	struct StakeClaimNFTViewState: Identifiable, Equatable {
+		let id: Int
+
+		let thumbnail: TokenThumbnail.Content
+		let status: StakeClaimNFTStatus
+		let tokenAmount: String
+	}
+}
+
+// MARK: - PoolUnitsList.LSUComponent.View
+extension PoolUnitsList.LSUComponent {
+	public struct ViewState: Equatable, Identifiable {
+		public var id: String {
+			title
+		}
+
+		let title: String
+		let imageURL: URL = .init(string: "www.wp.pl")!
+
+		let liquidStakeUnits: NonEmpty<IdentifiedArrayOf<LiquidStakeUnitViewState>>?
+		let stakeClaimNFTs: NonEmpty<IdentifiedArrayOf<StakeClaimNFTViewState>>?
+	}
+
+	@MainActor
+	public struct View: SwiftUI.View {
+		private let store: Store<PoolUnitsList.LSUComponent.ViewState, PoolUnitsList.LSUComponent.ViewAction>
+
+		public init(store: Store<PoolUnitsList.LSUComponent.ViewState, PoolUnitsList.LSUComponent.ViewAction>) {
+			self.store = store
+		}
+
+		public var body: some SwiftUI.View {
+			WithViewStore(store) { viewStore in
+				VStack(alignment: .leading, spacing: .medium2) {
+					Text(viewStore.title)
+
+					if let liquidStakeUnits = viewStore.liquidStakeUnits {
+						Text("LIQUID STAKE UNITS")
+							.foregroundColor(.app.gray2)
+							.textStyle(.body2HighImportance)
+
+						ForEach(liquidStakeUnits) { liquidStakeUnit in
+							HStack {
+								HStack(spacing: .small1) {
+									TokenThumbnail(
+										liquidStakeUnit.thumbnail,
+										size: .small
+									)
+
+									HStack {
+										VStack(alignment: .leading) {
+											Text(liquidStakeUnit.symbol)
+												.foregroundColor(.app.gray1)
+												.textStyle(.body2HighImportance)
+											Text("Staked")
+												.foregroundColor(.app.gray2)
+												.textStyle(.body2HighImportance)
+										}
+
+										Spacer()
+
+										VStack(alignment: .trailing) {
+											Text(liquidStakeUnit.tokenAmount)
+												.foregroundColor(.app.gray1)
+												.textStyle(.body2HighImportance)
+											Text(liquidStakeUnit.stakedAmmount)
+												.foregroundColor(.app.gray2)
+												.textStyle(.body2HighImportance)
+										}
+									}
+								}
+							}
+							.padding(.medium2)
+							.overlay(
+								RoundedRectangle(cornerRadius: .small1)
+									.stroke(.app.gray4, lineWidth: 1)
+							)
+						}
+					}
+
+					if let stakeClaimNFTs = viewStore.stakeClaimNFTs {
+						VStack(alignment: .leading, spacing: .small2) {
+							Text("STAKE CLAIM NFTS")
+								.foregroundColor(.app.gray2)
+								.textStyle(.body2HighImportance)
+
+							ForEach(stakeClaimNFTs) { stakeClaimNFT in
+								HStack {
+									HStack(spacing: .small1) {
+										TokenThumbnail(
+											stakeClaimNFT.thumbnail,
+											size: .tiny
+										)
+
+										Text(stakeClaimNFT.status.localized)
+											.foregroundColor(.app.gray2)
+											.textStyle(.body2HighImportance)
+									}
+
+									Spacer()
+
+									Text(stakeClaimNFT.tokenAmount)
+										.foregroundColor(.app.gray1)
+										.textStyle(.secondaryHeader)
+								}
+								.padding(.medium3)
+								.overlay(
+									RoundedRectangle(cornerRadius: .small1)
+										.stroke(.app.gray4, lineWidth: 1)
+								)
+							}
+						}
+					}
+				}
+				.padding(.medium3)
+			}
+		}
+	}
+}
+
 extension PoolUnitsList.ViewState {
 	public static var preview: Self {
 		.init(
@@ -73,131 +221,5 @@ extension PoolUnitsList.ViewState {
 				]
 			)
 		)
-	}
-}
-
-// MARK: - StakeClaimNFTStatus
-enum StakeClaimNFTStatus: Equatable {
-	case unstaking
-	case readyToClaim
-
-	var localized: String {
-		switch self {
-		case .unstaking:
-			return "Unstaking"
-		case .readyToClaim:
-			return "Ready to Claim"
-		}
-	}
-}
-
-extension PoolUnitsList.LSUComponent {
-	struct LiquidStakeUnitViewState: Identifiable, Equatable {
-		var id: String {
-			symbol
-		}
-
-		let thumbnail: TokenThumbnail.Content
-		let symbol: String
-		let tokenAmount: String
-
-		let stakedAmmount: String
-	}
-
-	struct StakeClaimNFTViewState: Identifiable, Equatable {
-		let id: Int
-
-		let thumbnail: TokenThumbnail.Content
-		let status: StakeClaimNFTStatus
-		let tokenAmount: String
-	}
-}
-
-// MARK: - PoolUnitsList.LSUComponent.View
-extension PoolUnitsList.LSUComponent {
-	public struct ViewState: Equatable, Identifiable {
-		public var id: String {
-			title
-		}
-
-		let title: String
-		let imageURL: URL = .init(string: "www.wp.pl")!
-
-		let liquidStakeUnits: NonEmpty<IdentifiedArrayOf<LiquidStakeUnitViewState>>?
-		let stakeClaimNFTs: NonEmpty<IdentifiedArrayOf<StakeClaimNFTViewState>>?
-	}
-
-	@MainActor
-	public struct View: SwiftUI.View {
-		private let store: Store<PoolUnitsList.LSUComponent.ViewState, PoolUnitsList.LSUComponent.ViewAction>
-
-		public init(store: Store<PoolUnitsList.LSUComponent.ViewState, PoolUnitsList.LSUComponent.ViewAction>) {
-			self.store = store
-		}
-
-		// style...
-		public var body: some SwiftUI.View {
-			WithViewStore(store) { viewStore in
-				VStack(alignment: .leading, spacing: .medium2) {
-					Text(viewStore.title)
-
-					if let liquidStakeUnits = viewStore.liquidStakeUnits {
-						Text("LIQUID STAKE UNITS")
-
-						ForEach(liquidStakeUnits) {
-							Text("\($0.description)")
-						}
-					}
-
-					if let stakeClaimNFTs = viewStore.stakeClaimNFTs {
-						VStack(alignment: .leading, spacing: .small2) {
-							Text("STAKE CLAIM NFTS")
-								.foregroundColor(.app.gray2)
-								.textStyle(.body2HighImportance)
-
-							ForEach(stakeClaimNFTs) { stakeClaimNFT in
-								HStack(alignment: .center) {
-									HStack(spacing: .small1) {
-										TokenThumbnail(
-											stakeClaimNFT.thumbnail,
-											size: .tiny
-										)
-
-										Text(stakeClaimNFT.status.localized)
-											.foregroundColor(.app.gray2)
-											.textStyle(.body2HighImportance)
-									}
-
-									Spacer()
-
-									Text(stakeClaimNFT.tokenAmount)
-										.foregroundColor(.app.gray1)
-										.textStyle(.secondaryHeader)
-								}
-								.padding(.medium3)
-								.overlay(
-									RoundedRectangle(cornerRadius: .small1)
-										.stroke(.app.gray4, lineWidth: 1)
-								)
-							}
-						}
-					}
-				}
-			}
-		}
-	}
-}
-
-// MARK: - PoolUnitsList.LSUComponent.ViewState + CustomStringConvertible
-extension PoolUnitsList.LSUComponent.ViewState: CustomStringConvertible {
-	public var description: String {
-		"\(id), \(imageURL), \(stakeClaimNFTs), \(title)"
-	}
-}
-
-// MARK: - PoolUnitsList.LSUComponent.LiquidStakeUnitViewState + CustomStringConvertible
-extension PoolUnitsList.LSUComponent.LiquidStakeUnitViewState: CustomStringConvertible {
-	var description: String {
-		"\(id), \(stakedAmmount), \(symbol), \(tokenAmount)"
 	}
 }
