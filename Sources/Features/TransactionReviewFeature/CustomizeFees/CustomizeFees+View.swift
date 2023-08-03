cadb09f16af9583a900c5c4fbfb5a9f2d8dceeaa
Ghenadie Vasiliev-Pusca <ghenadie.vasiliev-pusca@rdx.works>
2023-08-03 15:31:02 +0300

    wip

--- Sources/Features/TransactionReviewFeature/CustomizeFees/CustomizeFees+View.swift
+++ Sources/Features/TransactionReviewFeature/CustomizeFees/CustomizeFees+View.swift
@@ -20,17 +20,17 @@ extension CustomizeFees.State {
 			modeSwitchTitle: {
 				switch transactionFee.mode {
 				case .normal:
-					return "View Advanced Mode"
+					return "View Advanced Mode" // TODO: strings
 				case .advanced:
-					return "View Normal Mode"
+					return "View Normal Mode" // TODO: strings
 				}
 			}(),
 			feePayer: feePayerSelection.selected,
 			noFeePayerText: {
 				if transactionFee.totalFee.lockFee == .zero {
-					return "None required"
+					return "None required" // TODO: strings
 				} else {
-					return "No Account selected"
+					return "No Account selected" // TODO: strings
 				}
 			}(),
 			networkFee: transactionFee.networkFee,
@@ -39,7 +39,7 @@ extension CustomizeFees.State {
 			insufficientBalanceMessage: {
 				if let feePayer = feePayerSelection.selected {
 					if feePayer.xrdBalance < transactionFee.totalFee.lockFee {
-						return "Insufficient balance to pay the transaction fee, please choose another account"
+						return "Insufficient balance to pay the transaction fee, please choose another account" // TODO: strings
 					}
 				}
 				return nil
@@ -71,11 +71,11 @@ extension CustomizeFees {
 				ScrollView {
 					VStack {
 						VStack {
-							Text("Customize Fees")
+							Text("Customize Fees") // TODO: strings
 								.textStyle(.sheetTitle)
 								.foregroundColor(.app.gray1)
 								.padding(.bottom, .small1)
-							Text("Choose what account to pay the transaction fee from, or add a “tip” to speed up your transaction if necessary.")
+							Text("Choose what account to pay the transaction fee from, or add a “tip” to speed up your transaction if necessary.") // TODO: strings
 								.textStyle(.body1Regular)
 								.foregroundColor(.app.gray1)
 								.multilineTextAlignment(.center)
@@ -88,43 +88,9 @@ extension CustomizeFees {
 						.padding(.medium1)
 
 						if viewStore.isNormalMode {
-							feesBreakdownView(viewStore)
-								.padding(.medium1)
-								.background(.app.gray5)
+							normalModeFeesBreakdownView(viewStore)
 						} else {
-							VStack(spacing: .medium1) {
-								AppTextField(
-									primaryHeading: "XRD to Lock for Network & Royalty Fees",
-									placeholder: "",
-									text: viewStore.binding(
-										get: \.totalNetworkAndRoyaltyFee,
-										send: ViewAction.totalNetworkAndRoyaltyFeesChanged
-									)
-								)
-
-								AppTextField(
-									primaryHeading: "Tip to Lock (% of Network Fee)",
-									placeholder: "",
-									text: viewStore.binding(
-										get: \.tipPercentage,
-										send: ViewAction.tipPercentageChanged
-									)
-								)
-								.keyboardType(.decimalPad)
-
-								HStack {
-									Text("Transaction Fee")
-										.textStyle(.body1Link)
-										.foregroundColor(.app.gray2)
-										.textCase(.uppercase)
-									Spacer()
-									Text(viewStore.totalFee)
-										.textStyle(.body1Header)
-										.foregroundColor(.app.gray1)
-								}
-							}
-							.multilineTextAlignment(.trailing)
-							.padding(.horizontal, .medium1)
+							advancedModeBreakdownView(viewStore)
 						}
 					}
 					.padding(.vertical, .medium3)
@@ -148,13 +114,13 @@ extension CustomizeFees {
 		func feePayerView(_ viewStore: ViewStoreOf<CustomizeFees>) -> some SwiftUI.View {
 			VStack(alignment: .leading) {
 				HStack {
-					Text("Pay Fee From")
+					Text("Pay Fee From") // TODO: strings
 						.textStyle(.body1Link)
 						.foregroundColor(.app.gray2)
 						.textCase(.uppercase)
 
 					Spacer()
-					Button("Change") {
+					Button("Change") { // TODO: strings
 						viewStore.send(.changeFeePayerTapped)
 					}
 					.textStyle(.body1StandaloneLink)
@@ -168,55 +134,86 @@ extension CustomizeFees {
 					)
 					.cornerRadius(.small1)
 				} else {
-					AppTextField(placeholder: "", text: .constant(viewStore.noFeePayerText))
-						.disabled(true)
-//					Text(viewStore.noFeePayerText)
-//						.foregroundColor(.app.gray2)
-//						.textStyle(.body1Header)
-//						.background(.app.gray5)
+					AppTextField(
+						placeholder: "",
+						text: .constant(viewStore.noFeePayerText)
+					)
+					.disabled(true)
 				}
 
 				if let insufficientBalanceMessage = viewStore.insufficientBalanceMessage {
-					WarningErrorView(text: "Insufficient balance to pay the transaction fee", type: .error)
+					WarningErrorView(text: insufficientBalanceMessage, type: .error)
 				}
 			}
 		}
 
-		func feesBreakdownView(_ viewStore: ViewStoreOf<CustomizeFees>) -> some SwiftUI.View {
-			VStack(spacing: .small1) {
-				HStack {
-					Text("Network Fee")
-						.textStyle(.body1Link)
-						.foregroundColor(.app.gray2)
-						.textCase(.uppercase)
-					Spacer()
-					Text(viewStore.networkFee.formatted(false))
-						.textStyle(.body1HighImportance)
-						.foregroundColor(viewStore.networkFee == .zero ? .app.gray3 : .app.gray1)
-				}
+		@ViewBuilder
+		func advancedModeBreakdownView(_ viewStore: ViewStoreOf<CustomizeFees>) -> some SwiftUI.View {
+			VStack(spacing: .medium1) {
+				AppTextField(
+					primaryHeading: "XRD to Lock for Network & Royalty Fees", // TODO: strings
+					placeholder: "",
+					text: viewStore.binding(
+						get: \.totalNetworkAndRoyaltyFee,
+						send: ViewAction.totalNetworkAndRoyaltyFeesChanged
+					)
+				)
 
-				HStack {
-					Text("Royalty Fees")
-						.textStyle(.body1Link)
-						.foregroundColor(.app.gray2)
-						.textCase(.uppercase)
-					Spacer()
-					Text(viewStore.royaltyFee.formatted(false))
-						.textStyle(.body1HighImportance)
-						.foregroundColor(viewStore.royaltyFee == .zero ? .app.gray3 : .app.gray1)
-				}
+				AppTextField(
+					primaryHeading: "Tip to Lock (% of Network Fee)", // TODO: strings
+					placeholder: "",
+					text: viewStore.binding(
+						get: \.tipPercentage,
+						send: ViewAction.tipPercentageChanged
+					)
+				)
+				.keyboardType(.decimalPad)
+
+				transactionFeeView(fee: viewStore.totalFee)
+			}
+			.multilineTextAlignment(.trailing)
+			.padding(.horizontal, .medium1)
+		}
+
+		@ViewBuilder
+		func normalModeFeesBreakdownView(_ viewStore: ViewStoreOf<CustomizeFees>) -> some SwiftUI.View {
+			VStack(spacing: .small1) {
+				normalModeFeeView(title: "Network Fees", fee: viewStore.networkFee) // TODO: strings
+				normalModeFeeView(title: "Royalty Fees", fee: viewStore.royaltyFee) // TODO: strings
 
 				Divider()
-				HStack {
-					Text("Transaction Fee")
-						.textStyle(.body1Link)
-						.foregroundColor(.app.gray2)
-						.textCase(.uppercase)
-					Spacer()
-					Text(viewStore.totalFee)
-						.textStyle(.body1Header)
-						.foregroundColor(.app.gray1)
-				}
+
+				transactionFeeView(fee: viewStore.totalFee)
+			}
+			.padding(.medium1)
+			.background(.app.gray5)
+		}
+
+		@ViewBuilder
+		func normalModeFeeView(title: String, fee: BigDecimal) -> some SwiftUI.View {
+			HStack {
+				Text(title)
+					.textStyle(.body1Link)
+					.foregroundColor(.app.gray2)
+					.textCase(.uppercase)
+				Spacer()
+				Text(fee.formatted(false))
+					.textStyle(.body1HighImportance)
+					.foregroundColor(fee == .zero ? .app.gray3 : .app.gray1)
+			}
+		}
+
+		@ViewBuilder
+		func transactionFeeView(fee: String) -> some SwiftUI.View {
+			HStack {
+				Text("Transaction Fee") // TODO: strings
+					.textStyle(.body1Link)
+					.foregroundColor(.app.gray2)
+					.textCase(.uppercase)
+				Spacer()
+				Text(fee)
+					.textStyle(.body1Header)
+					.foregroundColor(.app.gray1)
 			}
 		}
 	}
@@ -225,7 +222,7 @@ extension CustomizeFees {
 private extension BigDecimal {
 	func formatted(_ showsZero: Bool) -> String {
 		if !showsZero, self == .zero {
-			return "None Due"
+			return "None Due" // TODO: strings
 		}
 		return "\(format()) XRD"
 	}
