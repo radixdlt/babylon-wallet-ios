6e2e7b371add8a6ad93e166c0e972a6bead7e4f3
Maciek Czarnik <maciek.czarnik@rdx.works>
2023-08-09 13:11:24 +0200

    Wire up the close interaction

--- Sources/Features/AssetsFeature/Components/PoolUnitsList/Components/PoolUnit/PoolUnitDetails.swift
+++ Sources/Features/AssetsFeature/Components/PoolUnitsList/Components/PoolUnit/PoolUnitDetails.swift
@@ -4,7 +4,18 @@ import FeaturePrelude
 public struct PoolUnitDetails: Sendable, FeatureReducer {
 	public struct State: Sendable, Hashable {}
 
+	@Dependency(\.dismiss) var dismiss
+
 	public enum ViewAction: Sendable, Equatable {
 		case closeButtonTapped
+	}
+
+	public func reduce(into state: inout State, viewAction: ViewAction) -> EffectTask<Action> {
+		switch viewAction {
+		case .closeButtonTapped:
+			return .run { _ in
+				await dismiss()
+			}
+		}
 	}
 }
