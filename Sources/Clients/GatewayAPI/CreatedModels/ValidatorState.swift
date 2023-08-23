ca29be34d1b1636d45c8aa10e929d9549ebebc49
Ghenadie Vasiliev-Pusca <ghenadie.vasiliev-pusca@rdx.works>
2023-08-23 10:50:27 +0300

    update state decoding

--- Sources/Clients/GatewayAPI/CreatedModels/ValidatorState.swift
+++ Sources/Clients/GatewayAPI/CreatedModels/ValidatorState.swift
@@ -1,48 +1,5 @@
 import Foundation
 
-// MARK: - GatewayAPI.StateEntityDetailsComponentState
-extension GatewayAPI {
-	public enum StateEntityDetailsComponentState: Codable, Hashable, EmptyObjectDecodable {
-		case validatorFieldState(ValidatorSubFieldState)
-
-		public enum CodingKeys: String, CodingKey {
-			case value
-			case substateType = "substate_type"
-		}
-
-		enum SubstateType: String, Codable {
-			case validatorFieldState = "ValidatorFieldState"
-		}
-
-		public var validatorState: ValidatorSubFieldState? {
-			if case let .validatorFieldState(wrapped) = self {
-				return wrapped
-			}
-			return nil
-		}
-
-		public init(from decoder: Decoder) throws {
-			let container = try decoder.container(keyedBy: CodingKeys.self)
-
-			let type = try container.decode(SubstateType.self, forKey: .substateType)
-
-			switch type {
-			case .validatorFieldState:
-				self = try .validatorFieldState(container.decode(ValidatorSubFieldState.self, forKey: .value))
-			}
-		}
-
-		public func encode(to encoder: Encoder) throws {
-			var container = encoder.container(keyedBy: CodingKeys.self)
-			switch self {
-			case let .validatorFieldState(validator):
-				try container.encode(validator, forKey: .value)
-				try container.encode(SubstateType.validatorFieldState, forKey: .substateType)
-			}
-		}
-	}
-}
-
 extension GatewayAPI {
 	struct Entity: Codable {
 		let entityAddress: String
@@ -52,15 +9,15 @@ extension GatewayAPI {
 		}
 	}
 
-	public struct ValidatorSubFieldState: Codable, Hashable {
+	public struct ValidatorState: Codable, Hashable, EmptyObjectDecodable {
 		public let stakeUnitResourceAddress: String
 		public let stakeXRDVaultAddress: String
 		public let unstakeClaimTokenResourceAddress: String
 
-		enum CodingKeys: String, CodingKey {
+		public enum CodingKeys: String, CodingKey {
 			case stakeXRDVaultAddress = "stake_xrd_vault"
 			case stakeUnitResourceAddress = "stake_unit_resource_address"
-			case unstakeClaimTokenResourceAddress = "unstake_claim_token_resource_address"
+			case unstakeClaimTokenResourceAddress = "claim_token_resource_address"
 		}
 
 		public init(from decoder: Decoder) throws {
--- Sources/Profile/AppPreferences/Gateway/Radix+Gateway.swift
+++ Sources/Profile/AppPreferences/Gateway/Radix+Gateway.swift
@@ -24,7 +24,7 @@ extension Radix {
 }
 
 extension Radix.Gateway {
-	public static let `default` = Radix.Gateway.hammunet
+	public static let `default` = Radix.Gateway.enkinet
 }
 
 extension Radix.Gateway {
