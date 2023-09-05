c3b33173116e75482723650e33f875603e77b09f
Maciek Czarnik <maciek.czarnik@rdx.works>
2023-09-05 11:12:23 +0200

    Once again...

--- Sources/Clients/TransactionClient/Models/TransactionModels.swift
+++ Sources/Clients/TransactionClient/Models/TransactionModels.swift
@@ -390,12 +390,12 @@ extension TransactionFee {
 
 		/// Network fees -> https://radixdlt.atlassian.net/wiki/spaces/S/pages/3134783512/Manifest+Mutation+Cost+Addition+Estimates
 		// swiftformat:disable all
-		public static let lockFeeInstructionCost = 					try! BigDecimal(fromString: "0.08581566997")
-		public static let fungibleGuaranteeInstructionCost = 		try! BigDecimal(fromString: "0.00908532837")
-		public static let nonFungibleGuranteeInstructionCost = 	try! BigDecimal(fromString: "0.00954602837")
-		public static let signatureCost = 								try! BigDecimal(fromString: "0.01109974758")
-		public static let notarizingCost = 								try! BigDecimal(fromString: "0.0081393944")
-		public static let notarizingCostWhenNotaryIsSignatory = 	try! BigDecimal(fromString: "0.0084273944")
+		public static let lockFeeInstructionCost =              try! BigDecimal(fromString: "0.08581566997")
+		public static let fungibleGuaranteeInstructionCost =    try! BigDecimal(fromString: "0.00908532837")
+		public static let nonFungibleGuranteeInstructionCost =  try! BigDecimal(fromString: "0.00954602837")
+		public static let signatureCost =                       try! BigDecimal(fromString: "0.01109974758")
+		public static let notarizingCost =                      try! BigDecimal(fromString: "0.0081393944")
+		public static let notarizingCostWhenNotaryIsSignatory = try! BigDecimal(fromString: "0.0084273944")
 		//	swiftformat:enable all
 
 		public static func notarizingCost(_ notaryIsSignatory: Bool) -> BigDecimal {
