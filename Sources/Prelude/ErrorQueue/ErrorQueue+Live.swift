d39d86b05df63df2abacc6ed5fd009fe6411dfb5
Alexander Cyon <116169792+CyonAlexRDX@users.noreply.github.com>
2023-09-06 14:19:15 +0200

    [ABW-2065] Dont show Cancelled error to user (#721)

--- Sources/Prelude/ErrorQueue/ErrorQueue+Live.swift
+++ Sources/Prelude/ErrorQueue/ErrorQueue+Live.swift
@@ -1,5 +1,6 @@
 import AsyncAlgorithms
 import Dependencies
+import Foundation
 
 extension ErrorQueue: DependencyKey {
 	public typealias Value = ErrorQueue
@@ -9,6 +10,21 @@ extension ErrorQueue: DependencyKey {
 		return Self(
 			errors: { errors.share().eraseToAnyAsyncSequence() },
 			schedule: { error in
+
+				if error is CancellationError {
+					loggerGlobal.warning("Suppressed `CancellationError`, i.e. preventing scheduling of this error on the ErrorQueue")
+					return
+				}
+
+				if
+					case let nsError = error as NSError,
+					nsError.domain == NSURLErrorDomain,
+					nsError.code == NSURLErrorCancelled
+				{
+					loggerGlobal.warning("Suppressed NSError with domain `NSURLErrorDomain`, i.e. preventing scheduling of this error on the ErrorQueue. \nDetails: code: \(nsError.code) - description: \"\(nsError.localizedDescription)\"")
+					return
+				}
+
 				if !_XCTIsTesting {
 					// easy to think a test failed if we print this warning during tests.
 					loggerGlobal.error("An error occurred: \(String(describing: error))")
