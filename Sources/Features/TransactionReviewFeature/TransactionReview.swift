c1251e369a1dbdfeef12cf47bdd733eedb895801
kugel3 <gustaf.kugelberg@rdx.works>
2023-09-06 16:01:20 +0200

    Cleaning

--- Sources/Features/TransactionReviewFeature/TransactionReview.swift
+++ Sources/Features/TransactionReviewFeature/TransactionReview.swift
@@ -90,7 +90,6 @@ public struct TransactionReview: Sendable, FeatureReducer {
 
 	public enum ViewAction: Sendable, Equatable {
 		case appeared
-		case closeTapped
 		case showRawTransactionTapped
 
 		case approvalSliderSlid
@@ -213,9 +212,6 @@ public struct TransactionReview: Sendable, FeatureReducer {
 				}
 				await send(.internal(.previewLoaded(preview)))
 			}
-
-		case .closeTapped:
-			return .none
 
 		case .showRawTransactionTapped:
 			switch state.displayMode {
