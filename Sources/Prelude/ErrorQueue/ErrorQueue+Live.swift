a3bca2dcdda5a29fca20918fcd5922f2902ecfe0
Alexander Cyon <alex.cyon@rdx.works>
2023-09-06 12:21:08 +0200

    Prevent scheduling NSError where code is NSURLErrorCancelled also prevent scheduling of CancellationError

--- Sources/Prelude/ErrorQueue/ErrorQueue+Live.swift
+++ Sources/Prelude/ErrorQueue/ErrorQueue+Live.swift
@@ -1,5 +1,6 @@
 import AsyncAlgorithms
 import Dependencies
+import Foundation
 
 extension ErrorQueue: DependencyKey {
 	public typealias Value = ErrorQueue
@@ -9,6 +10,20 @@ extension ErrorQueue: DependencyKey {
 		return Self(
 			errors: { errors.share().eraseToAnyAsyncSequence() },
 			schedule: { error in
+
+				if error is CancellationError {
+					loggerGlobal.warning("Suppressed `CancellationError`, i.e. preventing scheduling of this error on the ErrorQueue")
+					return
+				}
+
+				if
+					case let nsError = error as NSError,
+					nsError.code == NSURLErrorCancelled
+				{
+					loggerGlobal.warning("Suppressed NSError with code NSURLErrorCancelled, i.e. preventing scheduling of this error on the ErrorQueue")
+					return
+				}
+
 				if !_XCTIsTesting {
 					// easy to think a test failed if we print this warning during tests.
 					loggerGlobal.error("An error occurred: \(String(describing: error))")
