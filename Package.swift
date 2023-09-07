0ac3ab2a50b92760eff251aa267228024679d295
Maciek <137085416+maciek-rdx@users.noreply.github.com>
2023-09-07 10:23:49 +0200

    ABW-2176 RCnet v3.1 (#724)
    Co-authored-by: Alexander Cyon <alex.cyon@rdx.works>

--- Package.swift
+++ Package.swift
@@ -1041,7 +1041,7 @@ package.addModules([
 		dependencies: [
 			"Cryptography",
 			.product(name: "EngineToolkit", package: "swift-engine-toolkit") {
-				.package(url: "https://github.com/radixdlt/swift-engine-toolkit", branch: "release/rcnet-v3")
+				.package(url: "https://github.com/radixdlt/swift-engine-toolkit", exact: "0.12.1-293dd4c")
 			},
 		],
 		tests: .no
