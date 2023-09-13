2444225e91fde97260c7362387350d7acc829765
Alexander Cyon <116169792+CyonAlexRDX@users.noreply.github.com>
2023-09-13 11:53:06 +0200

    [ABW-2261] Default `isDeveloperModeEnabled` to `false`  (#754)

--- Tests/ProfileTests/ProfileTests.swift
+++ Tests/ProfileTests/ProfileTests.swift
@@ -538,7 +538,7 @@ final class ProfileTests: TestCase {
 
 		XCTAssertTrue(profile.appPreferences.security.isCloudProfileSyncEnabled, "iCloud sync should be opt-out.")
 
-		XCTAssertTrue(profile.appPreferences.security.isDeveloperModeEnabled, "Developer mode should default to on")
+		XCTAssertFalse(profile.appPreferences.security.isDeveloperModeEnabled, "Developer mode should default to `false`.")
 
 		let curve25519FactorSourceMnemonic = try Mnemonic(
 			phrase: "bright club bacon dinner achieve pull grid save ramp cereal blush woman humble limb repeat video sudden possible story mask neutral prize goose mandate",
