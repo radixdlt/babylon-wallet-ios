0d807590a104c30d525c436c061cf974e2943e87
Alexander Cyon <116169792+CyonAlexRDX@users.noreply.github.com>
2023-09-15 13:22:04 +0200

    [ABW-2046] Fix reentrancy problems with ProfileStore (`ManagedAtomicLazyRef`) (#760)

--- Package.swift
+++ Package.swift
@@ -783,10 +783,14 @@ package.addModules([
 	.client(
 		name: "ProfileStore",
 		dependencies: [
+			.product(
+				name: "Atomics",
+				package: "swift-atomics"
+			),
+			"DeviceFactorSourceClient", // FIXME: break out to `BaseProfileClient` or similar
+			"MnemonicClient",
 			"Profile",
 			"SecureStorageClient",
-			"MnemonicClient",
-			"DeviceFactorSourceClient", // FIXME: break out to `BaseProfileClient` or similar
 		],
 		tests: .yes()
 	),
@@ -962,11 +966,15 @@ package.addModules([
 	.module(
 		name: "Profile",
 		dependencies: [
+			.product(
+				name: "ComposableArchitecture",
+				// actually just CasePaths
+				package: "swift-composable-architecture"
+			),
 			"Cryptography",
 			"EngineKit",
 			"RadixConnectModels",
 			"Resources",
-			.product(name: "ComposableArchitecture", package: "swift-composable-architecture"), // actually just CasePaths
 		],
 		tests: .yes(
 			dependencies: [
@@ -1070,6 +1078,9 @@ package.addModules([
 			},
 			.product(name: "AsyncExtensions", package: "AsyncExtensions") {
 				.package(url: "https://github.com/sideeffect-io/AsyncExtensions", from: "0.5.1")
+			},
+			.product(name: "Atomics", package: "swift-atomics") {
+				.package(url: "https://github.com/apple/swift-atomics", exact: "1.1.0")
 			},
 			.product(name: "BigInt", package: "BigInt") {
 				.package(url: "https://github.com/attaswift/BigInt", from: "5.3.0")
