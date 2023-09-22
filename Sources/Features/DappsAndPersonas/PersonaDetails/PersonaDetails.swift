a8aa7f793cfec0e7dad027b31183ccb17a8582b5
kugel3 <gustaf.kugelberg@rdx.works>
2023-09-22 02:47:12 +0200

    Fix reloading of dApps in PersonaDetails

--- Sources/Features/DappsAndPersonas/PersonaDetails/PersonaDetails.swift
+++ Sources/Features/DappsAndPersonas/PersonaDetails/PersonaDetails.swift
@@ -62,7 +62,6 @@ public struct PersonaDetails: Sendable, FeatureReducer {
 
 	public enum ViewAction: Sendable, Equatable {
 		case appeared
-		case accountTapped(AccountAddress)
 		case dAppTapped(Profile.Network.AuthorizedDapp.ID)
 		case editPersonaTapped
 		case editAccountSharingTapped
@@ -130,27 +129,29 @@ public struct PersonaDetails: Sendable, FeatureReducer {
 
 	public func reduce(into state: inout State, childAction: ChildAction) -> Effect<Action> {
 		switch childAction {
-		case let .destination(.presented(.editPersona(.delegate(.personaSaved(persona))))):
-			guard persona.id == state.mode.id else { return .none }
-			return .run { [mode = state.mode] send in
-				let updated = try await reload(in: mode)
-				await send(.internal(.reloaded(updated)))
-				await send(.delegate(.personaChanged(persona.id)))
-			} catch: { error, _ in
-				loggerGlobal.error("Failed to reload, error: \(error)")
-			}
+		case let .destination(.presented(presentedAction)):
+			switch presentedAction {
+			case let .editPersona(.delegate(.personaSaved(persona))):
+				guard persona.id == state.mode.id else { return .none }
+				return reloadEffect(mode: state.mode, notifyDelegate: true)
 
-		case .destination(.presented(.confirmForgetAlert(.confirmTapped))):
-			guard case let .dApp(dApp, persona: persona) = state.mode else {
+			case .dAppDetails(.delegate(.dAppForgotten)):
+				state.destination = nil
+				return reloadEffect(mode: state.mode, notifyDelegate: false)
+
+			case .confirmForgetAlert(.confirmTapped):
+				guard case let .dApp(dApp, persona: persona) = state.mode else { return .none }
+				let (personaID, dAppID, networkID) = (persona.id, dApp.dAppDefinitionAddress, dApp.networkID)
+				return .run { send in
+					try await authorizedDappsClient.deauthorizePersonaFromDapp(personaID, dAppID, networkID)
+					await send(.delegate(.personaDeauthorized))
+				} catch: { error, _ in
+					loggerGlobal.error("Failed to deauthorize persona \(personaID) from dApp \(dAppID), error: \(error)")
+					errorQueue.schedule(error)
+				}
+
+			default:
 				return .none
-			}
-			let (personaID, dAppID, networkID) = (persona.id, dApp.dAppDefinitionAddress, dApp.networkID)
-			return .run { send in
-				try await authorizedDappsClient.deauthorizePersonaFromDapp(personaID, dAppID, networkID)
-				await send(.delegate(.personaDeauthorized))
-			} catch: { error, _ in
-				loggerGlobal.error("Failed to deauthorize persona \(personaID) from dApp \(dAppID), error: \(error)")
-				errorQueue.schedule(error)
 			}
 
 		case .destination:
@@ -166,9 +167,6 @@ public struct PersonaDetails: Sendable, FeatureReducer {
 				await send(.internal(.dAppsUpdated(addingDappMetadata(to: dApps))))
 			}
 
-		case let .accountTapped(address):
-			return .none
-
 		case let .dAppTapped(dAppID):
 			return .run { send in
 				let dApp = try await authorizedDappsClient.getDetailedDapp(dAppID)
@@ -177,7 +175,6 @@ public struct PersonaDetails: Sendable, FeatureReducer {
 				loggerGlobal.error("Could not get dApp details \(dAppID), error: \(error)")
 				errorQueue.schedule(error)
 			}
-			return .none
 
 		case .editPersonaTapped:
 			switch state.mode {
@@ -217,8 +214,12 @@ public struct PersonaDetails: Sendable, FeatureReducer {
 
 			return .none
 
-		case let .dAppsUpdated(dApps):
-			guard case let .general(persona, _) = state.mode else { return .none }
+		case let .dAppsUpdated(updatedDapps):
+			guard case .general(let persona, var dApps) = state.mode else { return .none }
+			for updatedDapp in updatedDapps where dApps.ids.contains(updatedDapp.id) {
+				dApps[id: updatedDapp.id] = updatedDapp
+			}
+
 			state.mode = .general(persona, dApps: dApps)
 			return .none
 
@@ -237,6 +238,18 @@ public struct PersonaDetails: Sendable, FeatureReducer {
 		case let .dAppLoaded(dApp):
 			state.destination = .dAppDetails(.init(dApp: dApp, context: .personaDetails))
 			return .none
+		}
+	}
+
+	private func reloadEffect(mode: State.Mode, notifyDelegate: Bool) -> Effect<Action> {
+		.run { send in
+			let updated = try await reload(in: mode)
+			await send(.internal(.reloaded(updated)))
+			if notifyDelegate {
+				await send(.delegate(.personaChanged(mode.id)))
+			}
+		} catch: { error, _ in
+			loggerGlobal.error("Failed to reload, error: \(error)")
 		}
 	}
 
