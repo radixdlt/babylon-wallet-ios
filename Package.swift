3c55e2de5e78adad2b0cd1c20081ffdd0ac0e0c3
Ghenadie Vasiliev-Pusca <ghenadie.vasiliev-pusca@rdx.works>
2023-09-27 13:36:25 +0300

    Upgrade to use final class

--- Package.swift
+++ Package.swift
@@ -1035,7 +1035,7 @@ package.addModules([
 		dependencies: [
 			"Cryptography",
 			.product(name: "EngineToolkit", package: "swift-engine-toolkit") {
-				.package(url: "https://github.com/radixdlt/swift-engine-toolkit", exact: "1.0.0-8651223")
+				.package(url: "https://github.com/radixdlt/swift-engine-toolkit", branch: "add_final_classes")
 			},
 		],
 		tests: .no
---
+++ Sources/EngineKit/Codable/NonFungibleGlobalID+Codable.swift
@@ -0,0 +1,15 @@
+import EngineToolkit
+import Foundation
+
+extension NonFungibleGlobalId: Codable {
+	public func encode(to encoder: Encoder) throws {
+		var container = encoder.singleValueContainer()
+		try container.encode(asStr())
+	}
+
+	public convenience init(from decoder: Decoder) throws {
+		let container = try decoder.singleValueContainer()
+		let raw = try container.decode(String.self)
+		try self.init(nonFungibleGlobalId: raw)
+	}
+}
