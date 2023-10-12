81ee2d14b3d4f67251e43c6f04a1f0f180eb38b7
kugel3 <gustaf.kugelberg@rdx.works>
2023-10-12 14:54:39 +0200

    Add progress view to DerivePublicKeys

--- Sources/Features/DerivePublicKeys/DerivePublicKeys+View.swift
+++ Sources/Features/DerivePublicKeys/DerivePublicKeys+View.swift
@@ -51,6 +51,9 @@ extension DerivePublicKeys {
 						LedgerRowView(viewState: .init(factorSource: ledger))
 					}
 
+					ProgressView()
+						.padding(.top, .large3)
+
 					Spacer(minLength: 0)
 				}
 				.padding(.horizontal, .medium1)
