56b26f9d4ff99af32e5ac3bcd813c982746fcad5
Alexander Cyon <alex.cyon@rdx.works>
2023-10-15 15:17:53 +0200

    wip

---
+++ RadixWallet/Features/SecurityStructureConfigurationList/Coordinator/SecurityStructureConfigurationListCoordinator+View.swift
@@ -0,0 +1,62 @@
+import ComposableArchitecture
+import SwiftUI
+
+// MARK: - SecurityStructureConfigurationListCoordinator.View
+extension SecurityStructureConfigurationListCoordinator {
+	@MainActor
+	public struct View: SwiftUI.View {
+		private let store: StoreOf<SecurityStructureConfigurationListCoordinator>
+
+		public init(store: StoreOf<SecurityStructureConfigurationListCoordinator>) {
+			self.store = store
+		}
+
+		public var body: some SwiftUI.View {
+			SecurityStructureConfigurationList.View(
+				store: store.scope(
+					state: \.configList,
+					action: { .child(.configList($0)) }
+				)
+			)
+			.destination(store: store)
+		}
+	}
+}
+
+extension View {
+	@MainActor
+	fileprivate func destination(store: StoreOf<SecurityStructureConfigurationListCoordinator>) -> some View {
+		let destinationStore = store.scope(state: \.$destination, action: { .child(.destination($0)) })
+		return manageSecurityStructureCoordinator(with: destinationStore)
+	}
+
+	@MainActor
+	private func manageSecurityStructureCoordinator(with destinationStore: PresentationStoreOf<SecurityStructureConfigurationListCoordinator.Destination>) -> some View {
+		sheet(
+			store: destinationStore,
+			state: /SecurityStructureConfigurationListCoordinator.Destination.State.manageSecurityStructureCoordinator,
+			action: SecurityStructureConfigurationListCoordinator.Destination.Action.manageSecurityStructureCoordinator,
+			content: { ManageSecurityStructureCoordinator.View(store: $0) }
+		)
+	}
+}
+
+// #if DEBUG
+// import SwiftUI
+import ComposableArchitecture //
+//// MARK: - SecurityStructureConfigurationList_Preview
+// struct SecurityStructureConfigurationList_Preview: PreviewProvider {
+//	static var previews: some View {
+//		SecurityStructureConfigurationListCoordinator.View(
+//			store: .init(
+//				initialState: .previewValue,
+//				reducer: SecurityStructureConfigurationListCoordinator.init
+//			)
+//		)
+//	}
+// }
+//
+// extension SecurityStructureConfigurationListCoordinator.State {
+//	public static let previewValue = Self()
+// }
+// #endif
---
+++ RadixWallet/Features/SecurityStructureConfigurationList/Coordinator/SecurityStructureConfigurationListCoordinator.swift
@@ -0,0 +1,103 @@
+import ComposableArchitecture
+import SwiftUI
+
+// MARK: - SecurityStructureConfigurationListCoordinator
+public struct SecurityStructureConfigurationListCoordinator: Sendable, FeatureReducer {
+	public struct State: Sendable, Hashable {
+		public var configList: SecurityStructureConfigurationList.State
+
+		@PresentationState
+		public var destination: Destination.State? = nil
+
+		public init(
+			configList: SecurityStructureConfigurationList.State = .init()
+		) {
+			self.configList = configList
+		}
+	}
+
+	public enum InternalAction: Sendable, Equatable {
+		case loadDetailsForSecurityStructureResult(TaskResult<SecurityStructureConfigurationDetailed>)
+	}
+
+	public enum ChildAction: Sendable, Equatable {
+		case configList(SecurityStructureConfigurationList.Action)
+		case destination(PresentationAction<Destination.Action>)
+	}
+
+	// MARK: - Destination
+
+	public struct Destination: Reducer {
+		public enum State: Equatable, Hashable {
+			case manageSecurityStructureCoordinator(ManageSecurityStructureCoordinator.State)
+		}
+
+		public enum Action: Equatable {
+			case manageSecurityStructureCoordinator(ManageSecurityStructureCoordinator.Action)
+		}
+
+		public var body: some ReducerOf<Self> {
+			Scope(
+				state: /State.manageSecurityStructureCoordinator,
+				action: /Action.manageSecurityStructureCoordinator
+			) {
+				ManageSecurityStructureCoordinator()
+			}
+		}
+	}
+
+	// MARK: - Reducer
+
+	@Dependency(\.errorQueue) var errorQueue
+	@Dependency(\.appPreferencesClient) var appPreferencesClient
+
+	public init() {}
+
+	public var body: some ReducerOf<Self> {
+		Scope(state: \.configList, action: /Action.child .. ChildAction.configList) {
+			SecurityStructureConfigurationList()
+		}
+		Reduce(core)
+			.ifLet(\.$destination, action: /Action.child .. ChildAction.destination) {
+				Destination()
+			}
+	}
+
+	public func reduce(into state: inout State, internalAction: InternalAction) -> Effect<Action> {
+		switch internalAction {
+		case let .loadDetailsForSecurityStructureResult(.success(config)):
+			state.destination = .manageSecurityStructureCoordinator(.init(mode: .existing(config)))
+			return .none
+		case let .loadDetailsForSecurityStructureResult(.failure(error)):
+			loggerGlobal.error("Failed to load details for security structure config reference, error: \(error)")
+			errorQueue.schedule(error)
+			return .none
+		}
+	}
+
+	public func reduce(into state: inout State, childAction: ChildAction) -> Effect<Action> {
+		switch childAction {
+		case .configList(.delegate(.createNewStructure)):
+			state.destination = .manageSecurityStructureCoordinator(.init())
+			return .none
+
+		case let .configList(.delegate(.displayDetails(configReference))):
+
+			return .run { send in
+				let taskResult = await TaskResult {
+					try await appPreferencesClient.getDetailsOfSecurityStructure(configReference)
+				}
+				await send(.internal(.loadDetailsForSecurityStructureResult(taskResult)))
+			}
+
+		case let .destination(.presented(.manageSecurityStructureCoordinator(.delegate(.done(.success(config)))))):
+			let configReference = config.asReference()
+			state.configList.configs[id: configReference.id] = .init(configReference: configReference)
+			state.destination = nil
+			return .none
+
+		default:
+			return .none
+		}
+	}
+}
