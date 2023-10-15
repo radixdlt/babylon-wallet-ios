e24e4680ccb9de6b0ec92cd4bea5c97412aa232b
Alexander Cyon <alex.cyon@rdx.works>
2023-10-15 15:07:51 +0200

    wip

---
+++ RadixWallet/Features/AccountPreferencesFeature/Children/ThirdPartyDeposits/ResourcesList+Reducer.swift
@@ -0,0 +1,272 @@
+import ComposableArchitecture
+import SwiftUI
+
+// MARK: - ResourcesListMode
+public enum ResourcesListMode: Hashable, Sendable {
+	public typealias ExceptionRule = ThirdPartyDeposits.DepositAddressExceptionRule
+	case allowDenyAssets(ExceptionRule)
+	case allowDepositors
+}
+
+// MARK: - ResourceViewState
+public struct ResourceViewState: Hashable, Sendable, Identifiable {
+	public enum Address: Hashable, Sendable {
+		case assetException(ThirdPartyDeposits.AssetException)
+		case allowedDepositor(ThirdPartyDeposits.DepositorAddress)
+	}
+
+	public var id: Address { address }
+
+	let iconURL: URL?
+	let name: String?
+	let address: Address
+}
+
+// MARK: - ResourcesList
+public struct ResourcesList: FeatureReducer, Sendable {
+	public struct State: Hashable, Sendable {
+		var allDepositorAddresses: OrderedSet<ResourceViewState.Address> {
+			switch mode {
+			case .allowDenyAssets:
+				OrderedSet(thirdPartyDeposits.assetsExceptionList.map { .assetException($0) })
+			case .allowDepositors:
+				OrderedSet(thirdPartyDeposits.depositorsAllowList.map { .allowedDepositor($0) })
+			}
+		}
+
+		var resourcesForDisplay: [ResourceViewState] {
+			switch mode {
+			case let .allowDenyAssets(exception):
+				let addresses: [ResourceViewState.Address] = thirdPartyDeposits.assetsExceptionList
+					.filter { $0.exceptionRule == exception }
+					.map { .assetException($0) }
+
+				return loadedResources.filter { addresses.contains($0.address) }
+			case .allowDepositors:
+				return loadedResources
+			}
+		}
+
+		var mode: ResourcesListMode
+		var thirdPartyDeposits: ThirdPartyDeposits
+		let networkID: NetworkID
+		var loadedResources: [ResourceViewState] = []
+
+		@PresentationState
+		var destinations: Destinations.State? = nil
+	}
+
+	public enum ViewAction: Equatable, Sendable {
+		case task
+		case addAssetTapped
+		case assetRemove(ResourceViewState.Address)
+		case exceptionListChanged(ThirdPartyDeposits.DepositAddressExceptionRule)
+	}
+
+	public enum ChildAction: Equatable, Sendable {
+		case destinations(PresentationAction<Destinations.Action>)
+	}
+
+	public enum DelegateAction: Equatable, Sendable {
+		case updated(ThirdPartyDeposits)
+	}
+
+	public enum InternalAction: Equatable, Sendable {
+		case resourceLoaded(OnLedgerEntity.Resource?, ResourceViewState.Address)
+		case resourcesLoaded([OnLedgerEntity.Resource]?)
+	}
+
+	public struct Destinations: Reducer, Sendable {
+		public enum State: Equatable, Hashable, Sendable {
+			case addAsset(AddAsset.State)
+			case confirmAssetDeletion(AlertState<Action.ConfirmDeletionAlert>)
+		}
+
+		public enum Action: Hashable, Sendable {
+			case addAsset(AddAsset.Action)
+			case confirmAssetDeletion(ConfirmDeletionAlert)
+
+			public enum ConfirmDeletionAlert: Hashable, Sendable {
+				case confirmTapped(ResourceViewState.Address)
+				case cancelTapped
+			}
+		}
+
+		public var body: some ReducerOf<Self> {
+			Scope(state: /State.addAsset, action: /Action.addAsset) {
+				AddAsset()
+			}
+		}
+	}
+
+	@Dependency(\.onLedgerEntitiesClient) var onLedgerEntitiesClient
+
+	public var body: some ReducerOf<Self> {
+		Reduce(core)
+			.ifLet(\.$destinations, action: /Action.child .. ChildAction.destinations) {
+				Destinations()
+			}
+	}
+
+	public func reduce(into state: inout State, viewAction: ViewAction) -> Effect<Action> {
+		switch viewAction {
+		case .task:
+			let addresses: [ResourceAddress] = state.allDepositorAddresses.map(\.resourceAddress)
+			return .run { send in
+				let loadResourcesResult = try? await onLedgerEntitiesClient.getResources(addresses)
+				await send(.internal(.resourcesLoaded(loadResourcesResult)))
+			}
+
+		case .addAssetTapped:
+			state.destinations = .addAsset(.init(
+				mode: state.mode,
+				alreadyAddedResources: state.allDepositorAddresses,
+				networkID: state.networkID
+			))
+			return .none
+
+		case let .assetRemove(resource):
+			state.destinations = .confirmAssetDeletion(.confirmAssetDeletion(
+				state.mode.removeTitle,
+				state.mode.removeConfirmationMessage,
+				resourceAddress: resource
+			))
+			return .none
+
+		case let .exceptionListChanged(exception):
+			state.mode = .allowDenyAssets(exception)
+			return .none
+		}
+	}
+
+	public func reduce(into state: inout State, childAction: ChildAction) -> Effect<Action> {
+		switch childAction {
+		case let .destinations(.presented(.addAsset(.delegate(.addAddress(mode, newAsset))))):
+			state.mode = mode
+			state.destinations = nil
+
+			return .run { send in
+				let loadResourceResult = try? await onLedgerEntitiesClient.getResource(newAsset.resourceAddress)
+				await send(.internal(.resourceLoaded(loadResourceResult, newAsset)))
+			}
+
+		case let .destinations(.presented(.confirmAssetDeletion(.confirmTapped(resource)))):
+			state.loadedResources.removeAll(where: { $0.address == resource })
+			switch resource {
+			case let .assetException(resource):
+				state.thirdPartyDeposits.assetsExceptionList.removeAll(where: { $0.address == resource.address })
+			case let .allowedDepositor(depositorAddress):
+				state.thirdPartyDeposits.depositorsAllowList.remove(depositorAddress)
+			}
+
+			return .send(.delegate(.updated(state.thirdPartyDeposits)))
+		case .destinations:
+			return .none
+		}
+	}
+
+	public func reduce(into state: inout State, internalAction: InternalAction) -> Effect<Action> {
+		switch internalAction {
+		case let .resourceLoaded(resource, newAsset):
+			state.loadedResources.append(.init(iconURL: resource?.metadata.iconURL, name: resource?.metadata.name, address: newAsset))
+
+			switch newAsset {
+			case let .assetException(resource):
+				state.thirdPartyDeposits.assetsExceptionList.updateOrAppend(resource)
+			case let .allowedDepositor(depositorAddress):
+				state.thirdPartyDeposits.depositorsAllowList.updateOrAppend(depositorAddress)
+			}
+
+			return .send(.delegate(.updated(state.thirdPartyDeposits)))
+
+		case let .resourcesLoaded(resources):
+			guard let resources else {
+				state.loadedResources = state.allDepositorAddresses.map {
+					ResourceViewState(iconURL: nil, name: nil, address: $0)
+				}
+				return .none
+			}
+
+			state.loadedResources = state.allDepositorAddresses.map { address in
+				let resourceDetails = resources.first { $0.resourceAddress == address.resourceAddress }
+				return .init(
+					iconURL: resourceDetails?.metadata.iconURL,
+					name: resourceDetails?.metadata.name,
+					address: address
+				)
+			}
+			return .none
+		}
+	}
+}
+
+extension AlertState<ResourcesList.Destinations.Action.ConfirmDeletionAlert> {
+	static func confirmAssetDeletion(
+		_ title: String,
+		_ message: String,
+		resourceAddress: ResourceViewState.Address
+	) -> AlertState {
+		AlertState {
+			TextState(title)
+		} actions: {
+			ButtonState(role: .destructive, action: .confirmTapped(resourceAddress)) {
+				TextState(L10n.Common.remove)
+			}
+			ButtonState(role: .cancel, action: .cancelTapped) {
+				TextState(L10n.Common.cancel)
+			}
+		} message: {
+			TextState(message)
+		}
+	}
+}
+
+extension ResourceViewState.Address {
+	var resourceAddress: ResourceAddress {
+		switch self {
+		case let .assetException(resource):
+			resource.address
+		case let .allowedDepositor(depositorAddress):
+			depositorAddress.resourceAddress
+		}
+	}
+}
+
+extension ResourcesListMode {
+	var removeTitle: String {
+		switch self {
+		case .allowDenyAssets:
+			L10n.AccountSettings.SpecificAssetsDeposits.removeAsset
+		case .allowDepositors:
+			L10n.AccountSettings.SpecificAssetsDeposits.removeDepositor
+		}
+	}
+
+	var removeConfirmationMessage: String {
+		switch self {
+		case .allowDenyAssets(.allow):
+			L10n.AccountSettings.SpecificAssetsDeposits.removeAssetMessageAllow
+		case .allowDenyAssets(.deny):
+			L10n.AccountSettings.SpecificAssetsDeposits.removeAssetMessageDeny
+		case .allowDepositors:
+			"The badge will be removed from the list" // FIXME: Strings
+		}
+	}
+}
+
+extension ThirdPartyDeposits.DepositorAddress {
+	var resourceAddress: ResourceAddress {
+		switch self {
+		case let .resourceAddress(address):
+			return address
+		case let .nonFungibleGlobalID(nonFungibleGlobalID):
+			return try! nonFungibleGlobalID.resourceAddress().asSpecific()
+		}
+	}
+}
+
+extension ThirdPartyDeposits.AssetException {
+	func updateExceptionRule(_ rule: ThirdPartyDeposits.DepositAddressExceptionRule) -> Self {
+		.init(address: address, exceptionRule: rule)
+	}
+}
---
+++ RadixWallet/Features/AccountPreferencesFeature/Children/ThirdPartyDeposits/ResourcesList+View.swift
@@ -0,0 +1,210 @@
+import ComposableArchitecture
+import SwiftUI
+extension ResourcesList.State {
+	var viewState: ResourcesList.ViewState {
+		.init(
+			resources: .init(uncheckedUniqueElements: resourcesForDisplay),
+			info: switch mode {
+			case .allowDenyAssets(.allow) where resourcesForDisplay.isEmpty:
+				L10n.AccountSettings.SpecificAssetsDeposits.emptyAllowAll
+			case .allowDenyAssets(.allow):
+				L10n.AccountSettings.SpecificAssetsDeposits.allowInfo
+			case .allowDenyAssets(.deny) where resourcesForDisplay.isEmpty:
+				L10n.AccountSettings.SpecificAssetsDeposits.emptyDenyAll
+			case .allowDenyAssets(.deny):
+				L10n.AccountSettings.SpecificAssetsDeposits.denyInfo
+			case .allowDepositors where resourcesForDisplay.isEmpty:
+				"Add a specific badge by its resource address to allow all deposits from its holder." // FIXME: Strings
+			case .allowDepositors:
+				"The holder of the following badges may always deposit accounts to this account." // FIXME: Strings
+			},
+			mode: mode
+		)
+	}
+}
+
+extension ResourcesList {
+	public struct ViewState: Equatable {
+		let resources: IdentifiedArrayOf<ResourceViewState>
+		let info: String
+		let mode: ResourcesListMode
+	}
+
+	@MainActor
+	public struct View: SwiftUI.View {
+		let store: StoreOf<ResourcesList>
+		init(store: StoreOf<ResourcesList>) {
+			self.store = store
+		}
+
+		public var body: some SwiftUI.View {
+			WithViewStore(store, observe: \.viewState, send: { .view($0) }) { viewStore in
+				VStack(spacing: .medium1) {
+					headerView(viewStore)
+					if !viewStore.resources.isEmpty {
+						listView(viewStore)
+					}
+					Spacer(minLength: 0)
+				}
+				.onFirstTask { @MainActor in
+					await viewStore.send(.task).finish()
+				}
+				.padding(.top, .medium1)
+				.background(.app.gray5)
+				.destination(store: store)
+				.navigationTitle(viewStore.mode.navigationTitle)
+				.defaultNavBarConfig()
+				.footer {
+					Button(viewStore.mode.addButtonTitle) {
+						viewStore.send(.addAssetTapped)
+					}
+					.buttonStyle(.primaryRectangular)
+				}
+			}
+		}
+	}
+}
+
+extension ResourcesList.View {
+	@ViewBuilder
+	func headerView(_ viewStore: ViewStoreOf<ResourcesList>) -> some SwiftUI.View {
+		Group {
+			if case let .allowDenyAssets(exceptionRule) = viewStore.mode {
+				Picker(
+					"Select expcetion list", // FIXME: Strings
+					selection: viewStore.binding(
+						get: { _ in exceptionRule },
+						send: { .exceptionListChanged($0) }
+					)
+				) {
+					ForEach(ThirdPartyDeposits.DepositAddressExceptionRule.allCases, id: \.self) {
+						Text($0.text)
+					}
+				}
+				.pickerStyle(.segmented)
+			}
+
+			Text(viewStore.info)
+				.textStyle(.body1HighImportance)
+				.foregroundColor(.app.gray2)
+				.multilineTextAlignment(.center)
+		}
+		.padding(.horizontal, .medium1)
+	}
+
+	@ViewBuilder
+	func listView(_ viewStore: ViewStoreOf<ResourcesList>) -> some SwiftUI.View {
+		List {
+			ForEach(viewStore.resources) { row in
+				resourceRowView(row, viewStore)
+			}
+		}
+		.scrollContentBackground(.hidden)
+		.listStyle(.grouped)
+	}
+
+	@ViewBuilder
+	func resourceRowView(_ viewState: ResourceViewState, _ viewStore: ViewStoreOf<ResourcesList>) -> some SwiftUI.View {
+		HStack {
+			if case .globalNonFungibleResourceManager = viewState.address.resourceAddress.decodedKind {
+				NFTThumbnail(viewState.iconURL)
+			} else {
+				TokenThumbnail(.known(viewState.iconURL))
+			}
+
+			VStack(alignment: .leading, spacing: .zero) {
+				Text(viewState.name ?? "")
+					.textStyle(.body1HighImportance)
+					.foregroundColor(.app.gray1)
+				AddressView(
+					viewState.address.ledgerIdentifiable,
+					isTappable: false
+				)
+				.foregroundColor(.app.gray2)
+			}
+			.padding(.leading, .medium3)
+
+			Spacer(minLength: 0)
+
+			AssetIcon(.asset(AssetResource.trash))
+				.onTapGesture {
+					viewStore.send(.assetRemove(viewState.address))
+				}
+		}
+		.frame(minHeight: .largeButtonHeight)
+	}
+}
+
+private extension View {
+	@MainActor
+	func destination(store: StoreOf<ResourcesList>) -> some View {
+		let destinationStore = store.scope(state: \.$destinations, action: { .child(.destinations($0)) })
+		return addAsset(with: destinationStore)
+			.confirmDeletionAlert(with: destinationStore)
+	}
+
+	@MainActor
+	func addAsset(with destinationStore: PresentationStoreOf<ResourcesList.Destinations>) -> some View {
+		sheet(
+			store: destinationStore,
+			state: /ResourcesList.Destinations.State.addAsset,
+			action: ResourcesList.Destinations.Action.addAsset,
+			content: { AddAsset.View(store: $0) }
+		)
+	}
+
+	@MainActor
+	func confirmDeletionAlert(with destinationStore: PresentationStoreOf<ResourcesList.Destinations>) -> some View {
+		alert(
+			store: destinationStore,
+			state: /ResourcesList.Destinations.State.confirmAssetDeletion,
+			action: ResourcesList.Destinations.Action.confirmAssetDeletion
+		)
+	}
+}
+
+extension ThirdPartyDeposits.DepositAddressExceptionRule {
+	var text: String {
+		switch self {
+		case .allow:
+			L10n.AccountSettings.SpecificAssetsDeposits.allow
+		case .deny:
+			L10n.AccountSettings.SpecificAssetsDeposits.deny
+		}
+	}
+}
+
+extension ResourcesListMode {
+	var addButtonTitle: String {
+		switch self {
+		case .allowDenyAssets:
+			L10n.AccountSettings.SpecificAssetsDeposits.addAnAssetButton
+		case .allowDepositors:
+			L10n.AccountSettings.ThirdPartyDeposits.allowSpecificDepositorsButton
+		}
+	}
+
+	var navigationTitle: String {
+		switch self {
+		case .allowDenyAssets:
+			L10n.AccountSettings.specificAssetsDeposits
+		case .allowDepositors:
+			L10n.AccountSettings.ThirdPartyDeposits.allowSpecificDepositors
+		}
+	}
+}
+
+extension ResourceViewState.Address {
+	var ledgerIdentifiable: LedgerIdentifiable {
+		switch self {
+		case let .assetException(exception):
+			.address(.resource(exception.address))
+
+		case let .allowedDepositor(.resourceAddress(resourceAddress)):
+			.address(.resource(resourceAddress))
+
+		case let .allowedDepositor(.nonFungibleGlobalID(nonFungibleGlobalID)):
+			.address(.nonFungibleGlobalID(nonFungibleGlobalID))
+		}
+	}
+}
