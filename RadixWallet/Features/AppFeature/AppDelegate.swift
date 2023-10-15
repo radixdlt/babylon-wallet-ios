fa4cc5338f3632456dba4244ce433d18e3fce0bc
Alexander Cyon <alex.cyon@rdx.works>
2023-10-15 15:08:09 +0200

    wip

---
+++ RadixWallet/Features/AppFeature/AppDelegate.swift
@@ -0,0 +1,13 @@
+import ComposableArchitecture
+import SwiftUI
+public final class AppDelegate: NSObject, UIApplicationDelegate {
+	public func application(
+		_ application: UIApplication,
+		configurationForConnecting connectingSceneSession: UISceneSession,
+		options: UIScene.ConnectionOptions
+	) -> UISceneConfiguration {
+		let sceneConfig = UISceneConfiguration(name: nil, sessionRole: connectingSceneSession.role)
+		sceneConfig.delegateClass = SceneDelegate.self
+		return sceneConfig
+	}
+}
