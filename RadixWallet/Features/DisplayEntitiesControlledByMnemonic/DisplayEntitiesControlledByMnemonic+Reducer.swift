c6013e996d469a94413f85a22310d4cafc65a2d0
Alexander Cyon <alex.cyon@rdx.works>
2023-10-15 15:16:19 +0200

    wip

---
+++ RadixWallet/Features/DisplayEntitiesControlledByMnemonic/DisplayEntitiesControlledByMnemonic+Reducer.swift
@@ -0,0 +1,40 @@
+import ComposableArchitecture
+import SwiftUI
+
+// MARK: - DisplayEntitiesControlledByMnemonic
+public struct DisplayEntitiesControlledByMnemonic: Sendable, FeatureReducer {
+	public struct State: Sendable, Hashable, Identifiable {
+		public typealias ID = EntitiesControlledByFactorSource.ID
+		public var id: ID { accountsForDeviceFactorSource.id }
+
+		public var deviceFactorSource: DeviceFactorSource { accountsForDeviceFactorSource.deviceFactorSource }
+
+		public let accountsForDeviceFactorSource: EntitiesControlledByFactorSource
+		public let displayRevealMnemonicLink: Bool
+
+		public init(accountsForDeviceFactorSource: EntitiesControlledByFactorSource, displayRevealMnemonicLink: Bool) {
+			self.accountsForDeviceFactorSource = accountsForDeviceFactorSource
+			self.displayRevealMnemonicLink = displayRevealMnemonicLink
+		}
+	}
+
+	public enum ViewAction: Sendable, Equatable {
+		case appeared
+		case tapped
+	}
+
+	public enum DelegateAction: Sendable, Equatable {
+		case openDetails
+	}
+
+	public init() {}
+
+	public func reduce(into state: inout State, viewAction: ViewAction) -> Effect<Action> {
+		switch viewAction {
+		case .appeared:
+			.none
+		case .tapped:
+			.send(.delegate(.openDetails))
+		}
+	}
+}
