a8d4df075a96e9f1b4d254fc9d5d182bbc2a98ed
Alexander Cyon <alex.cyon@rdx.works>
2023-10-15 15:08:19 +0200

    wip

---
+++ RadixWallet/Features/AnswerSecurityQuestions/Children/FreeformQuestion/AnswerSecurityQuestionFreeform.swift
@@ -0,0 +1,57 @@
+import ComposableArchitecture
+import SwiftUI
+
+// MARK: - AnswerSecurityQuestionFreeform
+public struct AnswerSecurityQuestionFreeform: Sendable, FeatureReducer {
+	public struct State: Sendable, Hashable, Identifiable {
+		public var id: SecurityQuestion.ID { question.id }
+		public let question: SecurityQuestion
+		public var answer: NonEmptyString? = nil
+		public let isLast: Bool
+		public let keyDerivationScheme: SecurityQuestionsFactorSource.KeyDerivationScheme
+		public var answerToQuestion: AnswerToSecurityQuestion?
+		public var rawAnswerToQuestion: AbstractAnswerToSecurityQuestion<NonEmptyString>? {
+			guard let nonEmptyAnswer = answer else {
+				return nil
+			}
+			return .init(answer: nonEmptyAnswer, to: question)
+		}
+
+		public init(
+			keyDerivationScheme: SecurityQuestionsFactorSource.KeyDerivationScheme,
+			question: SecurityQuestion,
+			isLast: Bool
+		) {
+			self.keyDerivationScheme = keyDerivationScheme
+			self.question = question
+			self.isLast = isLast
+		}
+	}
+
+	public enum ViewAction: Sendable, Hashable {
+		case submitAnswer(SecurityQuestionAnswerAsEntropy)
+		case answerChanged(String)
+	}
+
+	public enum DelegateAction: Sendable, Hashable {
+		case answered(AnswerToSecurityQuestion)
+	}
+
+	public init() {}
+
+	public func reduce(into state: inout State, viewAction: ViewAction) -> Effect<Action> {
+		switch viewAction {
+		case let .answerChanged(answer):
+			state.answer = NonEmpty(answer)
+			return .none
+
+		case let .submitAnswer(answer):
+			let answerToQuestion = AnswerToSecurityQuestion(
+				answer: answer,
+				to: state.question
+			)
+			state.answerToQuestion = answerToQuestion
+			return .send(.delegate(.answered(answerToQuestion)))
+		}
+	}
+}
