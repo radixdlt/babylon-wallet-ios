22ea26fb8f1524bee9570dc94358cd151ff18f87
Alexander Cyon <alex.cyon@rdx.works>
2023-10-15 15:14:09 +0200

    wip

---
+++ RadixWallet/Features/CreateAccount/Children/NameAccount/NameAccount+View.swift
@@ -0,0 +1,147 @@
+import ComposableArchitecture
+import SwiftUI
+extension NameAccount.State {
+	var viewState: NameAccount.ViewState {
+		.init(state: self)
+	}
+}
+
+extension NameAccount {
+	public struct ViewState: Equatable {
+		public let namePlaceholder: String
+		public let titleText: String
+		public let subtitleText: String
+		public let entityName: String
+		public let sanitizedNameRequirement: SanitizedNameRequirement?
+		public let hint: Hint?
+		public let useLedgerAsFactorSource: Bool
+
+		public struct SanitizedNameRequirement: Equatable {
+			public let sanitizedName: NonEmptyString
+		}
+
+		init(state: State) {
+			self.namePlaceholder = L10n.CreateAccount.NameNewAccount.placeholder
+			self.titleText = state.isFirst ? L10n.CreateAccount.titleFirst : L10n.CreateAccount.titleNotFirst
+			self.subtitleText = L10n.CreateAccount.NameNewAccount.subtitle
+
+			self.useLedgerAsFactorSource = state.useLedgerAsFactorSource
+			self.entityName = state.inputtedName
+			if let sanitizedName = state.sanitizedName {
+				if sanitizedName.count > Profile.Network.Account.nameMaxLength {
+					self.sanitizedNameRequirement = nil
+					self.hint = .error("Account label too long") // FIXME: Strings (duplicate)
+				} else {
+					self.sanitizedNameRequirement = .init(sanitizedName: sanitizedName)
+					self.hint = nil
+				}
+			} else {
+				self.sanitizedNameRequirement = nil
+				self.hint = nil
+			}
+		}
+	}
+
+	@MainActor
+	public struct View: SwiftUI.View {
+		private let store: StoreOf<NameAccount>
+
+		public init(store: StoreOf<NameAccount>) {
+			self.store = store
+		}
+
+		public var body: some SwiftUI.View {
+			WithViewStore(store, observe: \.viewState, send: { .view($0) }) { viewStore in
+				ScrollView {
+					VStack(spacing: 0) {
+						title(with: viewStore.state)
+							.padding(.bottom, .medium1)
+
+						subtitle(with: viewStore.state)
+							.padding(.bottom, .large1)
+
+						let nameBinding = viewStore.binding(
+							get: \.entityName,
+							send: { .textFieldChanged($0) }
+						)
+
+						AppTextField(
+							placeholder: viewStore.namePlaceholder,
+							text: nameBinding,
+							hint: viewStore.hint
+						)
+						.keyboardType(.asciiCapable)
+						.autocorrectionDisabled()
+						.padding(.bottom, .medium3)
+
+						useLedgerAsFactorSource(with: viewStore)
+					}
+					.padding([.bottom, .horizontal], .medium1)
+				}
+				#if os(iOS)
+				.toolbar(.visible, for: .navigationBar)
+				#endif
+				.footer {
+					WithControlRequirements(
+						viewStore.sanitizedNameRequirement,
+						forAction: { viewStore.send(.confirmNameButtonTapped($0.sanitizedName)) }
+					) { action in
+						Button(L10n.CreateAccount.NameNewAccount.continue, action: action)
+							.buttonStyle(.primaryRectangular)
+					}
+				}
+			}
+		}
+	}
+}
+
+extension NameAccount.View {
+	private func title(with viewState: NameAccount.ViewState) -> some View {
+		Text(viewState.titleText)
+			.foregroundColor(.app.gray1)
+			.textStyle(.sheetTitle)
+	}
+
+	private func subtitle(with viewState: NameAccount.ViewState) -> some View {
+		VStack {
+			Text(viewState.subtitleText)
+				.fixedSize(horizontal: false, vertical: true)
+				.padding(.horizontal, .large1)
+				.multilineTextAlignment(.center)
+				.foregroundColor(.app.gray1)
+				.textStyle(.body1Regular)
+
+			Text(L10n.CreateAccount.NameNewAccount.explanation)
+				.foregroundColor(.app.gray2)
+				.textStyle(.body1Regular)
+		}
+	}
+
+	private func useLedgerAsFactorSource(
+		with viewStore: ViewStoreOf<NameAccount>
+	) -> some SwiftUI.View {
+		ToggleView(
+			title: L10n.CreateEntity.NameNewEntity.ledgerTitle,
+			subtitle: L10n.CreateEntity.NameNewEntity.ledgerSubtitle,
+			isOn: viewStore.binding(
+				get: \.useLedgerAsFactorSource,
+				send: { .useLedgerAsFactorSourceToggled($0) }
+			)
+		)
+	}
+}
+
+// #if DEBUG
+// import SwiftUI
+import ComposableArchitecture //
+// struct NameAccount_Previews: PreviewProvider {
+//	static var previews: some View {
+//		NameAccount.View(
+//			store: .init(
+//				initialState: .init(isFirst: true),
+//				reducer: NameNewEntity.init
+//			)
+//		)
+//	}
+// }
+// #endif
---
+++ RadixWallet/Features/CreateAccount/Coordinator/CreateAccountCoordinator+View.swift
@@ -0,0 +1,88 @@
+import ComposableArchitecture
+import SwiftUI
+extension CreateAccountCoordinator.State {
+	fileprivate var viewState: CreateAccountCoordinator.ViewState {
+		.init(shouldDisplayNavBar: shouldDisplayNavBar)
+	}
+}
+
+extension CreateAccountCoordinator {
+	public struct ViewState: Sendable, Equatable {
+		let shouldDisplayNavBar: Bool
+	}
+
+	@MainActor
+	public struct View: SwiftUI.View {
+		private let store: StoreOf<CreateAccountCoordinator>
+
+		public init(store: StoreOf<CreateAccountCoordinator>) {
+			self.store = store
+		}
+
+		public var body: some SwiftUI.View {
+			WithViewStore(store, observe: \.viewState, send: { .view($0) }) { viewStore in
+				NavigationStackStore(
+					store.scope(state: \.path, action: { .child(.path($0)) })
+				) {
+					IfLetStore(
+						store.scope(state: \.root, action: { .child(.root($0)) })
+					) {
+						destination(for: $0, shouldDisplayNavBar: viewStore.shouldDisplayNavBar)
+						#if os(iOS)
+							.toolbar {
+								if viewStore.shouldDisplayNavBar {
+									ToolbarItem(placement: .primaryAction) {
+										CloseButton {
+											store.send(.view(.closeButtonTapped))
+										}
+									}
+								}
+							}
+						#endif
+					}
+					// This is required to disable the animation of internal components during transition
+					.transaction { $0.animation = nil }
+				} destination: {
+					destination(for: $0, shouldDisplayNavBar: viewStore.shouldDisplayNavBar)
+				}
+				#if os(iOS)
+				.navigationTransition(.slide, interactivity: .disabled)
+				#endif // iOS
+			}
+		}
+
+		private func destination(
+			for store: StoreOf<CreateAccountCoordinator.Destinations>,
+			shouldDisplayNavBar: Bool
+		) -> some SwiftUI.View {
+			ZStack {
+				SwitchStore(store) { state in
+					switch state {
+					case .step1_nameAccount:
+						CaseLet(
+							/CreateAccountCoordinator.Destinations.State.step1_nameAccount,
+							action: CreateAccountCoordinator.Destinations.Action.step1_nameAccount,
+							then: { NameAccount.View(store: $0) }
+						)
+					case .step2_creationOfAccount:
+						CaseLet(
+							/CreateAccountCoordinator.Destinations.State.step2_creationOfAccount,
+							action: CreateAccountCoordinator.Destinations.Action.step2_creationOfAccount,
+							then: { CreationOfAccount.View(store: $0) }
+						)
+					case .step3_completion:
+						CaseLet(
+							/CreateAccountCoordinator.Destinations.State.step3_completion,
+							action: CreateAccountCoordinator.Destinations.Action.step3_completion,
+							then: { NewAccountCompletion.View(store: $0) }
+						)
+					}
+				}
+			}
+			#if os(iOS)
+			.navigationBarBackButtonHidden(!shouldDisplayNavBar)
+			.navigationBarHidden(!shouldDisplayNavBar)
+			#endif // iOS
+		}
+	}
+}
