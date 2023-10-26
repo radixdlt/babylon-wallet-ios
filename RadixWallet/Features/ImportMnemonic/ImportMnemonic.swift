eb36a2e2d040a3bc10831da8f5682d4e704f76e8
Alexander Cyon <alex.cyon@rdx.works>
2023-10-26 12:44:21 +0200

    fix issue with 3 letter bip39 word when losing focus

--- RadixWallet/Features/ImportMnemonic/ImportMnemonic.swift
+++ RadixWallet/Features/ImportMnemonic/ImportMnemonic.swift
@@ -310,7 +310,15 @@ public struct ImportMnemonic: Sendable, FeatureReducer {
 
 		case let .word(id, child: .delegate(.lostFocus(displayText))):
 			switch lookup(input: displayText, state) {
-			case .known(.ambigous), .unknown(.notInList):
+			case let .known(.ambigous(candidates, input)):
+				if let exactMatch = candidates.first(where: { $0.word == input }) {
+					state.words[id: id]?.value = .complete(text: displayText, word: exactMatch, completion: ImportMnemonicWord.State.WordValue.Completion.auto(match: .exact))
+				} else {
+					state.words[id: id]?.value = .incomplete(text: displayText, hasFailedValidation: true)
+				}
+				return .none
+
+			case .unknown(.notInList):
 				state.words[id: id]?.value = .incomplete(text: displayText, hasFailedValidation: true)
 				return .none
 
