bbc3947791fcfcd0e52de474f468431ac836f387
Ghenadie Vasiliev-Pusca <ghenadie.vasiliev-pusca@rdx.works>
2023-10-27 10:14:44 +0300

    wip

--- RadixWalletTests/ProfileTests/TestCases/SnapshotTests/SnapshotJSONTests.swift
+++ RadixWalletTests/ProfileTests/TestCases/SnapshotTests/SnapshotJSONTests.swift
@@ -5,7 +5,7 @@ import XCTest
 
 // MARK: - SnapshotJSONTests
 final class SnapshotJSONTests: TestCase {
-	func omit_test_generate() throws {
+	func test_generate() throws {
 		let plaintextSnapshot: ProfileSnapshot = try readTestFixture(
 			bundle: Bundle(for: Self.self),
 			// This Profile has been built using the PROD version of app, version `1.0.0 (5)`
