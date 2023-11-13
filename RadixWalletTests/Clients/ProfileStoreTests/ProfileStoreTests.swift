e9c3fc56e7d516bbc1a6316f3ab77960a3a5ca24
Alexander Cyon <alex.cyon@rdx.works>
2023-11-13 09:31:56 +0100

    WIP

--- RadixWalletTests/Clients/ProfileStoreTests/ProfileStoreTests.swift
+++ RadixWalletTests/Clients/ProfileStoreTests/ProfileStoreTests.swift
@@ -380,96 +380,96 @@ final class ProfileStoreNewProfileTests: TestCase {
 			)
 		}
 	}
+
+	func test__GIVEN__no_profile__WHEN__init__THEN__profile_lastUsedOnDevice_equals_creatingDevice() async throws {
+		try await withTimeLimit {
+			let profile = await withTestClients {
+				// GIVEN no profile
+				$0.noProfile()
+			} operation: {
+				// WHEN ProfileStore.init()
+				await ProfileStore.init().profile
+			}
+
+			// THEN profile lastUsedOnDevice == creatingDevice
+			XCTAssertNoDifference(
+				profile.header.lastUsedOnDevice,
+				profile.header.creatingDevice
+			)
+		}
+	}
+
+	func test__GIVEN__no_profile__WHEN__init__THEN__profile_id_not_equals_creatingDeviceID() async throws {
+		try await withTimeLimit {
+			let profile = await withTestClients {
+				// GIVEN no profile
+				$0.noProfile()
+			} operation: {
+				// WHEN ProfileStore.init()
+				await ProfileStore.init().profile
+			}
+
+			// THEN profile.id != profile.creatingDevice.id
+			XCTAssertNotEqual(
+				profile.header.id,
+				profile.header.creatingDevice.id
+			)
+		}
+	}
+
+	func test__GIVEN__no_profile__WHEN__init__THEN__newly_generated_DeviceFactorSource_is_persisted() throws {
+		withTestClients {
+			// GIVEN no profile
+			$0.noProfile()
+
+			$0.mnemonicClient.generate = { _, _ in
+				"zoo zoo zoo zoo zoo zoo zoo zoo zoo zoo zoo wrong"
+			}
+
+			then(&$0)
+		} operation: {
+			// WHEN ProfileStore.init()
+			ProfileStore.init()
+		}
+
+		func then(_ d: inout DependencyValues) {
+			d.secureStorageClient.saveMnemonicForFactorSource = {
+				XCTAssertNoDifference(
+					$0.factorSource.id.description,
+					// THEN profile uses newly generated DeviceFactorSource
+					"device:09a501e4fafc7389202a82a3237a405ed191cdb8a4010124ff8e2c9259af1327"
+				)
+			}
+		}
+	}
+
+	func test__GIVEN__no_profile__WHEN__init__THEN__newly_generated_mnemonic_is_persisted() throws {
+		let mnemonic: Mnemonic = "zoo zoo zoo zoo zoo zoo zoo zoo zoo zoo zoo wrong"
+		withTestClients {
+			// GIVEN no profile
+			$0.noProfile()
+
+			$0.mnemonicClient.generate = { _, _ in
+				mnemonic
+			}
+
+			then(&$0)
+		} operation: {
+			// WHEN ProfileStore.init()
+			ProfileStore.init()
+		}
+
+		func then(_ d: inout DependencyValues) {
+			d.secureStorageClient.saveMnemonicForFactorSource = {
+				XCTAssertNoDifference(
+					$0.mnemonicWithPassphrase.mnemonic,
+					// THEN profile uses newly generated mnemonic
+					mnemonic
+				)
+			}
+		}
+	}
 	/*
-	 func test__GIVEN__no_profile__WHEN__init__THEN__profile_lastUsedOnDevice_equals_creatingDevice() async throws {
-	 	try await withTimeLimit {
-	 		let profile = await withTestClients {
-	 			// GIVEN no profile
-	 			$0.noProfile()
-	 		} operation: {
-	 			// WHEN ProfileStore.init()
-	 			await ProfileStore.init().profile
-	 		}
-
-	 		// THEN profile lastUsedOnDevice == creatingDevice
-	 		XCTAssertNoDifference(
-	 			profile.header.lastUsedOnDevice,
-	 			profile.header.creatingDevice
-	 		)
-	 	}
-	 }
-
-	 func test__GIVEN__no_profile__WHEN__init__THEN__profile_id_not_equals_creatingDeviceID() async throws {
-	 	try await withTimeLimit {
-	 		let profile = await withTestClients {
-	 			// GIVEN no profile
-	 			$0.noProfile()
-	 		} operation: {
-	 			// WHEN ProfileStore.init()
-	 			await ProfileStore.init().profile
-	 		}
-
-	 		// THEN profile.id != profile.creatingDevice.id
-	 		XCTAssertNotEqual(
-	 			profile.header.id,
-	 			profile.header.creatingDevice.id
-	 		)
-	 	}
-	 }
-
-	 func test__GIVEN__no_profile__WHEN__init__THEN__newly_generated_DeviceFactorSource_is_persisted() throws {
-	 	withTestClients {
-	 		// GIVEN no profile
-	 		$0.noProfile()
-
-	 		$0.mnemonicClient.generate = { _, _ in
-	 			"zoo zoo zoo zoo zoo zoo zoo zoo zoo zoo zoo wrong"
-	 		}
-
-	 		then(&$0)
-	 	} operation: {
-	 		// WHEN ProfileStore.init()
-	 		ProfileStore.init()
-	 	}
-
-	 	func then(_ d: inout DependencyValues) {
-	 		d.secureStorageClient.saveMnemonicForFactorSource = {
-	 			XCTAssertNoDifference(
-	 				$0.factorSource.id.description,
-	 				// THEN profile uses newly generated DeviceFactorSource
-	 				"device:09a501e4fafc7389202a82a3237a405ed191cdb8a4010124ff8e2c9259af1327"
-	 			)
-	 		}
-	 	}
-	 }
-
-	 func test__GIVEN__no_profile__WHEN__init__THEN__newly_generated_mnemonic_is_persisted() throws {
-	 	let mnemonic: Mnemonic = "zoo zoo zoo zoo zoo zoo zoo zoo zoo zoo zoo wrong"
-	 	withTestClients {
-	 		// GIVEN no profile
-	 		$0.noProfile()
-
-	 		$0.mnemonicClient.generate = { _, _ in
-	 			mnemonic
-	 		}
-
-	 		then(&$0)
-	 	} operation: {
-	 		// WHEN ProfileStore.init()
-	 		ProfileStore.init()
-	 	}
-
-	 	func then(_ d: inout DependencyValues) {
-	 		d.secureStorageClient.saveMnemonicForFactorSource = {
-	 			XCTAssertNoDifference(
-	 				$0.mnemonicWithPassphrase.mnemonic,
-	 				// THEN profile uses newly generated mnemonic
-	 				mnemonic
-	 			)
-	 		}
-	 	}
-	 }
-
 	 func test__GIVEN__no_profile__WHEN__init__THEN__newly_generated_mnemonic_is_used_with_empty_passphrase() throws {
 	 	withTestClients {
 	 		// GIVEN no profile
