95ab69ff9c0c57057a0d2d712055ec0c7c57e192
Alexander Cyon <alex.cyon@rdx.works>
2023-11-20 15:00:34 +0100

    L10nify strings in ConfirmSkippingBDFS

--- RadixWallet/Features/ProfileBackupsFeature/RestoreProfileFromBackup/Children/ImportSeedPhrasesFlow/ConfirmSkippingBDFS/ConfirmSkippingBDFS+View.swift
+++ RadixWallet/Features/ProfileBackupsFeature/RestoreProfileFromBackup/Children/ImportSeedPhrasesFlow/ConfirmSkippingBDFS/ConfirmSkippingBDFS+View.swift
@@ -21,13 +21,13 @@ extension ConfirmSkippingBDFS {
 		public var body: some SwiftUI.View {
 			WithViewStore(store, observe: \.viewState, send: { .view($0) }) { viewStore in
 				VStack(spacing: .medium2) {
-					// FIXME: Strings
-					Text("No Main Seed Phrase?")
+					Text(L10n.RecoverSeedPhrase.Header.titleNoMainSeedPhrase)
 						.textStyle(.sheetTitle)
 						.padding(.horizontal, -.small2)
 
 					ScrollView(.vertical, showsIndicators: true) {
-						Text("The Radix Wallet always uses a single main “Babylon” seed phrase to generate new Personas and new Accounts (when not using a Ledger device).\n\nIf you do not have access to your previous main seed phrase, you can skip entering it for now. The Radix Wallet will create a new one, which will be used for new Personas and Accounts.\n\nYour old Accounts and Personas will still be listed, but you will have to enter their original seed phrase to use them. Alternatively, you can hide them if you no longer are interested in using them.")
+						// we use `.init` to preserve bold attribute of string.
+						Text(.init(L10n.RecoverSeedPhrase.Header.subtitleNoMainSeedPhrase))
 							.textStyle(.body1Regular)
 							.foregroundColor(.app.gray1)
 							.multilineTextAlignment(.leading)
@@ -40,8 +40,7 @@ extension ConfirmSkippingBDFS {
 						}
 					}
 
-					// FIXME: Strings
-					Button("Skip Main Seed Phrase Entry") {
+					Button(L10n.RecoverSeedPhrase.skipMainSeedPhraseButton) {
 						store.send(.view(.confirmTapped))
 					}
 					.buttonStyle(.primaryRectangular)
