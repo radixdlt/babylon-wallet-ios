d9cfb981c1541cbc91f5e66cc4330d6b98e8b1a9
kugel3 <gustaf.kugelberg@rdx.works>
2023-11-28 14:14:55 +0100

    wip

--- RadixWallet/Features/SettingsFeature/ManualAccountRecoveryScan/ManualAccountRecoveryScanCoordinator+Reducer.swift
+++ RadixWallet/Features/SettingsFeature/ManualAccountRecoveryScan/ManualAccountRecoveryScanCoordinator+Reducer.swift
@@ -1,8 +1,8 @@
 import ComposableArchitecture
 import SwiftUI
 
-// MARK: - ManualAccountRecoveryScanCoordinator
-public struct ManualAccountRecoveryScanCoordinator: Sendable, FeatureReducer {
+// MARK: - ManualAccountRecoverySeedPhraseCoordinator
+public struct ManualAccountRecoverySeedPhraseCoordinator: Sendable, FeatureReducer {
 	public typealias Store = StoreOf<Self>
 
 	public struct State: Sendable, Hashable {
@@ -11,22 +11,18 @@ public struct ManualAccountRecoveryScanCoordinator: Sendable, FeatureReducer {
 
 	public struct Path: Sendable, Hashable, Reducer {
 		public enum State: Sendable, Hashable {
-			case chooseSeedPhrase(ChooseSeedPhrase.State)
-			case chooseLedger(ChooseLedger.State)
+//			case chooseSeedPhrase(ChooseSeedPhrase.State)
 		}
 
 		public enum Action: Sendable, Equatable {
-			case chooseSeedPhrase(ChooseSeedPhrase.Action)
-			case chooseLedger(ChooseLedger.Action)
+//			case chooseSeedPhrase(ChooseSeedPhrase.Action)
 		}
 
 		public var body: some ReducerOf<Self> {
-			Scope(state: /State.chooseLedger, action: /Action.chooseLedger) {
-				ChooseLedger()
-			}
-			Scope(state: /State.chooseSeedPhrase, action: /Action.chooseSeedPhrase) {
-				ChooseSeedPhrase()
-			}
+			EmptyReducer()
+//			Scope(state: /State.chooseSeedPhrase, action: /Action.chooseSeedPhrase) {
+//				ChooseSeedPhrase()
+//			}
 		}
 	}
 
@@ -56,16 +52,53 @@ public struct ManualAccountRecoveryScanCoordinator: Sendable, FeatureReducer {
 	}
 }
 
-extension ManualAccountRecoveryScanCoordinator {
-	public struct ChooseLedger: Sendable, FeatureReducer {
-		public typealias Store = StoreOf<Self>
+// MARK: - ManualAccountRecoveryLedgerCoordinator
+public struct ManualAccountRecoveryLedgerCoordinator: Sendable, FeatureReducer {
+	public typealias Store = StoreOf<Self>
 
-		public struct State: Sendable, Hashable {}
+	public struct State: Sendable, Hashable {
+		public var path: StackState<Path.State>
 	}
 
-	public struct ChooseSeedPhrase: Sendable, FeatureReducer {
-		public typealias Store = StoreOf<Self>
+	public struct Path: Sendable, Hashable, Reducer {
+		public enum State: Sendable, Hashable {
+//			case chooseSeedPhrase(ChooseSeedPhrase.State)
+		}
 
-		public struct State: Sendable, Hashable {}
+		public enum Action: Sendable, Equatable {
+//			case chooseSeedPhrase(ChooseSeedPhrase.Action)
+		}
+
+		public var body: some ReducerOf<Self> {
+			EmptyReducer()
+//			Scope(state: /State.chooseSeedPhrase, action: /Action.chooseSeedPhrase) {
+//				ChooseSeedPhrase()
+//			}
+		}
+	}
+
+	public enum ViewAction: Sendable, Equatable {
+		case appeared
+	}
+
+	public enum ChildAction: Sendable, Equatable {
+		case path(StackActionOf<Path>)
+	}
+
+	public func reduce(into state: inout State, childAction: ChildAction) -> Effect<Action> {
+		switch childAction {
+		case let .path(.element(id: id, action: pathAction)):
+			reduce(into: &state, id: id, pathAction: pathAction)
+		case let .path(.popFrom(id: id)):
+			.none
+		case let .path(.push(id: id, state: pathState)):
+			.none
+		}
+	}
+
+	public func reduce(into state: inout State, id: StackElementID, pathAction: Path.Action) -> Effect<Action> {
+//		switch pathAction {
+//		}
+		.none
 	}
 }
--- RadixWallet/Features/SettingsFeature/ManualAccountRecoveryScan/ManualAccountRecoveryScanCoordinator+View.swift
+++ RadixWallet/Features/SettingsFeature/ManualAccountRecoveryScan/ManualAccountRecoveryScanCoordinator+View.swift
@@ -1,8 +1,8 @@
 import ComposableArchitecture
 import SwiftUI
 
-// MARK: - ManualAccountRecoveryScanCoordinator.View
-extension ManualAccountRecoveryScanCoordinator {
+// MARK: - ManualAccountRecoverySeedPhraseCoordinator.View
+extension ManualAccountRecoverySeedPhraseCoordinator {
 	@MainActor
 	public struct View: SwiftUI.View {
 		private let store: Store
@@ -13,7 +13,7 @@ extension ManualAccountRecoveryScanCoordinator {
 	}
 }
 
-extension ManualAccountRecoveryScanCoordinator.View {
+extension ManualAccountRecoverySeedPhraseCoordinator.View {
 	public var body: some View {
 		NavigationStackStore(
 			store.scope(state: \.path, action: { .child(.path($0)) })
@@ -25,82 +25,80 @@ extension ManualAccountRecoveryScanCoordinator.View {
 	}
 }
 
-private extension ManualAccountRecoveryScanCoordinator.View {
-	struct RootView: View {
-		let store: StoreOf<ManualAccountRecoveryScanCoordinator.Path>
-
-		var body: some View {
-			SwitchStore(store) { state in
-				switch state {
-				case .chooseSeedPhrase:
-					CaseLet(
-						/ManualAccountRecoveryScanCoordinator.Path.State.chooseSeedPhrase,
-						action: ManualAccountRecoveryScanCoordinator.Path.Action.chooseSeedPhrase,
-						then: { ManualAccountRecoveryScanCoordinator.ChooseSeedPhrase.View(store: $0) }
-					)
-				case .chooseLedger:
-					CaseLet(
-						/ManualAccountRecoveryScanCoordinator.Path.State.chooseLedger,
-						action: ManualAccountRecoveryScanCoordinator.Path.Action.chooseLedger,
-						then: { ManualAccountRecoveryScanCoordinator.ChooseLedger.View(store: $0) }
-					)
-				}
-			}
-		}
-	}
-
+// MARK: - ManualAccountRecoverySeedPhraseCoordinator.View.PathView
+private extension ManualAccountRecoverySeedPhraseCoordinator.View {
 	struct PathView: View {
-		let store: StoreOf<ManualAccountRecoveryScanCoordinator.Path>
+		let store: StoreOf<ManualAccountRecoverySeedPhraseCoordinator.Path>
 
 		var body: some View {
-			SwitchStore(store) { state in
-				switch state {
-				case .chooseSeedPhrase:
-					CaseLet(
-						/ManualAccountRecoveryScanCoordinator.Path.State.chooseSeedPhrase,
-						action: ManualAccountRecoveryScanCoordinator.Path.Action.chooseSeedPhrase,
-						then: { ManualAccountRecoveryScanCoordinator.ChooseSeedPhrase.View(store: $0) }
-					)
-				case .chooseLedger:
-					CaseLet(
-						/ManualAccountRecoveryScanCoordinator.Path.State.chooseLedger,
-						action: ManualAccountRecoveryScanCoordinator.Path.Action.chooseLedger,
-						then: { ManualAccountRecoveryScanCoordinator.ChooseLedger.View(store: $0) }
-					)
-				}
-			}
-		}
-	}
-}
-
-// MARK: - ManualAccountRecoveryScanCoordinator.ChooseLedger.View
-extension ManualAccountRecoveryScanCoordinator.ChooseLedger {
-	@MainActor
-	public struct View: SwiftUI.View {
-		private let store: Store
-
-		public init(store: Store) {
-			self.store = store
-		}
-
-		public var body: some SwiftUI.View {
-			Color.blue
-		}
-	}
-}
-
-// MARK: - ManualAccountRecoveryScanCoordinator.ChooseSeedPhrase.View
-extension ManualAccountRecoveryScanCoordinator.ChooseSeedPhrase {
-	@MainActor
-	public struct View: SwiftUI.View {
-		private let store: Store
-
-		public init(store: Store) {
-			self.store = store
-		}
-
-		public var body: some SwiftUI.View {
 			Color.red
+//			SwitchStore(store) { state in
+//				switch state {
+//				case .chooseSeedPhrase:
+//					CaseLet(
+//						/ManualAccountRecoveryScanCoordinator.Path.State.chooseSeedPhrase,
+//						action: ManualAccountRecoveryScanCoordinator.Path.Action.chooseSeedPhrase,
+//						then: { ManualAccountRecoveryScanCoordinator.ChooseSeedPhrase.View(store: $0) }
+//					)
+//				case .chooseLedger:
+//					CaseLet(
+//						/ManualAccountRecoveryScanCoordinator.Path.State.chooseLedger,
+//						action: ManualAccountRecoveryScanCoordinator.Path.Action.chooseLedger,
+//						then: { ManualAccountRecoveryScanCoordinator.ChooseLedger.View(store: $0) }
+//					)
+//				}
+//			}
+		}
+	}
+}
+
+// MARK: - ManualAccountRecoveryLedgerCoordinator.View
+extension ManualAccountRecoveryLedgerCoordinator {
+	@MainActor
+	public struct View: SwiftUI.View {
+		private let store: Store
+
+		public init(store: Store) {
+			self.store = store
+		}
+	}
+}
+
+extension ManualAccountRecoveryLedgerCoordinator.View {
+	public var body: some View {
+		NavigationStackStore(
+			store.scope(state: \.path, action: { .child(.path($0)) })
+		) {
+			Color.orange
+		} destination: {
+			PathView(store: $0)
+		}
+	}
+}
+
+// MARK: - ManualAccountRecoveryLedgerCoordinator.View.PathView
+private extension ManualAccountRecoveryLedgerCoordinator.View {
+	struct PathView: View {
+		let store: StoreOf<ManualAccountRecoveryLedgerCoordinator.Path>
+
+		var body: some View {
+			Color.red
+//			SwitchStore(store) { state in
+//				switch state {
+//				case .chooseSeedPhrase:
+//					CaseLet(
+//						/ManualAccountRecoveryScanCoordinator.Path.State.chooseSeedPhrase,
+//						action: ManualAccountRecoveryScanCoordinator.Path.Action.chooseSeedPhrase,
+//						then: { ManualAccountRecoveryScanCoordinator.ChooseSeedPhrase.View(store: $0) }
+//					)
+//				case .chooseLedger:
+//					CaseLet(
+//						/ManualAccountRecoveryScanCoordinator.Path.State.chooseLedger,
+//						action: ManualAccountRecoveryScanCoordinator.Path.Action.chooseLedger,
+//						then: { ManualAccountRecoveryScanCoordinator.ChooseLedger.View(store: $0) }
+//					)
+//				}
+//			}
 		}
 	}
 }
