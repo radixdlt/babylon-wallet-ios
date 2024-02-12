3b4f6a4969be0ce618ee8cb47e61ad41a5ec52dd
Alexander Cyon <alex.cyon@rdx.works>
2024-02-12 20:13:18 +0100

    WIP

--- RadixWallet/Clients/ROLAClient/ROLAClient+Live.swift
+++ RadixWallet/Clients/ROLAClient/ROLAClient+Live.swift
@@ -145,32 +145,6 @@ extension ROLAClient {
 	}
 }
 
-extension RETPublicKeyHash {
-	public struct InvalidPublicKeyHashLength: Error {
-		public let got: Int
-		public let expected: Int
-	}
-
-	static let hashLength = 29
-
-	public init(hashing publicKey: SLIP10.PublicKey) throws {
-		let hashBytes = try Sargon.hash(data: publicKey.compressedData).suffix(Self.hashLength)
-
-		guard
-			hashBytes.count == Self.hashLength
-		else {
-			throw InvalidPublicKeyHashLength(got: hashBytes.count, expected: Self.hashLength)
-		}
-
-		switch publicKey {
-		case .ecdsaSecp256k1:
-			self = .secp256k1(value: hashBytes)
-		case .eddsaEd25519:
-			self = .ed25519(value: hashBytes)
-		}
-	}
-}
-
 extension OnLedgerEntity.Metadata {
 	public func ownerKeyHashes() throws -> [RETPublicKeyHash]? {
 		try ownerKeys?.value.map { hash in
