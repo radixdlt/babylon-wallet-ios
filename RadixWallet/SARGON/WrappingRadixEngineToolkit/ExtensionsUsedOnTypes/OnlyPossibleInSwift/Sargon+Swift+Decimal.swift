930a4550be30788d5a16c3bbc154973379396780
Alexander Cyon <alex.cyon@rdx.works>
2024-02-12 14:34:57 +0100

    WIP

--- RadixWallet/SARGON/WrappingRadixEngineToolkit/ExtensionsUsedOnTypes/OnlyPossibleInSwift/Sargon+Swift+Decimal.swift
+++ RadixWallet/SARGON/WrappingRadixEngineToolkit/ExtensionsUsedOnTypes/OnlyPossibleInSwift/Sargon+Swift+Decimal.swift
@@ -13,3 +13,53 @@ extension RETDecimal: ExpressibleByFloatLiteral {
 		panic()
 	}
 }
+
+extension RETDecimal {
+	public static func + (lhs: RETDecimal, rhs: RETDecimal) -> RETDecimal {
+		try! lhs.add(other: rhs)
+	}
+
+	public static func += (lhs: inout RETDecimal, rhs: RETDecimal) {
+		lhs = lhs + rhs
+	}
+
+	public static func - (lhs: RETDecimal, rhs: RETDecimal) -> RETDecimal {
+		try! lhs.sub(other: rhs)
+	}
+
+	public static func -= (lhs: inout RETDecimal, rhs: RETDecimal) {
+		lhs = lhs - rhs
+	}
+
+	public static func * (lhs: RETDecimal, rhs: RETDecimal) -> RETDecimal {
+		try! lhs.mul(other: rhs)
+	}
+
+	public static func *= (lhs: inout RETDecimal, rhs: RETDecimal) {
+		lhs = lhs * rhs
+	}
+
+	public static func / (lhs: RETDecimal, rhs: RETDecimal) -> RETDecimal {
+		try! lhs.div(other: rhs)
+	}
+
+	public static func < (lhs: RETDecimal, rhs: RETDecimal) -> Bool {
+		lhs.lessThan(other: rhs)
+	}
+
+	public static func <= (lhs: RETDecimal, rhs: RETDecimal) -> Bool {
+		lhs.lessThanOrEqual(other: rhs)
+	}
+
+	public static func > (lhs: RETDecimal, rhs: RETDecimal) -> Bool {
+		lhs.greaterThan(other: rhs)
+	}
+
+	public static func >= (lhs: RETDecimal, rhs: RETDecimal) -> Bool {
+		lhs.greaterThanOrEqual(other: rhs)
+	}
+
+	public static prefix func - (value: RETDecimal) -> RETDecimal {
+		.zero - value
+	}
+}
--- RadixWallet/SARGON/WrappingRadixEngineToolkit/LowLevel/RETDecimal+Extensions.swift
+++ RadixWallet/SARGON/WrappingRadixEngineToolkit/LowLevel/RETDecimal+Extensions.swift
@@ -65,54 +65,6 @@ extension RETDecimal {
 	public func greaterThanOrEqual(other: RETDecimal) -> Bool {
 		panic()
 	}
-
-	public static func + (lhs: RETDecimal, rhs: RETDecimal) -> RETDecimal {
-		try! lhs.add(other: rhs)
-	}
-
-	public static func += (lhs: inout RETDecimal, rhs: RETDecimal) {
-		lhs = lhs + rhs
-	}
-
-	public static func - (lhs: RETDecimal, rhs: RETDecimal) -> RETDecimal {
-		try! lhs.sub(other: rhs)
-	}
-
-	public static func -= (lhs: inout RETDecimal, rhs: RETDecimal) {
-		lhs = lhs - rhs
-	}
-
-	public static func * (lhs: RETDecimal, rhs: RETDecimal) -> RETDecimal {
-		try! lhs.mul(other: rhs)
-	}
-
-	public static func *= (lhs: inout RETDecimal, rhs: RETDecimal) {
-		lhs = lhs * rhs
-	}
-
-	public static func / (lhs: RETDecimal, rhs: RETDecimal) -> RETDecimal {
-		try! lhs.div(other: rhs)
-	}
-
-	public static func < (lhs: RETDecimal, rhs: RETDecimal) -> Bool {
-		lhs.lessThan(other: rhs)
-	}
-
-	public static func <= (lhs: RETDecimal, rhs: RETDecimal) -> Bool {
-		lhs.lessThanOrEqual(other: rhs)
-	}
-
-	public static func > (lhs: RETDecimal, rhs: RETDecimal) -> Bool {
-		lhs.greaterThan(other: rhs)
-	}
-
-	public static func >= (lhs: RETDecimal, rhs: RETDecimal) -> Bool {
-		lhs.greaterThanOrEqual(other: rhs)
-	}
-
-	public static prefix func - (value: RETDecimal) -> RETDecimal {
-		.zero - value
-	}
 }
 
 // MARK: Clamping
@@ -153,26 +105,11 @@ extension UInt {
 }
 
 extension RETDecimal {
-	public static let machineReadableDecimalSeparator = "."
-	public static let scale: Int = 18
-
 	/// Parse a local respecting string
 	public init(
 		formattedString: String,
 		locale: Locale = .autoupdatingCurrent
 	) throws {
-		// Pad with a leading zero, to make numbers with leading decimal separator parsable
-		var string = "0" + formattedString
-		// If the locale recognizes a grouping separator, we strip that from the string
-		if let groupingSeparator = locale.groupingSeparator {
-			string.replace(groupingSeparator, with: "")
-		}
-		// If the locale recognizes a decimal separator that is different from the machine readable one, we replace it with that
-		if let decimalSeparator = locale.decimalSeparator, decimalSeparator != RETDecimal.machineReadableDecimalSeparator {
-			string.replace(decimalSeparator, with: RETDecimal.machineReadableDecimalSeparator)
-		}
-
-//		try self.init(value: string)
 		panic()
 	}
 
@@ -182,34 +119,14 @@ extension RETDecimal {
 		totalPlaces: UInt = .defaultMaxPlacesFormatted,
 		useGroupingSeparator: Bool = true
 	) -> String {
-		func format(_ number: RETDecimal) -> String {
-			number.formattedPlain(locale: locale, useGroupingSeparator: useGroupingSeparator)
-		}
-
-		let roundedToTotalPlaces = rounded(totalPlaces: totalPlaces)
-
-		if let multiplier = roundedToTotalPlaces.multiplier() {
-			let scaled = roundedToTotalPlaces / multiplier.value
-			let integerCount = scaled.digits().count - RETDecimal.scale
-			guard integerCount <= totalPlaces else {
-				return formattedEngineeringNotation(locale: locale, totalPlaces: .maxPlacesEngineeringNotation)
-			}
-			return format(scaled) + " " + multiplier.suffix
-		} else {
-			return format(roundedToTotalPlaces)
-		}
+		panic()
 	}
 
 	public func formattedEngineeringNotation(
 		locale: Locale = .autoupdatingCurrent,
 		totalPlaces: UInt = .maxPlacesEngineeringNotation
 	) -> String {
-		let rounded = rounded(totalPlaces: totalPlaces)
-		let integerCount = rounded.digits().count - RETDecimal.scale
-		let exponent = UInt(integerCount - 1)
-		let scaled = rounded / .init(exponent: exponent)
-
-		return scaled.formattedPlain(locale: locale, useGroupingSeparator: false) + "e\(exponent)"
+		panic()
 	}
 
 	/// A human readable, locale respecting string. Does not perform any rounding or truncation.
@@ -217,62 +134,11 @@ extension RETDecimal {
 		locale: Locale = .autoupdatingCurrent,
 		useGroupingSeparator: Bool = true
 	) -> String {
-		guard !isZero() else { return "0" }
-
-		let sign = isNegative() ? "-" : ""
-		let decimalSeparator = locale.decimalSeparator ?? "."
-
-		let digits = digits()
-		let integerCount = digits.count - RETDecimal.scale
-		let trailingZeroCount = digits.trailingZeroCount
-
-		var (integerPart, decimalPart) = digits.split(after: integerCount)
-
-		if integerCount <= 0 {
-			// If we don't have any integers, we just use "0"
-			integerPart = "0"
-		} else if useGroupingSeparator, let groupingSeparator = locale.groupingSeparator {
-			integerPart.insertGroupingSeparatorInInteger(groupingSeparator)
-		}
-
-		if trailingZeroCount >= RETDecimal.scale {
-			// No non-zero decimals, we only have an integerpart
-			return sign + integerPart
-		} else {
-			let zerosToPad = Swift.max(-integerCount, 0)
-			return sign + integerPart + decimalSeparator + .zeros(length: zerosToPad) + decimalPart.dropLast(trailingZeroCount)
-		}
+		panic()
 	}
 
 	/// The digits of the number, without separators or sign. The scale is fixed at 18, meaning the last 18 digits correspond to the decimal part.
 	public func digits() -> String {
-//		do {
-//			return try abs().mantissa()
-//		} catch {
-//			assertionFailure("RETDecimal: Failed to get digits from \(asStr())")
-//			return mantissa().replacingOccurrences(of: "-", with: "")
-//		}
-		panic()
-	}
-
-	/// Rounds to `totalPlaces` digits, counting both the integer and decimal parts, as well as any leading zeros
-	private func rounded(totalPlaces: UInt) -> RETDecimal {
-		let digits = digits()
-		// If we only have decimals, we will still count the 0 before the separator as an integer
-		let integerCount = UInt(Swift.max(digits.count - RETDecimal.scale, 1))
-		if integerCount > totalPlaces {
-			let scale = RETDecimal(exponent: integerCount - totalPlaces)
-			return (self / scale).rounded(decimalPlaces: 0) * scale
-		} else {
-			// The remaining digits are decimals and we keep up to totalPlaces of them
-			let decimalsToKeep = totalPlaces - integerCount
-			return rounded(decimalPlaces: decimalsToKeep)
-		}
-	}
-
-	private func multiplier() -> Multiplier? {
-//		guard let abs = try? abs() else { return nil }
-//		return Multiplier.allCases.last(where: { $0.value <= abs })
 		panic()
 	}
 }
@@ -295,31 +161,5 @@ extension RETDecimal.Multiplier {
 		case .billion: "B"
 		case .trillion: "T"
 		}
-	}
-}
-
-private extension String {
-	func split(after lengthOfFirstPart: Int) -> (String, String) {
-		let lengthOfSecondPart = count - lengthOfFirstPart
-		assert(lengthOfSecondPart >= 0)
-		return (String(dropLast(lengthOfSecondPart)), String(suffix(lengthOfSecondPart)))
-	}
-
-	mutating func insertGroupingSeparatorInInteger(_ separator: String) {
-		let digits = count
-		let separatorCount = (digits - 1) / 3
-		guard separatorCount > 0 else { return }
-		for i in 1 ... separatorCount {
-			let location = index(startIndex, offsetBy: digits - 3 * i)
-			insert(contentsOf: separator, at: location)
-		}
-	}
-
-	static func zeros(length: Int) -> String {
-		String(repeating: "0", count: length)
-	}
-
-	var trailingZeroCount: Int {
-		reversed().enumerated().first { $0.element != "0" }?.offset ?? count
 	}
 }
