d87662c59604235581d1488ad2a84253c345cf96
kugel3 <gustaf.kugelberg@rdx.works>
2024-02-17 22:58:13 +0100

    wip

--- RadixWallet/Prelude/Extensions/Collection+Extra.swift
+++ RadixWallet/Prelude/Extensions/Collection+Extra.swift
@@ -22,6 +22,17 @@ extension Collection {
 	}
 }
 
+extension MutableCollection where Self: RandomAccessCollection {
+	public mutating func sort<Value: Comparable>(
+		by keyPath: KeyPath<Element, Value>,
+		_ comparator: (Value, Value) -> Bool = (<)
+	) {
+		sort {
+			comparator($0[keyPath: keyPath], $1[keyPath: keyPath])
+		}
+	}
+}
+
 extension OrderedSet where Element: Hashable {
 	/// Add or remove the given element
 	public mutating func toggle(_ element: Element) {
