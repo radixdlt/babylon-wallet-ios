f48f118bd88cd6b03ae0162a6d2a0ea61530296f
kugel3 <gustaf.kugelberg@rdx.works>
2024-02-18 20:43:24 +0100

    wip

---
+++ RadixWallet/Clients/TransactionHistoryClient/TransactionHistoryClient+Interface.swift
@@ -0,0 +1,39 @@
+import Foundation
+
+// MARK: - TransactionHistoryClient
+public struct TransactionHistoryClient: Sendable, DependencyKey {
+	public var getTransactionHistory: GetTransactionHistory
+}
+
+// MARK: TransactionHistoryClient.GetTransactionHistory
+extension TransactionHistoryClient {
+	public typealias GetTransactionHistory = @Sendable (_ account: AccountAddress, _ cursor: String?) async throws -> TransactionHistoryResponse
+}
+
+// MARK: - TransactionHistoryResponse
+public struct TransactionHistoryResponse: Sendable, Hashable {
+	public let cursor: String?
+	public let items: [TransactionHistoryItem]
+}
+
+// MARK: - TransactionHistoryItem
+public struct TransactionHistoryItem: Sendable, Hashable {
+	let time: Date
+	let message: String?
+	let actions: [Action]
+	let manifestType: ManifestType
+
+	enum Action: Sendable, Hashable {
+		case deposit(RETDecimal, ResourceBalance)
+		case withdrawal(RETDecimal, ResourceBalance)
+		case settings
+	}
+
+	enum ManifestType {
+		case transfer
+		case contribute
+		case claim
+		case depositSettings
+		case other
+	}
+}
