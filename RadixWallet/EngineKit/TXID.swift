c5844fdce7450ebfdd3c1b2845958753a687b6bc
kugel3 <gustaf.kugelberg@rdx.works>
2024-02-24 00:51:28 +0100

    Refactor address formatting

--- RadixWallet/EngineKit/TXID.swift
+++ RadixWallet/EngineKit/TXID.swift
@@ -3,6 +3,10 @@ import EngineToolkit
 public typealias TXID = TransactionHash
 
 extension TXID {
+	public func formatted(_ format: AddressFormat = .default) -> String {
+		bytes().hex()
+	}
+
 	public var hex: String {
 		bytes().hex()
 	}
