510e497385ff723f5dfff38d3197570e2ba23fd6
kugel3 <gustaf.kugelberg@rdx.works>
2024-02-23 15:52:42 +0100

    wip

--- RadixWallet/Clients/GatewayAPI/CoreAPI/CoreAPI_BinaryPlaintextMessageContent.swift
+++ RadixWallet/Clients/GatewayAPI/CoreAPI/CoreAPI_BinaryPlaintextMessageContent.swift
@@ -2,17 +2,14 @@ import Foundation
 
 extension CoreAPI {
 	public struct BinaryPlaintextMessageContent: Codable, Hashable {
-		public private(set) var type: PlaintextMessageContentType
 		/** The hex-encoded value of a message that the author decided to provide as raw bytes. */
 		public private(set) var valueHex: String
 
-		public init(type: PlaintextMessageContentType, valueHex: String) {
-			self.type = type
+		public init(valueHex: String) {
 			self.valueHex = valueHex
 		}
 
 		public enum CodingKeys: String, CodingKey, CaseIterable {
-			case type
 			case valueHex = "value_hex"
 		}
 	}
