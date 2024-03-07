e5733e8682c1965bb28b6e5564850b7729e8100b
kugel3 <gustaf.kugelberg@rdx.works>
2024-03-07 01:30:31 +0100

    More filter

--- RadixWallet/Core/DesignSystem/Styles/BlueButtonStyle.swift
+++ RadixWallet/Core/DesignSystem/Styles/BlueButtonStyle.swift
@@ -2,12 +2,12 @@ import SwiftUI
 
 // MARK: - BlueButtonStyle
 
-extension ButtonStyle where Self == BlueButtonStyle {
-	public static var blue: BlueButtonStyle { .init() }
+extension ButtonStyle where Self == BlueTextButtonStyle {
+	public static var blueText: BlueTextButtonStyle { .init() }
 }
 
-// MARK: - BlueButtonStyle
-public struct BlueButtonStyle: ButtonStyle {
+// MARK: - BlueTextButtonStyle
+public struct BlueTextButtonStyle: ButtonStyle {
 	public func makeBody(configuration: ButtonStyle.Configuration) -> some View {
 		configuration.label
 			.textStyle(.body1StandaloneLink)
