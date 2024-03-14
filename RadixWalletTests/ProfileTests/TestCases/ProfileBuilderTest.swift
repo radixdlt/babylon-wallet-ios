94101f8b3e94adff3a11ad0d8cd058ec181bdd3f
Alexander Cyon <alex.cyon@rdx.works>
2024-03-14 16:13:00 +0100

    more tests

--- RadixWalletTests/ProfileTests/TestCases/ProfileBuilderTest.swift
+++ RadixWalletTests/ProfileTests/TestCases/ProfileBuilderTest.swift
@@ -5,7 +5,13 @@ import XCTest
 final class ProfileBuilderTest: TestCase {
 	// MOVE ME to some appropriate file.
 	func test_tx_guarantee_default_preset_is_99() throws {
-		try XCTAssertEqual(AppPreferences.Transaction().defaultDepositGuarantee, RETDecimal(value: "0.99"))
+		let expectedPreset = try RETDecimal(value: "0.99")
+		XCTAssertEqual(AppPreferences.Transaction.defaultDepositGuaranteePreset, expectedPreset)
+		XCTAssertEqual(AppPreferences.Transaction().defaultDepositGuarantee, expectedPreset)
+		XCTAssertEqual(AppPreferences().transaction.defaultDepositGuarantee, expectedPreset)
+
+		// assert the initializer of Transaction still uses the passed in value...
+		XCTAssertEqual(AppPreferences.Transaction(defaultDepositGuarantee: 2).defaultDepositGuarantee, 2)
 	}
 
 	func test_profile_builder() throws {
