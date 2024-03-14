5b12d8067c1ee1776927637535fca285c8877f9b
Alexander Cyon <alex.cyon@rdx.works>
2024-03-14 15:53:29 +0100

    Lower PRESET default deposit guarantee to 99% (from 100%), for NEW Profiles.

--- RadixWalletTests/ProfileTests/TestCases/ProfileBuilderTest.swift
+++ RadixWalletTests/ProfileTests/TestCases/ProfileBuilderTest.swift
@@ -3,6 +3,11 @@ import Foundation
 import XCTest
 
 final class ProfileBuilderTest: TestCase {
+	// MOVE ME to some appropriate file.
+	func test_tx_guarantee_default_preset_is_99() throws {
+		try XCTAssertEqual(AppPreferences.Transaction().defaultDepositGuarantee, RETDecimal(value: "0.99"))
+	}
+
 	func test_profile_builder() throws {
 		let profile = ProfileBuilder()
 			.bdfs()
