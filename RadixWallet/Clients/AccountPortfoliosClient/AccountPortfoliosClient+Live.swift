0abfc783753d6f4f88368e047bf852a2038aba2a
Ghenadie Vasiliev-Pusca <ghenadie.vasiliev-pusca@rdx.works>
2024-03-21 16:45:59 +0200

    wip

--- RadixWallet/Clients/AccountPortfoliosClient/AccountPortfoliosClient+Live.swift
+++ RadixWallet/Clients/AccountPortfoliosClient/AccountPortfoliosClient+Live.swift
@@ -65,85 +65,99 @@ extension AccountPortfoliosClient: DependencyKey {
 			}
 		}
 
-		return AccountPortfoliosClient(
-			fetchAccountPortfolios: { accountAddresses, forceRefresh in
-				let gateway = await gatewaysClient.getCurrentGateway()
-				await state.setRadixGateway(gateway)
-				if forceRefresh {
-					for accountAddress in accountAddresses {
-						cacheClient.removeFolder(.onLedgerEntity(.account(accountAddress.asGeneral)))
-					}
-				}
-
-				/// Explicetely load and set the currency target and visibility to make sure
-				/// it is available for usage before resources are loaded
-				let preferences = await appPreferencesClient.getPreferences().display
-				await state.setSelectedCurrency(preferences.fiatCurrencyPriceTarget)
-				await state.setIsCurrencyAmountVisble(preferences.isCurrencyAmountVisible)
-
-				let accounts = try await onLedgerEntitiesClient.getAccounts(accountAddresses).map(\.nonEmptyVaults)
-
-				let portfolios = accounts.map { AccountPortfolio(account: $0) }
-				await state.handlePortfoliosUpdate(portfolios)
-
-				/// Put together all resources from already fetched and new accounts
-				let currentAccounts = state.portfoliosSubject.value.wrappedValue.map { $0.values.map(\.account) } ?? []
-				let allResources: [ResourceAddress] = {
-					if gateway == .mainnet {
-						/// Only Mainnet resources have prices
-						return (currentAccounts + accounts).flatMap(\.resourcesWithPrices) + [.mainnetXRDAddress]
-					} else {
-						#if DEBUG
-						/// Helpful for testing on stokenet
-						return [
-							.mainnetXRDAddress,
-							try! .init(validatingAddress:
-								"resource_rdx1t4tjx4g3qzd98nayqxm7qdpj0a0u8ns6a0jrchq49dyfevgh6u0gj3"
-							),
-							try! .init(validatingAddress:
-								"resource_rdx1t45js47zxtau85v0tlyayerzrgfpmguftlfwfr5fxzu42qtu72tnt0"
-							),
-							try! .init(validatingAddress:
-								"resource_rdx1tk7g72c0uv2g83g3dqtkg6jyjwkre6qnusgjhrtz0cj9u54djgnk3c"
-							),
-							try! .init(validatingAddress:
-								"resource_rdx1tkk83magp3gjyxrpskfsqwkg4g949rmcjee4tu2xmw93ltw2cz94sq"
-							),
-						]
-						#else
-						/// No price for resources on testnets
-						return []
-						#endif
-					}
-				}()
-
-				await applyTokenPrices(Array(allResources), forceRefresh: forceRefresh)
-
-				// Load additional details
-				_ = await accounts.parallelMap {
-					await fetchPoolAndStakeUnitsDetails($0, cachingStrategy: forceRefresh ? .forceUpdate : .useCache)
-				}
-
-				return Array(state.portfoliosSubject.value.wrappedValue!.values)
-			},
-			fetchAccountPortfolio: { accountAddress, forceRefresh in
-				if forceRefresh {
+		@Sendable
+		func fetchAccountPortfolios(_ accountAddresses: [AccountAddress], _ forceResfresh: Bool) async throws -> [AccountPortfolio] {
+			let gateway = await gatewaysClient.getCurrentGateway()
+			await state.setRadixGateway(gateway)
+			if forceRefresh {
+				for accountAddress in accountAddresses {
 					cacheClient.removeFolder(.onLedgerEntity(.account(accountAddress.asGeneral)))
 				}
+			}
 
-				let account = try await onLedgerEntitiesClient.getAccount(accountAddress)
-				let portfolio = AccountPortfolio(account: account)
+			/// Explicetely load and set the currency target and visibility to make sure
+			/// it is available for usage before resources are loaded
+			let preferences = await appPreferencesClient.getPreferences().display
+			await state.setSelectedCurrency(preferences.fiatCurrencyPriceTarget)
+			await state.setIsCurrencyAmountVisble(preferences.isCurrencyAmountVisible)
 
-				let currentResources = await state.tokenPrices.keys
-				await applyTokenPrices(
-					currentResources + account.resourcesWithPrices,
-					forceRefresh: forceRefresh
-				)
+			let accounts = try await onLedgerEntitiesClient.getAccounts(accountAddresses).map(\.nonEmptyVaults)
 
-				await state.handlePortfolioUpdate(portfolio)
-				await fetchPoolAndStakeUnitsDetails(account, cachingStrategy: forceRefresh ? .forceUpdate : .useCache)
+			let portfolios = accounts.map { AccountPortfolio(account: $0) }
+			await state.handlePortfoliosUpdate(portfolios)
 
-				return portfolio
+			/// Put together all resources from already fetched and new accounts
+			let currentAccounts = state.portfoliosSubject.value.wrappedValue.map { $0.values.map(\.account) } ?? []
+			let allResources: [ResourceAddress] = {
+				if gateway == .mainnet {
+					/// Only Mainnet resources have prices
+					return (currentAccounts + accounts).flatMap(\.resourcesWithPrices) + [.mainnetXRDAddress]
+				} else {
+					#if DEBUG
+					/// Helpful for testing on stokenet
+					return [
+						.mainnetXRDAddress,
+						try! .init(validatingAddress:
+							"resource_rdx1t4tjx4g3qzd98nayqxm7qdpj0a0u8ns6a0jrchq49dyfevgh6u0gj3"
+						),
+						try! .init(validatingAddress:
+							"resource_rdx1t45js47zxtau85v0tlyayerzrgfpmguftlfwfr5fxzu42qtu72tnt0"
+						),
+						try! .init(validatingAddress:
+							"resource_rdx1tk7g72c0uv2g83g3dqtkg6jyjwkre6qnusgjhrtz0cj9u54djgnk3c"
+						),
+						try! .init(validatingAddress:
+							"resource_rdx1tkk83magp3gjyxrpskfsqwkg4g949rmcjee4tu2xmw93ltw2cz94sq"
+						),
+					]
+					#else
+					/// No price for resources on testnets
+					return []
+					#endif
+				}
+			}()
+
+			await applyTokenPrices(Array(allResources), forceRefresh: forceRefresh)
+
+			// Load additional details
+			_ = await accounts.parallelMap {
+				await fetchPoolAndStakeUnitsDetails($0, cachingStrategy: forceRefresh ? .forceUpdate : .useCache)
+			}
+
+			return Array(state.portfoliosSubject.value.wrappedValue!.values)
+		}
+
+		@Sendable
+		func fetchAccountPortfolio(_ accountAddress: AccountAddress, _ forceRefresh: Bool) async throws -> AccountPortfolio {
+			if forceRefresh {
+				cacheClient.removeFolder(.onLedgerEntity(.account(accountAddress.asGeneral)))
+			}
+
+			let account = try await onLedgerEntitiesClient.getAccount(accountAddress)
+			let portfolio = AccountPortfolio(account: account)
+
+			let currentResources = await state.tokenPrices.keys
+			await applyTokenPrices(
+				currentResources + account.resourcesWithPrices,
+				forceRefresh: forceRefresh
+			)
+
+			await state.handlePortfolioUpdate(portfolio)
+			await fetchPoolAndStakeUnitsDetails(account, cachingStrategy: forceRefresh ? .forceUpdate : .useCache)
+
+			return portfolio
+		}
+
+		return AccountPortfoliosClient(
+			fetchAccountPortfolios: { accountAddresses, forceRefresh in
+				try await Task.detached {
+					try await fetchAccountPortfolios(accountAddresses, forceRefresh)
+				}.value
+			},
+			fetchAccountPortfolio: { accountAddress, forceRefresh in
+				try await Task.detached {
+					try await fetchAccountPortfolio(accountAddress, forceRefresh)
+				}.value
 			},
 			portfolioUpdates: {
 				state.portfoliosSubject
