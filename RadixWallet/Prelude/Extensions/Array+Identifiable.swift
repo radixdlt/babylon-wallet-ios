f0accd790e506f592849f0d4147c286f5ed53ab9
Ghenadie Vasiliev-Pusca <ghenadie.vasiliev-pusca@rdx.works>
2024-04-08 13:34:14 +0300

    Inform about duplicated elements in DEBUG

--- RadixWallet/Prelude/Extensions/Array+Identifiable.swift
+++ RadixWallet/Prelude/Extensions/Array+Identifiable.swift
@@ -3,7 +3,14 @@ extension Array where Element: Identifiable {
 	/// Returns an `IdentifiedArray` of the `Element`, omitting clashing elements
 	public func asIdentifiable() -> IdentifiedArrayOf<Element> {
 		var array: IdentifiedArrayOf<Element> = []
-		array.append(contentsOf: self)
+		for element in self {
+			let (alreadyExists, _) = array.append(element)
+			if alreadyExists {
+				#if DEBUG
+				assertionFailure("The source array does not contain unique elements, id clash for \(element.id)")
+				#endif
+			}
+		}
 		return array
 	}
 }
