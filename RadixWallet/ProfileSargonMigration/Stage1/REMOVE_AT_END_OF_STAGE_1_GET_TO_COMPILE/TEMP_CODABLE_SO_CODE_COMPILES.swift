cf1c65eb52dc5debc413088dc61f7df3ed4ae6ac
Alexander Cyon <alex.cyon@rdx.works>
2024-04-15 13:12:33 +0200

    WIP

--- RadixWallet/ProfileSargonMigration/Stage1/REMOVE_AT_END_OF_STAGE_1_GET_TO_COMPILE/TEMP_CODABLE_SO_CODE_COMPILES.swift
+++ RadixWallet/ProfileSargonMigration/Stage1/REMOVE_AT_END_OF_STAGE_1_GET_TO_COMPILE/TEMP_CODABLE_SO_CODE_COMPILES.swift
@@ -11,17 +11,6 @@ extension FiatCurrency {
 	}
 }
 
-// MARK: - Gateways + Codable
-extension Gateways: Codable {
-	public init(from decoder: Decoder) throws {
-		sargonProfileREMOVEAtEndOfStage1TEMP()
-	}
-
-	public func encode(to encoder: Encoder) throws {
-		sargonProfileREMOVEAtEndOfStage1TEMP()
-	}
-}
-
 // MARK: - AppearanceID + Codable
 extension AppearanceID: Codable {
 	public func encode(to encoder: Encoder) throws {
