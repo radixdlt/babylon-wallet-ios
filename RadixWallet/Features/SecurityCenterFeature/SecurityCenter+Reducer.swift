32bfd2d09bf40397dbddc0f3761a2f418cd245d3
kugel3 <gustaf.kugelberg@rdx.works>
2024-04-17 14:24:28 +0200

    wip

--- RadixWallet/Features/SecurityCenterFeature/SecurityCenter+Reducer.swift
+++ RadixWallet/Features/SecurityCenterFeature/SecurityCenter+Reducer.swift
@@ -2,10 +2,35 @@ import ComposableArchitecture
 
 public struct SecurityCenter: Sendable, FeatureReducer {
 	public struct State: Sendable, Hashable {
-		public var status: SecurityCenterStatus = .recoverable
-		public var securityFactorsStatus: SecurityFactorsStatus = .active
-		public var configurationBackupStatus: ConfigurationBackupStatus = .backedUp
+		public var status: Status = .good
 	}
 
-	public enum ViewAction: Sendable, Equatable {}
+	public enum Item: Hashable {
+		case securityFactors
+		case configurationBackup
+	}
+
+	// MARK: - Status
+	public enum Status: Hashable {
+		case good
+		case bad(RecoverabilityIssue, Problem, actionsRequired: [Item])
+
+		public enum RecoverabilityIssue: Hashable {
+			case walletNotRecoverable
+			case entitiesNotRecoverable(accounts: Int, personas: Int)
+			case recoveryRequired
+		}
+
+		public enum Problem: Hashable {
+			case problem3
+			case problem5
+			case problem6
+			case problem7
+			case problem9
+		}
+	}
+
+	public enum ViewAction: Sendable, Equatable {
+		case statusTapped
+	}
 }
