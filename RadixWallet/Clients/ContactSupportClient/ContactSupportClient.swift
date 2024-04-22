0d9f79c542dfdc3564135286d684e1f5f4d83822
Matias Bzurovski <matias.bzurovski@rdx.works>
2024-04-22 13:44:16 +0200

    Add comment

--- RadixWallet/Clients/ContactSupportClient/ContactSupportClient.swift
+++ RadixWallet/Clients/ContactSupportClient/ContactSupportClient.swift
@@ -1,5 +1,11 @@
 // MARK: - ContactSupportClient
 struct ContactSupportClient: Sendable {
+	/// Opens an external mail app with a preloaded subject, recipient and body that allows the user to contact support.
+	///
+	/// The external mail application will be the first one available from the following list:
+	/// - Gmail
+	/// - Outlook
+	/// - Apple Mail
 	var openEmail: OpenEmail
 }
 
