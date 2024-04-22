54e1d75db1c5a7c641cc6c68974dc2b272636acd
Alexander Cyon <alex.cyon@rdx.works>
2024-04-22 17:49:32 +0200

    WIP

--- RadixWallet/ProfileSargonMigration/Stage1/Stage1MigrateToSargon+LedgerHardwareWalletModel.swift
+++
@@ -1,8 +0,0 @@
-import Foundation
-import Sargon
-
-extension LedgerHardwareWalletModel {
-	public var rawValue: String {
-		sargonProfileFinishMigrateAtEndOfStage1()
-	}
-}
--- RadixWallet/ProfileSargonMigration/Stage1/Stage1MigrateToSargon+Mnemonic.swift
+++ RadixWallet/ProfileSargonMigration/Stage1/Stage1MigrateToSargon+Mnemonic.swift
@@ -1,16 +1,2 @@
 import Foundation
 import Sargon
-
-extension Mnemonic {
-	public init(words: some Collection<BIP39Word>) throws {
-		sargonProfileFinishMigrateAtEndOfStage1()
-	}
-
-	public init(phrase: String, language: BIP39Language) throws {
-		sargonProfileFinishMigrateAtEndOfStage1()
-	}
-
-	public static var sample24ZooVote: Self {
-		sargonProfileFinishMigrateAtEndOfStage1()
-	}
-}
