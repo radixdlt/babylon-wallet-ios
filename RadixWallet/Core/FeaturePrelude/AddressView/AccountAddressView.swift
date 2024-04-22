6d1d3f3d9b5bd04da11d34c4593e0a5964d7f919
Matias Bzurovski <matias.bzurovski@rdx.works>
2024-04-22 19:07:49 +0200

    More components added to AccountAddressView

--- RadixWallet/Core/FeaturePrelude/AddressView/AccountAddressView.swift
+++ RadixWallet/Core/FeaturePrelude/AddressView/AccountAddressView.swift
@@ -8,13 +8,16 @@ struct AccountAddressView: View {
 	@State private var qrImage: Result<CGImage, Error>? = nil
 
 	@Dependency(\.qrGeneratorClient) var qrGeneratorClient
+	@Dependency(\.gatewaysClient) var gatewaysClient
+	@Dependency(\.openURL) var openURL
 
 	var body: some View {
 		VStack(spacing: .medium3) {
 			top
-
-			Spacer()
+			fullAddress
+			viewOnDashboard
 		}
+		.padding([.horizontal, .bottom], .medium3)
 		.withNavigationBar(closeAction: closeAction)
 		.toolbar(.visible, for: .navigationBar)
 		.presentationDetents([.large])
@@ -37,7 +40,44 @@ struct AccountAddressView: View {
 			qrCode
 				.padding(.horizontal, .large2)
 		}
+	}
+
+	private var fullAddress: some View {
+		VStack(spacing: .large3) {
+			VStack(spacing: .small2) {
+				Text("Full address")
+					.foregroundColor(.app.gray1)
+
+				Text(address.address)
+					.multilineTextAlignment(.center)
+			}
+			actions
+		}
+		.textStyle(.body1Header)
+		.padding(.vertical, .medium1)
 		.padding(.horizontal, .medium3)
+		.background(Color.app.gray5)
+		.cornerRadius(.small1)
+	}
+
+	private var actions: some View {
+		HStack(spacing: .large3) {
+			Button("Copy", image: .copy) {}
+
+			Button("Enlarge", image: .copy) {}
+
+			Button("Share", systemImage: "square.and.arrow.up") {}
+		}
+		.padding(.horizontal, .medium2)
+		.foregroundColor(.app.gray1)
+	}
+
+	private var viewOnDashboard: some View {
+		Button("View on Radix Dashboard", action: viewOnRadixDashboard)
+			.buttonStyle(.secondaryRectangular(
+				shouldExpand: true,
+				trailingImage: .init(.iconLinkOut)
+			))
 	}
 
 	private var qrCode: some View {
@@ -66,6 +106,18 @@ struct AccountAddressView: View {
 			self.qrImage = .success(image)
 		} catch {
 			self.qrImage = .failure(error)
+		}
+	}
+
+	private func viewOnRadixDashboard() {
+		Task { [openURL, gatewaysClient] in
+			let path = "account/" + address.formatted(.raw)
+			let currentNetwork = await gatewaysClient.getCurrentGateway().network
+			await openURL(
+				Radix.Dashboard.dashboard(forNetwork: currentNetwork)
+					.url
+					.appending(path: path)
+			)
 		}
 	}
 }
