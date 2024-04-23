aa1c45b777dea13d5bc41bdaa9d4df5f5fc1418f
Alexander Cyon <alex.cyon@rdx.works>
2024-04-23 11:01:14 +0200

    WIP

--- RadixWallet/ProfileSargonMigration/Stage1/Stage1MigrateToSargon+LedgerHardwareWalletFactorSource.swift
+++
@@ -1,6 +0,0 @@
-import Foundation
-import Sargon
-
-extension Sargon.LedgerHardwareWalletFactorSource: Identifiable {
-	public typealias ID = FactorSourceIDFromHash
-}
--- RadixWallet/ProfileSargonMigration/Stage1/Stage1MigrateToSargon+Persona.swift
+++ RadixWallet/ProfileSargonMigration/Stage1/Stage1MigrateToSargon+Persona.swift
@@ -16,3 +16,35 @@ extension PersonaData.Entry.Kind {
 		}
 	}
 }
+
+// MARK: - PersonaDataCollectionProtocol
+public protocol PersonaDataCollectionProtocol {
+	associatedtype Element: Hashable & Identifiable
+	var first: Element? { get }
+}
+
+// MARK: - Sargon.PersonaDataIdentifiedPhoneNumber + Identifiable
+extension Sargon.PersonaDataIdentifiedPhoneNumber: Identifiable {
+	public typealias ID = PersonaDataEntryID
+}
+
+// MARK: - Sargon.PersonaDataIdentifiedEmailAddress + Identifiable
+extension Sargon.PersonaDataIdentifiedEmailAddress: Identifiable {
+	public typealias ID = PersonaDataEntryID
+}
+
+// MARK: - Sargon.CollectionOfPhoneNumbers + PersonaDataCollectionProtocol
+extension Sargon.CollectionOfPhoneNumbers: PersonaDataCollectionProtocol {
+	public typealias Element = Sargon.PersonaDataIdentifiedPhoneNumber
+	public var first: Element? {
+		self.collection.first
+	}
+}
+
+// MARK: - Sargon.CollectionOfEmailAddresses + PersonaDataCollectionProtocol
+extension Sargon.CollectionOfEmailAddresses: PersonaDataCollectionProtocol {
+	public typealias Element = Sargon.PersonaDataIdentifiedEmailAddress
+	public var first: Element? {
+		self.collection.first
+	}
+}
