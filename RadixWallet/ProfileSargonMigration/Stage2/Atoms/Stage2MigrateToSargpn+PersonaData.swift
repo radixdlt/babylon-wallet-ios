cf46d9f5526c9a7560eea1f2caf1d54552090b14
Alexander Cyon <alex.cyon@rdx.works>
2024-04-23 13:43:35 +0200

    WIP

--- RadixWallet/ProfileSargonMigration/Stage2/Atoms/Stage2MigrateToSargpn+PersonaData.swift
+++ RadixWallet/ProfileSargonMigration/Stage2/Atoms/Stage2MigrateToSargpn+PersonaData.swift
@@ -5,21 +5,20 @@ extension PersonaData {
 	public func responseValidation(
 		for request: P2P.Dapp.Request.PersonaDataRequestItem
 	) -> P2P.Dapp.Request.RequestValidation {
-//		let allExisting = Dictionary(grouping: entries.map(\.value), by: \.discriminator)
-//
-//		var result = P2P.Dapp.Request.RequestValidation()
-//		for (kind, kindRequest) in request.kindRequests {
-//			let values = allExisting[kind] ?? []
-//			switch validate(values, for: kindRequest) {
-//			case let .left(missingEntry):
-//				result.missingEntries[kind] = missingEntry
-//			case let .right(responseValues):
-//				result.existingRequestedEntries[kind] = responseValues
-//			}
-//		}
-//
-//		return result
-		sargonProfileFinishMigrateAtEndOfStage1()
+		let allExisting = Dictionary(grouping: entries.map(\.value), by: \.discriminator)
+
+		var result = P2P.Dapp.Request.RequestValidation()
+		for (kind, kindRequest) in request.kindRequests {
+			let values = allExisting[kind] ?? []
+			switch validate(values, for: kindRequest) {
+			case let .left(missingEntry):
+				result.missingEntries[kind] = missingEntry
+			case let .right(responseValues):
+				result.existingRequestedEntries[kind] = responseValues
+			}
+		}
+
+		return result
 	}
 
 	private func validate(
@@ -29,16 +28,15 @@ extension PersonaData {
 		P2P.Dapp.Request.MissingEntry,
 		[Entry]
 	> {
-//		switch request {
-//		case .entry:
-//			guard let first = entries.first else { return .left(.missingEntry) }
-//			return .right([first])
-//		case let .number(number):
-//			let values = Set(entries.prefix(Int(number.quantity)))
-//			let missing = Int(number.quantity) - values.count
-//			guard missing <= 0 else { return .left(.missing(missing)) }
-//			return .right(Array(values))
-//		}
-		sargonProfileFinishMigrateAtEndOfStage1()
+		switch request {
+		case .entry:
+			guard let first = entries.first else { return .left(.missingEntry) }
+			return .right([first])
+		case let .number(number):
+			let values = Set(entries.prefix(Int(number.quantity)))
+			let missing = Int(number.quantity) - values.count
+			guard missing <= 0 else { return .left(.missing(missing)) }
+			return .right(Array(values))
+		}
 	}
 }
