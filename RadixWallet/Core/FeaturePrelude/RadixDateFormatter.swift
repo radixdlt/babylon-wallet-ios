9e5f3da0c780bf56c21ae085cd6f4ed2f2b659c2
kugel3 <gustaf.kugelberg@rdx.works>
2024-04-26 16:52:59 +0200

    DateFormatter

--- RadixWallet/Core/FeaturePrelude/RadixDateFormatter.swift
+++ RadixWallet/Core/FeaturePrelude/RadixDateFormatter.swift
@@ -3,20 +3,24 @@ import Foundation
 // MARK: - RadixDateFormatter
 public enum RadixDateFormatter {
 	public static func string(from date: Date) -> String {
-		let dateString = { date.formatted(date: .numeric, time: .shortened) }
+		let dateString = { date.formatted(date: .abbreviated, time: .omitted) }
 
 		let calendar = Calendar.current
 
-		if calendar.isDateInToday(date) {
-			let timeInterval = -date.timeIntervalSinceNow
-			if timeInterval < 60 {
+		if calendar.isDateInTomorrow(date) {
+			return L10n.TimeFormatting.tomorrow
+		} else if calendar.isDateInToday(date) {
+			let timeInterval = date.timeIntervalSinceNow
+			if timeInterval > 0 {
+				return L10n.TimeFormatting.today
+			} else if timeInterval > -60 {
 				return L10n.TimeFormatting.justNow
+			} else {
+				guard let relative = relativeFormatter.string(from: -timeInterval) else {
+					return dateString() // This should never happen
+				}
+				return L10n.TimeFormatting.ago(relative)
 			}
-			guard let relative = relativeFormatter.string(from: timeInterval) else {
-				return dateString() // This should never happen
-			}
-			return L10n.TimeFormatting.ago(relative)
-
 		} else if calendar.isDateInYesterday(date) {
 			return L10n.TimeFormatting.yesterday
 		} else {
