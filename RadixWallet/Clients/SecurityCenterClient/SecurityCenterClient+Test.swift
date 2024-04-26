0ee3234becbe4d53b4b563a21a6d3342f9da1348
kugel3 <gustaf.kugelberg@rdx.works>
2024-04-26 14:52:45 +0200

    securitycenterclient wip

--- RadixWallet/Clients/SecurityCenterClient/SecurityCenterClient+Test.swift
+++ RadixWallet/Clients/SecurityCenterClient/SecurityCenterClient+Test.swift
@@ -5,8 +5,8 @@ import os
 
 extension DependencyValues {
 	public var securityCenterClient: SecurityCenterClient {
-		get { self[CloudBackupClient.self] }
-		set { self[CloudBackupClient.self] = newValue }
+		get { self[SecurityCenterClient.self] }
+		set { self[SecurityCenterClient.self] = newValue }
 	}
 }
 
@@ -15,20 +15,10 @@ extension SecurityCenterClient: TestDependencyKey {
 	public static let previewValue: Self = .noop
 
 	public static let noop = Self(
-		checkAccountStatus: { throw NoopError() },
-		lastBackup: { _ in throw NoopError() },
-		queryProfile: { _ in throw NoopError() },
-		queryAllProfiles: { throw NoopError() },
-		uploadProfile: { _ in throw NoopError() },
-		deleteProfile: { _ in }
+		problems: { throw NoopError() },
 	)
 
 	public static let testValue = Self(
-		checkAccountStatus: unimplemented("\(Self.self).checkAccountStatus"),
-		lastBackup: unimplemented("\(Self.self).lastBackup"),
-		queryProfile: unimplemented("\(Self.self).queryProfile"),
-		queryAllProfiles: unimplemented("\(Self.self).queryAllProfiles"),
-		uploadProfile: unimplemented("\(Self.self).uploadProfile"),
-		deleteProfile: unimplemented("\(Self.self).deleteProfile")
+		problems: unimplemented("\(Self.self).problems")
 	)
 }
