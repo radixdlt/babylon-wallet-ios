435d21005c2232e00cf280672c38e258ab841d50
kugel3 <gustaf.kugelberg@rdx.works>
2024-04-26 17:06:32 +0200

    cleanup securitproblem

--- RadixWallet/Features/SecurityCenterFeature/SecurityCenter+Reducer.swift
+++ RadixWallet/Features/SecurityCenterFeature/SecurityCenter+Reducer.swift
@@ -1,23 +1,18 @@
 import ComposableArchitecture
 
 // MARK: - SecurityProblem
-// FIXME: GK - Move somewhere
 public enum SecurityProblem: Hashable, Sendable, Identifiable {
-	case problem3(accounts: Int, personas: Int)
 	case problem5
 	case problem6
 	case problem7
-	case problem9
 
 	public var id: Int { number }
 
 	public var number: Int {
 		switch self {
-		case .problem3: 3
 		case .problem5: 5
 		case .problem6: 6
 		case .problem7: 7
-		case .problem9: 9
 		}
 	}
 }
