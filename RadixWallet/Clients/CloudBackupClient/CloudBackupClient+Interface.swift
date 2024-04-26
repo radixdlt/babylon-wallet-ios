585d9d71fbd53a85a284936ca4ab5ae03fc9a799
kugel3 <gustaf.kugelberg@rdx.works>
2024-04-27 00:39:51 +0200

    migration starting to work

--- RadixWallet/Clients/CloudBackupClient/CloudBackupClient+Interface.swift
+++ RadixWallet/Clients/CloudBackupClient/CloudBackupClient+Interface.swift
@@ -4,20 +4,40 @@ import DependenciesAdditions
 import os
 
 // MARK: - CloudBackupClient
-public struct CloudBackupClient: DependencyKey {
+public struct CloudBackupClient: DependencyKey, Sendable {
+	public let migrateKeychainProfiles: MigrateKeychainProfiles
 	public let checkAccountStatus: CheckAccountStatus
 	public let lastBackup: LastBackup
-	public let queryProfile: QueryProfile
-	public let queryAllProfiles: QueryAllProfiles
+	public let loadProfile: LoadProfile
+	public let loadAllProfiles: LoadAllProfiles
 	public let uploadProfile: UploadProfile
 	public let deleteProfile: DeleteProfile
+
+	public init(
+		migrateKeychainProfiles: @escaping MigrateKeychainProfiles,
+		checkAccountStatus: @escaping CheckAccountStatus,
+		lastBackup: @escaping LastBackup,
+		loadProfile: @escaping LoadProfile,
+		loadAllProfiles: @escaping LoadAllProfiles,
+		uploadProfile: @escaping UploadProfile,
+		deleteProfile: @escaping DeleteProfile
+	) {
+		self.migrateKeychainProfiles = migrateKeychainProfiles
+		self.checkAccountStatus = checkAccountStatus
+		self.lastBackup = lastBackup
+		self.loadProfile = loadProfile
+		self.loadAllProfiles = loadAllProfiles
+		self.uploadProfile = uploadProfile
+		self.deleteProfile = deleteProfile
+	}
 }
 
 extension CloudBackupClient {
-	public typealias CheckAccountStatus = () async throws -> CKAccountStatus
-	public typealias LastBackup = (UUID) async throws -> Date?
-	public typealias QueryProfile = (UUID) async throws -> Profile?
-	public typealias QueryAllProfiles = () async throws -> [Profile]
-	public typealias UploadProfile = (Profile) async throws -> CKRecord
-	public typealias DeleteProfile = (UUID) async throws -> Void
+	public typealias MigrateKeychainProfiles = @Sendable () async throws -> [CKRecord]
+	public typealias CheckAccountStatus = @Sendable () async throws -> CKAccountStatus
+	public typealias LastBackup = @Sendable (UUID) async throws -> Date?
+	public typealias LoadProfile = @Sendable (UUID) async throws -> Profile?
+	public typealias LoadAllProfiles = @Sendable () async throws -> [Profile]
+	public typealias UploadProfile = @Sendable (Profile) async throws -> CKRecord
+	public typealias DeleteProfile = @Sendable (UUID) async throws -> Void
 }
