0eb46d12dd72f7138735c3ba5de273874ac0e78e
kugel3 <gustaf.kugelberg@rdx.works>
2024-05-09 10:42:06 +0200

    FIX ABW-3294 tappable warnings

--- RadixWallet/Clients/SecurityCenterClient/SecurityCenterClient+Interface.swift
+++ RadixWallet/Clients/SecurityCenterClient/SecurityCenterClient+Interface.swift
@@ -15,17 +15,21 @@ extension SecurityCenterClient {
 
 // MARK: - SecurityProblem
 public enum SecurityProblem: Hashable, Sendable, Identifiable {
+	case problem3(accounts: Int, personas: Int)
 	case problem5
 	case problem6
 	case problem7
+	case problem9
 
 	public var id: Int { number }
 
 	public var number: Int {
 		switch self {
+		case .problem3: 3
 		case .problem5: 5
 		case .problem6: 6
 		case .problem7: 7
+		case .problem9: 9
 		}
 	}
 }
--- RadixWallet/Features/SecurityCenterFeature/ConfigurationBackup+View.swift
+++ RadixWallet/Features/SecurityCenterFeature/ConfigurationBackup+View.swift
@@ -116,28 +116,32 @@ extension ConfigurationBackup {
 		let problem: SecurityProblem
 
 		var body: some SwiftUI.View {
-			HStack(spacing: 0) {
-				Image(.error)
-					.padding(.trailing, .medium3)
+			if let text = warning(for: problem) {
+				HStack(spacing: 0) {
+					Image(.error)
+						.padding(.trailing, .medium3)
 
-				Text(warning(for: problem))
-					.multilineTextAlignment(.leading)
-					.textStyle(.body2HighImportance)
+					Text(text)
+						.multilineTextAlignment(.leading)
+						.textStyle(.body2HighImportance)
 
-				Spacer(minLength: 0)
+					Spacer(minLength: 0)
+				}
+				.foregroundStyle(.app.alert)
+				.padding(.vertical, .small1)
+				.padding(.horizontal, .medium3)
+				.background(.app.lightAlert)
+				.roundedCorners(radius: .small1)
 			}
-			.foregroundStyle(.app.alert)
-			.padding(.vertical, .small1)
-			.padding(.horizontal, .medium3)
-			.background(.app.lightAlert)
-			.roundedCorners(radius: .small1)
 		}
 
-		func warning(for problem: SecurityProblem) -> String {
+		func warning(for problem: SecurityProblem) -> String? {
 			switch problem {
+			case .problem3: nil
 			case .problem5: L10n.ConfigurationBackup.problem5WarningIOS
 			case .problem6: L10n.ConfigurationBackup.problem6Warning
 			case .problem7: L10n.ConfigurationBackup.problem7Warning
+			case .problem9: nil
 			}
 		}
 	}
