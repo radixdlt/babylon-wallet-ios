c6c5ee3b364a0b31fd488e5985bd437db6ab5dba
Matias Bzurovski <matias.bzurovski@rdx.works>
2024-05-20 17:44:40 +0200

    Remove applyIf helpers

--- RadixWallet/Core/DesignSystem/Extensions/View+Extra.swift
+++ RadixWallet/Core/DesignSystem/Extensions/View+Extra.swift
@@ -27,26 +27,3 @@ extension View {
 		AnyView(self)
 	}
 }
-
-// MARK: - Apply
-extension View {
-	/// Applies `modifier` if given `condition` is met.
-	@ViewBuilder
-	func applyIf<Modifier>(_ condition: Bool, @ViewBuilder modifier: () -> Modifier) -> some View where Modifier: ViewModifier {
-		if condition {
-			self.modifier(modifier())
-		} else {
-			self
-		}
-	}
-
-	/// Applies `transform` if given `condition` is met.
-	@ViewBuilder
-	func applyIf<Content>(_ condition: Bool, @ViewBuilder transform: (Self) -> Content) -> some View where Content: View {
-		if condition {
-			transform(self)
-		} else {
-			self
-		}
-	}
-}
