db064202ce83b55b4be270c45cbe7d895ddedbbe
Matias Bzurovski <matias.bzurovski@rdx.works>
2024-05-29 16:31:36 +0200

    Delete DeviceFactorSourceControlled and MnemonicHandling related models

--- RadixWallet/Features/HomeFeature/Models/AccountWithInfo.swift
+++ RadixWallet/Features/HomeFeature/Models/AccountWithInfo.swift
@@ -5,31 +5,12 @@ public struct AccountWithInfo: Sendable, Hashable {
 	public var account: Account
 
 	public var isDappDefinitionAccount: Bool = false
-	public var deviceFactorSourceControlled: DeviceFactorSourceControlled?
 
 	init(account: Account) {
 		self.account = account
-		self.deviceFactorSourceControlled = Self.makeDeviceFactorSourceControlled(account)
-	}
-
-	private static func makeDeviceFactorSourceControlled(_ account: Account) -> DeviceFactorSourceControlled? {
-		switch account.securityState {
-		case let .unsecured(unsecuredEntityControl):
-			if unsecuredEntityControl.transactionSigning.factorSourceID.kind == .device {
-				DeviceFactorSourceControlled(
-					factorSourceID: unsecuredEntityControl.transactionSigning.factorSourceID
-				)
-			} else {
-				nil
-			}
-		}
 	}
 
 	public var id: AccountAddress { account.address }
 	public var isLegacyAccount: Bool { account.isLegacy }
 	public var isLedgerAccount: Bool { account.isLedgerControlled }
-
-	public var mnemonicHandlingCallToAction: MnemonicHandling? {
-		deviceFactorSourceControlled?.mnemonicHandlingCallToAction
-	}
 }
