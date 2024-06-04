9cc59118370f4f43762393596a6ac6ad4e62916a
kugel3 <gustaf.kugelberg@rdx.works>
2024-06-04 12:40:42 +0200

    Refactor ResetWallet

--- RadixWallet/Clients/ResetWalletClient/ResetWalletClient+Live.swift
+++ RadixWallet/Clients/ResetWalletClient/ResetWalletClient+Live.swift
@@ -4,16 +4,25 @@ extension ResetWalletClient: DependencyKey {
 	public static let liveValue: Self = {
 		let walletDidResetSubject = AsyncPassthroughSubject<Void>()
 
+		@Dependency(\.errorQueue) var errorQueue
+		@Dependency(\.appPreferencesClient) var appPreferencesClient
 		@Dependency(\.cacheClient) var cacheClient
 		@Dependency(\.radixConnectClient) var radixConnectClient
 		@Dependency(\.userDefaults) var userDefaults
 
 		return Self(
 			resetWallet: {
-				cacheClient.removeAll()
-				await radixConnectClient.disconnectAll()
-				userDefaults.removeAll()
-				walletDidResetSubject.send(())
+				do {
+					// TODO: Is this the best order?
+					try await appPreferencesClient.deleteProfileAndFactorSources(true)
+					cacheClient.removeAll()
+					await radixConnectClient.disconnectAll()
+					userDefaults.removeAll()
+					walletDidResetSubject.send(())
+				} catch {
+					loggerGlobal.error("Failed to delete profile: \(error)")
+					errorQueue.schedule(error)
+				}
 			},
 			walletDidReset: {
 				walletDidResetSubject.eraseToAnyAsyncSequence()
