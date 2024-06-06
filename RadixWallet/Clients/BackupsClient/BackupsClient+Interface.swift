0c04419458f1888a31225a91929017a27b10b54e
kugel3 <gustaf.kugelberg@rdx.works>
2024-06-06 22:13:17 +0200

    remove lookupProfileSnapshotByHeader

--- RadixWallet/Clients/BackupsClient/BackupsClient+Interface.swift
+++ RadixWallet/Clients/BackupsClient/BackupsClient+Interface.swift
@@ -6,7 +6,6 @@ public typealias DeviceID = UUID
 public struct BackupsClient: Sendable {
 	public var snapshotOfProfileForExport: SnapshotOfProfileForExport
 	public var loadProfileBackups: LoadProfileBackups
-	public var lookupProfileSnapshotByHeader: LookupProfileSnapshotByHeader
 	public var importProfileSnapshot: ImportProfileSnapshot
 	public var didExportProfileSnapshot: DidExportProfileSnapshot
 	public var loadDeviceID: LoadDeviceID
@@ -14,14 +13,12 @@ public struct BackupsClient: Sendable {
 	public init(
 		snapshotOfProfileForExport: @escaping SnapshotOfProfileForExport,
 		loadProfileBackups: @escaping LoadProfileBackups,
-		lookupProfileSnapshotByHeader: @escaping LookupProfileSnapshotByHeader,
 		importProfileSnapshot: @escaping ImportProfileSnapshot,
 		didExportProfileSnapshot: @escaping DidExportProfileSnapshot,
 		loadDeviceID: @escaping LoadDeviceID
 	) {
 		self.snapshotOfProfileForExport = snapshotOfProfileForExport
 		self.loadProfileBackups = loadProfileBackups
-		self.lookupProfileSnapshotByHeader = lookupProfileSnapshotByHeader
 		self.importProfileSnapshot = importProfileSnapshot
 		self.didExportProfileSnapshot = didExportProfileSnapshot
 		self.loadDeviceID = loadDeviceID
@@ -31,7 +28,6 @@ public struct BackupsClient: Sendable {
 extension BackupsClient {
 	public typealias SnapshotOfProfileForExport = @Sendable () async throws -> Profile
 	public typealias LoadProfileBackups = @Sendable () async -> Profile.HeaderList?
-	public typealias LookupProfileSnapshotByHeader = @Sendable (Profile.Header) async throws -> (Profile?, Bool)
 	public typealias ImportProfileSnapshot = @Sendable (Profile, Set<FactorSourceIDFromHash>, Bool) async throws -> Void
 	public typealias DidExportProfileSnapshot = @Sendable (Profile) throws -> Void
 	public typealias LoadDeviceID = @Sendable () async -> UUID?
