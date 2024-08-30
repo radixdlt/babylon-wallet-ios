376459c29733c94205b921cbd9639688786602aa
Matias Bzurovski <matias.bzurovski@rdx.works>
2024-08-30 15:13:35 +0200

    WIP

--- RadixWallet/Clients/AccountLockersClient/AccountLockersClient+Live.swift
+++ RadixWallet/Clients/AccountLockersClient/AccountLockersClient+Live.swift
@@ -14,26 +14,72 @@ extension AccountLockersClient {
 			let accountValues = await accountsClient.accountsOnCurrentNetwork()
 
 			for try await (dapps, accounts) in combineLatest(dappValues, accountValues) {
-				_ = try await filterDappsWithAccountLockers(dapps)
+				let dappsWithLockers = try await filterDappsWithAccountLockers(dapps)
+				let lockersPerAccount = getLockersPerAccount(accounts: accounts, dapps: dappsWithLockers)
+				print("M- Lockers per account: \(lockersPerAccount)")
+
+				// Return [AccountAddress: [Claims]]
 			}
 		}
 
 		@Sendable
-		func filterDappsWithAccountLockers(_ dapps: AuthorizedDapps) async throws -> AuthorizedDapps {
-			let dappsWithPrimaryLocker = try await onLedgerEntitiesClient.getEntities(addresses: dapps.map(\.dAppDefinitionAddress.asGeneral), metadataKeys: .dappMetadataKeys, cachingStrategy: .readFromLedgerSkipWrite)
-				.compactMap(\.account)
-				.filter { account in
-					account.details?.primaryLocker != nil
-				}
-				.map(\.address)
+		func filterDappsWithAccountLockers(_ dapps: AuthorizedDapps) async throws -> [DappWithLockerAddress] {
+			// Filter dapps for which user doesn't want to see deposits
+			let dappsWithDepositsVisible = dapps
+				.filter(\.isDepositsVisible)
+				.map(\.dappDefinitionAddress.asGeneral)
 
-			let result = dapps.filter { dapp in
-				dappsWithPrimaryLocker.contains(dapp.dappDefinitionAddress)
+			// Get entity details from those dapps, to filter only those that actually have a primary locker.
+			let dappsWithLocker = try await onLedgerEntitiesClient.getEntities(addresses: dappsWithDepositsVisible, metadataKeys: .dappMetadataKeys, cachingStrategy: .readFromLedgerSkipWrite)
+				.compactMap(\.account)
+				.compactMap { account -> (AccountAddress, String)? in
+					guard let lockerAddress = account.details?.primaryLocker else {
+						return nil
+					}
+					return (account.address, lockerAddress)
+				}
+
+			// Loop over the original input to only return those that actually have a primary locker
+			return dapps.compactMap { dapp in
+				guard let dappWithLocker = dappsWithLocker.first(where: { $0.0 == dapp.dappDefinitionAddress }) else {
+					return nil
+				}
+				return DappWithLockerAddress(dapp: dapp, lockerAddress: dappWithLocker.1)
+			}
+		}
+
+		@Sendable
+		func getLockersPerAccount(accounts: Accounts, dapps: [DappWithLockerAddress]) -> [AccountAddress: [String]] {
+			var result: [AccountAddress: [String]] = [:]
+			for account in accounts {
+				var lockerAddresses: [String] = []
+				for dapp in dapps {
+					let accountsInDapp = dapp.dapp.accountsInDapp
+					if accountsInDapp.contains(account.address) {
+						lockerAddresses.append(dapp.lockerAddress)
+					}
+				}
+				if !lockerAddresses.isEmpty {
+					result[account.address] = lockerAddresses
+				}
 			}
 
 			return result
 		}
 
 		return .init(startMonitoring: startMonitoring)
+	}
+
+	private struct DappWithLockerAddress: Sendable, Hashable {
+		let dapp: AuthorizedDapp
+		let lockerAddress: String
+	}
+}
+
+private extension AuthorizedDapp {
+	var accountsInDapp: [AccountAddress] {
+		referencesToAuthorizedPersonas
+			.compactMap { $0.sharedAccounts?.ids }
+			.flatMap { $0 }
 	}
 }
