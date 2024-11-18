ade55e247b719c40f7229c0c60f5dc1b8e5b57c7
Alexander Cyon <alex.cyon@gmail.com>
2024-11-18 20:37:43 +0100

    WIP

--- RadixWallet/Features/SettingsFeature/DebugSettings/Children/DebugFactorInstancesCacheContents/DebugFactorInstancesCacheContents+View.swift
+++ RadixWallet/Features/SettingsFeature/DebugSettings/Children/DebugFactorInstancesCacheContents/DebugFactorInstancesCacheContents+View.swift
@@ -1,4 +1,5 @@
 import SwiftUI
+import Sargon
 
 // MARK: - DebugFactorInstancesCacheContents.View
 extension DebugFactorInstancesCacheContents {
@@ -10,18 +11,80 @@ extension DebugFactorInstancesCacheContents {
                 VStack(spacing: .small2) {
                     loadable(store.factorInstances) {
                         ProgressView()
-                    } successContent: { factorInstances in
-                        VStack {
-//                            ForEach(factorInstances) { (factorSourceID, instancesForPresetsOfFactor) in
-//                                Text("factorSourceID \(factorSourceID), #\(instancesForPresetsOfFactor.count)derivation presets")
-//                            }
-                            Text("Loaded 🐶🎉🐁 #\(factorInstances.count)")
-                        }
+                    } successContent: { instances in
+						content(instances: instances)
                     }
                 }
             }.task {
                 store.send(.view(.task))
             }
         }
+		
+		@ViewBuilder
+		private func content(instances: Instances) -> some SwiftUI.View {
+			ScrollView {
+				VStack(alignment: .leading) {
+					ForEach(Array(instances.keys), id: \.self) { (key: FactorSourceIDFromHash) in
+						Section("Source `\(key.idPrefix())`") {
+							let listOfList: [[HierarchicalDeterministicFactorInstance]] = instances[key]!
+							ForEach(listOfList, id: \.self) { (instancesForFactorForPath: [HierarchicalDeterministicFactorInstance]) in
+								if let firstInstance = instancesForFactorForPath.first {
+									Section(firstInstance.indexAgnosticPath()) {
+										ForEach(instancesForFactorForPath, id: \.self) { (instance: HierarchicalDeterministicFactorInstance) in
+											VStack(alignment: .leading) {
+												Text("Index `\(instance.derivationEntityIndex())`")
+												
+												Text("PublicKey `\(instance.publicKeySuffix())`")
+											}
+											.border(Color.gray, width: 2)
+											.padding()
+										}
+									}
+									.padding()
+								}
+								
+							}
+						}
+						.padding()
+					}
+				}
+			}
+		}
     }
+}
+extension FactorSourceIDFromHash {
+	fileprivate func idPrefix() -> String {
+		String(toString().prefix(10))
+	}
+}
+extension HierarchicalDeterministicFactorInstance {
+	fileprivate func indexAgnosticPath() -> String {
+		publicKey.derivationPath.indexAgnosticPath()
+	}
+	fileprivate func derivationEntityIndex() -> String {
+		let index = publicKey.derivationPath.lastPathComponent.indexInLocalKeySpace()
+		return String(reflecting: index)
+	}
+	fileprivate func publicKeySuffix() -> String {
+		String(publicKey().suffix(6))
+	}
+	fileprivate func publicKey() -> String {
+		publicKey.publicKey.hex
+	}
+}
+extension DerivationPath {
+	fileprivate func indexAgnosticPath() -> String {
+		let components = self.path.components
+		let network = components[2]
+		let entityKind = components[3]
+		let keyKind = components[4]
+		let entityIndex = components[5]
+		let keySpace = entityIndex.keySpace
+		let keySpaceStr = switch keySpace {
+		case .securified: "S"
+		case let .unsecurified(isHardened): isHardened ? "H" : ""
+		}
+		return "\(network)/\(entityKind)/\(keyKind)/\(keySpaceStr)?"
+		
+	}
 }
