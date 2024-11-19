ed289f98b070ee7f4d7b6555f009d893373f96ab
Alexander Cyon <alex.cyon@rdx.works>
2024-11-19 12:36:53 +0100

    wip

--- RadixWallet/Features/SettingsFeature/DebugSettings/Children/DebugFactorInstancesCacheContents/DebugFactorInstancesCacheContents+View.swift
+++ RadixWallet/Features/SettingsFeature/DebugSettings/Children/DebugFactorInstancesCacheContents/DebugFactorInstancesCacheContents+View.swift
@@ -1,94 +1,74 @@
-import SwiftUI
 import Sargon
+import SwiftUI
 
 // MARK: - DebugFactorInstancesCacheContents.View
 extension DebugFactorInstancesCacheContents {
-    struct View: SwiftUI.View {
-        let store: StoreOf<DebugFactorInstancesCacheContents>
-        
-        var body: some SwiftUI.View {
-            WithPerceptionTracking {
-                VStack(spacing: .small2) {
+	struct View: SwiftUI.View {
+		let store: StoreOf<DebugFactorInstancesCacheContents>
+
+		var body: some SwiftUI.View {
+			WithPerceptionTracking {
+				VStack(spacing: .small2) {
 					Button("Delete Cache File") {
 						store.send(.view(.deleteButtonTapped))
 					}
 					.buttonStyle(.primaryRectangular(isDestructive: true))
-                    loadable(store.factorInstances) {
-                        ProgressView()
-                    } successContent: { instances in
+					loadable(store.factorInstances) {
+						ProgressView()
+					} successContent: { instances in
 						content(instances: instances)
-                    }
-                }
-            }.task {
-                store.send(.view(.task))
-            }
-        }
-		
+					}
+				}
+				.padding()
+				.navigationTitle("FactorInstances cache")
+			}.task {
+				store.send(.view(.task))
+			}
+		}
+
 		@ViewBuilder
 		private func content(instances: Instances) -> some SwiftUI.View {
 			ScrollView {
 				VStack(alignment: .leading) {
 					ForEach(Array(instances.keys), id: \.self) { (key: FactorSourceIDFromHash) in
-						Section("Source `\(key.idPrefix())`") {
-							let listOfList: [[HierarchicalDeterministicFactorInstance]] = instances[key]!
-							ForEach(listOfList, id: \.self) { (instancesForFactorForPath: [HierarchicalDeterministicFactorInstance]) in
+						Section {
+							let listOfList: [[FactorInstanceForDebugPurposes]] = instances[key]!
+							ForEach(listOfList, id: \.self) { (instancesForFactorForPath: [FactorInstanceForDebugPurposes]) in
 								if let firstInstance = instancesForFactorForPath.first {
-									Section(firstInstance.indexAgnosticPath()) {
-										ForEach(instancesForFactorForPath, id: \.self) { (instance: HierarchicalDeterministicFactorInstance) in
+									Section {
+										ForEach(instancesForFactorForPath, id: \.self) { (instance: FactorInstanceForDebugPurposes) in
 											VStack(alignment: .leading) {
-												Text("Index `\(instance.derivationEntityIndex())`")
-												
-												Text("PublicKey `\(instance.publicKeySuffix())`")
+												Text("Index `\(instance.derivationEntityIndex)`")
+
+												Text("PublicKey `\(String(reflecting: instance.publicKeyHex.prefix(8)))`")
 											}
-											.border(Color.gray, width: 2)
-											.padding()
+											.frame(maxWidth: .infinity)
+											.padding(.horizontal, .medium3)
+											.padding(.vertical, .small2)
+											.border(Color.gray, width: 1)
 										}
+									} header: {
+										VStack {
+											Text("Agnostic `\(firstInstance.indexAgnosticDerivationPath)`").font(.title)
+											Text("#\(instancesForFactorForPath.count) instances")
+										}
+										.padding()
 									}
-									.padding()
 								}
-								
 							}
+						} header: {
+							Text("Source `\(key.idPrefix())`")
+								.font(.largeTitle)
 						}
-						.padding()
 					}
 				}
 			}
 		}
-    }
+	}
 }
+
 extension FactorSourceIDFromHash {
 	fileprivate func idPrefix() -> String {
 		String(toString().prefix(10))
-	}
-}
-extension HierarchicalDeterministicFactorInstance {
-	fileprivate func indexAgnosticPath() -> String {
-		publicKey.derivationPath.indexAgnosticPath()
-	}
-	fileprivate func derivationEntityIndex() -> String {
-		let index = publicKey.derivationPath.lastPathComponent.indexInLocalKeySpace()
-		return String(reflecting: index)
-	}
-	fileprivate func publicKeySuffix() -> String {
-		String(publicKey().suffix(6))
-	}
-	fileprivate func publicKey() -> String {
-		publicKey.publicKey.hex
-	}
-}
-extension DerivationPath {
-	fileprivate func indexAgnosticPath() -> String {
-		let components = self.path.components
-		let network = components[2]
-		let entityKind = components[3]
-		let keyKind = components[4]
-		let entityIndex = components[5]
-		let keySpace = entityIndex.keySpace
-		let keySpaceStr = switch keySpace {
-		case .securified: "S"
-		case let .unsecurified(isHardened): isHardened ? "H" : ""
-		}
-		return "\(network)/\(entityKind)/\(keyKind)/\(keySpaceStr)?"
-		
 	}
 }
--- RadixWallet/Features/SettingsFeature/DebugSettings/Children/DebugFactorInstancesCacheContents/DebugFactorInstancesCacheContents.swift
+++ RadixWallet/Features/SettingsFeature/DebugSettings/Children/DebugFactorInstancesCacheContents/DebugFactorInstancesCacheContents.swift
@@ -1,13 +1,13 @@
 import Sargon
 
-public typealias Instances = [FactorSourceIDFromHash: [[HierarchicalDeterministicFactorInstance]]]
+public typealias Instances = [FactorSourceIDFromHash: [[FactorInstanceForDebugPurposes]]]
 
 // MARK: - DebugFactorInstancesCacheContents
 @Reducer
 struct DebugFactorInstancesCacheContents: Sendable, FeatureReducer {
 	@ObservableState
 	struct State: Sendable, Hashable {
-        var factorInstances: Loadable<Instances> = .idle
+		var factorInstances: Loadable<Instances> = .idle
 		init() {}
 	}
 
@@ -17,29 +17,29 @@ struct DebugFactorInstancesCacheContents: Sendable, FeatureReducer {
 		case task
 		case deleteButtonTapped
 	}
-    
-    enum InternalAction: Sendable, Equatable {
-        case loadedInstances(Instances)
+
+	enum InternalAction: Sendable, Equatable {
+		case loadedInstances(Instances)
 		case failedToDelete(String)
-    }
+	}
 
 	var body: some ReducerOf<Self> {
 		Reduce(core)
 	}
-    
-    func reduce(into state: inout State, internalAction: InternalAction) -> Effect<Action> {
-        switch internalAction {
+
+	func reduce(into state: inout State, internalAction: InternalAction) -> Effect<Action> {
+		switch internalAction {
 		case let .failedToDelete(error):
 			struct Err: LocalizedError {
 				let localizedDescription: String
 			}
 			state.factorInstances = .failure(Err(localizedDescription: error))
 			return .none
-        case let .loadedInstances(instances):
-            state.factorInstances = .success(instances)
-            return .none
-        }
-    }
+		case let .loadedInstances(instances):
+			state.factorInstances = .success(instances)
+			return .none
+		}
+	}
 
 	func reduce(into state: inout State, viewAction: ViewAction) -> Effect<Action> {
 		switch viewAction {
@@ -51,12 +51,11 @@ struct DebugFactorInstancesCacheContents: Sendable, FeatureReducer {
 				await send(.internal(.failedToDelete(error.localizedDescription)))
 			}
 		case .task:
-            state.factorInstances = .loading
-                return .run { send in
-                    
-                    let instances = await SargonOS.shared.factorInstancesInCache()
-                    await send(.internal(.loadedInstances(instances)))
-                }
+			state.factorInstances = .loading
+			return .run { send in
+				let instances = await SargonOS.shared.debugFactorInstancesInCache()
+				await send(.internal(.loadedInstances(instances)))
+			}
 		}
 	}
 }
