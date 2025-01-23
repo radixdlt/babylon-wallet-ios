3569482b2f95f1fe16691d754c7656458577bcd7
Matias Bzurovski <matias.bzurovski@rdx.works>
2025-01-23 19:29:22 +0100

    update AccountPermissionChooseAccounts flow to sign with Sargon

--- RadixWallet/Features/DappInteractionFeature/Children/AccountPermissionChooseAccounts/AccountPermissionChooseAccounts+Reducer.swift
+++ RadixWallet/Features/DappInteractionFeature/Children/AccountPermissionChooseAccounts/AccountPermissionChooseAccounts+Reducer.swift
@@ -20,9 +20,6 @@ struct AccountPermissionChooseAccounts: Sendable, FeatureReducer {
 		let dappMetadata: DappMetadata
 		var chooseAccounts: ChooseAccounts.State
 
-		@PresentationState
-		var destination: Destination.State?
-
 		init(
 			challenge: DappToWalletInteractionAuthChallengeNonce?,
 			accessKind: AccessKind,
@@ -55,9 +52,10 @@ struct AccountPermissionChooseAccounts: Sendable, FeatureReducer {
 	}
 
 	enum InternalAction: Sendable, Equatable {
-		case proveAccountOwnership(SigningFactors, AuthenticationDataToSignForChallengeResponse)
+		case handleSignedAuthIntent(SignedAuthIntent, selectedAccounts: [Account])
 	}
 
+	@CasePathable
 	enum ChildAction: Sendable, Equatable {
 		case chooseAccounts(ChooseAccounts.Action)
 	}
@@ -70,148 +68,75 @@ struct AccountPermissionChooseAccounts: Sendable, FeatureReducer {
 		case failedToProveOwnership(of: [Account])
 	}
 
-	struct Destination: DestinationReducer {
-		enum State: Sendable, Hashable {
-			case signing(Signing.State)
-		}
-
-		enum Action: Sendable, Equatable {
-			case signing(Signing.Action)
-		}
-
-		var body: some ReducerOf<Self> {
-			Scope(state: /State.signing, action: /Action.signing) {
-				Signing()
-			}
-		}
-	}
-
 	@Dependency(\.errorQueue) var errorQueue
 	@Dependency(\.accountsClient) var accountsClient
 	@Dependency(\.rolaClient) var rolaClient
 	@Dependency(\.factorSourcesClient) var factorSourcesClient
 
 	var body: some ReducerOf<Self> {
-		Scope(state: \.chooseAccounts, action: /Action.child .. ChildAction.chooseAccounts) {
+		Scope(state: \.chooseAccounts, action: \.child.chooseAccounts) {
 			ChooseAccounts()
 		}
 
 		Reduce(core)
-			.ifLet(destinationPath, action: /Action.destination) {
-				Destination()
-			}
 	}
-
-	private let destinationPath: WritableKeyPath<State, PresentationState<Destination.State>> = \.$destination
 
 	func reduce(into state: inout State, viewAction: ViewAction) -> Effect<Action> {
 		switch viewAction {
 		case let .continueButtonTapped(selectedAccounts):
-			let selectedAccounts = IdentifiedArray(uncheckedUniqueElements: selectedAccounts.map(\.account))
+			let selectedAccounts = selectedAccounts.map(\.account)
 
 			guard let challenge = state.challenge else {
 				return .send(.delegate(.continue(
 					accessKind: state.accessKind,
-					chosenAccounts: .withoutProofOfOwnership(selectedAccounts)
+					chosenAccounts: .withoutProofOfOwnership(selectedAccounts.asIdentified())
 				)))
 			}
 
-			guard let signers = NonEmpty<Set<AccountOrPersona>>(rawValue: Set(selectedAccounts.map { AccountOrPersona.account($0) })) else {
-				return .send(.delegate(.continue(
-					accessKind: state.accessKind,
-					chosenAccounts: .withoutProofOfOwnership(selectedAccounts)
-				)))
+			guard let metadata = state.dappMetadata.requestMetadata else {
+				assertionFailure("Unable to sign Account Permission without the request metadata")
+				return .none
 			}
-
-			let createAuthPayloadRequest = AuthenticationDataToSignForChallengeRequest(
-				challenge: challenge,
-				origin: state.dappMetadata.origin,
-				dAppDefinitionAddress: state.dappMetadata.dAppDefinitionAddress
-			)
 
 			return .run { send in
-				let dataToSign = try rolaClient.authenticationDataToSignForChallenge(createAuthPayloadRequest)
-				let networkID = await accountsClient.getCurrentNetworkID()
-				let signingFactors = try await factorSourcesClient.getSigningFactors(.init(
-					networkID: networkID,
-					signers: signers,
-					signingPurpose: .signAuth
-				))
-				await send(.internal(.proveAccountOwnership(signingFactors, dataToSign)))
+				let signedAuthIntent = try await SargonOS.shared.signAuthAccounts(accountAddresses: selectedAccounts.map(\.address), challengeNonce: challenge, metadata: metadata)
+				await send(.internal(.handleSignedAuthIntent(signedAuthIntent, selectedAccounts: selectedAccounts)))
+			} catch: { _, send in
+				loggerGlobal.error("Failed to sign proof of ownership")
+				await send(.delegate(.failedToProveOwnership(of: selectedAccounts)))
 			}
 		}
 	}
 
 	func reduce(into state: inout State, internalAction: InternalAction) -> Effect<Action> {
 		switch internalAction {
-		case let .proveAccountOwnership(signingFactors, authenticationDataToSignForChallenge):
-			state.destination = .signing(.init(
-				factorsLeftToSignWith: signingFactors,
-				signingPurposeWithPayload: .signAuth(authenticationDataToSignForChallenge)
-			))
-			return .none
-		}
-	}
+		case let .handleSignedAuthIntent(signedAuthIntent, selectedAccounts):
+			var accountsLeftToVerifyDidSign: Set<Account.ID> = Set(selectedAccounts.map(\.id))
 
-	func reduce(into state: inout State, presentedAction: Destination.Action) -> Effect<Action> {
-		let selectedAccounts = (state.chooseAccounts.selectedAccounts ?? []).map(\.account)
-
-		switch presentedAction {
-		case let .signing(.delegate(signingAction)):
-			switch signingAction {
-			case .cancelSigning:
-				state.destination = nil
-				return cancelSigningEffect(state: &state)
-
-			case let .finishedSigning(.signAuth(signedAuthChallenge)):
-				state.destination = nil
-
-				var accountsLeftToVerifyDidSign: Set<Account.ID> = Set(selectedAccounts.map(\.id))
-				let walletAccountsWithProof: [WalletToDappInteractionResponse.Accounts.WithProof] = signedAuthChallenge.entitySignatures.map {
-					let account = try! $0.signerEntity.asAccount()
-					accountsLeftToVerifyDidSign.remove(account.id)
-					let proof = WalletToDappInteractionAuthProof(entitySignature: $0)
-					return WalletToDappInteractionResponse.Accounts.WithProof(account: .init(account: account), proof: proof)
+			let accountProofs: [WalletToDappInteractionResponse.Accounts.WithProof] = signedAuthIntent.intentSignaturesPerOwner.compactMap { item -> WalletToDappInteractionResponse.Accounts.WithProof? in
+				guard
+					let accountAddress = item.owner.accountAddress,
+					let account = selectedAccounts.first(where: { $0.address == accountAddress })
+				else {
+					return nil
 				}
-				guard accountsLeftToVerifyDidSign.isEmpty else {
-					loggerGlobal.error("Failed to sign with all accounts..")
-					return .send(.delegate(.failedToProveOwnership(of: selectedAccounts)))
-				}
+				accountsLeftToVerifyDidSign.remove(account.id)
 
-				let chosenAccounts: AccountPermissionChooseAccountsResult = .withProofOfOwnership(
-					challenge: signedAuthChallenge.challenge,
-					walletAccountsWithProof.asIdentified()
-				)
-				return .send(.delegate(.continue(accessKind: state.accessKind, chosenAccounts: chosenAccounts)))
+				let proof = WalletToDappInteractionAuthProof(intentSignatureOfOwner: item)
+				return .init(account: .init(account: account), proof: proof)
+			}
 
-			case .failedToSign:
-				state.destination = nil
-				loggerGlobal.error("Failed to sign proof of ownership")
-				return .send(.delegate(.failedToProveOwnership(of: selectedAccounts)))
-
-			case .finishedSigning(.signTransaction), .finishedSigning(.signPreAuthorization):
-				state.destination = nil
-				assertionFailure("wrong signing, signed tx, expected auth...")
-				loggerGlobal.error("Failed to sign proof of ownership")
+			// Verify there is a signature for each address
+			guard accountsLeftToVerifyDidSign.isEmpty else {
+				loggerGlobal.error("Failed to sign with all accounts..")
 				return .send(.delegate(.failedToProveOwnership(of: selectedAccounts)))
 			}
 
-		default:
-			return .none
+			let chosenAccounts: AccountPermissionChooseAccountsResult = .withProofOfOwnership(
+				challenge: signedAuthIntent.intent.challengeNonce,
+				accountProofs.asIdentified()
+			)
+			return .send(.delegate(.continue(accessKind: state.accessKind, chosenAccounts: chosenAccounts)))
 		}
-	}
-
-	func reduceDismissedDestination(into state: inout State) -> Effect<Action> {
-		if case .signing = state.destination {
-			cancelSigningEffect(state: &state)
-		} else {
-			.none
-		}
-	}
-
-	private func cancelSigningEffect(state: inout State) -> Effect<Action> {
-		// FIXME: How to cancel?
-		loggerGlobal.error("Cancelled signing")
-		return .none
 	}
 }
