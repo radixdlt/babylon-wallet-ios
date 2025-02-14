905aa168e42fb5ed884da5689c0eee9fd7006037
Ghenadie Vasiliev-Pusca <ghenadie.vasiliev-pusca@rdx.works>
2025-02-14 15:25:53 +0200

    fix expiration

--- RadixWallet/Features/PreAuthorizationReview/PreAuthorizationReview.swift
+++ RadixWallet/Features/PreAuthorizationReview/PreAuthorizationReview.swift
@@ -287,7 +287,7 @@ extension PreAuthorizationReview.State {
 	var isExpired: Bool {
 		switch expiration {
 		case let .atTime(value):
-			value.date <= Date.now
+			value.date <= Date.now || secondsToExpiration == 0
 		case .afterDelay:
 			false
 		}
