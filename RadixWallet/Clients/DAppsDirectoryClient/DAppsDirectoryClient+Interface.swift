689ba401c8646f1ed2062e83941b780b25fa86a9
GhenadieVP <ghenadie@radixpublishing.com>
2025-06-24 13:11:45 +0300

    improve decoding and add category field

--- RadixWallet/Clients/DAppsDirectoryClient/DAppsDirectoryClient+Interface.swift
+++ RadixWallet/Clients/DAppsDirectoryClient/DAppsDirectoryClient+Interface.swift
@@ -14,7 +14,7 @@ extension DAppsDirectoryClient {
 	}
 
 	typealias DApps = IdentifiedArrayOf<DApp>
-	struct DApp: Codable, Sendable, Hashable, Identifiable {
+	struct DApp: Sendable, Hashable, Identifiable {
 		var id: DappDefinitionAddress {
 			address
 		}
@@ -22,19 +22,114 @@ extension DAppsDirectoryClient {
 		let name: String
 		let address: DappDefinitionAddress
 		let tags: IdentifiedArrayOf<Tag>
+		let dAppCategory: Category
+	}
+}
+
+extension DAppsDirectoryClient.DApp: Codable {
+	enum Key: CodingKey {
+		case name
+		case address
+		case tags
+		case dAppCategory
+	}
+
+	init(from decoder: Decoder) throws {
+		let container = try decoder.container(keyedBy: Key.self)
+		self.name = try container.decode(String.self, forKey: .name)
+		self.address = try container.decode(DappDefinitionAddress.self, forKey: .address)
+		self.tags = try container.decode([Result<Tag, DecodingError>].self, forKey: .tags)
+			.compactMap { try? $0.get() }
+			.asIdentified()
+		self.dAppCategory = try container.decodeIfPresent(Category.self, forKey: .dAppCategory) ?? .other
 	}
 }
 
 extension DAppsDirectoryClient.DApp {
-	enum Tag: String, Identifiable, CaseIterable, Codable {
-		case defi = "DeFi"
-		case dex = "DEX"
-		case token = "Token"
-		case trade = "Trade"
-		case marketplace = "Marketplace"
-		case nfts = "NFTs"
-		case lending = "Lending"
-		case tools = "Tools"
-		case dashboard = "Dashboard"
+	enum Tag: String, Identifiable, CaseIterable {
+		case defi
+		case dex
+		case token
+		case trade
+		case marketplace
+		case nfts
+		case lending
+		case tools
+		case dashboard
+	}
+
+	enum Category: String, CaseIterable {
+		case defi
+		case dao
+		case utility
+		case nft
+		case meme
+
+		case other
+	}
+}
+
+extension DAppsDirectoryClient.DApp.Tag {
+	var title: String {
+		switch self {
+		case .defi:
+			"DeFi"
+		case .dex:
+			"DEX"
+		case .token:
+			"Token"
+		case .trade:
+			"Trade"
+		case .marketplace:
+			"Marketplace"
+		case .nfts:
+			"NFTs"
+		case .lending:
+			"Lending"
+		case .tools:
+			"Tools"
+		case .dashboard:
+			"Dashboard"
+		}
+	}
+}
+
+extension DAppsDirectoryClient.DApp.Tag: Codable {
+	init(from decoder: Decoder) throws {
+		let container = try decoder.singleValueContainer()
+		let rawString = try container.decode(String.self)
+
+		if let `case` = Self(rawValue: rawString.lowercased()) {
+			self = `case`
+		} else {
+			throw DecodingError.dataCorruptedError(
+				in: container,
+				debugDescription: "Cannot initialize UserType from invalid String value \(rawString)"
+			)
+		}
+	}
+}
+
+extension DAppsDirectoryClient.DApp.Category: Codable {
+	init(from decoder: Decoder) throws {
+		let container = try decoder.singleValueContainer()
+		let rawString = try container.decode(String.self)
+
+		if let `case` = Self(rawValue: rawString.lowercased()) {
+			self = `case`
+		} else {
+			self = .other
+		}
+	}
+}
+
+extension Result: Decodable where Success: Decodable, Failure == DecodingError {
+	public init(from decoder: Decoder) throws {
+		do {
+			let container = try decoder.singleValueContainer()
+			self = try .success(container.decode(Success.self))
+		} catch let err as Failure {
+			self = .failure(err)
+		}
 	}
 }
