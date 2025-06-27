e5c546c78dd4c2000e5be68f1544e63cb041a59e
GhenadieVP <ghenadie@radixpublishing.com>
2025-06-27 14:18:37 +0300

    adjustments

--- RadixWallet/Features/DApps/AuthorizedDApps/AuthorizedDApps+View.swift
+++ RadixWallet/Features/DApps/AuthorizedDApps/AuthorizedDApps+View.swift
@@ -39,7 +39,7 @@ extension DAppsDirectory.AuthorizedDappsFeature {
 					.scrollDisabled(!viewStore.hasAnyDApps)
 					.background(.secondaryBackground)
 					.refreshable {
-						await viewStore.send(.pullToRefreshStarted).finish()
+						viewStore.send(.pullToRefreshStarted)
 					}
 				}
 				.destinations(with: store)
